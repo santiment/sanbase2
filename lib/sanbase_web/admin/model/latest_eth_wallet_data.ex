36f673b08dc4713e6cbf48f87938681844a7e2e1
Valentin Mihov <valentin.mihov@gmail.com>
2017-10-16 23:35:10 +0300

    Add ex_admin entities for all models

---
+++ lib/sanbase_web/admin/model/latest_eth_wallet_data.ex
@@ -0,0 +1,7 @@
+defmodule Sanbase.ExAdmin.Model.LatestEthWalletData do
+  use ExAdmin.Register
+
+  register_resource Sanbase.Model.LatestEthWalletData do
+
+  end
+end
---
+++ lib/sanbase_web/admin/model/tracked_eth.ex
@@ -0,0 +1,7 @@
+defmodule Sanbase.ExAdmin.Model.TrackedEth do
+  use ExAdmin.Register
+
+  register_resource Sanbase.Model.TrackedEth do
+
+  end
+end

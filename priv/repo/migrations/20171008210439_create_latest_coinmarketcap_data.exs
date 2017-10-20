77b7b4310ee0175c58a076c22d072e0dde86fea7
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-10-20 15:12:02 +0300

    changed real/float to numeric/decimal for money values

--- priv/repo/migrations/20171008210439_create_latest_coinmarketcap_data.exs
+++ priv/repo/migrations/20171008210439_create_latest_coinmarketcap_data.exs
@@ -6,8 +6,8 @@ defmodule Sanbase.Repo.Migrations.CreateLatestCoinmarketcapData do
       add :coinmaketcap_id, :string, null: false
       add :name, :string
       add :symbol, :string
-      add :price_usd, :numeric
-      add :market_cap_usd, :numeric
+      add :price_usd, :decimal
+      add :market_cap_usd, :decimal
       add :update_time, :timestamp, null: false
     end
 

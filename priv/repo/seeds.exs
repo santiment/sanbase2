9af9224e1a633adc94b193ded5fd32a1065483e5
Valentin Mihov <valentin.mihov@gmail.com>
2017-10-20 11:47:21 +0300

    Setup seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,11 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+alias Sanbase.Model.Project
+alias Sanbase.Model.ProjectBtcAddress
+alias Sanbase.Model.ProjectEthAddress
+alias Sanbase.Repo
+
 [
   %Project{ticker: "ANT", name: "Aragon", coinmarketcap_id: "aragon"},
   %Project{ticker: "CFI", name: "Cofound.it", coinmarketcap_id: "cofound-it"},
@@ -17,72 +22,48 @@
   %Project{ticker: "SAN", name: "Santiment", coinmarketcap_id: "santiment"},
   %Project{ticker: "SNT", name: "Status.im", coinmarketcap_id: "status"},
 ]
-|> Enum.map(&Repo.insert!/1)
+|> Enum.each(&Repo.insert!/1)
 
+[
+  %ProjectEthAddress{
+    project: Repo.get_by(Project, name: "Aragon"),
+    address: "0xcafe1a77e84698c83ca8931f54a755176ef75f2c"
+  },
+  %ProjectEthAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "0x683a0aafa039406c104d814b9f244eea721445a7"
+  },
+  %ProjectEthAddress{
+    project: Repo.get_by(Project, name: "Santiment"),
+    address: "0x6dd5a9f47cfbc44c04a0a4452f0ba792ebfbcc9a"
+  },
+  %ProjectEthAddress{
+    project: Repo.get_by(Project, name: "Status.im"),
+    address: "0xA646E29877d52B9e2De457ECa09C724fF16D0a2B"
+  },
+]
+|> Enum.each(&Repo.insert!/1)
 
-
-/* Insert known addresses */
-INSERT INTO project_eth_address (project_id, address)
-  SELECT
-    project.id,
-    t.address
-  FROM
-    project,
-    (VALUES
-      ('Aragon', '0xcafe1a77e84698c83ca8931f54a755176ef75f2c'),
-      ('district0x', '0xd20e4d854c71de2428e1268167753e4c7070ae68'),
-      ('Encrypgen', '0x683a0aafa039406c104d814b9f244eea721445a7'),
-      ('Etherisc', '0x9B0F6a5a667CB92aF0cd15DbE90E764e32f69e77'),
-      ('Etherisc', '0x35792029777427920ce7aDecccE9e645465e9C72'),
-      ('Expanse/Tokenlab', '0xd1ea8853619aaad66f3f6c14ca22430ce6954476'),
-      ('Expanse/Tokenlab', '0xf83fd4b62ccb4b5c4213278b6b506eb2f19988d0'),
-      ('Indorse', '0x1c82ee5b828455F870eb2998f2c9b6Cc2d52a5F6'),
-      ('Indorse', '0x26967201d4d1e1aa97554838defa4fc4d010ff6f'),
-      ('Iconomi', '0x154Af3E01eC56Bc55fD585622E33E3dfb8a248d8'),
-      ('Maecenas', '0x02DC3b8AB87c562CdCE707647bd1ba21C390Faf4'),
-      ('Maecenas', '0x9B60874D7bc4e4fBDd142e0F5a12002e4F7715a6'),
-      ('Musiconomi', '0xc7CD9d874F93F2409F39A95987b3E3C738313925'),
-      ('Santiment', '0x6dd5a9f47cfbc44c04a0a4452f0ba792ebfbcc9a'),
-      ('Status.im', '0xA646E29877d52B9e2De457ECa09C724fF16D0a2B')
-    ) AS t (name, address)
-  WHERE
-    project.name = t.name
-
-ON CONFLICT (address) DO UPDATE
-  SET address = EXCLUDED.address;
-
-INSERT INTO project_btc_address (project_id, address)
-  SELECT
-    project.id,
-    t.address
-  FROM
-    project,
-    (VALUES
-      ('Encrypgen', '13MoQt2n9cHNzbpt8PfeVYp2cehgzRgj6v'),
-      ('Encrypgen', '16bv1XAqh1YadAWHgDWgxKuhhns7T2EywG'),
-      ('Encrypgen', '3PtQX3dKbUb7Q8LguBrWVcwddf2yCkDJW9'),
-      ('Encrypgen', '33TK711ktEUJxERdTtkoE5cTaPVsi3JDya'),
-      ('Encrypgen', '3BnvD37EBj9EN89wrruPV44KxpYrYKTfQB')
-    ) AS t (name, address)
-  WHERE
-    project.name = t.name
-
-ON CONFLICT (address) DO UPDATE
-  SET address = EXCLUDED.address;
-
-
-/* Set known addresses to tracked */
-
-INSERT INTO tracked_eth (address)
-  SELECT
-    address
-  FROM
-    project_eth_address
-ON CONFLICT DO NOTHING;
-
-INSERT INTO tracked_btc (address)
-  SELECT
-    address
-  FROM
-    project_btc_address
-ON CONFLICT DO NOTHING;
+[
+  %ProjectBtcAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "13MoQt2n9cHNzbpt8PfeVYp2cehgzRgj6v"
+  },
+  %ProjectBtcAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "16bv1XAqh1YadAWHgDWgxKuhhns7T2EywG"
+  },
+  %ProjectBtcAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "3PtQX3dKbUb7Q8LguBrWVcwddf2yCkDJW9"
+  },
+  %ProjectBtcAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "33TK711ktEUJxERdTtkoE5cTaPVsi3JDya"
+  },
+  %ProjectBtcAddress{
+    project: Repo.get_by(Project, name: "Encrypgen"),
+    address: "3BnvD37EBj9EN89wrruPV44KxpYrYKTfQB"
+  },
+]
+|> Enum.each(&Repo.insert!/1)

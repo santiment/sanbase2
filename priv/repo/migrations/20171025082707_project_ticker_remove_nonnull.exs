96acb4900c9911d0ba40453f46e5c72935483807
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-10-25 11:32:13 +0300

    project.ticker - remove not null; project.coinmarketcap_id - remove unique

---
+++ priv/repo/migrations/20171025082707_project_ticker_remove_nonnull.exs
@@ -0,0 +1,15 @@
+defmodule Sanbase.Repo.Migrations.ProjectTickerRemoveNonnull do
+  use Ecto.Migration
+
+  def up do
+    alter table("project") do
+      modify :ticker, :string, null: true
+    end
+  end
+
+  def down do
+    alter table("project") do
+      modify :ticker, :string, null: false
+    end
+  end
+end

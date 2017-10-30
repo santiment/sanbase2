6e17904634f5c268afa681ed129a11a32ed31e8c
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-10-30 23:20:29 +0200

    simplified model for importing from spreadsheet

--- lib/sanbase_web/admin/model/btt.ex
+++
@@ -1,7 +0,0 @@
-defmodule Sanbase.ExAdmin.Model.Btt do
-  use ExAdmin.Register
-
-  register_resource Sanbase.Model.Btt do
-
-  end
-end
--- priv/repo/migrations/20171017125741_create_icos.exs
+++ priv/repo/migrations/20171017125741_create_icos.exs
@@ -8,18 +8,12 @@ defmodule Sanbase.Repo.Migrations.CreateIcos do
       add :end_date, :date
       add :tokens_issued_at_ico, :integer
       add :tokens_sold_at_ico, :integer
-      add :tokens_team, :integer
-      add :usd_btc_icoend, :decimal
       add :funds_raised_btc, :decimal
+      add :usd_btc_icoend, :decimal
       add :usd_eth_icoend, :decimal
-      add :ico_contributors, :integer
-      add :highest_bonus_percent_for_ico, :decimal
-      add :bounty_campaign, :boolean
-      add :percent_tokens_for_bounties, :decimal
       add :minimal_cap_amount, :decimal
-      add :minimal_cap_archived, :boolean
       add :maximal_cap_amount, :decimal
-      add :maximal_cap_archived, :boolean
+      add :cap_currency_id, references(:currencies)
     end
 
     create unique_index(:icos, [:project_id])

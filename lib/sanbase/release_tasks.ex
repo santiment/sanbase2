8e18bacbe307eb85d389b44dc5e4e075d6804f96
Valentin Mihov <valentin.mihov@gmail.com>
2017-11-06 17:19:11 +0100

    Remove the influxdb initialization task

--- lib/sanbase/release_tasks.ex
+++ lib/sanbase/release_tasks.ex
@@ -1,13 +1,11 @@
 defmodule Sanbase.ReleaseTasks do
   alias Sanbase.ReleaseTasks.Migrate
-  alias Sanbase.ReleaseTasks.InfluxDB
 
   def run do
     # Load the code for myapp, but don't start it
     :ok = Application.load(:sanbase)
 
     Migrate.run
-    InfluxDB.run
 
     # Signal shutdown
     IO.puts "Success!"

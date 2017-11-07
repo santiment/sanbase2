be97957c66d0c1e766a74319bdca0fbc9a1e6224
Valentin Mihov <valentin.mihov@gmail.com>
2017-11-07 09:29:31 +0000

    Small refactoring and fix the Jenkinsfile

--- lib/sanbase/notifications/check_prices.ex
+++ lib/sanbase/notifications/check_prices.ex
@@ -4,71 +4,32 @@ defmodule Sanbase.Notifications.CheckPrices do
   alias Sanbase.Repo
   alias Sanbase.Model.Project
   alias Sanbase.Prices.Store
-  alias Sanbase.Notifications.Notification
-  alias Sanbase.Notifications.Type
+  alias Sanbase.Notifications.CheckPrices.ComputeMovements
 
-  import Ecto.Query
   import Sanbase.DateTimeUtils, only: [seconds_ago: 1]
 
-  require Logger
-
-  @notification_name "price_change"
-  @cooldown_period_in_sec 60 * 20 # 20 minutes
-  @check_interval_in_sec 60 * 20 # 20 minutes
+  @cooldown_period_in_sec 60 * 60 # 60 minutes
+  @check_interval_in_sec 60 * 60 # 60 minutes
   @price_change_threshold 5 # percent
 
   def exec do
-    type_id = price_notification_type_id()
-
-    Project
-    |> Repo.all()
-    |> Enum.reject(&recent_notification?(&1, type_id))
-    |> Enum.map(&price_difference/1)
-    |> Enum.filter(fn [price_difference, _] ->
-      Kernel.abs(price_difference) > @price_change_threshold / 100
-    end)
-    |> Enum.map(&send_notification(&1, type_id))
+    ComputeMovements.projects_to_monitor(seconds_ago(@cooldown_period_in_sec))
+    |> Enum.map(&fetch_price_points/1)
+    |> ComputeMovements.compute_notifications(@price_change_threshold)
+    |> Enum.map(&send_notification/1)
   end
 
-  defp price_difference(project) do
-    Store.fetch_price_points(price_ticker(project), seconds_ago(@check_interval_in_sec), DateTime.utc_now())
-    |> price_difference_for_prices(project)
+  defp fetch_price_points(project) do
+    {
+      project,
+      Store.fetch_price_points(price_ticker(project), seconds_ago(@check_interval_in_sec), DateTime.utc_now())
+    }
   end
 
-  defp price_difference_for_prices([], project), do: [0, project]
-
-  defp price_difference_for_prices(prices, project) do
-    [_ts, last_price | _] = List.last(prices)
-    [_ts, first_price | _] = List.first(prices)
-
-    [(last_price - first_price) / first_price, project]
-  end
-
-  defp send_notification([price_difference, project], type_id) do
+  defp send_notification({notification, price_difference, project}) do
     Tesla.post(webhook_url(), notification_payload(price_difference, project), headers: %{"Content-Type" => "application/json"})
 
-    Repo.insert!(%Notification{
-      project_id: project.id,
-      type_id: type_id
-    })
-  end
-
-  defp recent_notification?(%Project{id: id}, type_id) do
-    cooldown_time = notification_cooldown_time()
-
-    Notification
-    |> where([n], project_id: ^id, type_id: ^type_id)
-    |> where([n], n.inserted_at > ^cooldown_time)
-    |> Repo.aggregate(:count, :id)
-    |> Kernel.>(0)
-  end
-
-  defp notification_cooldown_time(), do: seconds_ago(@cooldown_period_in_sec)
-
-  defp price_notification_type_id do
-    type = Repo.get_by(Type, name: @notification_name) || Repo.insert!(%Type{name: @notification_name})
-
-    type.id
+    Repo.insert!(notification)
   end
 
   defp price_ticker(%Project{ticker: ticker}) do

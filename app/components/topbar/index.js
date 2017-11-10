0454b9cbfaf79bc0eafa3afa59dea3a449ebcb43
Vladimir Bobrov <ceo.bobrov@yandex.ru>
2017-11-10 16:04:25 +0700

    Styles of topbar component has been moved to separate file

--- app/components/topbar/index.js
+++ app/components/topbar/index.js
@@ -1,14 +1,17 @@
+import styles from './index.scss'
+
 export default (props) => (
   <div className="row topbar">
+    <style dangerouslySetInnerHTML={{ __html: styles }}></style>
     <div className="col-lg-6">
-      <div style={{'paddingTop': '24px', 'paddingLeft': '16px'}}>
+      <div className="search" style={{'paddingTop': '24px', 'paddingLeft': '16px'}}>
         <i className="material-icons">search</i>
       </div>
     </div>
     <div className="col-lg-6">
       <ul className="nav-right pull-right list-unstyled">
         <li>
-          <span style={{'display': 'inline-block', 'paddingTop': '22px', 'paddingLeft': '24px', 'paddingRight': '24px', 'fontSize': '14px'}}>12.5 Ξ</span>
+          <span className="balance">12.5 Ξ</span>
         </li>
         <li>
         </li>

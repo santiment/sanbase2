075182b1d4991c3b979482d505a83824c13d349f
Vladimir Bobrov <ceo.bobrov@yandex.ru>
2017-11-10 17:45:04 +0700

    Inline code ahs been removed

--- app/components/header-page/index.js
+++ app/components/header-page/index.js
@@ -1,13 +1,12 @@
+import styles from './roadmap.scss'
+
 export default ({ description, name }) => (
-  <div className="row">
+  <div className="row header-page">
+    <style dangerouslySetInnerHTML={{ __html: styles }}></style>
     <div className="col-lg-12">
       <h1>{ name }</h1>
       <p>{ description }</p>
     </div>
-    <style jsx>{`
-       p {
-         margin-left: 16px;
-       }
-    `}</style>
+    <style></style>
   </div>
 )

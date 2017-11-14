49477b8b9a1e6c77a2989ecfc3670cf1a682f1ac
Tzanko Matev <tsanko@gmail.com>
2017-11-15 00:13:45 +0200

    Add etherscan env in deployment.yaml

--- config/config.exs
+++ config/config.exs
@@ -69,5 +69,3 @@ config :sanbase, Sanbase.ExternalServices.Etherscan.Worker,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
-
-

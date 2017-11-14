cb7ff2f565f3a250e1c9c2cb4e757331e0b4d9df
Tzanko Matev <tsanko@gmail.com>
2017-11-15 01:56:04 +0200

    Add forgotten migration & refactor Requests
    * Add the migration file which was forgotten in previous commit
    * Split Etherscan.Requests to submodules in different files

---
+++ lib/sanbase/external_services/etherscan/balance_requests.ex
@@ -0,0 +1,24 @@
+defmodule Sanbase.ExternalServices.Etherscan.Requests.Balance do
+
+  alias Sanbase.ExternalServices.Etherscan.Requests
+  alias __MODULE__
+
+  defstruct [:status, :message, :result]
+
+  defp get_query(address) do
+    [
+      module: "account",
+      action: "balance",
+      address: address,
+      tag: "latest",
+    ]
+  end
+
+  def get(address) do
+    Requests.get("/", query: get_query(address))
+    |> case do
+         %{status: 200, body: body} -> Poison.decode!(body, as: %Balance{})
+       end
+  end
+
+end

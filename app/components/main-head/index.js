cd8af1898ad11e17ff9b937ffd50c524224a2b5b
Valentin Mihov <valentin.mihov@gmail.com>
2017-11-27 19:02:18 +0200

    Add a favicon to sanbase

--- app/components/main-head/index.js
+++ app/components/main-head/index.js
@@ -4,6 +4,7 @@ export default (props) => (
   <Head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="shortcut icon" href="/static/cashflow/img/favicon.png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet"/>

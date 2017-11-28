a011e174a5c836ab984a8107851c95cdcb0e4e4a
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-11-28 16:34:52 +0500

    Added standard linter for js. Fix some typos, styles and unused vars, etc

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
-export function websiteUrl() {
-  if (typeof(window) != 'undefined') {
-    return "//" + window.location.host;
+export function websiteUrl () {
+  if (typeof (window) !== 'undefined') {
+    return '//' + window.location.host
   }
 
-  return process.env.WEBSITE_URL || "http://localhost:4000";
+  return process.env.WEBSITE_URL || 'http://localhost:4000'
 }

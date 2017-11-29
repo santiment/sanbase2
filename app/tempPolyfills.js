7fa53f1fc984e1424427af7fe375a1369869427b
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-11-29 14:49:21 +0500

    Added polyfill hack explanation in the codebase
    Dan Abramove approved this gist for JEST testing env.
    More about this: https://dev.to/letsbsocial1/requestanimationframe--polyfill-in-react-16-2ce

--- app/tempPolyfills.js
+++ app/tempPolyfills.js
@@ -1,3 +1,6 @@
+// Why this was happened?
+// https://dev.to/letsbsocial1/requestanimationframe--polyfill-in-react-16-2ce
+// Dan Abramov approved this gist for JEST env.
 const raf = global.requestAnimationFrame = cb => {
   setTimeout(cb, 0)
 }

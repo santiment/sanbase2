7a22229da2c1f1d42fd6242020c592c1dfcbc2aa
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-03 02:34:49 +0500

    clean smth

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,7 +3,7 @@ import TodoList from '../components/TodoList.js'
 import fetch from 'isomorphic-unfetch'
 
 export default class extends React.Component {
-  static async getInitialProps({ res }) {
-    return redirect_to(res, "/cashflow");
+  static async getInitialProps ({ res }) {
+    return redirect_to(res, '/cashflow')
   }
 }

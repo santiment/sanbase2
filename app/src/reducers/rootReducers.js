3d8c25c19eb650e3f1064aa2dc945b01ac84b0ee
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-03 02:08:57 +0500

    Add Cashflow without Jquery and bootstrap.

--- app/src/reducers/rootReducers.js
+++ app/src/reducers/rootReducers.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import user, { initialState as userState } from './user'
+import projects, { initialState as projectsState } from './projects'
 
 export const intitialState = {
-  user: userState
+  user: userState,
+  projects: projectsState
 }
 
 export default combineReducers({
-  user
+  user,
+  projects
 })

dbe43be3fce942e8a6b5cbed4007e6d4e519dd50
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-03 15:07:28 +0500

    Add nive-side menu. Layout with breakpoints

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,70 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   Route,
+  NavLink as Link,
   Switch
 } from 'react-router-dom'
 import './App.css'
 import Login from './Login'
 import Cashflow from './Cashflow'
+import logo from './assets/logo_sanbase.png'
+import withSizes from 'react-sizes'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Switch>
-          <Route exact path='/cashflow' component={Cashflow} />
-          <Route path={'/login'} component={Login} />
-          <Route exact path={'/'} component={Cashflow} />
-        </Switch>
-      </div>
-    )
-  }
-}
+export const SideMenu = () => (
+  <div className='side-menu'>
+    <div className='brand'>
+      <img
+        src={logo}
+        width='115'
+        height='22'
+        alt='SANbase' />
+    </div>
+    <i
+      className='fa fa-bars fa-2x toggle-btn'
+      data-toggle='collapse'
+      data-target='#menu-content' />
+    <div className='menu-list'>
+      <ul id='menu-content' className='menu-content collapse out'>
+        <li>
+          Dashboard (tbd)
+        </li>
+        <li data-toggle='collapse' data-target='#products'>
+          Data-feeds
+        </li>
+        <ul className='sub-menu' id='products'>
+          <li>Overview (tbd)</li>
+          <li>
+            <Link
+              activeClassName='selected'
+              to='/cashflow'>
+              Cash Flow
+            </Link>
+          </li>
+        </ul>
+        <li>
+          Signals
+        </li>
+        <li>
+          Roadmap
+        </li>
+      </ul>
+    </div>
+  </div>
+)
 
-export default App
+export const App = ({isDesktop}) => (
+  <div className='App'>
+    {isDesktop && <SideMenu />}
+    <Switch>
+      <Route exact path='/cashflow' component={Cashflow} />
+      <Route path={'/login'} component={Login} />
+      <Route exact path={'/'} component={Cashflow} />
+    </Switch>
+  </div>
+)
+
+const mapSizesToProps = ({ width }) => ({
+  isDesktop: width > 620
+})
+
+export default withSizes(mapSizesToProps)(App)

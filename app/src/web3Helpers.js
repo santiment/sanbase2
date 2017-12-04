a3bdf68c715e8d0151ccc482ae5baae766a341b3
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-04 18:22:09 +0500

    Add GraphQL for auth [wip]
    Create auth mutation request in login container.
    This mutation is WIP.

--- app/src/web3Helpers.js
+++ app/src/web3Helpers.js
@@ -18,48 +18,17 @@ export const setupWeb3 = cbk => {
 
 export const signMessage = account => {
   const message = `Login in Santiment with address ${account}`
+  const hashMessage = window.web3.fromUtf8(message)
   return new Promise((resolve, reject) => {
-    window.web3.personal.sign(toHex(message), account, (error, res) => {
+    window.web3.personal.sign(hashMessage, account, (error, signature) => {
       if (!error) {
-        resolve(res)
+        resolve({
+          hashMessage,
+          signature
+        })
       } else {
         reject(error)
       }
     })
   })
-}
-
-// While we don't have hashMessage method in metamask web3,
-// we need own hash function. eth.sha3 is not the right function
-// Solved function got from utils package of web3
-// https://github.com/ethereum/web3.js/blob/62dd77c7f43522eb568a001cf4c4df726f8efa69/lib/utils/utils.js
-
-/**
- * Auto converts string value into it's hex representation.
- *
- * @method toHex
- * @param {String}
- * @return {String}
- */
-const toHex = val => {
-  return fromAscii(val)
-}
-
-/**
- * Should be called to get hex representation (prefixed by 0x) of ascii string
- *
- * @method fromAscii
- * @param {String} string
- * @param {Number} optional padding
- * @returns {String} hex representation of input string
- */
-const fromAscii = str => {
-  let hex = ''
-  for (let i = 0; i < str.length; i++) {
-    const code = str.charCodeAt(i)
-    const n = code.toString(16)
-    hex += n.length < 2 ? '0' + n : n
-  }
-
-  return '0x' + hex
 }

ba21d840862a38541ee2f09cd95fd5cdac986a2c
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-05 16:48:32 +0500

    Add Roadmap dom model, Signals mock, fixes
    Some fixes
    Remove unused actions
    Added Roadmap dom without styles
    Added Signals without dom and styles

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,8 @@ import withSizes from 'react-sizes'
 import './App.css'
 import Login from './Login'
 import Cashflow from './Cashflow'
+import Roadmap from './Roadmap'
+import Signals from './Signals'
 import SideMenu from './SideMenu'
 
 export const App = ({isDesktop}) => (
@@ -14,6 +16,8 @@ export const App = ({isDesktop}) => (
     {isDesktop && <SideMenu />}
     <Switch>
       <Route exact path='/cashflow' component={Cashflow} />
+      <Route exact path='/roadmap' component={Roadmap} />
+      <Route exact path='/signals' component={Signals} />
       <Route path={'/login'} component={Login} />
       <Route exact path={'/'} component={Cashflow} />
     </Switch>
---
+++ app/src/Signals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signals = props => {
+  return (
+    <div>
+      Signals
+    </div>
+  )
+}
+
+export default Signals

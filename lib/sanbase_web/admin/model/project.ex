222c0cdd90ade72b9d35be6b65d91aba530d8ed1
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-12-07 11:27:30 +0200

    Added "New Ico" button to project details page; changed project.icos columns in project details page

--- lib/sanbase_web/admin/model/project.ex
+++ lib/sanbase_web/admin/model/project.ex
@@ -125,11 +125,30 @@ defmodule Sanbase.ExAdmin.Model.Project do
 
     show project do
       attributes_table
+
       panel "Icos" do
-        table_for project.icos do
+        markup_contents do
+          a ".btn .btn-primary", href: "/admin/icos/new?project_id="<>to_string(project.id) do
+            "New Ico"
+          end
+        end
+
+        table_for Sanbase.Repo.preload(project.icos, :cap_currency) do
           column :id, link: true
           column :start_date
           column :end_date
+          column :tokens_issued_at_ico
+          column :tokens_sold_at_ico
+          column :funds_raised_btc
+          column :funds_raised_usd
+          column :funds_raised_eth
+          column :usd_btc_icoend
+          column :usd_eth_icoend
+          column :minimal_cap_amount
+          column :maximal_cap_amount
+          column :main_contract_address
+          column :comments
+          column :cap_currency
         end
       end
     end

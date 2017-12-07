138af2154ccc5ced5a5afcd37fc94689c3d23a97
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-12-07 11:27:30 +0200

    Added "New Ico" button to project details page; changed project.icos columns in project details page

--- lib/sanbase_web/admin/model/project.ex
+++ lib/sanbase_web/admin/model/project.ex
@@ -8,11 +8,30 @@ defmodule Sanbase.ExAdmin.Model.Project do
 
     show project do
       attributes_table
+
       panel "Icos" do
-        table_for project.icos do
+        markup_contents do
+          a ".btn .btn-primary", href: "/admin/icos/new?project_id="<>to_string(project.id) do
+            "New Ico"
+          end
+        end
+
+        table_for Sanbase.Repo.preload(project.icos, :cap_currency) do
           column :id, link: true
           column :start_date
           column :end_date
+          column :tokens_issued_at_ico
+          column :tokens_sold_at_ico
+          column :funds_raised_btc
+          column :funds_raised_usd
+          column :funds_raised_eth
+          column :usd_btc_icoend
+          column :usd_eth_icoend
+          column :minimal_cap_amount
+          column :maximal_cap_amount
+          column :main_contract_address
+          column :comments
+          column :cap_currency
         end
       end
     end
@@ -25,12 +44,12 @@ defmodule Sanbase.ExAdmin.Model.Project do
 
   def set_defaults(conn, params, resource, :new) do
     resource = resource
-    |> set_project_infrastructure_default()
+    |> set_infrastructure_default()
 
     {conn, params, resource}
   end
 
-  defp set_project_infrastructure_default(%Project{infrastructure_id: nil}=project) do
+  defp set_infrastructure_default(%Project{infrastructure_id: nil}=project) do
     infrastructure = Infrastructure.get("ETH")
 
     case infrastructure do
@@ -39,5 +58,5 @@ defmodule Sanbase.ExAdmin.Model.Project do
     end
   end
 
-  defp set_project_infrastructure_default(%Project{}=project), do: project
+  defp set_infrastructure_default(%Project{}=project), do: project
 end

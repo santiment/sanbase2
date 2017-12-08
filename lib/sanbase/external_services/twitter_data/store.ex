277d703f4bf348977af7e5fc49fe750a50931918
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2017-12-08 15:17:01 +0200

    Use the Twitter api to fetch information about projects' twitter accounts

---
+++ lib/sanbase/external_services/twitter_data/store.ex
@@ -0,0 +1,110 @@
+defmodule Sanbase.ExternalServices.TwitterData.Store do
+  @moduledoc ~S"""
+    A module for storing and fetching twitter account data from a time series data store
+  """
+
+  use Instream.Connection, otp_app: :sanbase
+
+  import Sanbase.Utils, only: [parse_config_value: 1]
+
+
+  alias Sanbase.Influxdb.Measurement
+  alias Sanbase.ExternalServices.TwitterData.Store
+
+  def import(%Measurement{} = measurement) do
+    measurement
+    |> Measurement.convert_measurement_for_import()
+    |> write_data()
+  end
+
+  def import(measurements) when is_list(measurements) do
+    measurements
+    |> Stream.map(&Measurement.convert_measurement_for_import/1)
+    |> Stream.map(&write_data/1)
+    |> Stream.run()
+  end
+
+  def time_series_for_measurement(measurement_name, from, to) do
+    select_from_to_query(measurement_name, from, to)
+    |> Store.query()
+    |> parse_twitter_data_series()
+  end
+
+  def last_record_for_measurement(measurement_name) do
+    select_last_record_query(measurement_name)
+    |> Store.query()
+    |> parse_last_twitter_record()
+  end
+
+  def create_db() do
+    get_config(:database)
+    |> Instream.Admin.Database.create()
+    |> Store.execute()
+  end
+
+  def drop_measurement(measurement_name) do
+    "DROP MEASUREMENT #{measurement_name}"
+    |> Store.execute()
+  end
+
+  defp write_data(data) do
+    :ok = Store.write(data)
+  end
+
+  defp select_from_to_query(measurement_name, from, to) do
+    ~s/SELECT time, followers_count
+    FROM "#{measurement_name}"
+    WHERE time >= #{DateTime.to_unix(from, :nanoseconds)}
+    AND time <= #{DateTime.to_unix(to, :nanoseconds)}/
+  end
+
+  defp select_last_record_query(measurement_name) do
+    ~s/SELECT LAST(followers_count) FROM "#{measurement_name}"/
+  end
+
+  defp parse_twitter_data_series(%{results: [%{error: error}]}), do: raise(error)
+
+  defp parse_twitter_data_series(%{
+         results: [
+           %{
+             series: [
+               %{
+                 values: twitter_data_series
+               }
+             ]
+           }
+         ]
+       }) do
+    twitter_data_series
+    |> Enum.map(fn [iso8601_datetime, followers_count] ->
+         {:ok, datetime, _} = DateTime.from_iso8601(iso8601_datetime)
+         {datetime, followers_count}
+       end)
+  end
+
+  defp parse_twitter_data_series(_), do: []
+
+  defp parse_last_twitter_record(%{
+         results: [
+           %{
+             series: [
+               %{
+                 values: [[iso8601_datetime, followers_count]]
+               }
+             ]
+           }
+         ]
+       }) do
+    {:ok, datetime, _} = DateTime.from_iso8601(iso8601_datetime)
+
+    {datetime, followers_count}
+  end
+
+  defp parse_last_twitter_record(_), do: nil
+
+ defp get_config(key, default \\ nil) do
+    Application.fetch_env!(:sanbase, __MODULE__)
+    |> Keyword.get(key, default)
+    |> parse_config_value()
+  end
+end
---
+++ lib/sanbase/external_services/twitter_data/worker.ex
@@ -0,0 +1,120 @@
+defmodule Sanbase.ExternalServices.TwitterData.Worker do
+  @moduledoc ~S"""
+    A worker that regularly polls twitter for account data and stores it in
+    a time series database.
+  """
+
+  @rate_limiter_name :twitter_api_rate_limiter
+
+  use GenServer, restart: :permanent, shutdown: 5_000
+
+  require Logger
+
+  import Ecto.Query
+  import Sanbase.Utils, only: [parse_config_value: 1]
+
+  alias Sanbase.Repo
+  alias Sanbase.Model.Project
+  alias Sanbase.Influxdb.Measurement
+  alias Sanbase.ExternalServices.RateLimiting.Server
+  alias Sanbase.ExternalServices.TwitterData.{Worker, Store}
+
+  @default_update_interval 1000 * 60 * 60 * 6
+
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+
+    update_interval_ms = get_config(:update_interval, @default_update_interval)
+
+    if get_config(:sync_enabled, false) do
+      Store.create_db()
+
+      GenServer.cast(self(), :sync)
+      {:ok, %{update_interval_ms: update_interval_ms}}
+    else
+      :ignore
+    end
+  end
+
+  def handle_cast(:sync, %{update_interval_ms: update_interval_ms} = state) do
+    query =
+      from(
+        p in Project,
+        select: p.twitter_link,
+        where: not is_nil(p.twitter_link)
+      )
+
+    Repo.all(query)
+    |> Task.async_stream(
+         &fetch_and_store(&1),
+         ordered: false,
+         max_concurency: System.schedulers_online() * 2, # IO bound
+         timeout: 15 * 1000 * 60 # twitter api time window
+       )
+    |> Stream.run()
+
+    Process.send_after(self(), {:"$gen_cast", :sync}, update_interval_ms)
+    {:noreply, state}
+  end
+
+  def handle_info({:EXIT, pid, :normal}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:EXIT, pid, reason}, state) do
+    Logger.debug("Child process with pid #{pid} exitted unexpectedly. Reason: #{inspect(reason)}")
+    {:noreply, state}
+  end
+
+  defp fetch_and_store("https://twitter.com/" <> twitter_name) do
+    # Ignore trailing slash and everything after it
+    [twitter_name | _] = String.split(twitter_name, "/")
+
+    twitter_name
+    |> fetch_twitter_user_data()
+    |> store_twitter_user_data()
+  end
+
+  defp fetch_and_store(args) do
+    Logger.warn("Invalid parameters while fetching twitter data: " <> inspect(args))
+  end
+
+  defp fetch_twitter_user_data(twitter_name) do
+    Server.wait(@rate_limiter_name)
+
+    # GET https://api.twitter.com/1.1/users/show.json?screen_name=twitter_name
+    ExTwitter.user(twitter_name, include_entities: false)
+    |> convert_to_measurement(twitter_name)
+  end
+
+  defp store_twitter_user_data(user_data_measurement) do
+    user_data_measurement
+    |> Store.import()
+  end
+
+  defp convert_to_measurement(
+         %ExTwitter.Model.User{followers_count: followers_count} = user_data,
+         measurement_name
+       ) do
+    %Measurement{
+      timestamp: DateTime.to_unix(DateTime.utc_now(), :nanosecond),
+      fields: twitter_data_to_fields(user_data),
+      tags: [],
+      name: measurement_name
+    }
+  end
+
+  defp twitter_data_to_fields(%ExTwitter.Model.User{followers_count: followers_count} = user_data) do
+    %{followers_count: followers_count}
+  end
+
+  defp get_config(key, default \\ nil) do
+    Application.fetch_env!(:sanbase, __MODULE__)
+    |> Keyword.get(key, default)
+    |> parse_config_value()
+end
+end

9c903bec8d9768917fb2f06294fef55bd475ac19
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-12-08 17:48:45 +0200

    added many-to-many ico currencies to ico table

--- config/config.exs
+++ config/config.exs
@@ -54,7 +54,6 @@ config :ex_admin,
     Sanbase.ExAdmin.Model.Infrastructure,
     Sanbase.ExAdmin.Model.MarketSegment,
     Sanbase.ExAdmin.Model.Ico,
-    Sanbase.ExAdmin.Model.IcoCurrencies,
     Sanbase.ExAdmin.Notifications.Type
   ],
   basic_auth: [

683f21e1a5949363a62130e60a25319127065876
Valentin Mihov <valentin.mihov@gmail.com>
2017-12-09 14:43:01 +0200

    Revert "Revert "Feature/login""

---
+++ app/src/index.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import axios from 'axios'
+import { multiClientMiddleware } from 'redux-axios-middleware'
+import ApolloClient from 'apollo-client'
+import gql from 'graphql-tag'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+import App from './App'
+import reducers from './reducers/rootReducers.js'
+import { loadState, saveState } from './utils/localStorage'
+import setAuthorizationToken from './utils/setAuthorizationToken'
+import './index.css'
+let withTracker = null
+if (process.env.NODE_ENV === 'production') {
+  withTracker = require('./withTracker')
+}
+
+const origin = process.env.WEBSITE_URL || 'http://localhost:4000'
+
+const httpLink = createHttpLink({ uri: `${origin}/graphql` })
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = loadState() ? loadState().token : undefined
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+const clients = {
+  sanbaseClient: {
+    client: axios.create({
+      baseURL: `${origin}/api`,
+      responseType: 'json'
+    })
+  }
+}
+
+loadState() && setAuthorizationToken(loadState().token)
+
+const middleware = [multiClientMiddleware(clients)]
+
+const store = createStore(reducers,
+  {user: loadState()} || {},
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+client.query({
+  query: gql`
+    query {
+      currentUser {
+        id,
+        email,
+        username,
+        ethAccounts{
+          address,
+          sanBalance
+        }
+      }
+    }
+  `
+})
+.then(response => {
+  store.dispatch({
+    type: 'CHANGE_USER_DATA',
+    user: response.data.currentUser
+  })
+})
+.catch(error => console.error(error))
+
+store.subscribe(() => {
+  // TODO: Yura Zatsepin: 2017-12-07 11:23:
+  // we need add throttle when save action was hapenned
+  saveState(store.getState().user)
+})
+
+const Application = withTracker ? withTracker(App) : App
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <Router>
+        <Route path='/' component={Application} />
+      </Router>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById('root'))

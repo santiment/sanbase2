f5d91999d874e2d1e1900bd29101affcb6284977
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-12 12:33:13 +0500

    Added Error boundary and raven logs
      Added Error boundary.
      Added Raven Logs.
      Added CHANGELOG.
      Some fixes.

---
+++ app/src/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Raven from 'raven-js'
+import { Message } from 'semantic-ui-react'
+
+class ErrorBoundary extends Component {
+  state = { // eslint-disable-line
+    error: null
+  }
+
+  componentDidCatch (error, errorInfo) {
+    this.setState({ error })
+    Raven.captureException(error, { extra: errorInfo })
+  }
+
+  render () {
+    if (this.state.error) {
+      return (
+        <div className='page wrapper'>
+          <Message
+            size='massive'
+            negative
+            onClick={() => Raven.lastEventId() && Raven.showReportDialog()} >
+            <Message.Header>We're sorry — something's gone wrong.</Message.Header>
+            {Raven.lastEventId() && <p>Error ID: {Raven.lastEventId()}</p>}
+            <p>Our team has been notified, but click here fill out a report.</p>
+          </Message>
+        </div>
+      )
+    } else {
+      return this.props.children
+    }
+  }
+}
+
+export default ErrorBoundary

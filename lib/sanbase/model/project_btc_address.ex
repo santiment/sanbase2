66cade3808291e126dd31dcebb50f991927c0ea9
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-12-13 14:11:40 +0200

    added wallets PT columns

--- lib/sanbase/model/project_btc_address.ex
+++ lib/sanbase/model/project_btc_address.ex
@@ -6,12 +6,13 @@ defmodule Sanbase.Model.ProjectBtcAddress do
   schema "project_btc_address" do
     field :address, :string
     belongs_to :project, Project
+    field :project_transparency, :boolean, default: false
   end
 
   @doc false
   def changeset(%ProjectBtcAddress{} = project_btc_address, attrs \\ %{}) do
     project_btc_address
-    |> cast(attrs, [:address, :project_id])
+    |> cast(attrs, [:address, :project_id, :project_transparency])
     |> validate_required([:address, :project_id])
     |> unique_constraint(:address)
   end

a32e5a46250eaf29a681477e47f4bc86bfa921fd
Vladimir Bobrov <ceo.bobrov@yandex.ru>
2017-12-14 18:49:53 +0700

    Issues has been fixed

--- app/src/components/GeneralInfoBlock.js
+++ app/src/components/GeneralInfoBlock.js
@@ -8,8 +8,25 @@ const propTypes = {
 
 const GeneralInfoBlock = ({info}) => (
   <div>
+    <p className='social-icons'>
+      <a href='#'>
+        <i className='fa fa-globe' />
+      </a>
+      <a href='#'>
+        <i className='fa fa-slack' />
+      </a>
+      <a href='#'>
+        <i className='fa fa-twitter' />
+      </a>
+      <a href='#'>
+        <i className='fa fa-medium' />
+      </a>
+      <a href='#'>
+        <i className='fa fa-github' />
+      </a>
+    </p>
     <hr />
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         Market Cap
       </div>
@@ -17,15 +34,19 @@ const GeneralInfoBlock = ({info}) => (
         ${info.market_cap_usd}
       </div>
     </div>
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         Volume
       </div>
       <div>
         $57,345,121
+        <span className='diff down'>
+          <i className='fa fa-caret-down' />
+          &nbsp; 8.87%
+        </span>
       </div>
     </div>
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         Circulating
       </div>
@@ -33,7 +54,7 @@ const GeneralInfoBlock = ({info}) => (
         $57,345,121
       </div>
     </div>
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         Total supply
       </div>
@@ -41,7 +62,7 @@ const GeneralInfoBlock = ({info}) => (
         $57,345,121
       </div>
     </div>
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         Rank
       </div>
@@ -49,7 +70,7 @@ const GeneralInfoBlock = ({info}) => (
         {info.id}
       </div>
     </div>
-    <div className='rowInfo'>
+    <div className='row-info'>
       <div>
         ROI since ICO
       </div>

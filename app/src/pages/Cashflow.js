a271ab585fe0c6d13bf23e9b8b958966c0f8f50f
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-18 15:31:17 +0500

    Fix table format on Cashflow page

--- app/src/pages/Cashflow.js
+++ app/src/pages/Cashflow.js
@@ -39,7 +39,7 @@ const formatTxOutWallet = wallets => {
     const txOut = wallet.tx_out || '0.00'
     return (
       <div key={index}>
-        {txOut.toLocaleString('en-US')}
+        {formatNumber(txOut)}
       </div>
     )
   })

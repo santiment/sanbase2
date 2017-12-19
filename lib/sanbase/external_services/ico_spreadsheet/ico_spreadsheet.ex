63c60a25ffe6e271c1f62d9a9267111a69fd5bc7
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2017-12-19 17:18:26 +0200

    better parse error reporting in import spreadsheet

--- lib/sanbase/external_services/ico_spreadsheet/ico_spreadsheet.ex
+++ lib/sanbase/external_services/ico_spreadsheet/ico_spreadsheet.ex
@@ -47,8 +47,10 @@ defmodule Sanbase.ExternalServices.IcoSpreadsheet do
   end
 
   defp parse_value_row(value_row, column_indices) do
+    project_name = get_value!(value_row, column_indices.project_name)
+
     res = column_indices
-    |> Enum.map(&parse_value(value_row, &1))
+    |> Enum.map(&parse_value(value_row, &1, project_name))
     |> Enum.into(%{})
     |> handle_wallets()
     |> handle_infrastructure()
@@ -66,47 +68,47 @@ defmodule Sanbase.ExternalServices.IcoSpreadsheet do
     end
   end
 
-  defp parse_value(value_row, {column, index}) do
+  defp parse_value(value_row, {column, index}, project_name) do
     value = get_value!(value_row, index)
     value =
     case column do
       c when c in [:ico_start_date, :ico_end_date] ->
-        parse_date(value)
+        parse_date(value, {project_name, column})
       c when c in [:tokens_issued_at_ico] ->
-        parse_int(value)
+        parse_int(value, {project_name, column})
       c when c in [:tokens_sold_at_ico, :usd_btc_icoend, :funds_raised_btc, :funds_raised_usd, :funds_raised_eth, :usd_eth_icoend, :minimal_cap_amount, :maximal_cap_amount] ->
-        parse_decimal(value)
+        parse_decimal(value, {project_name, column})
       c when c in [:ico_currencies] ->
-        parse_comma_delimited(value)
-      _ -> parse_string(value)
+        parse_comma_delimited(value, {project_name, column})
+      _ -> parse_string(value, {project_name, column})
     end
 
     {column, value}
   end
 
-  defp parse_int(value) when is_binary(value) do
+  defp parse_int(value, {project_name, column}) when is_binary(value) do
     case Integer.parse(value) do
       {result, _} -> result
       _ -> #TODO: return error
-        Logger.warn("parse_int error: #{inspect value}")
+        Logger.warn("[#{project_name}|#{column}] parse_int error: #{inspect value}")
         nil
     end
   end
 
-  defp parse_int(value), do: value
+  defp parse_int(value, _context), do: value
 
-  defp parse_decimal(value) when is_binary(value) do
+  defp parse_decimal(value, {project_name, column}) when is_binary(value) do
     case Decimal.parse(value) do
       {:ok, result} -> result
       _ -> #TODO: return error
-        Logger.warn("parse_decimal error: #{inspect value}")
+        Logger.warn("[#{project_name}|#{column}] parse_decimal error: #{inspect value}")
         nil
     end
   end
 
-  defp parse_decimal(value), do: value
+  defp parse_decimal(value, _context), do: value
 
-  defp parse_boolean(value) do
+  defp parse_boolean(value, {project_name, column}) do
     value = if(is_binary(value)) do String.downcase(value) else value end
 
     case value do
@@ -114,42 +116,42 @@ defmodule Sanbase.ExternalServices.IcoSpreadsheet do
       v when v in ["no", "false", 0] -> false
       nil -> nil
       _ -> #TODO: return error
-        Logger.warn("parse_boolean error: #{inspect value}")
+        Logger.warn("[#{project_name}|#{column}] parse_boolean error: #{inspect value}")
         nil
     end
   end
 
-  defp parse_date(value) when is_integer(value) do
+  defp parse_date(value, _context) when is_integer(value) do
     #the -2 is to account for an Excel bug (search in internet)
     Date.add(~D[1900-01-01], value - 2)
   end
 
-  defp parse_date(nil), do: nil
+  defp parse_date(nil, _context), do: nil
 
-  defp parse_date(value) do
+  defp parse_date(value, {project_name, column}) do
     #TODO: return error
-    Logger.warn("parse_date error: #{inspect value}")
+    Logger.warn("[#{project_name}|#{column}] parse_date error: #{inspect value}")
     nil
   end
 
-  defp parse_comma_delimited(value) when is_binary(value) do
+  defp parse_comma_delimited(value, _context) when is_binary(value) do
     value
     |> String.split([",", ";"])
     |> Enum.map(&String.trim(&1))
     |> Enum.filter(&(String.length(&1) > 0))
   end
 
-  defp parse_comma_delimited(nil), do: []
+  defp parse_comma_delimited(nil, _context), do: []
 
-  defp parse_comma_delimited(value) do
+  defp parse_comma_delimited(value, {project_name, column}) do
     #TODO: return error
-    Logger.warn("parse_comma_delimited error: #{inspect value}")
+    Logger.warn("[#{project_name}|#{column}] parse_comma_delimited error: #{inspect value}")
     []
   end
 
-  defp parse_string(nil), do: nil
+  defp parse_string(nil, _context), do: nil
 
-  defp parse_string(value) do
+  defp parse_string(value, _context) do
     to_string(value)
   end
 

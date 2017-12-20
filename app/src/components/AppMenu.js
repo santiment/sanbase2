e39fda1b731d6bd7dc61063c07dda2358bbd20db
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-20 19:36:33 +0300

    update menu, remove not used links.

--- app/src/components/AppMenu.js
+++ app/src/components/AppMenu.js
@@ -4,40 +4,20 @@ import './AppMenu.css'
 
 const AppMenu = ({handleNavigation}) => (
   <ul className='menu-list'>
-    <li>
-      <Icon
-        className='toggle-btn'
-        name='home 2x' />
-      Dashboard (tbd)
+    <li
+      onClick={() => handleNavigation('projects')}>
+      <Icon name='list 2x' />
+      Projects
     </li>
-    <li>
-      <Icon
-        name='list 2x' />
-      Data-feeds
-    </li>
-    <ul className='sub-menu'>
-      <li>
-        <Icon
-          name='circle' />
-        Overview (tbd)
-      </li>
-      <li
-        onClick={() => handleNavigation('cashflow')}>
-        <Icon
-          name='circle' />
-        Cashflow
-      </li>
-    </ul>
     <li
       onClick={() => handleNavigation('signals')}>
-      <Icon
-        name='th 2x' />
+      <Icon name='th 2x' />
       Signals
     </li>
     <li
       onClick={() => handleNavigation('roadmap')}>
       <Icon
-        name='comment-o 2x' />
+        name='map 2x' />
       Roadmap
     </li>
   </ul>

85528328d03dc6df9ba35eb120d73dc6b5204c13
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2017-12-22 17:00:56 +0200

    Add on delete cascade

--- priv/repo/migrations/20171219162029_add_user_followed_project_table.exs
+++ priv/repo/migrations/20171219162029_add_user_followed_project_table.exs
@@ -4,8 +4,8 @@ defmodule Sanbase.Repo.Migrations.AddUserFollowedProjectTable do
 
   def change do
     create table(:user_followed_project) do
-      add :project_id, references(:project)
-      add :user_id, references(:users)
+      add :project_id, references(:project, on_delete: :delete_all), null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
     end
 
     create unique_index(:user_followed_project, [:project_id, :user_id], name: :projet_user_constraint)

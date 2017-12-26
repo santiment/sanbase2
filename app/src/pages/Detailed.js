8d0947fb96bb26e384ae097f9e4e0dc4ebc7d58c
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-12-26 15:08:53 +0500

    Added general info block
      Only links + marketcap and price data now

--- app/src/pages/Detailed.js
+++ app/src/pages/Detailed.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {
   compose,
-  pure,
   lifecycle
 } from 'recompose'
 import { Redirect } from 'react-router-dom'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { retrieveProjects } from './Cashflow.actions.js'
 import ProjectIcon from './../components/ProjectIcon'
 import PanelBlock from './../components/PanelBlock'
 import GeneralInfoBlock from './../components/GeneralInfoBlock'
@@ -17,10 +17,29 @@ import FinancialsBlock from './../components/FinancialsBlock'
 import './Detailed.css'
 
 const propTypes = {
-  match: PropTypes.object.isRequired
+  match: PropTypes.object.isRequired,
+  projects: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+  generalInfo: PropTypes.object
 }
 
-export const Detailed = ({data: { loading, project }, match}) => {
+export const HiddenElements = () => ''
+
+const getProjectByTicker = (match, projects) => {
+  const selectedTicker = match.params.ticker
+  const project = projects.find(el => {
+    const ticker = el.ticker || ''
+    return ticker.toLowerCase() === selectedTicker
+  })
+  return project
+}
+
+export const Detailed = ({
+  match,
+  projects,
+  loading,
+  generalInfo
+}) => {
   if (loading) {
     return (
       <div className='page detailed'>
@@ -28,6 +47,7 @@ export const Detailed = ({data: { loading, project }, match}) => {
       </div>
     )
   }
+  const project = getProjectByTicker(match, projects)
 
   if (!project) {
     return (
@@ -44,101 +64,83 @@ export const Detailed = ({data: { loading, project }, match}) => {
           <h1><ProjectIcon name={project.name} size={28} /> {project.name} ({project.ticker.toUpperCase()})</h1>
           <p>Manage entire organisations using the blockchain.</p>
         </div>
-        <div className='detailed-buttons'>
-          <button className='add-to-dashboard'>
-            <i className='fa fa-plus' />
-            &nbsp; Add to Dashboard
-          </button>
+        <HiddenElements>
+          <div className='detailed-buttons'>
+            <button className='add-to-dashboard'>
+              <i className='fa fa-plus' />
+              &nbsp; Add to Dashboard
+            </button>
+          </div>
+        </HiddenElements>
+      </div>
+      <HiddenElements>
+        <div className='panel'>
+          <Tabs className='activity-panel'>
+            <TabList className='nav'>
+              <Tab className='nav-item' selectedClassName='active'>
+                <button className='nav-link'>
+                  Social Mentions
+                </button>
+              </Tab>
+              <Tab className='nav-item' selectedClassName='active'>
+                <button className='nav-link'>
+                  Social Activity over Time
+                </button>
+              </Tab>
+              <Tab className='nav-item' selectedClassName='active'>
+                <button className='nav-link'>
+                  Sentiment/Intensity
+                </button>
+              </Tab>
+              <Tab className='nav-item' selectedClassName='active'>
+                <button className='nav-link'>
+                  Github Activity
+                </button>
+              </Tab>
+              <Tab className='nav-item' selectedClassName='active'>
+                <button className='nav-link'>
+                  SAN Community
+                </button>
+              </Tab>
+            </TabList>
+            <TabPanel>
+              Social Mentions
+            </TabPanel>
+            <TabPanel>
+              Social Activity over Time
+            </TabPanel>
+            <TabPanel>
+              Sentiment/Intensity
+            </TabPanel>
+            <TabPanel>
+              Github Activity
+            </TabPanel>
+            <TabPanel>
+              SAN Community
+            </TabPanel>
+          </Tabs>
         </div>
-      </div>
-      <div className='panel'>
-        <Tabs className='main-chart'>
-          <TabList className='nav'>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                $2.29 USD (not real) &nbsp;
-                <span className='diff down'>
-                  <i className='fa fa-caret-down' />
-                    &nbsp; 8.87% (not real)
-                </span>
-              </button>
-            </Tab>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                2.29 BTC (not real) &nbsp;
-                <span className='diff up'>
-                  <i className='fa fa-caret-up' />
-                    &nbsp; 8.87% (not real)
-                </span>
-              </button>
-            </Tab>
-          </TabList>
-          <TabPanel>
-            1
-          </TabPanel>
-          <TabPanel>
-            2
-          </TabPanel>
-        </Tabs>
-      </div>
-      <div className='panel'>
-        <Tabs className='activity-panel'>
-          <TabList className='nav'>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                Social Mentions
-              </button>
-            </Tab>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                Social Activity over Time
-              </button>
-            </Tab>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                Sentiment/Intensity
-              </button>
-            </Tab>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                Github Activity
-              </button>
-            </Tab>
-            <Tab className='nav-item' selectedClassName='active'>
-              <button className='nav-link'>
-                SAN Community
-              </button>
-            </Tab>
-          </TabList>
-          <TabPanel>
-            Social Mentions
-          </TabPanel>
-          <TabPanel>
-            Social Activity over Time
-          </TabPanel>
-          <TabPanel>
-            Sentiment/Intensity
-          </TabPanel>
-          <TabPanel>
-            Github Activity
-          </TabPanel>
-          <TabPanel>
-            SAN Community
-          </TabPanel>
-        </Tabs>
-      </div>
-      <PanelBlock title='Blockchain Analytics' />
-      <div className='analysis'>
-        <PanelBlock title='Signals/Volatility' />
-        <PanelBlock title='Expert Analyses' />
-        <PanelBlock title='News/Press' />
-      </div>
+      </HiddenElements>
+      <HiddenElements>
+        <PanelBlock title='Blockchain Analytics' />
+        <div className='analysis'>
+          <PanelBlock title='Signals/Volatility' />
+          <PanelBlock title='Expert Analyses' />
+          <PanelBlock title='News/Press' />
+        </div>
+      </HiddenElements>
       <div className='information'>
-        <PanelBlock title='General Info'>
-          <GeneralInfoBlock info={project} />
+        <PanelBlock
+          isUnauthorized={generalInfo.isUnauthorized}
+          isLoading={generalInfo.isLoading}
+          title='General Info'>
+          <GeneralInfoBlock {...generalInfo.project} />
         </PanelBlock>
-        <PanelBlock title='Financials'>
-          <FinancialsBlock info={project} />
+        <PanelBlock
+          isUnauthorized={generalInfo.isUnauthorized}
+          isLoading={generalInfo.isLoading}
+          title='Financials'>
+          <FinancialsBlock {...generalInfo.project} />
         </PanelBlock>
       </div>
     </div>
@@ -147,10 +149,24 @@ export const Detailed = ({data: { loading, project }, match}) => {
 
 Detailed.propTypes = propTypes
 
-const projectDetails = graphql(gql`
-  query {
+const mapStateToProps = state => {
+  return {
+    projects: state.projects.items,
+    loading: state.projects.isLoading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // TODO: have to retrive only selected project
+    retrieveProjects: () => dispatch(retrieveProjects)
+  }
+}
+
+const queryProject = gql`
+  query project($id: ID!) {
     project(
-      id: 1,      
+      id: $id,
     ){
       id,
       name,
@@ -162,38 +178,61 @@ const projectDetails = graphql(gql`
       redditLink,
       twitterLink,
       whitepaperLink,
-      slackLink
+      slackLink,
+      btcBalance,
+      projectTransparency,
+      projectTransparencyDescription,
+      projectTransparencyStatus,
+      tokenAddress,
+      fundsRaisedIcos { amount, currencyCode },
+      latestCoinmarketcapData {
+        priceUsd,
+        updateTime,
+        marketCapUsd
+      }
     }
   }
-`, {
-  options: (props) => ({
-    variables: {
-      ticker: props.match.params.ticker
-    }
-  })
-})
+`
 
-const mapStateToProps = state => {
-  return {
+const mapDataToProps = ({data}) => {
+  const isLoading = data.loading
+  const isEmpty = !!data.project
+  const isError = !!data.error
+  const errorMessage = data.error ? data.error.message : ''
+  const isUnauthorized = data.error ? /\bunauthorized/.test(data.error.message) : false
+  if (data.error && !isUnauthorized) {
+    // If our API server is not reponed with data
+    throw new Error(data.error.message)
   }
+  const project = data.project
+
+  return {generalInfo: {isLoading, isEmpty, isError, project, errorMessage, isUnauthorized}}
 }
 
-const mapDispatchToProps = dispatch => {
+const mapPropsToOptions = ({match, projects}) => {
+  const project = getProjectByTicker(match, projects)
   return {
+    skip: !project,
+    variables: {
+      id: project ? project.id : 0
+    }
   }
 }
 
 const enhance = compose(
-  projectDetails,
   connect(
     mapStateToProps,
     mapDispatchToProps
   ),
+  graphql(queryProject, {
+    props: mapDataToProps,
+    options: mapPropsToOptions
+  }),
   lifecycle({
     componentDidMount () {
+      this.props.retrieveProjects()
     }
-  }),
-  pure
+  })
 )
 
 export default enhance(Detailed)

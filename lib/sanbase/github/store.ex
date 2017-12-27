2b3609e425368eb9ccbd8a4c7e5953ec7b8a8fd4
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2017-12-27 17:18:16 +0200

    Start using Influxdb Store module

--- lib/sanbase/github/store.ex
+++ lib/sanbase/github/store.ex
@@ -2,19 +2,10 @@ defmodule Sanbase.Github.Store do
   # A module for storing and fetching github activity data from/to a time series data store
   #
   # Currently using InfluxDB for the time series data.
-  use Instream.Connection, otp_app: :sanbase
+  use Sanbase.Influxdb.Store
 
   alias Sanbase.Influxdb.Measurement
   alias Sanbase.Github.Store
-
-  def import(measurements) do
-    measurements
-    |> Stream.map(&Measurement.convert_measurement_for_import/1)
-    |> Stream.chunk_every(288) # 1 day of 5 min resolution data
-    |> Enum.map(fn data_for_import ->
-      :ok = Store.write(data_for_import)
-    end)
-  end
 
   def first_activity_datetime(ticker) do
     ~s/SELECT FIRST(activity) FROM "#{ticker}"/
@@ -29,32 +20,20 @@ defmodule Sanbase.Github.Store do
   end
 
   defp parse_measurement_datetime(%{
-    results: [%{
-      series: [%{
-        values: [[iso8601_datetime, _price]]
-      }]
-    }]
-  }) do
+         results: [
+           %{
+             series: [
+               %{
+                 values: [[iso8601_datetime, _price]]
+               }
+             ]
+           }
+         ]
+       }) do
     {:ok, datetime, _} = DateTime.from_iso8601(iso8601_datetime)
 
     datetime
   end
 
   defp parse_measurement_datetime(_), do: nil
-
-  def drop_ticker(ticker) do
-    %{results: _} = "DROP MEASUREMENT #{ticker}"
-    |> Store.execute()
-  end
-
-  def create_db() do
-    database()
-    |> Instream.Admin.Database.create()
-    |> Store.execute()
-  end
-
-  defp database() do
-    Application.fetch_env!(:sanbase, Sanbase.Github.Store)
-    |> Keyword.get(:database)
-  end
 end

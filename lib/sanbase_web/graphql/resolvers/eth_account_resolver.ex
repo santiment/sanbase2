ff553d58ab169cb76697b7f096526861dc43524e
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2017-12-28 18:40:28 +0200

    Add async to eth_account field

--- lib/sanbase_web/graphql/resolvers/eth_account_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/eth_account_resolver.ex
@@ -1,7 +1,11 @@
 defmodule SanbaseWeb.Graphql.Resolvers.EthAccountResolver do
   alias Sanbase.Auth.EthAccount
 
+  import Absinthe.Resolution.Helpers, only: [async: 1]
+
   def san_balance(eth_account, _, _) do
-    {:ok, EthAccount.san_balance(eth_account)}
+    async(fn ->
+      {:ok, EthAccount.san_balance(eth_account)}
+    end)
   end
 end

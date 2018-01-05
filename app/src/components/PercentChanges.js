9847dd9c76e74ce0d0cf8d9e2b5a39783be3b475
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-01-05 15:25:03 +0500

    Added percent changes 24h in PDP

---
+++ app/src/components/PercentChanges.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import cx from 'classnames'
+import './PercentChanges.css'
+
+export const PercentChanges = ({changes}) => (
+  <div className={cx({
+    'percent-changes--positive': changes >= 0,
+    'percent-changes--negative': changes < 0
+  })}>
+    <i className={cx({
+      'fa': true,
+      'fa-caret-up': changes >= 0,
+      'fa-caret-down': changes < 0
+    })} />&nbsp;
+    {changes < 0 ? changes.toString().split('-')[1] : changes}
+  </div>
+)
+
+export default PercentChanges

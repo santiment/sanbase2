40be870c15dc0b3d27ec186b16cc5de0d68a3465
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-01-08 16:06:35 +0200

    Migration to rerun ICO import

--- priv/repo/migrations/20180108110118_clear_projects_run_ico_import2.exs
+++ priv/repo/migrations/20180108110118_clear_projects_run_ico_import2.exs
@@ -6,17 +6,19 @@ defmodule Sanbase.Repo.Migrations.ClearProjectsRunIcoImport2 do
   import Supervisor.Spec
 
   def up do
-    faktory_host = System.get_env("FAKTORY_HOST")
+    # Making the migration no-op. The data migration is rerun in 20180108140221_clear_projects_run_ico_import3.exs
 
-    if !is_nil(faktory_host) do
-      opts = [strategy: :one_for_one, name: Sanbase.Supervisor, max_restarts: 5, max_seconds: 1]
-      Faktory.Configuration.init
-      Supervisor.start_link([supervisor(Faktory.Supervisor, [])], opts)
-
-      SanbaseWorkers.DataMigrations.ClearProjectsRunIcoImport.perform_async([])
-    else
-      Logger.warn("FAKTORY_HOST variable missing. Skipping ICO import.")
-    end
+    # faktory_host = System.get_env("FAKTORY_HOST")
+    #
+    # if !is_nil(faktory_host) do
+    #   opts = [strategy: :one_for_one, name: Sanbase.Supervisor, max_restarts: 5, max_seconds: 1]
+    #   Faktory.Configuration.init
+    #   Supervisor.start_link([supervisor(Faktory.Supervisor, [])], opts)
+    #
+    #   SanbaseWorkers.DataMigrations.ClearProjectsRunIcoImport.perform_async([])
+    # else
+    #   Logger.warn("FAKTORY_HOST variable missing. Skipping ICO import.")
+    # end
   end
 
   def down do

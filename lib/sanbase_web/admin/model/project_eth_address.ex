b3312b4259c65e23c3e8b27998ff6852d9cbfc2c
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-01-11 11:38:55 +0200

    Ex_admin - FK dropdowns are ordered by name

--- lib/sanbase_web/admin/model/project_eth_address.ex
+++ lib/sanbase_web/admin/model/project_eth_address.ex
@@ -1,7 +1,17 @@
 defmodule Sanbase.ExAdmin.Model.ProjectEthAddress do
   use ExAdmin.Register
 
-  register_resource Sanbase.Model.ProjectEthAddress do
+  import Ecto.Query, warn: false
 
+  alias Sanbase.Model.Project
+
+  register_resource Sanbase.Model.ProjectEthAddress do
+    form project do
+      inputs do
+        input project, :address
+        input project, :project, collection: from(p in Project, order_by: p.name) |> Sanbase.Repo.all()
+        input project, :project_transparency
+      end
+    end
   end
 end

c07d8cd8d668afec34a91c9070f90ed73dd117c2
Valentin Mihov <valentin.mihov@gmail.com>
2018-01-13 00:59:58 +0200

    Reduce the number of points erturned by the TBR API
    The limit is 500, but the exact number of points returned might be a bit
    higher, as the response is chunked into equal chunks and each is
    averaged/sumed.
    
    The TBR is using average for the reduction. The transaction volume is
    using sum.

--- test/sanbase_web/graphql/etherbi_api_test.exs
+++ test/sanbase_web/graphql/etherbi_api_test.exs
@@ -5,12 +5,16 @@ defmodule Sanbase.Github.EtherbiApiTest do
   import Mockery
 
   test "fetch burn rate", context do
+    burn_rate = [
+      [1514766000, 91716892495405965698400256],
+      [1514770144, 359319706108516227858038784],
+      [1514778068, 31034050000000001245184]
+    ]
+
     mock HTTPoison, :get,
         {:ok, %HTTPoison.Response{
           status_code: 200,
-          body: "[[1514766000, 91716892495405965698400256],\
-              [1514770144, 359319706108516227858038784],\
-              [1514778068, 31034050000000001245184]]"
+          body: Poison.encode!(burn_rate)
         }}
 
       ticker = "SAN"
@@ -42,11 +46,57 @@ defmodule Sanbase.Github.EtherbiApiTest do
       assert %{"burnRate" => "31034050000000001245184"} in burn_rates
   end
 
-  test "fetch transaction volume", context do
+  test "fetch big response for the burn rate", context do
+    burn_rate = Stream.cycle([
+      [1514766000, 91716892495405965698400256],
+      [1514770144, 359319706108516227858038784],
+      [1514778068, 31034050000000001245184]
+    ])
+    |> Enum.take(20000)
+
     mock HTTPoison, :get,
-        {:ok, %HTTPoison.Response{status_code: 200, body: "[[1514765863, 5810803200000000000],
-              [1514766007, 700000000000001803841],
-              [1514770144, 1749612781540000000000]]"}}
+        {:ok, %HTTPoison.Response{
+          status_code: 200,
+          body: Poison.encode!(burn_rate)
+        }}
+
+      ticker = "SAN"
+      datetime1 = DateTime.from_naive!(~N[2018-01-01 12:00:00], "Etc/UTC")
+      datetime2 = DateTime.from_naive!(~N[2017-01-01 21:45:00], "Etc/UTC")
+      datetime1_unix = DateTime.to_unix(datetime1, :second)
+      datetime2_unix = DateTime.to_unix(datetime2, :second)
+
+      query = """
+      {
+        burnRate(
+          ticker: "#{ticker}",
+          from: "#{datetime1}",
+          to: "#{datetime2}")
+          {
+            burnRate
+          }
+        }
+      """
+
+      result =
+        context.conn
+        |> post("/graphql", query_skeleton(query, "burnRate"))
+
+      burn_rates = json_response(result, 200)["data"]["burnRate"]
+
+      assert length(burn_rates) == 500
+  end
+
+  test "fetch transaction volume", context do
+    transaction_volume = Stream.cycle([
+      [1514765863, 5810803200000000000],
+      [1514766007, 700000000000001803841],
+      [1514770144, 1749612781540000000000]
+    ])
+    |> Enum.take(21000)
+
+    mock HTTPoison, :get,
+        {:ok, %HTTPoison.Response{status_code: 200, body: Poison.encode!(transaction_volume)}}
 
       ticker = "SAN"
       datetime1 = DateTime.from_naive!(~N[2018-01-01 12:00:00], "Etc/UTC")
@@ -72,9 +122,7 @@ defmodule Sanbase.Github.EtherbiApiTest do
 
       transaction_volumes = json_response(result, 200)["data"]["transactionVolume"]
 
-      assert %{"transactionVolume" => "5810803200000000000"} in transaction_volumes
-      assert %{"transactionVolume" => "700000000000001803841"} in transaction_volumes
-      assert %{"transactionVolume" => "1749612781540000000000"} in transaction_volumes
+      assert length(transaction_volumes) == 500
   end
 
   defp query_skeleton(query, query_name) do

102367bda34f95027d3570d35e2c9e1e3372abe6
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-12 12:14:23 +0200

    Unificate getting values from the config via macros

--- lib/sanbase/internal_services/ethauth.ex
+++ lib/sanbase/internal_services/ethauth.ex
@@ -1,7 +1,8 @@
 defmodule Sanbase.InternalServices.Ethauth do
   use Tesla
 
-  import Sanbase.Utils, only: [parse_config_value: 1]
+  require Sanbase.Utils.Config
+  alias Sanbase.Utils.Config
 
   def verify_signature(signature, address, message_hash) do
     %Tesla.Env{status: 200, body: body} = get(client(), "recover", query: [sign: signature, hash: message_hash])
@@ -20,20 +21,14 @@ defmodule Sanbase.InternalServices.Ethauth do
   end
 
   defp client() do
-    ethauth_url = config(:url)
-    basic_auth_username = config(:basic_auth_username)
-    basic_auth_password = config(:basic_auth_password)
+    ethauth_url = Config.get(:url)
+    basic_auth_username = Config.get(:basic_auth_username)
+    basic_auth_password = Config.get(:basic_auth_password)
 
     Tesla.build_client [
       {Tesla.Middleware.BaseUrl, ethauth_url},
       {Tesla.Middleware.BasicAuth, username: basic_auth_username, password: basic_auth_password},
       Tesla.Middleware.Logger
     ]
-  end
-
-  defp config(key) do
-    Application.get_env(:sanbase, __MODULE__)
-    |> Keyword.get(key)
-    |> parse_config_value()
   end
 end
--- lib/sanbase/utils/config.ex
+++ lib/sanbase/utils/config.ex
@@ -1,13 +1,29 @@
 defmodule Sanbase.Utils.Config do
-  def parse_config_value({:system, env_key, default}), do: System.get_env(env_key) || default
+  def parse_config_value({:system, env_key, default}) do
+    System.get_env(env_key) || default
+  end
 
-  def parse_config_value({:system, env_key}), do: System.get_env(env_key)
+  def parse_config_value({:system, env_key}) do
+    System.get_env(env_key)
+  end
 
-  def parse_config_value(value), do: value
+  def parse_config_value(value) do
+    value
+  end
 
   defmacro get(key, default \\ nil) do
-    Application.fetch_env!(:sanbase, __MODULE__)
-    |> Keyword.get(key, default)
-    |> parse_config_value()
+    quote bind_quoted: [key: key, default: default] do
+      Application.fetch_env!(:sanbase, __MODULE__)
+      |> Keyword.get(key, default)
+      |> Sanbase.Utils.Config.parse_config_value()
+    end
+  end
+
+  defmacro module_get(module, key, default \\ nil) do
+    quote bind_quoted: [module: module, key: key, default: default] do
+      Application.fetch_env!(:sanbase, module)
+      |> Keyword.get(key, default)
+      |> Sanbase.Utils.Config.parse_config_value()
+    end
   end
 end

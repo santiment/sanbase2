ab8e1f2e1b259370316ce51e48da04b8a63f8759
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-12 11:06:35 +0200

    backup

--- lib/sanbase/utils/config.ex
+++ lib/sanbase/utils/config.ex
@@ -1,11 +1,11 @@
-defmodule Sanbase.Utils do
+defmodule Sanbase.Utils.Config do
   def parse_config_value({:system, env_key, default}), do: System.get_env(env_key) || default
 
   def parse_config_value({:system, env_key}), do: System.get_env(env_key)
 
   def parse_config_value(value), do: value
 
-  defmacro get_config(key, default \\ nil) do
+  defmacro get(key, default \\ nil) do
     Application.fetch_env!(:sanbase, __MODULE__)
     |> Keyword.get(key, default)
     |> parse_config_value()

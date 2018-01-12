26c1c38117de0a84134d5d250172f42359a5a512
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-12 12:29:29 +0200

    Reduce recv timeout to 15s. Change mocking library in tests. Remove unused functions

--- lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
@@ -1,6 +1,9 @@
 defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
   import Sanbase.Utils, only: [parse_config_value: 1]
 
+  @http_client Mockery.of("HTTPoison")
+  @recv_timeout 15_000
+
   def burn_rate(_root, %{ticker: ticker, from: from, to: to}, _resolution) do
     from_unix = DateTime.to_unix(from, :seconds)
     to_unix = DateTime.to_unix(to, :seconds)
@@ -8,8 +11,8 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
     etherbi_url = get_config(:url)
     url = "#{etherbi_url}/burn_rate?ticker=#{ticker}&from_timestamp=#{from_unix}&to_timestamp=#{to_unix}"
 
-    options = [recv_timeout: 30_000]
-    case HTTPoison.get(url, options) do
+    options = [recv_timeout: @recv_timeout]
+    case @http_client.get(url, options) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, result} = Poison.decode(body)
         result =
@@ -34,8 +37,8 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
 
     etherbi_url = get_config(:url)
     url = "#{etherbi_url}/transaction_volume?ticker=#{ticker}&from_timestamp=#{from_unix}&to_timestamp=#{to_unix}"
-    options = [recv_timeout: 30_000]
-    case HTTPoison.get(url, options) do
+    options = [recv_timeout: @recv_timeout]
+    case @http_client.get(url, options) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, result} = Poison.decode(body)
 

f980cdae2f2f52102e76e8d7aa637df6b49e8f9a
Valentin Mihov <valentin.mihov@gmail.com>
2018-01-13 00:59:58 +0200

    Reduce the number of points erturned by the TBR API
    The limit is 500, but the exact number of points returned might be a bit
    higher, as the response is chunked into equal chunks and each is
    averaged/sumed.
    
    The TBR is using average for the reduction. The transaction volume is
    using sum.

--- lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
@@ -4,6 +4,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
 
   @http_client Mockery.of("HTTPoison")
   @recv_timeout 15_000
+  @max_result_size 500
 
   def burn_rate(_root, %{ticker: ticker, from: from, to: to}, _resolution) do
     from_unix = DateTime.to_unix(from, :seconds)
@@ -18,6 +19,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
         {:ok, result} = Poison.decode(body)
         result =
           result
+          |> reduce_result_size(&average/1)
           |> Enum.map(fn [timestamp, br] ->
             %{datetime: DateTime.from_unix!(timestamp), burn_rate: Decimal.new(br)}
           end)
@@ -38,6 +40,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
 
     etherbi_url = Config.get(:url)
     url = "#{etherbi_url}/transaction_volume?ticker=#{ticker}&from_timestamp=#{from_unix}&to_timestamp=#{to_unix}"
+
     options = [recv_timeout: @recv_timeout]
     case @http_client.get(url, options) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
@@ -45,6 +48,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
 
         result =
           result
+          |> reduce_result_size(&Enum.sum/1)
           |> Enum.map(fn [timestamp, trx_volume] ->
             %{datetime: DateTime.from_unix!(timestamp), transaction_volume: Decimal.new(trx_volume)}
           end)
@@ -57,5 +61,37 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
       _ ->
         {:error, "Cannot fetch burn transaction volume for ticker #{ticker}"}
     end
+  end
+
+  defp reduce_result_size(list, _) when length(list) < 2 * @max_result_size, do: list
+
+  defp reduce_result_size(list, reduce_function) do
+    chunk_size = trunc(length(list) / @max_result_size)
+
+    list
+    |> Enum.chunk_every(chunk_size)
+    |> Enum.map(fn chunk ->
+      [
+        min_timestamp(chunk),
+        reduced_value(chunk, reduce_function)
+      ]
+    end)
+  end
+
+  defp min_timestamp(chunk) do
+    chunk
+    |> Enum.map(&hd/1)
+    |> Enum.min
+  end
+
+  defp reduced_value(chunk, reduce_function) do
+    chunk
+    |> Enum.map(&tl/1)
+    |> Enum.map(&hd/1)
+    |> reduce_function.()
+  end
+
+  defp average(list) do
+    Enum.sum(list) / length(list)
   end
 end

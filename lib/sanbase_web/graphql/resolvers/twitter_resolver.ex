b65838a92e0340be06e8933c954c7e0f7c57be41
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-18 19:15:34 +0200

    Select the first record with matching ticker and not empty twitter link

--- lib/sanbase_web/graphql/resolvers/twitter_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/twitter_resolver.ex
@@ -3,22 +3,31 @@ defmodule SanbaseWeb.Graphql.Resolvers.TwitterResolver do
   alias Sanbase.Model.Project
   alias Sanbase.ExternalServices.TwitterData.Store
 
+  import Ecto.Query
+
   def twitter_data(_root, %{ticker: ticker}, _resolution) do
-    with %Project{twitter_link: twitter_link} <- Repo.get_by(Project, ticker: ticker),
+    with query <-
+           from(
+             p in Project,
+             where: p.ticker == ^ticker and not is_nil(p.twitter_link),
+             select: p.twitter_link
+           ),
+         [twitter_link|_] <- Repo.all(query),
          twitter_name <- extract_twitter_name(twitter_link),
          {datetime, followers_count} <- Store.last_record_for_measurement(twitter_name) do
-      {:ok, %{
-        ticker: ticker,
-        datetime: datetime,
-        twitter_name: twitter_name,
-        followers_count: followers_count
-      }}
+      {:ok,
+       %{
+         ticker: ticker,
+         datetime: datetime,
+         twitter_name: twitter_name,
+         followers_count: followers_count
+       }}
     else
       {:error, reason} ->
         {:error, "Cannot fetch twitter data for ticker #{ticker}: #{reason}"}
 
-      _ ->
-        {:error, "Cannot fetch twitter data for ticker #{ticker}"}
+      error ->
+        {:error, "Cannot fetch twitter data for ticker #{ticker}: #{inspect(error)}"}
     end
   end
 
@@ -27,7 +36,13 @@ defmodule SanbaseWeb.Graphql.Resolvers.TwitterResolver do
         %{ticker: ticker, from: from, to: to, interval: interval},
         _resolution
       ) do
-    with %Project{twitter_link: twitter_link} <- Repo.get_by(Project, ticker: ticker),
+    with query <-
+           from(
+             p in Project,
+             where: p.ticker == ^ticker and not is_nil(p.twitter_link),
+             select: p.twitter_link
+           ),
+         [twitter_link|_] <- Repo.all(query),
          twitter_name <- extract_twitter_name(twitter_link),
          twitter_historical_data <-
            Store.all_records_for_measurement!(twitter_name, from, to, interval) do
@@ -42,8 +57,8 @@ defmodule SanbaseWeb.Graphql.Resolvers.TwitterResolver do
       {:error, reason} ->
         {:error, "Cannot fetch twitter history data for ticker #{ticker}: #{reason}"}
 
-      _ ->
-        {:error, "Cannot fetch twitter history data for ticker #{ticker}"}
+      error ->
+        {:error, "Cannot fetch twitter history data for ticker #{ticker}: #{inspect(error)}"}
     end
   end
 

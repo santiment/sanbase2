eb1d4e41b5ab70ebe9344de12f0e15f5b6a29794
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-01-22 00:53:15 +0500

    Added Form Field with validation

--- app/src/pages/EventVotesNew.js
+++ app/src/pages/EventVotesNew.js
@@ -4,7 +4,8 @@ import { Route } from 'react-router-dom'
 import cx from 'classnames'
 import {
   Input,
-  Button
+  Button,
+  Message
 } from 'semantic-ui-react'
 import Panel from './../components/Panel'
 import Post from './../components/Post'
@@ -41,30 +42,65 @@ const ReactFormInput = FormField(({fieldApi, ...rest}) => {
   return (<Input
     value={getValue() || ''}
     onChange={e => setValue(e.target.value)}
-    onBlur={(e) => {
-      setTouched()
+    onBlur={e => {
+      if (e.target.value && e.target.value.length > 0) {
+        setTouched()
+      }
     }}
     {...rest}
   />)
 })
 
+const errorValidator = values => {
+  return {
+    link: !values.link ||
+      !values.link.match(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/)
+        ? 'Input must contain a valid URL' : null
+  }
+}
+
+const successValidator = values => {
+  return {
+    link: values.link &&
+      values.link.match(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/)
+        ? 'Thank you for entering link' : null
+  }
+}
+
+const isError = formApi => (
+  !!formApi.getValue().link &&
+    formApi.getValue().link.length > 2 &&
+    formApi.getTouched().link &&
+    !!formApi.getError().link
+)
+
 const CreateLink = ({history, changePost}) => {
   return (
-    <Form onSubmit={values => {
-      changePost(values, 'title')
-    }}>
+    <Form
+      validateError={errorValidator}
+      validateSuccess={successValidator}
+      onSubmit={values => {
+        console.log('check')
+        changePost(values, 'title')
+      }}>
       {formApi => (<form
         className='event-posts-new-step'
         onSubmit={formApi.submitForm}
         autoComplete='off'>
         <label>Link</label>
         <ReactFormInput
-          focus
           fluid
           field='link'
+          error={isError(formApi)}
           placeholder='Paster a URL (e.g. https://twitter/insight)' />
+        {isError(formApi) &&
+          <Message negative>
+            {formApi.getError().link}
+          </Message>}
         <div className='event-posts-new-step-control'>
           <Button
+            disabled={!!formApi.getError().link}
+            positive={!!formApi.getSuccess().link}
             type='submit'>
             Next
           </Button>

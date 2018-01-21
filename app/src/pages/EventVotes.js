d83af0a5029e7b5aafaa73ea3d4efa2e9556485d
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-01-21 21:28:04 +0500

    Creating the master post of insight. (wip)

--- app/src/pages/EventVotes.js
+++ app/src/pages/EventVotes.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import Panel from './../components/Panel'
+import PostsList from './../components/PostsList'
+import { Icon } from 'semantic-ui-react'
+import './EventVotes.css'
+
+const posts = {
+  'asffe2f2f': {
+    title: 'Ripple pump!',
+    link: 'https://medium.com',
+    votes: 25,
+    author: 'sdfefw',
+    created: new Date()
+  },
+  'a23429r3f': {
+    title: 'Eth pump!',
+    link: 'https://medium.com',
+    votes: 95,
+    author: 'jcasdfiu',
+    created: new Date()
+  },
+  'asf2342ff': {
+    title: 'Gcoin pump!',
+    link: 'https://medium.com',
+    votes: 15,
+    author: 'jcasdfiu',
+    created: new Date()
+  }
+}
 
 const EventVotes = () => {
   return (
     <div className='page event-votes'>
-      <Panel withoutHeader>
-        Votes
+      <Panel>
+        <div className='event-votes-header'>
+          <span>Insights</span>
+          <NavLink
+            className='event-votes-navigation__add-link'
+            to={'/events/votes/new'}>
+            <Icon name='als' /> Add new insight
+          </NavLink>
+          <div className='event-votes-navigation'>
+            <NavLink
+              className='event-votes-navigation__link'
+              activeClassName='event-votes-navigation__link--active'
+              exact
+              to={'/events/votes'}>
+              POPULAR
+            </NavLink>
+            <NavLink
+              className='event-votes-navigation__link'
+              activeClassName='event-votes-navigation__link--active'
+              exact
+              to={'/events/votes/newest'}>
+              NEWEST
+            </NavLink>
+          </div>
+        </div>
+        <PostsList posts={posts} />
       </Panel>
     </div>
   )

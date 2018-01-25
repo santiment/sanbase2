60ed01ff7fb3d69e169884ae270e208d892704b2
Valentin Mihov <valentin.mihov@gmail.com>
2018-01-25 18:23:20 +0200

    Divide the san tokens by the token decimals

--- lib/sanbase/internal_services/ethauth.ex
+++ lib/sanbase/internal_services/ethauth.ex
@@ -5,7 +5,8 @@ defmodule Sanbase.InternalServices.Ethauth do
   alias Sanbase.Utils.Config
 
   def verify_signature(signature, address, message_hash) do
-    %Tesla.Env{status: 200, body: body} = get(client(), "recover", query: [sign: signature, hash: message_hash])
+    %Tesla.Env{status: 200, body: body} =
+      get(client(), "recover", query: [sign: signature, hash: message_hash])
 
     %{"recovered" => recovered} = Poison.decode!(body)
 
@@ -19,15 +20,20 @@ defmodule Sanbase.InternalServices.Ethauth do
     |> Decimal.new()
   end
 
+  def san_token_decimals() do
+    :math.pow(10, 18)
+    |> round()
+  end
+
   defp client() do
     ethauth_url = Config.get(:url)
     basic_auth_username = Config.get(:basic_auth_username)
     basic_auth_password = Config.get(:basic_auth_password)
 
-    Tesla.build_client [
+    Tesla.build_client([
       {Tesla.Middleware.BaseUrl, ethauth_url},
       {Tesla.Middleware.BasicAuth, username: basic_auth_username, password: basic_auth_password},
       Tesla.Middleware.Logger
-    ]
+    ])
   end
 end

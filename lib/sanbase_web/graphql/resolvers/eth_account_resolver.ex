a901e8d7310911b527c1d87e32c768988d50023c
Valentin Mihov <valentin.mihov@gmail.com>
2018-01-25 18:45:03 +0200

    Return absolute SAN tokens for the user balance

--- lib/sanbase_web/graphql/resolvers/eth_account_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/eth_account_resolver.ex
@@ -1,7 +1,8 @@
 defmodule SanbaseWeb.Graphql.Resolvers.EthAccountResolver do
   alias Sanbase.Auth.EthAccount
+  alias Sanbase.InternalServices.Ethauth
 
   def san_balance(eth_account, _, _) do
-    {:ok, EthAccount.san_balance(eth_account)}
+    {:ok, Decimal.div(EthAccount.san_balance(eth_account), Ethauth.san_token_decimals())}
   end
 end

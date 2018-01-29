6f38ea99a200c6e463ae920fbc38d9c3d5672f4f
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-29 12:49:25 +0200

    Refactor parts of getting transactions. Start implementing BurnRate

---
+++ lib/sanbase/etherbi/burn_rate/fetcher.ex
@@ -0,0 +1,53 @@
+defmodule Sanbase.Etherbi.BurnRate.Fetcher do
+
+  @seconds_in_month 60 * 60 * 24 * 30
+
+  def burn_rate(ticker) do
+    case generate_from_to_interval_unix(ticker) do
+      {from_datetime, to_datetime} ->
+        Logger.info("Getting burn rate for #{ticker}")
+        @etherbi_api.get_burn_rate(ticker, from_datetime, to_datetime)
+
+      _ ->
+        {:ok, []}
+    end
+  end
+
+  # Private functions
+
+  # Get a tuple `{from, to}` to use in a query or `nil` if there is no info.
+  # If there is no record in the DB for that address and Etherbi's
+  # first transaction timestamp API returns no result then there are no transactions
+  # In that case return `nil` and detect in the caller that no query should be made
+  defp generate_from_to_interval_unix(ticker) do
+    # Returns {:ok, nil} if there are no records for that measurement
+    {:ok, from_datetime} =
+      Store.last_datetime(ticker)
+
+    from_datetime =
+      if from_datetime do
+        from_datetime
+      else
+        {:ok, from_datetime} = @etherbi_api.get_first_transaction_timestamp(address)
+        from_datetime
+      end
+
+    if from_datetime do
+      to_datetime = calculate_to_datetime(from_datetime, DateTime.utc_now())
+
+      {from_datetime, to_datetime}
+    else
+      nil
+    end
+  end
+
+  # If the difference between the datetimes is too large the query will be too big
+  # Allow the max difference between the datetimes to be 1 month
+  defp calculate_to_datetime(from_datetime, to_datetime) do
+    if DateTime.diff(to_datetime, from_datetime, :seconds) > @seconds_in_month do
+      Sanbase.DateTimeUtils.seconds_after(@seconds_in_month, from_datetime)
+    else
+      to_datetime
+    end
+  end
+end

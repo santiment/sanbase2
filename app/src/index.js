951de2f0b37c92127e1b447e5e3f13df91ec1d1f
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-01-30 10:42:36 +0500

    Added Error apollo link.

--- app/src/index.js
+++ app/src/index.js
@@ -12,6 +12,8 @@ import ApolloClient from 'apollo-client'
 import gql from 'graphql-tag'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
+import { from } from 'apollo-link'
+import { onError } from 'apollo-link-error'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 import App from './App'
@@ -41,6 +43,16 @@ const handleLoad = () => {
   }
 
   const httpLink = createHttpLink({ uri: `${origin}/graphql` })
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      )
+    }
+    if (networkError) { console.log(`[Network error]: ${networkError}`) }
+  })
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
     const token = loadState() ? loadState().token : undefined
@@ -54,7 +66,7 @@ const handleLoad = () => {
   })
 
   const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: from([authLink, errorLink, httpLink]),
     cache: new InMemoryCache()
   })
 

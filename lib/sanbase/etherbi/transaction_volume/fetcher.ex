b1deb1935469a7541e583f830dc29a92800f6688
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-01-30 14:49:14 +0200

    Reuse generating from_to datetime function

--- lib/sanbase/etherbi/transaction_volume/fetcher.ex
+++ lib/sanbase/etherbi/transaction_volume/fetcher.ex
@@ -1,7 +1,7 @@
 defmodule Sanbase.Etherbi.TransactionVolume.Fetcher do
   require Logger
 
-  alias Sanbase.Etherbi.TransactionVolume.Store
+  alias Sanbase.Etherbi.Utils
 
   @etherbi_api Mockery.of("Sanbase.Etherbi.EtherbiApi")
 
@@ -10,41 +10,17 @@ defmodule Sanbase.Etherbi.TransactionVolume.Fetcher do
   """
   @spec transaction_volume(binary()) :: {:ok, list()} | {:error, binary()}
   def transaction_volume(ticker) do
-    case generate_from_to_interval_unix(ticker) do
+    case Utils.generate_from_to_interval_unix(
+           ticker,
+           db_last_datetime: &Sanbase.Etherbi.TransactionVolume.Store.last_datetime/1,
+           etherbi_first_timestamp: &@etherbi_api.get_first_transaction_timestamp_ticker/1
+         ) do
       {from_datetime, to_datetime} ->
         Logger.info("Getting transaction volume for #{ticker}")
         @etherbi_api.get_transaction_volume(ticker, from_datetime, to_datetime)
 
       _ ->
         {:ok, []}
-    end
-  end
-
-  # Private functions
-
-  # Get a tuple `{from, to}` to use in a query or `nil` if there is no info.
-  # If there is no record in the DB for that address and Etherbi's
-  # first transaction timestamp API returns no result then there are no transactions
-  # In that case return `nil` and detect in the caller that no query should be made
-  defp generate_from_to_interval_unix(ticker) do
-    # Returns {:ok, nil} if there are no records for that measurement
-    {:ok, from_datetime} =
-      Store.last_datetime(ticker)
-
-    from_datetime =
-      if from_datetime do
-        from_datetime
-      else
-        {:ok, datetime} = @etherbi_api.get_first_transaction_timestamp_ticker(ticker)
-        datetime
-      end
-
-    if from_datetime do
-      to_datetime = Sanbase.Etherbi.Utils.calculate_to_datetime(from_datetime, DateTime.utc_now())
-
-      {from_datetime, to_datetime}
-    else
-      nil
     end
   end
 end

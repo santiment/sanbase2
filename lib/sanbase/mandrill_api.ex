9c197e9a7cc85d4129487bbcfb4c6cb7839c4b4f
Valentin Mihov <valentin.mihov@gmail.com>
2018-01-31 10:52:42 +0200

    Fix mandrill email sending

--- lib/sanbase/mandrill_api.ex
+++ lib/sanbase/mandrill_api.ex
@@ -2,13 +2,13 @@ defmodule Sanbase.MandrillApi do
   alias Sanbase.Utils.Config
   require Sanbase.Utils.Config
 
-  @send_email_url "https://mandrillapp.com/api/1.0/messages/send.json"
+  @send_email_url "https://mandrillapp.com/api/1.0/messages/send-template.json"
+  @environment Mix.env()
 
   def send(template, recepient, variables) do
     request_body =
       build_request(template, recepient, variables)
       |> Poison.encode!()
-      |> IO.inspect()
 
     case HTTPoison.post(@send_email_url, request_body) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
@@ -25,16 +25,19 @@ defmodule Sanbase.MandrillApi do
   defp build_request(template, recepient, variables) do
     %{
       template_name: template,
+      template_content: [],
       message: %{
         to: [
           %{
             email: recepient
           }
         ],
-        global_merge_vars: build_global_merge_vars(variables)
+        global_merge_vars: build_global_merge_vars(variables),
+        from_email: Config.module_get(__MODULE__, :from_email),
+        from_name: "Santiment Sanbase"
       },
-      key: Config.module_get(__MODULE__, :apikey),
-      from_email: Config.module_get(__MODULE__, :from_email)
+      tags: [@environment, template],
+      key: Config.module_get(__MODULE__, :apikey)
     }
   end
 

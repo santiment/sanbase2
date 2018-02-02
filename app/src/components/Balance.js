b6c0e9f73b6288420bb9e52144506353ccc73117
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-02 15:28:32 +0500

    Restyle Login page (WIP)

--- app/src/components/Balance.js
+++ app/src/components/Balance.js
@@ -1,22 +1,26 @@
 import React, { Fragment } from 'react'
+import { Message } from 'semantic-ui-react'
 import { formatNumber } from './../utils/formatting.js'
 
 export const Balance = ({user, onlyBalance = false}) => {
   return (
-    <div>{user.ethAccounts.map((account, index) => (
-      <Fragment key={index}>
-        {!onlyBalance &&
-        <div className='account-name'>
-          <a className='address'
-            href={`https://etherscan.io/address/${account.address}`}
-            target='_blank'>{account.address}
-          </a>
-        </div>}
-        <div className='account-balance'>
-          {formatNumber(account.sanBalance, 'SAN')}
-        </div>
-      </Fragment>
-    ))}
+    <div>
+      {user.ethAccounts && user.ethAccounts.length > 0
+      ? user.ethAccounts.map((account, index) => (
+        <Fragment key={index}>
+          {!onlyBalance &&
+          <div className='account-name'>
+            <a className='address'
+              href={`https://etherscan.io/address/${account.address}`}
+              target='_blank'>{account.address}
+            </a>
+          </div>}
+          <div className='account-balance'>
+            {formatNumber(account.sanBalance, 'SAN')}
+          </div>
+        </Fragment>
+      ))
+      : <Message>You don't connect any wallet with SAN tokens</Message>}
     </div>
   )
 }

0b8840fc664e1dac243c0169bf92feda06b7f36c
Valentin Mihov <valentin.mihov@gmail.com>
2018-02-02 17:31:02 +0200

    Allow to set moderation comments for the posts
    Change the Post to have a field `state` which holds if the post is
    approved or declined. Add a moderation_comment column, which will
    contain the comment from the person that declined the post.

--- test/sanbase/voting/post_test.exs
+++ test/sanbase/voting/post_test.exs
@@ -5,7 +5,28 @@ defmodule Sanbase.Voting.PostTest do
   alias Sanbase.Auth.User
   alias Sanbase.Voting.{Poll, Post}
 
-  test "changeset does not allow to approve the post" do
+  test "create_changeset does not allow to approve the post" do
+    poll =
+      Poll.current_poll_changeset()
+      |> Repo.insert!()
+
+    user =
+      %User{salt: User.generate_salt()}
+      |> Repo.insert!()
+
+    post =
+      %Post{user_id: user.id, poll_id: poll.id}
+      |> Post.create_changeset(%{
+        link: "http://example.com",
+        title: "Awesome article!",
+        state: Post.approved_state()
+      })
+      |> Repo.insert!()
+
+    assert post.state == nil
+  end
+
+  test "changeset allows to change the state of the post" do
     poll =
       Poll.current_poll_changeset()
       |> Repo.insert!()
@@ -19,18 +40,10 @@ defmodule Sanbase.Voting.PostTest do
       |> Post.changeset(%{
         link: "http://example.com",
         title: "Awesome article!",
-        approved_at: Timex.now()
+        state: Post.approved_state()
       })
       |> Repo.insert!()
 
-    assert post.approved_at == nil
-  end
-
-  test "approve_changeset is updating the approved_at column" do
-    changeset =
-      %Post{}
-      |> Post.approve_changeset()
-
-    assert Timex.diff(Timex.now(), changeset.changes[:approved_at], :seconds) == 0
+    assert post.state == Post.approved_state()
   end
 end

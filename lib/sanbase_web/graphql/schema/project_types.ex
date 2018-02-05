92401677ff9530f0d4ae59c39cd5ef1f28dfd0c4
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-02-05 14:04:19 +0200

    Add project description field

--- lib/sanbase_web/graphql/schema/project_types.ex
+++ lib/sanbase_web/graphql/schema/project_types.ex
@@ -26,6 +26,7 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     field(:telegram_link, :string)
     field(:token_address, :string)
     field(:team_token_wallet, :string)
+    field(:description, :string)
     field(:token_decimals, :integer)
 
     field :market_segment, :string do
@@ -141,6 +142,7 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     field(:token_address, :string)
     field(:team_token_wallet, :string)
     field(:token_decimals, :integer)
+    field(:description, :string)
 
     field :market_segment, :string do
       resolve(&ProjectResolver.market_segment/3)
@@ -157,100 +159,6 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     end
 
     field(:project_transparency_description, :string)
-
-    field(:coinmarketcap_id, :string)
-
-    field :symbol, :string do
-      resolve(&ProjectResolver.symbol/3)
-    end
-
-    field :rank, :integer do
-      resolve(&ProjectResolver.rank/3)
-    end
-
-    field :price_usd, :decimal do
-      resolve(&ProjectResolver.price_usd/3)
-    end
-
-    field :volume_usd, :decimal do
-      resolve(&ProjectResolver.volume_usd/3)
-    end
-
-    field :marketcap_usd, :decimal do
-      resolve(&ProjectResolver.marketcap_usd/3)
-    end
-
-    field :available_supply, :decimal do
-      resolve(&ProjectResolver.available_supply/3)
-    end
-
-    field :total_supply, :decimal do
-      resolve(&ProjectResolver.total_supply/3)
-    end
-
-    field :percent_change_1h, :decimal, name: "percent_change1h" do
-      resolve(&ProjectResolver.percent_change_1h/3)
-    end
-
-    field :percent_change_24h, :decimal, name: "percent_change24h" do
-      resolve(&ProjectResolver.percent_change_24h/3)
-    end
-
-    field :percent_change_7d, :decimal, name: "percent_change7d" do
-      resolve(&ProjectResolver.percent_change_7d/3)
-    end
-  end
-
-  # Used in the project list query (public), so heavy computed fields are omitted
-  object :project_listing do
-    field(:id, non_null(:id))
-    field(:name, non_null(:string))
-    field(:ticker, :string)
-    field(:logo_url, :string)
-    field(:website_link, :string)
-    field(:btt_link, :string)
-    field(:facebook_link, :string)
-    field(:github_link, :string)
-    field(:reddit_link, :string)
-    field(:twitter_link, :string)
-    field(:whitepaper_link, :string)
-    field(:blog_link, :string)
-    field(:slack_link, :string)
-    field(:linkedin_link, :string)
-    field(:telegram_link, :string)
-    field(:token_address, :string)
-    field(:team_token_wallet, :string)
-    field(:token_decimals, :integer)
-    field(:total_supply, :integer)
-
-    field :market_segment, :string do
-      resolve(&ProjectResolver.market_segment/3)
-    end
-
-    field :infrastructure, :string do
-      resolve(&ProjectResolver.infrastructure/3)
-    end
-
-    field(:project_transparency, :boolean)
-
-    field :project_transparency_status, :string do
-      resolve(&ProjectResolver.project_transparency_status/3)
-    end
-
-    field(:project_transparency_description, :string)
-
-    field :eth_balance, :decimal do
-      resolve(&ProjectResolver.eth_balance(&1, &2, &3, true))
-    end
-
-    field :btc_balance, :decimal do
-      resolve(&ProjectResolver.btc_balance(&1, &2, &3, true))
-    end
-
-    # If there is no raw data for any currency for a given ico, then fallback one of the precalculated totals - one of Ico.funds_raised_usd, Ico.funds_raised_btc, Ico.funds_raised_eth (checked in that order)
-    field :funds_raised_icos, list_of(:currency_amount) do
-      resolve(&ProjectResolver.funds_raised_icos/3)
-    end
 
     field(:coinmarketcap_id, :string)
 
@@ -316,6 +224,7 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     field(:token_address, :string)
     field(:team_token_wallet, :string)
     field(:token_decimals, :integer)
+    field(:description, :string)
 
     field :market_segment, :string do
       resolve(&ProjectResolver.market_segment/3)
@@ -410,6 +319,7 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     field(:token_address, :string)
     field(:team_token_wallet, :string)
     field(:token_decimals, :integer)
+    field(:description, :string)
 
     field :market_segment, :string do
       resolve(&ProjectResolver.market_segment/3)

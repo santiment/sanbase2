9e8d546bb920bc66935c4d64436c723d47eb4b3b
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-05 14:02:06 +0500

    Added list of posts with approving state

--- app/stories/EventStory.js
+++ app/stories/EventStory.js
@@ -3,36 +3,59 @@ import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
 import Post from './../src/components/Post'
 import PostsList from './../src/components/PostsList'
-import EventVotesNew from './../src/pages/EventVotesNew.js'
+import EventVotesNew from './../src/pages/EventVotesNew/EventVotesNew.js'
 
 const posts = {
   'asffe2f2f': {
     title: 'Ripple pump!',
     link: 'https://medium.com',
+    user: {
+      id: 23,
+      username: 'Yura Z.'
+    },
+    state: 'approved',
     votes: 25,
-    author: 'sdfefw',
-    created: new Date()
+    createdAt: new Date()
+  },
+  'a2ffe2f2f': {
+    title: 'Btc pump!',
+    link: 'https://medium.com',
+    user: {
+      id: 23,
+      username: 'Yura Z.'
+    },
+    state: null,
+    createdAt: new Date()
   },
   'a23429r3f': {
     title: 'Eth pump!',
     link: 'https://medium.com',
+    user: {
+      id: 23,
+      username: 'Anka Z.'
+    },
+    state: 'declined',
+    moderationComment: 'Any reason of declined',
     votes: 95,
     author: 'jcasdfiu',
-    created: new Date()
+    createdAt: new Date()
   },
   'asf2342ff': {
     title: 'Gcoin pump!',
     link: 'https://medium.com',
+    user: {
+      id: 13
+    },
     votes: 15,
     author: 'jcasdfiu',
-    created: new Date()
+    createdAt: new Date()
   }
 }
 
 storiesOf('Event Votes', module)
   .add('Post', () => (
     <div>
-        Post component in EventVotes page
+        Post component in Insights page
       <hr />
       <div style={{margin: 20}}>
         <Post {...posts['asf2342ff']} />
@@ -42,6 +65,11 @@ storiesOf('Event Votes', module)
   .add('Posts List', () => (
     <div style={{margin: 20}}>
       <PostsList posts={posts} />
+    </div>
+  ))
+  .add("User's list of insights", () => (
+    <div style={{margin: 20}}>
+      <PostsList userId={23} posts={posts} />
     </div>
   ))
   .add('Post Create Form', () => (

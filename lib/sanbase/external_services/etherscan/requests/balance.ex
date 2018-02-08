bdfe5c46e3918e878aa8a2c1bf34e90a96d7bfb9
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-02-08 18:04:38 +0200

    Some refactoring on Etherscan

--- lib/sanbase/external_services/etherscan/requests/balance.ex
+++ lib/sanbase/external_services/etherscan/requests/balance.ex
@@ -2,6 +2,8 @@ defmodule Sanbase.ExternalServices.Etherscan.Requests.Balance do
   alias Sanbase.ExternalServices.Etherscan.Requests
   alias __MODULE__
 
+  require Logger
+
   defstruct [:status, :message, :result]
 
   defp get_query(address) do
@@ -16,7 +18,18 @@ defmodule Sanbase.ExternalServices.Etherscan.Requests.Balance do
   def get(address) do
     Requests.get("/", query: get_query(address))
     |> case do
-      %{status: 200, body: body} -> Poison.Decode.decode(body, as: %Balance{})
+      %{status: 200, body: body} ->
+        Poison.Decode.decode(body, as: %Balance{})
+
+      %{status: status, body: body} ->
+        error = "Error fetching transactions for #{address}. Status code: #{status}: #{body}"
+        Logger.warn(error)
+        {:error, error}
+
+      _ ->
+        error = "Error fetching transactions for #{address}"
+        Logger.warn(error)
+        {:error, error}
     end
   end
 end

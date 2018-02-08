17619c36e801df7fdfa68f66dc41a5c27049dbdf
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-08 13:05:22 +0500

    ProjectCard for mobile view (WIP)

--- app/src/pages/Projects/ProjectCard.js
+++ app/src/pages/Projects/ProjectCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Card, Statistic } from 'semantic-ui-react'
+import { Card, Button, Statistic, Label } from 'semantic-ui-react'
 import ProjectIcon from './../../components/ProjectIcon'
+import { formatNumber } from '../../utils/formatting'
+import { millify } from '../../utils/utils'
 
 // Project Name
 // Market Cap
@@ -8,45 +10,97 @@ import ProjectIcon from './../../components/ProjectIcon'
 // ETH Spent (30d) - total change in ETH balance for the last 30 days
 // Dev Activity (30d) - total dev activity for the last 30 days
 // Flag - signals flag. For now we only show it when Crypto Balance > Market Cap
+//
 
-const ProjectCard = (props) => {
+const MARKET_SEGMENT_COLORS = {
+  'Financial': 'violet',
+  'Media': 'yellow',
+  'Blockchain Network': 'teal',
+  'Prediction Market': 'olive',
+  'Advertising': 'orange',
+  'Transportation': 'grey',
+  'Gambling': 'red',
+  'Gaming': 'green',
+  'Legal': 'pink',
+  'Protocol': 'teal',
+  'Digital Identity': 'teal',
+  'Data': 'black'
+}
+
+const ProjectCard = ({
+  name,
+  rank,
+  ticker,
+  description,
+  marketSegment,
+  priceUsd,
+  percentChange24h,
+  volumeUsd,
+  githubData = null,
+  btcBalance = 0,
+  ethBalance = 0,
+  marketcapUsd,
+  teamTokenWallet
+}) => {
   return (
-    <Card fluid color='violet'>
+    <Card fluid color={MARKET_SEGMENT_COLORS[marketSegment]}>
       <Card.Content>
-        <ProjectIcon name='SAN' />
         <Card.Header>
-          Santiment Token
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <ProjectIcon name={ticker} />
+              {name}
+            </div>
+            <small>rank: {rank}</small>
+          </div>
         </Card.Header>
-        <Card.Meta>
-          Financial
-        </Card.Meta>
+        {marketSegment && <Card.Meta>
+          <Label
+            size='mini'
+            color={MARKET_SEGMENT_COLORS[marketSegment]} tag>
+            {marketSegment}
+          </Label>
+        </Card.Meta>}
         <Card.Description>
-          Awesome <strong>summary</strong> about this token
+          {!description
+            ? "We don't have any description about this project yet."
+            : description}
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Statistic.Group size='tiny' flated='left'>
+        <Statistic.Group size='mini' widths='two'>
           <Statistic >
             <Statistic.Label>Price</Statistic.Label>
-            <Statistic.Value>1.0$</Statistic.Value>
+            <Statistic.Value>{formatNumber(priceUsd, 'USD')}</Statistic.Value>
           </Statistic>
           <Statistic >
-            <Statistic.Label>24h</Statistic.Label>
-            <Statistic.Value>+10%</Statistic.Value>
+            <Statistic.Label>24h Price</Statistic.Label>
+            <Statistic.Value>{percentChange24h}%</Statistic.Value>
           </Statistic>
           <Statistic>
-            <Statistic.Label>Dev Activity</Statistic.Label>
-            <Statistic.Value>22</Statistic.Value>
-          </Statistic>
-          <Statistic>
-            <Statistic.Label>Crypto Balance</Statistic.Label>
-            <Statistic.Value>100.000$</Statistic.Value>
+            <Statistic.Label>Volume</Statistic.Label>
+            <Statistic.Value>{millify(parseFloat(volumeUsd))}</Statistic.Value>
           </Statistic>
           <Statistic>
             <Statistic.Label>MarketCap</Statistic.Label>
-            <Statistic.Value>100.000.000$</Statistic.Value>
+            <Statistic.Value>{millify(parseFloat(marketcapUsd))}</Statistic.Value>
+          </Statistic>
+          {!!githubData && <Statistic>
+            <Statistic.Label>Dev Activity</Statistic.Label>
+            <Statistic.Value>{githubData}</Statistic.Value>
+          </Statistic>}
+          <Statistic>
+            <Statistic.Label>Crypto Balance</Statistic.Label>
+            <Statistic.Value>ETH {ethBalance}</Statistic.Value>
           </Statistic>
         </Statistic.Group>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui three buttons'>
+          <Button basic size='large' icon='warning sign' />
+          <Button basic size='large' icon='pencil' />
+          <Button basic size='large' icon='star' />
+        </div>
       </Card.Content>
     </Card>
   )

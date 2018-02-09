70d91ecbe4c2c8cbd46ea2a64194090855a2f4a3
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-09 15:37:15 +0500

    Added float search btn (Cashflow Mobile)

--- app/src/pages/CashflowMobile.js
+++ app/src/pages/CashflowMobile.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
-import { compose } from 'recompose'
+import { SlideDown } from 'animate-components'
+import { compose, withState, lifecycle } from 'recompose'
 import ProjectCard from './Projects/ProjectCard'
+import FloatingButton from './Projects/FloatingButton'
+import Search from './../components/Search'
+import './CashflowMobile.css'
 
 const CashflowMobile = ({
-  Projects: {
-    projects = [],
-    loading = true,
-    isError = false,
-    isEmpty = true
-  }
+  Projects = {
+    projects: [],
+    loading: true,
+    isError: false,
+    isEmpty: true
+  },
+  history,
+  isSearchFocused = false,
+  focusSearch
 }) => {
+  const { projects = [] } = Projects
   return (
-    <div style={{padding: 20}}>
+    <div className='cashflow-mobile'>
+      {isSearchFocused &&
+        <SlideDown duration='0.3s' timingFunction='ease-out' as='div'>
+          <Search
+            focus={focusSearch}
+            onSelectProject={ticker => history.push(`/projects/${ticker.toLowerCase()}`)}
+            projects={projects} />
+        </SlideDown>}
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
+      <FloatingButton handleSearchClick={() => focusSearch(!isSearchFocused)} />
     </div>
   )
 }
@@ -39,7 +55,9 @@ const mapDataToProps = ({allProjects}) => {
   const isEmpty = !!allProjects.project
   const isError = !!allProjects.error
   const errorMessage = allProjects.error ? allProjects.error.message : ''
-  const projects = allProjects.allProjects
+  const projects = allProjects.allProjects ? allProjects.allProjects.filter((_, index) => {
+    return index < 10  // TODO: !
+  }) : []
   return {
     Projects: {
       loading,
@@ -52,6 +70,15 @@ const mapDataToProps = ({allProjects}) => {
 }
 
 const enhance = compose(
+  withState('isSearchFocused', 'focusSearch', false),
+  lifecycle({
+    componentDidUpdate (prevProps, prevState) {
+      if (this.props.isSearchFocused !== prevProps.isSearchFocused) {
+        const searchInput = document.querySelector('.search div input')
+        searchInput && searchInput.focus()
+      }
+    }
+  }),
   graphql(allProjectsGQL, {
     name: 'allProjects',
     props: mapDataToProps

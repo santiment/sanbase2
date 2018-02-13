7f2a08b4a2ef6a68c2af14fa30c067bb82e5969e
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-13 16:59:37 +0500

    Filters UI works. Filtered results in WIP

--- app/src/pages/Projects/Filters.js
+++ app/src/pages/Projects/Filters.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 import './Filters.css'
 
+export const DEFAULT_SORT_BY = 'marketcap'
+export const DEFAULT_FILTER_BY = {}
+
 const FilterDivider = ({children}) => (
   <div className='cashflow-mobile-filters__divider'>
     {children}
@@ -9,26 +12,66 @@ const FilterDivider = ({children}) => (
 )
 
 const Filters = ({
-  onFilterChanged
+  onFilterChanged,
+  filterBy = DEFAULT_FILTER_BY,
+  sortBy = DEFAULT_SORT_BY,
+  changeFilter,
+  changeSort
 }) => {
   return (
     <div className='cashflow-mobile-filters-overlay'>
       <div className='cashflow-mobile-filters'>
         <div className='cashflow-mobile-filters__header'>
-          <Button basic >Clear</Button>
+          <Button
+            basic
+            onClick={() => {
+              changeSort(DEFAULT_SORT_BY)
+              changeFilter(DEFAULT_FILTER_BY)
+            }}>
+            Clear
+          </Button>
           <h4>Filters</h4>
-          <Button onClick={() => onFilterChanged('check')} color='green'>Done</Button>
+          <Button
+            onClick={() => onFilterChanged({
+              sortBy,
+              filterBy
+            })}
+            color='green'>
+            Done
+          </Button>
         </div>
         <FilterDivider>
           Sort by
         </FilterDivider>
         <div className='cashflow-mobile-filters__item'>
-          <Button color='blue'>Marketcap</Button>
-          <Button basic >Github Activity</Button>
+          <Button
+            color={sortBy === 'marketcap' ? 'blue' : undefined}
+            onClick={() => changeSort('marketcap')}>
+            Marketcap
+          </Button>
+          <Button
+            color={sortBy === 'github_activity' ? 'blue' : undefined}
+            onClick={() => changeSort('github_activity')}>
+            Github Activity
+          </Button>
         </div>
         <FilterDivider>
           Filter by
         </FilterDivider>
+        <div className='cashflow-mobile-filters__item'>
+          <Button
+            basic
+            color={filterBy['signals'] ? 'blue' : undefined}
+            onClick={() => changeFilter({...filterBy, signals: !filterBy['signals']})}>
+            Any signal was detected
+          </Button>
+          <Button
+            basic
+            color={filterBy['spent_eth_30d'] ? 'blue' : undefined}
+            onClick={() => changeFilter({...filterBy, spent_eth_30d: !filterBy['spent_eth_30d']})}>
+            Spent eth in the last 30d
+          </Button>
+        </div>
       </div>
     </div>
   )

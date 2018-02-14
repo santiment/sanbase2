03e271ffe24041cc72dc9e75986a40e5678307c7
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-14 16:50:04 +0500

    Enhance monitoring of errors from server

--- app/src/index.js
+++ app/src/index.js
@@ -70,14 +70,25 @@ const handleLoad = () => {
 
   const linkError = onError(({graphQLErrors, networkError, operation}) => {
     if (graphQLErrors) {
-      graphQLErrors.forEach(({ message, locations, path }) => {
+      if (Array.isArray(graphQLErrors)) {
+        graphQLErrors.forEach(({ message, locations, path }) => {
+          const errorMessage = `[GraphQL error]:
+            Message: ${JSON.stringify(message)},
+            Location: ${JSON.stringify(locations)},
+            Path: ${JSON.stringify(path)}`
+          if (process.env.NODE_ENV === 'development') {
+            console.log(errorMessage)
+          }
+          Raven.captureException(errorMessage)
+        })
+      } else {
         if (process.env.NODE_ENV === 'development') {
           console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            `[GraphQL error]: ${JSON.stringify(graphQLErrors)}`
           )
         }
-        Raven.captureException(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-      })
+        Raven.captureException(`[GraphQL error]: ${JSON.stringify(graphQLErrors)}`)
+      }
     }
 
     if (networkError) {

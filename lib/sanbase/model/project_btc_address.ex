432d2a6b15e1d53206a0f1c6e355b88204190387
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-02-15 19:20:21 +0200

    Add downcase in changesets

--- lib/sanbase/model/project_btc_address.ex
+++ lib/sanbase/model/project_btc_address.ex
@@ -21,6 +21,7 @@ defmodule Sanbase.Model.ProjectBtcAddress do
     project_btc_address
     |> cast(attrs, [:address, :project_id])
     |> validate_required([:address, :project_id])
+    |> update_change(:address, &String.downcase/1)
     |> unique_constraint(:address)
   end
 end

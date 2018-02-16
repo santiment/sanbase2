b347156455ca1c3bd8fcd832de219fb88ea51c7b
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-16 13:17:05 +0500

    Added custom typeform url params
      Fix web3 deprecation warning
      Clean cashflow.js

--- app/src/components/FinancialsBlock.js
+++ app/src/components/FinancialsBlock.js
@@ -1,13 +1,53 @@
 import React from 'react'
+import moment from 'moment'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import { formatNumber } from '../utils/formatting'
-import {
-  formatBalanceWallet,
-  formatLastOutgoingWallet,
-  formatTxOutWallet
-} from './../pages/Cashflow'
 import './FinancialsBlock.css'
+
+const formatDate = date => moment(date).format('YYYY-MM-DD')
+
+const formatLastOutgoingWallet = wallets => {
+  return wallets.map((wallet, index) => {
+    const lastOutgoing = wallet.last_outgoing !== null
+      ? formatDate(wallet.last_outgoing) : 'No recent transfers'
+    return (
+      <div key={index}>
+        { lastOutgoing }
+      </div>
+    )
+  })
+}
+
+const formatTxOutWallet = wallets => {
+  return wallets.map((wallet, index) => {
+    const txOut = wallet.tx_out || '0.00'
+    return (
+      <div key={index}>
+        {formatNumber(txOut)}
+      </div>
+    )
+  })
+}
+
+const formatBalanceWallet = ({wallets, ethPrice}) => {
+  return wallets.map((wallet, index) => {
+    const balance = wallet.balance || 0
+    return (
+      <div className='wallet' key={index}>
+        <div className='usd first'>{formatNumber((balance * ethPrice), 'USD')}</div>
+        <div className='eth'>
+          <a
+            className='address'
+            href={'https://etherscan.io/address/' + wallet.address}
+            target='_blank'>Ξ{formatNumber(balance)}&nbsp;
+            <i className='fa fa-external-link' />
+          </a>
+        </div>
+      </div>
+    )
+  })
+}
 
 const propTypes = {
   projectTransparencyStatus: PropTypes.string

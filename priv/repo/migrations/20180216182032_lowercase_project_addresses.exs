a672e0cf095af3e3b734f1ee8884e0ae5cc4628f
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-02-16 20:59:44 +0200

    Remove unused aliases and vars

--- priv/repo/migrations/20180216182032_lowercase_project_addresses.exs
+++ priv/repo/migrations/20180216182032_lowercase_project_addresses.exs
@@ -1,10 +1,8 @@
 defmodule Sanbase.Repo.Migrations.LowercaseProjectAddresses do
   use Ecto.Migration
 
-  import Ecto.Query
   alias Sanbase.Model.{ProjectEthAddress, ProjectBtcAddress}
   alias Sanbase.Repo
-  alias Ecto.Multi
 
   def change do
     migrate_eth()
@@ -12,22 +10,20 @@ defmodule Sanbase.Repo.Migrations.LowercaseProjectAddresses do
   end
 
   def migrate_eth() do
-    updates =
-      Repo.all(ProjectEthAddress)
-      |> Enum.map(fn %ProjectEthAddress{address: address} = eth_addr ->
-        eth_addr
-        |> ProjectEthAddress.changeset(%{address: String.downcase(address)})
-      end)
-      |> Enum.map(&Repo.update/1)
+    Repo.all(ProjectEthAddress)
+    |> Enum.map(fn %ProjectEthAddress{address: address} = eth_addr ->
+      eth_addr
+      |> ProjectEthAddress.changeset(%{address: String.downcase(address)})
+    end)
+    |> Enum.map(&Repo.update/1)
   end
 
   def migrate_btc() do
-    updates =
-      Repo.all(ProjectBtcAddress)
-      |> Enum.map(fn %ProjectBtcAddress{address: address} = eth_addr ->
-        eth_addr
-        |> ProjectBtcAddress.changeset(%{address: String.downcase(address)})
-      end)
-      |> Enum.map(&Repo.update/1)
+    Repo.all(ProjectBtcAddress)
+    |> Enum.map(fn %ProjectBtcAddress{address: address} = eth_addr ->
+      eth_addr
+      |> ProjectBtcAddress.changeset(%{address: String.downcase(address)})
+    end)
+    |> Enum.map(&Repo.update/1)
   end
 end

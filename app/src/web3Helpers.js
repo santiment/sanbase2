25c9a9410ad9ef6e3abc8aa46cbbd540636282f9
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-16 13:17:05 +0500

    Added custom typeform url params
      Fix web3 deprecation warning
      Clean cashflow.js

--- app/src/web3Helpers.js
+++ app/src/web3Helpers.js
@@ -1,15 +1,16 @@
 let prevSelectedAccount = null
 
 export const hasMetamask = () => {
-  return window.web3 && window.web3.currentProvider.isMetaMask
+  return typeof window.web3 !== 'undefined' &&
+    window.web3.currentProvider.isMetaMask
 }
 
 export const setupWeb3 = cbk => {
-  if (!window.web3) { return }
-  const web3 = new Web3(window.web3.currentProvider) // eslint-disable-line
+  if (typeof window.web3 === 'undefined') { return }
+  const localWeb3 = new Web3(web3.currentProvider) // eslint-disable-line
   // Why the interval method here? ==> https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md
   setInterval(() => {
-    const selectedAccount = web3.eth.accounts[0]
+    const selectedAccount = localWeb3.eth.accounts[0]
     if (prevSelectedAccount !== selectedAccount) {
       prevSelectedAccount = selectedAccount
       cbk(false, selectedAccount || null)
@@ -19,9 +20,10 @@ export const setupWeb3 = cbk => {
 
 export const signMessage = account => {
   const message = `Login in Santiment with address ${account}`
-  const messageHash = window.web3.sha3('\x19Ethereum Signed Message:\n' + message.length + message)
+  const localWeb3 = new Web3(web3.currentProvider) // eslint-disable-line
+  const messageHash = localWeb3.sha3('\x19Ethereum Signed Message:\n' + message.length + message)
   return new Promise((resolve, reject) => {
-    window.web3.personal.sign(window.web3.fromUtf8(message), account, (error, signature) => {
+    localWeb3.personal.sign(localWeb3.fromUtf8(message), account, (error, signature) => {
       if (!error) {
         resolve({
           messageHash,

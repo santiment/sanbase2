548a2e7bdd58a589d45b08a9e253a840f3c6b7c8
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-02-19 13:41:27 +0500

    styles and rename files

---
+++ app/src/components/PostList.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { createSkeletonProvider } from '@trainline/react-skeletor'
+import Post from './Post.js'
+import './PostList.css'
+
+const PostList = ({
+  posts = {},
+  userId = null,
+  loading = true,
+  isError = false,
+  isEmpty = true,
+  votePost,
+  unvotePost
+}) => (
+  <div className='event-posts-list'>
+    {Object.keys(posts).map((key, index) => (
+      <Post
+        showStatus={!!userId}
+        index={index + 1}
+        key={index}
+        votePost={votePost}
+        unvotePost={unvotePost}
+        {...posts[key]} />
+    ))}
+  </div>
+)
+
+export default createSkeletonProvider(
+  {
+    posts: [{
+      title: '_____',
+      link: 'https://sanbase.net',
+      createdAt: new Date(),
+      user: {
+        username: ''
+      }
+    }, {
+      title: '_____',
+      link: 'https://sanbase.net',
+      createdAt: new Date(),
+      user: {
+        username: ''
+      }
+    }]
+  },
+  ({ posts }) => posts.length === 0,
+  () => ({
+    backgroundColor: '#bdc3c7',
+    color: '#bdc3c7'
+  })
+)(PostList)

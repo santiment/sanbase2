c49e7305289f1e1d01cee92dab379c439177d712
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-02-21 11:17:20 +0200

    price-volume diff - add ma

--- lib/sanbase_web/graphql/resolvers/tech_indicators_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/tech_indicators_resolver.ex
@@ -3,6 +3,9 @@ defmodule SanbaseWeb.Graphql.Resolvers.TechIndicatorsResolver do
 
   alias Sanbase.InternalServices.TechIndicators
 
+  @price_volume_diff_ma_approximation_window 14
+  @price_volume_diff_ma_comparison_window 7
+
   def macd(
         _root,
         %{

7d0f962a72a95dd28dec3b1f41a8284bbf7d4078
Milan Rusev <15890495+MilanRusev@users.noreply.github.com>
2018-03-03 12:44:12 +0200

    price-volume diff debug info

--- lib/sanbase/notifications/price_volume_diff.ex
+++ lib/sanbase/notifications/price_volume_diff.ex
@@ -24,10 +24,10 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
            seconds_ago(notifications_cooldown()),
            notification_type_name(currency)
          ) do
-      indicator = get_indicator(project.ticker, currency)
+      {indicator, debug_info} = get_indicator(project.ticker, currency)
 
       if check_notification(indicator) do
-        send_notification(project, currency, indicator)
+        send_notification(project, currency, indicator, debug_info)
       end
     end
   end
@@ -35,38 +35,52 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
   defp get_indicator(ticker, currency) do
     %{from_datetime: from_datetime, to_datetime: to_datetime} = get_calculation_interval()
 
-    TechIndicators.price_volume_diff_ma(
-      ticker,
-      currency,
-      from_datetime,
-      to_datetime,
-      "1d",
-      @approximation_window,
-      @comparison_window,
-      1
-    )
-    |> case do
-      {:ok,
-       [
-         %{
-           price_volume_diff: price_volume_diff,
-           price_change: price_change,
-           volume_change: volume_change
-         }
-       ]} ->
-        %{
-          price_volume_diff: nil_to_zero(price_volume_diff),
-          price_change: nil_to_zero(price_change),
-          volume_change: nil_to_zero(volume_change)
-        }
+    indicator =
+      TechIndicators.price_volume_diff_ma(
+        ticker,
+        currency,
+        from_datetime,
+        to_datetime,
+        "1d",
+        @approximation_window,
+        @comparison_window,
+        1
+      )
+      |> case do
+        {:ok,
+         [
+           %{
+             price_volume_diff: price_volume_diff,
+             price_change: price_change,
+             volume_change: volume_change
+           }
+         ]} ->
+          %{
+            price_volume_diff: nil_to_zero(price_volume_diff),
+            price_change: nil_to_zero(price_change),
+            volume_change: nil_to_zero(volume_change)
+          }
 
-      _ ->
-        %{
-          price_volume_diff: Decimal.new(0),
-          price_change: Decimal.new(0),
-          volume_change: Decimal.new(0)
-        }
-    end
+        _ ->
+          %{
+            price_volume_diff: Decimal.new(0),
+            price_change: Decimal.new(0),
+            volume_change: Decimal.new(0)
+          }
+      end
+
+    debug_info =
+      debug_info(
+        ticker,
+        currency,
+        from_datetime,
+        to_datetime,
+        "1d",
+        @approximation_window,
+        @comparison_window
+      )
+
+    {indicator, debug_info}
   end
 
   defp check_notification(%{price_volume_diff: price_volume_diff}) do
@@ -86,11 +100,15 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
     %{from_datetime: from_datetime, to_datetime: to_datetime}
   end
 
-  defp send_notification(project, currency, indicator) do
+  defp send_notification(project, currency, indicator, debug_info) do
     {:ok, %HTTPoison.Response{status_code: 204}} =
-      @http_service.post(webhook_url(), notification_payload(project, currency, indicator), [
-        {"Content-Type", "application/json"}
-      ])
+      @http_service.post(
+        webhook_url(),
+        notification_payload(project, currency, indicator, debug_info),
+        [
+          {"Content-Type", "application/json"}
+        ]
+      )
 
     Utils.insert_notification(project, notification_type_name(currency))
   end
@@ -98,13 +116,16 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
   defp notification_payload(
          %Project{name: name, ticker: ticker, coinmarketcap_id: coinmarketcap_id},
          currency,
-         %{price_change: price_change, volume_change: volume_change}
+         %{price_change: price_change, volume_change: volume_change},
+         debug_info
        ) do
     Poison.encode!(%{
       content:
         "#{name}: #{ticker}/#{String.upcase(currency)} #{notification_emoji(price_change)} Price #{
           notification_emoji(volume_change)
-        } Volume opposite trends. https://coinmarketcap.com/currencies/#{coinmarketcap_id}",
+        } Volume opposite trends. https://coinmarketcap.com/currencies/#{coinmarketcap_id} #{
+          debug_info
+        }",
       username: "Price-Volume Difference"
     })
   end
@@ -115,6 +136,34 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
       :lt -> ":small_red_triangle_down:"
       :gt -> ":small_red_triangle:"
       :eq -> " "
+    end
+  end
+
+  def debug_info(
+        ticker,
+        currency,
+        from_datetime,
+        to_datetime,
+        aggregate_interval,
+        approximation_window,
+        comparison_window
+      ) do
+    case debug_url() do
+      nil ->
+        nil
+
+      debug_url ->
+        from_unix = DateTime.to_unix(from_datetime)
+        to_unix = DateTime.to_unix(to_datetime)
+
+        debug_url =
+          "#{debug_url}?ticker=#{ticker}&currency=#{currency}&from_timestamp=#{from_unix}&to_timestamp=#{
+            to_unix
+          }&aggregate_interval=#{aggregate_interval}&approximation_window=#{approximation_window}&comparison_window=#{
+            comparison_window
+          }"
+
+        "[DEBUG INFO: #{debug_url}]"
     end
   end
 
@@ -136,6 +185,10 @@ defmodule Sanbase.Notifications.PriceVolumeDiff do
       |> Integer.parse()
 
     res
+  end
+
+  defp debug_url() do
+    Config.get(:debug_url)
   end
 
   defp notifications_enabled?() do

d5eb7b377c885bb26d2db926bd3a390759afd9a6
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-03-07 17:38:16 +0500

    Added image ulpad link

---
+++ app/src/components/ImageUpload.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+export default graphql(gql`
+  mutation($images: [Upload!]!) {
+    uploadImage(images: $images) {
+      contentHash
+      fileName
+      imageUrl
+      hashAlgorithm
+    }
+  }
+`)(({ mutate }) => (
+  <input
+    type='file'
+    multiple
+    required
+    onChange={({ target: { validity, files } }) => {
+      validity.valid && mutate({ variables: { images: files } }).then((...rest) => {
+        console.log(rest)
+      })
+    }}
+  />
+))

9c176c8d9aaacb7ce686e08fe6501f29e5a78125
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-03-07 17:38:16 +0500

    Added image ulpad link

--- app/src/index.js
+++ app/src/index.js
@@ -8,11 +8,11 @@ import axios from 'axios'
 import { multiClientMiddleware } from 'redux-axios-middleware'
 import Raven from 'raven-js'
 import createRavenMiddleware from 'raven-for-redux'
-import ApolloClient from 'apollo-client'
+import ApolloClient, { printAST } from 'apollo-client'
 import gql from 'graphql-tag'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
-import { from } from 'apollo-link'
+import { from, ApolloLink, Observable } from 'apollo-link'
 import { onError } from 'apollo-link-error'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
@@ -22,6 +22,7 @@ import { loadState, saveState } from './utils/localStorage'
 import setAuthorizationToken from './utils/setAuthorizationToken'
 import { hasMetamask } from './web3Helpers'
 import * as serviceWorker from './serviceWorker'
+
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
@@ -65,6 +66,7 @@ const handleLoad = () => {
   }
 
   const httpLink = createHttpLink({ uri: `${origin}/graphql` })
+
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
     const token = loadState() ? loadState().token : undefined
@@ -75,6 +77,53 @@ const handleLoad = () => {
         authorization: token ? `Bearer ${token}` : null
       }
     }
+  })
+
+  const isObject = value => value !== null && typeof value === 'object'
+
+  const uploadLink = new ApolloLink((operation, forward) => {
+    if (typeof FormData !== 'undefined' && isObject(operation.variables)) {
+      const files = operation.variables.images
+      if (files && files.length > 0) {
+        const { headers } = operation.getContext()
+        const formData = new FormData() // eslint-disable-line
+
+        const filesData = Object.keys(files).filter(key => {
+          return files[key].name
+        })
+        formData.append('query', printAST(operation.query))
+        let variables = {'images': []}
+        filesData.forEach(key => {
+          variables['images'].push(files[key].name)
+          formData.append(files[key].name, files[key])
+        })
+        formData.append('variables', JSON.stringify(variables))
+
+        return new Observable(observer => {
+          fetch(`${origin}/graphql`, { // eslint-disable-line
+            method: 'POST',
+            headers: {
+              ...headers
+            },
+            body: formData
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw Error(response.statusText)
+            }
+            return response.json()
+          })
+          .then(success => {
+            observer.next(success)
+            observer.complete()
+          }).catch(error => {
+            observer.next(error)
+            observer.error(error)
+          })
+        })
+      }
+    }
+    return forward(operation)
   })
 
   const linkError = onError(({graphQLErrors, networkError, operation}) => {
@@ -109,7 +158,7 @@ const handleLoad = () => {
   })
 
   const client = new ApolloClient({
-    link: from([authLink, linkError, httpLink]),
+    link: from([authLink, linkError, uploadLink, httpLink]),
     cache: new InMemoryCache()
   })
 

d077137c5b7f2f2bea0ae9586de0fcc45ac0c7e2
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-19 11:32:22 +0700

    Rework CMC fetching - use both historical data and graphs2's all top 1000 projects data from one request

--- lib/sanbase/external_services/coinmarketcap/ticker.ex
+++ lib/sanbase/external_services/coinmarketcap/ticker.ex
@@ -17,9 +17,10 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
     :percent_change_7d
   ]
 
-  alias Sanbase.ExternalServices.RateLimiting
-
   use Tesla
+
+  alias Sanbase.ExternalServices.RateLimiting
+  alias Sanbase.ExternalServices.Coinmarketcap.PricePoint
 
   plug(RateLimiting.Middleware, name: :api_coinmarketcap_rate_limiter)
   plug(Tesla.Middleware.BaseUrl, "https://api.coinmarketcap.com/v1/ticker")
@@ -43,8 +44,48 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
     |> Enum.map(&make_timestamp_integer/1)
   end
 
+  def convert_for_importing(%Ticker{
+        symbol: ticker,
+        last_updated: last_updated,
+        price_btc: price_btc,
+        price_usd: price_usd,
+        "24h_volume_usd": volume_usd,
+        market_cap_usd: marketcap_usd
+      }) do
+    price_point = %PricePoint{
+      marketcap: marketcap_usd,
+      volume_usd: volume_usd |> to_integer(),
+      price_btc: price_btc |> to_float(),
+      price_usd: price_usd |> to_float(),
+      datetime: DateTime.from_unix!(last_updated)
+    }
+
+    [
+      PricePoint.convert_to_measurement(price_point, "USD", ticker),
+      PricePoint.convert_to_measurement(price_point, "BTC", ticker)
+    ]
+  end
+
+  # Helper functions
+
   defp make_timestamp_integer(ticker) do
     {ts, ""} = Integer.parse(ticker.last_updated)
     %{ticker | last_updated: ts}
+  end
+
+  defp to_float(nil), do: nil
+  defp to_float(fl) when is_float(fl), do: fl
+
+  defp to_float(str) when is_binary(str) do
+    {num, _} = str |> Float.parse()
+    num
+  end
+
+  defp to_integer(nil), do: nil
+  defp to_integer(int) when is_integer(int), do: int
+
+  defp to_integer(str) when is_binary(str) do
+    {num, _} = str |> Integer.parse()
+    num
   end
 end

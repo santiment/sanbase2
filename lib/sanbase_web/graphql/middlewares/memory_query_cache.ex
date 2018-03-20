89bea4536adaa3fc3927042dc5408f9396183194
Valentin Mihov <valentin.mihov@gmail.com>
2018-03-20 10:51:08 +0700

    Reduce the resolution of the datetime arguments
    This prevents the cache from expiring when relative to DateTime.now
    times are used in the query

--- lib/sanbase_web/graphql/middlewares/memory_query_cache.ex
+++ lib/sanbase_web/graphql/middlewares/memory_query_cache.ex
@@ -4,10 +4,15 @@ defmodule SanbaseWeb.Graphql.Middlewares.MemoryQueryCache do
 
   alias Absinthe.Resolution
   alias Absinthe.Blueprint.Document.Field
+  alias Absinthe.Blueprint.Input.{Value, Argument}
+
+  @default_ttl 60 * 1000
 
   def call(resolution, config) do
+    ttl = Keyword.get(config, :ttl, @default_ttl)
+
     query =
-      generate_query(resolution.definition)
+      generate_query(resolution.definition, ttl)
       |> Poison.encode!()
 
     cache_key =
@@ -25,44 +30,55 @@ defmodule SanbaseWeb.Graphql.Middlewares.MemoryQueryCache do
     end
   end
 
-  @spec generate_query(Field.t()) :: Map.t()
-  defp generate_query(%Field{
-         name: name,
-         arguments: args,
-         selections: selections
-       }) do
+  @spec generate_query(Field.t(), :number) :: Map.t()
+  defp generate_query(
+         %Field{
+           name: name,
+           arguments: args,
+           selections: selections
+         },
+         ttl
+       ) do
     %{name: name}
-    |> add_args(args)
-    |> add_children(selections)
+    |> add_args(args, ttl)
+    |> add_children(selections, ttl)
   end
 
   defp generate_query(_), do: %{}
 
-  @spec add_args(Map.t(), List.t()) :: Map.t()
-  defp add_args(result, []), do: result
+  @spec add_args(Map.t(), List.t(), :number) :: Map.t()
+  defp add_args(result, [], _ttl), do: result
 
-  defp add_args(result, args) do
+  defp add_args(result, args, ttl) do
     args =
       args
-      |> Enum.map(fn %Absinthe.Blueprint.Input.Argument{
+      |> Enum.map(fn %Argument{
                        name: name,
-                       input_value: %Absinthe.Blueprint.Input.Value{data: value}
+                       input_value: %Value{data: value}
                      } ->
-        {name, value}
+        {name, convert_value(value, ttl)}
       end)
       |> Map.new()
 
     Map.put(result, :args, args)
   end
 
-  @spec add_children(Map.t(), List.t()) :: Map.t()
-  defp add_children(result, []), do: result
+  @spec add_children(Map.t(), List.t(), :number) :: Map.t()
+  defp add_children(result, [], _ttl), do: result
 
-  defp add_children(result, selections) do
+  defp add_children(result, selections, ttl) do
     children =
       selections
-      |> Enum.map(&generate_query/1)
+      |> Enum.map(&generate_query(&1, ttl))
 
     Map.put(result, :children, children)
   end
+
+  # Reduce the resolution of the datetime arguments, so that they don't change
+  # too often and invalidate the cache
+  defp convert_value(%DateTime{} = datetime, ttl) do
+    div(DateTime.to_unix(datetime, :millisecond), ttl)
+  end
+
+  defp convert_value(value, _ttl), do: value
 end

53199de17235644d9d4cdc02c67312779b806045
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-03-20 11:26:54 +0200

    remove testing timeout

--- lib/sanbase/external_services/etherscan/requests/tx.ex
+++ lib/sanbase/external_services/etherscan/requests/tx.ex
@@ -25,7 +25,7 @@ defmodule Sanbase.ExternalServices.Etherscan.Requests.Tx do
   """
   @spec get(String.t(), Integer.t(), Integer.t()) :: {:ok, list()} | {:error, String.t()}
   def get(address, startblock, endblock) do
-    Requests.get("/", query: get_query(address, startblock, endblock), opts: [recv_timeout: 1])
+    Requests.get("/", query: get_query(address, startblock, endblock))
     |> case do
       %{status: 200, body: body} ->
         {:ok, parse_tx_json(body)}

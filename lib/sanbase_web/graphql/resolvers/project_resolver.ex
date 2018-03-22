8da6ec82623c16ebabd8a56ff265ae4e631fa946
Valentin Mihov <valentin.mihov@gmail.com>
2018-03-22 23:33:39 +0700

    Set default expiration time for the cache
    Seems like Cachex.fetch does tnoe recognizes ttl as a valid option.

--- lib/sanbase_web/graphql/resolvers/project_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/project_resolver.ex
@@ -534,9 +534,9 @@ defmodule SanbaseWeb.Graphql.Resolvers.ProjectResolver do
     loader
     |> Dataloader.load(SanbaseRepo, :latest_eth_wallet_data, eth_address)
     |> on_load(fn loader ->
-      with latest_eth_wallet_data <-
+      with latest_eth_wallet_data when not is_nil(latest_eth_wallet_data) <-
              Dataloader.get(loader, SanbaseRepo, :latest_eth_wallet_data, eth_address),
-           balance when not is_nil(latest_eth_wallet_data) <- latest_eth_wallet_data.balance do
+           balance <- latest_eth_wallet_data.balance do
         {:ok, balance}
       else
         _ -> {:ok, nil}

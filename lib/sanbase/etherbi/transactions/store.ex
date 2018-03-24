0ad254bd4999135d141a4139c510418b06d31136
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-24 12:44:13 +0700

    Rewrite exchange fund flow to match the new database layout

--- lib/sanbase/etherbi/transactions/store.ex
+++ lib/sanbase/etherbi/transactions/store.ex
@@ -12,7 +12,7 @@ defmodule Sanbase.Etherbi.Transactions.Store do
     Updates the point for `address` in the special measurement used for saving
     the last queried timestamp for a given address
   """
-  @spec import_last_address_time(String.t(), String.t(), %DateTime{}) :: :ok | no_return()
+  @spec import_last_address_time(String.t(), atom, %DateTime{}) :: :ok | no_return()
   def import_last_address_time(_, _, nil), do: :ok
 
   def import_last_address_time(address, trx_type, last_datetime) do
@@ -28,37 +28,34 @@ defmodule Sanbase.Etherbi.Transactions.Store do
     |> Store.import()
   end
 
-  @doc ~s"""
-    Returns the last datetime that was quried for that particular address.
-    Returns `{:ok, datetime}` on successs, `{:error, reason}` otherwise
+  @doc ~S"""
+    Get a list of fund flows in and out of exchange. The query returns a tuple `{:ok, list}` or
+    `{:error, reason}`. `list` is list of`{:datetime, amount}`.
+    Positive `amount` indicates that more funds have flown into
+    than out of exchanges.
+    Negative `amount` indicates that more funds have flown out
+    than in of exchanges.
   """
-  @spec last_address_datetime(String.t(), String.t()) ::
-          {:ok, %DateTime{}} | {:ok, nil} | {:error, String.t()}
-  def last_address_datetime(address, trx_type) do
-    select_last_address_datetime(address, trx_type)
+  @spec transactions_in_out_difference(String.t(), %DateTime{}, %DateTime{}, String.t()) ::
+          {:ok, list()} | {:error, String.t()}
+  def transactions_in_out_difference(measurement, from, to, interval) do
+    transactions_in_out_difference_query(measurement, from, to, interval)
     |> Store.query()
-    |> parse_last_address_datetime()
+    |> parse_in_out_diff_time_series()
   end
 
   @doc ~S"""
-    Get all in and/or out transactions that happened with a given address.
-    Returns a tuple `{:ok, result}` on success, `{:error, error}` otherwise
+    Get a list of fund flows in and out of exchange. The query returns a tuple `list` or
+    raises an error. `list` is list of`{:datetime, amount}`.
+    Positive `amount` indicates that more funds have flown into
+    than out of exchanges.
+    Negative `amount` indicates that more funds have flown out
+    than in of exchanges.
   """
-  @spec transactions(binary(), %DateTime{}, %DateTime{}, binary()) ::
-          {:ok, list()} | {:error, binary()}
-  def transactions(measurement, from, to, transaction_type) do
-    transactions_from_to_query(measurement, from, to, transaction_type)
-    |> Store.query()
-    |> parse_transactions_time_series()
-  end
-
-  @doc ~S"""
-    Get all in and/or out transactions that happened with a given address. Retunrs `result`
-    on success, raises and error otherwise
-  """
-  @spec transactions!(binary(), %DateTime{}, %DateTime{}, binary()) :: list() | no_return()
-  def transactions!(measurement, from, to, transaction_type) do
-    case transactions(measurement, from, to, transaction_type) do
+  @spec transactions_in_out_difference!(String.t(), %DateTime{}, %DateTime{}, String.t()) ::
+          list() | no_return()
+  def transactions_in_out_difference!(measurement, from, to, interval) do
+    case transactions_in_out_difference(measurement, from, to, interval) do
       {:ok, result} -> result
       {:error, error} -> raise(error)
     end
@@ -66,80 +63,40 @@ defmodule Sanbase.Etherbi.Transactions.Store do
 
   # Private functions
 
-  defp select_last_address_datetime(address, trx_type) do
-    address_trx_type = "#{address}_#{trx_type}"
-
-    ~s/SELECT LAST(last_datetime)
-    FROM "#{@last_address_measurement}"
-    WHERE address_trx_type='#{address_trx_type}'/
-  end
-
-  defp transactions_from_to_query(measurement, from, to, "all") do
-    ~s/SELECT volume, address
+  defp transactions_in_out_difference_query(measurement, from, to, interval) do
+    ~s/SELECT (SUM(incoming_exchange_funds) - SUM(outgoing_exchange_funds)) as funds_flow
     FROM "#{measurement}"
     WHERE time >= #{DateTime.to_unix(from, :nanoseconds)}
-    AND time <= #{DateTime.to_unix(to, :nanoseconds)}/
-  end
-
-  defp transactions_from_to_query(measurement, from, to, transaction_type) do
-    ~s/SELECT volume, address
-    FROM "#{measurement}"
-    WHERE transaction_type = '#{transaction_type}'
-    AND time >= #{DateTime.to_unix(from, :nanoseconds)}
-    AND time <= #{DateTime.to_unix(to, :nanoseconds)}/
-  end
-
-  defp parse_transactions_time_series(%{error: error}) do
-    {:error, error}
+    AND time <= #{DateTime.to_unix(to, :nanoseconds)}
+    GROUP BY time(#{interval}) fill(0)/
   end
 
   defp parse_transactions_time_series(%{results: [%{error: error}]}) do
     {:error, error}
   end
 
-  defp parse_transactions_time_series(%{
+  defp parse_in_out_diff_time_series(%{
          results: [
            %{
              series: [
                %{
-                 values: transactions
+                 values: funds_flow_list
                }
              ]
            }
          ]
        }) do
     result =
-      transactions
-      |> Enum.map(fn [iso8601_datetime, volume, address] ->
+      funds_flow_list
+      |> Enum.map(fn [iso8601_datetime, funds_flow] ->
         {:ok, datetime, _} = DateTime.from_iso8601(iso8601_datetime)
-        {datetime, volume, address}
+        {datetime, funds_flow}
       end)
 
     {:ok, result}
   end
 
-  defp parse_transactions_time_series(_) do
+  defp parse_in_out_diff_time_series(_) do
     {:ok, []}
   end
-
-  defp parse_last_address_datetime(%{results: [%{error: error}]}) do
-    {:error, error}
-  end
-
-  defp parse_last_address_datetime(%{
-         results: [
-           %{
-             series: [
-               %{
-                 values: last_address
-               }
-             ]
-           }
-         ]
-       }) do
-    [[_datetime, iso8601_last_datetime]] = last_address
-    DateTime.from_unix(iso8601_last_datetime)
-  end
-
-  defp parse_last_address_datetime(_), do: {:ok, nil}
 end

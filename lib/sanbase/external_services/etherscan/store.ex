1669290132b1486036d79224e7e48f1fef2cdf5c
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-03-26 20:33:44 +0300

    Filter out transactions between own addresses from aggregation queries

--- lib/sanbase/external_services/etherscan/store.ex
+++ lib/sanbase/external_services/etherscan/store.ex
@@ -7,6 +7,7 @@ defmodule Sanbase.ExternalServices.Etherscan.Store do
 
   alias Sanbase.Influxdb.Measurement
   alias Sanbase.ExternalServices.Etherscan.Store
+  alias Sanbase.Model.Project
 
   @last_block_measurement "sanbase-internal-last-blocks-measurement"
 
@@ -286,8 +287,8 @@ defmodule Sanbase.ExternalServices.Etherscan.Store do
   defp sum_over_time_from_to_query(measurement, from, to, resolution, transaction_type) do
     ~s/SELECT time, SUM(trx_value)
     FROM "#{measurement}"
-    WHERE transaction_type = '#{transaction_type}'
-    AND trx_hash != ''
+    WHERE trx_hash != ''
+    AND #{construct_internal_eth_addresses_filter(measurement, transaction_type)}
     AND time >= #{DateTime.to_unix(from, :nanoseconds)}
     AND time <= #{DateTime.to_unix(to, :nanoseconds)}
     GROUP BY TIME(#{resolution}) fill(0)/
@@ -297,6 +298,7 @@ defmodule Sanbase.ExternalServices.Etherscan.Store do
     ~s/SELECT trx_hash, TOP(trx_value, #{limit}) as trx_value, transaction_type, from_addr, to_addr
     FROM "#{measurement}"
     WHERE trx_hash != ''
+    AND #{construct_internal_eth_addresses_filter(measurement, "all")}
     AND time >= #{DateTime.to_unix(from, :nanoseconds)}
     AND time <= #{DateTime.to_unix(to, :nanoseconds)}/
   end
@@ -304,8 +306,8 @@ defmodule Sanbase.ExternalServices.Etherscan.Store do
   defp select_top_transactions(measurement, from, to, transaction_type, limit) do
     ~s/SELECT trx_hash, TOP(trx_value, #{limit}) as trx_value, transaction_type, from_addr, to_addr
     FROM "#{measurement}"
-    WHERE transaction_type='#{transaction_type}'
-    AND trx_hash != ''
+    WHERE trx_hash != ''
+    AND #{construct_internal_eth_addresses_filter(measurement, transaction_type)}
     AND time >= #{DateTime.to_unix(from, :nanoseconds)}
     AND time <= #{DateTime.to_unix(to, :nanoseconds)}/
   end
@@ -388,4 +390,39 @@ defmodule Sanbase.ExternalServices.Etherscan.Store do
   end
 
   defp parse_transactions_time_series(_), do: {:ok, []}
+
+  defp construct_internal_eth_addresses_filter(ticker, transaction_type) do
+    ticker
+    |> Project.project_eth_addresses_by_ticker()
+    |> Map.get(:eth_addresses)
+    |> Enum.map(fn eth_address -> eth_address.address end)
+    |> filter_eth_addresses(transaction_type)
+  end
+
+  defp filter_eth_addresses([], _transaction_type), do: ""
+
+  defp filter_eth_addresses(addresses, transaction_type) when transaction_type in ["in", "IN"] do
+    "transaction_type = '#{transaction_type}' AND " <>
+      filter_eth_addresses_by_field(addresses, "from_addr")
+  end
+
+  defp filter_eth_addresses(addresses, transaction_type)
+       when transaction_type in ["out", "OUT"] do
+    "transaction_type = '#{transaction_type}' AND " <>
+      filter_eth_addresses_by_field(addresses, "to_addr")
+  end
+
+  defp filter_eth_addresses(addresses, transaction_type)
+       when transaction_type in ["all", "ALL"] do
+    ~s/(#{filter_eth_addresses(addresses, "in")})/ <>
+      " OR " <> ~s/(#{filter_eth_addresses(addresses, "out")})/
+  end
+
+  def filter_eth_addresses_by_field(addresses, field) do
+    addresses
+    |> Enum.map(&filter_address(&1, field))
+    |> Enum.join(" AND ")
+  end
+
+  defp filter_address(address, field), do: "#{field} != '#{address}'"
 end

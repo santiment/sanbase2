5beae281840975e2506db6ddb7dd464c4fa93629
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-26 11:31:25 +0700

    Set the tesla url before the follow redirect middleware, otherwise the follow redirect does not know the authority it merges onto

--- lib/sanbase/external_services/coinmarketcap/scraper.ex
+++ lib/sanbase/external_services/coinmarketcap/scraper.ex
@@ -9,8 +9,8 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Scraper do
 
   plug(RateLimiting.Middleware, name: :http_coinmarketcap_rate_limiter)
   plug(ErrorCatcher.Middleware)
-  plug(Tesla.Middleware.FollowRedirects, max_redirects: 10)
   plug(Tesla.Middleware.BaseUrl, "https://coinmarketcap.com/currencies")
+  plug(Tesla.Middleware.FollowRedirects, max_redirects: 10)
   plug(Tesla.Middleware.Compression)
   plug(Tesla.Middleware.Logger)
 

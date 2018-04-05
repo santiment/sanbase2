facdc8f044cd96526a4c2931ec6bc20527b50308
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-04-05 15:45:40 +0300

    API for fetching insights for given user

--- lib/sanbase_web/graphql/resolvers/post_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/post_resolver.ex
@@ -1,6 +1,8 @@
 defmodule SanbaseWeb.Graphql.Resolvers.PostResolver do
   require Logger
 
+  import Ecto.Query
+
   alias Sanbase.Auth.User
   alias Sanbase.Voting.{Poll, Post, Vote}
   alias Sanbase.Repo
@@ -10,6 +12,17 @@ defmodule SanbaseWeb.Graphql.Resolvers.PostResolver do
   def all_posts(_root, _args, _context) do
     posts = Post |> Repo.all()
 
+    {:ok, posts}
+  end
+
+  def all_posts_for_user(_root, %{user_id: user_id}, _context) do
+    query =
+      from(
+        p in Post,
+        where: p.user_id == ^user_id
+      )
+
+    posts = query |> Repo.all()
     {:ok, posts}
   end
 end

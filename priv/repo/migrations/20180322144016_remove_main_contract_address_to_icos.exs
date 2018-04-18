e2e17d7429b5f02636b58b766189efbc6ff3a037
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-04-18 17:50:54 +0300

    Migrate addresses from ico to project

--- priv/repo/migrations/20180322144016_remove_main_contract_address_to_icos.exs
+++
@@ -1,9 +0,0 @@
-defmodule Sanbase.Repo.Migrations.RemoveMainContractAddressToIcos do
-  use Ecto.Migration
-
-  def change do
-    alter table(:icos) do
-      remove(:main_contract_address)
-    end
-  end
-end

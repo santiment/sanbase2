0475bb38a50aa961fa68eafaa38d1d704525b4d2
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-04-20 17:51:38 +0300

    Add more fields for annon user allInsights request

--- lib/sanbase_web/graphql/schema/account_types.ex
+++ lib/sanbase_web/graphql/schema/account_types.ex
@@ -23,6 +23,11 @@ defmodule SanbaseWeb.Graphql.AccountTypes do
     end
   end
 
+  object :user_for_post do
+    field(:id, non_null(:id))
+    field(:username, :string)
+  end
+
   object :login do
     field(:token, non_null(:string))
     field(:user, non_null(:user))

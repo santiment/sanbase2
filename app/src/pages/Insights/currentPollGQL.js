e6a912d6b097258ebdddb57d072593c30b74adbe
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-04-21 14:43:42 +0500

    Fetched insights, when user is anonymous.

--- app/src/pages/Insights/currentPollGQL.js
+++ app/src/pages/Insights/currentPollGQL.js
@@ -1,19 +1,37 @@
 import gql from 'graphql-tag'
 
-export const currentPollGQL = gql`{
-  allInsights {
-    id
-    title
-    state
-    moderationComment
-    createdAt
-    votedAt
-    totalSanVotes
-    user {
+export const allInsightsPublicGQL = gql`
+  query allInsights {
+    allInsights {
       id
-      username
+      title
+      createdAt
+      state
+      totalSanVotes
+      user {
+        id
+        username
+      }
     }
   }
-}`
+`
 
-export default currentPollGQL
+export const allInsightsGQL = gql`
+  query allInsights {
+    allInsights {
+      id
+      title
+      createdAt
+      state
+      totalSanVotes
+      moderationComment
+      votedAt
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+
+export default allInsightsPublicGQL

273b8a23e226a416db7107ef5c3e93247e2317c3
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-04-23 11:15:12 +0300

    Add comments to gql cache. Rework all caching to work with :ok tuples. Run mix formatter

--- lib/sanbase_web/graphql/helpers/cache.ex
+++ lib/sanbase_web/graphql/helpers/cache.ex
@@ -6,6 +6,17 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
 
   alias __MODULE__, as: CacheMod
 
+  @doc ~s"""
+    Macro that's used instead of Absinthe's `resolve`. The value of the resolver is
+    1. get from a cache if it exists there
+    2. calculated and stored in the cache if it does not exist
+
+    The value of `captured_mfa_ast` when executed MUST be a concrete value but not
+    a new middleware.
+
+    The function MUST be a captured named function because its name is extracted
+    and used in the cache key.
+  """
   defmacro cache_resolve(captured_mfa_ast) do
     quote do
       middleware(
@@ -15,6 +26,38 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
     end
   end
 
+  @doc ~s"""
+    Macro that's used instead of Absinthe's `resolve`. The value of the resolver is
+    1. Get from a cache if it exists there
+    2. Calculated and stored in the cache if it does not exist
+
+    The value of `captured_mfa_ast` when executed MUST be a concrete value but not
+    a new middleware.
+
+    The function's name is not used but instead `fun_name` is used in the cache key
+  """
+  defmacro cache_resolve(captured_mfa_ast, fun_name) do
+    quote do
+      middleware(
+        Absinthe.Resolution,
+        CacheMod.from(unquote(captured_mfa_ast), unquote(fun_name))
+      )
+    end
+  end
+
+  @doc ~s"""
+    Macro that's used instead of Absinthe's `resolve`. The value of the resolver is
+    1. Get from a cache if it exists there
+    2. Calculated at a later point in time (when the middleware is executed)
+    and stored in the cache if it does not exist
+
+    The value of `captured_mfa_ast` MUST be a dataloader middleware tuple with three elements
+    `{:middleware, Absinthe.Middleware.Dataloder, callback}` where `callback` is a
+    function with arity 1 that accepts `loader` as a single parameter.
+
+    The function MUST be a captured named function because its name is extracted
+    and used in the cache key.
+  """
   defmacro cache_resolve_dataloader(captured_mfa_ast) do
     quote do
       middleware(
@@ -24,33 +67,37 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
     end
   end
 
-  def from(captured_mfa) when is_function(captured_mfa) do
-    fun_name = captured_mfa |> captured_mfa_name()
+  @doc ~s"""
+    Exposed as
+    sometimes it can be useful to use it outside the macros.
 
-    captured_mfa
-    |> resolver(fun_name)
-  end
+    Gets a function, name and arguments and returns a new function that:
+    1. On execution checks if the value is present in the cache and returns it
+    2. If it's not in the cache it gets executed and the value is stored in the cache.
 
-  def dataloader_from(captured_mfa) when is_function(captured_mfa) do
-    fun_name = captured_mfa |> captured_mfa_name()
-
-    captured_mfa
-    |> dataloader_resolver(fun_name)
-  end
-
-  def from(fun, fun_name) when is_function(fun) do
-    fun
-    |> resolver(fun_name)
-  end
-
+    NOTE: `cached_func` is a function with arity 0. That means if you want to use it
+    in your code and you want some arguments you should use it like this:
+      > Cache.func(
+      >   fn ->
+      >     fetch_last_price_record(pair)
+      >   end,
+      >   :fetch_price_last_record, %{pair: pair}
+      > ).()
+  """
   def func(cached_func, name, args \\ %{}) do
     fn ->
-      ConCache.get_or_store(@cache_name, cache_key(name, args), fn ->
-        cached_func.()
-      end)
+      {:ok, value} =
+        ConCache.get_or_store(@cache_name, cache_key(name, args), fn ->
+          {:ok, cached_func.()}
+        end)
+
+      value
     end
   end
 
+  @doc ~s"""
+    Clears the whole cache. Slow.
+  """
   def clear_all() do
     @cache_name
     |> ConCache.ets()
@@ -58,9 +105,31 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
     |> Enum.each(fn {key, _} -> ConCache.delete(@cache_name, key) end)
   end
 
+  @doc ~s"""
+    The size of the cache in megabytes
+  """
   def size(:megabytes) do
     bytes_size = :ets.info(ConCache.ets(@cache_name), :memory) * :erlang.system_info(:wordsize)
     (bytes_size / (1024 * 1024)) |> Float.round(2)
+  end
+
+  # Public so it can be used by the resolve macros. You should not use it.
+  def from(captured_mfa) when is_function(captured_mfa) do
+    fun_name = captured_mfa |> captured_mfa_name()
+
+    resolver(captured_mfa, fun_name)
+  end
+
+  # Public so it can be used by the resolve macros. You should not use it.
+  def from(fun, fun_name) when is_function(fun) do
+    resolver(fun, fun_name)
+  end
+
+  # Public so it can be used by the resolve macros. You should not use it.
+  def dataloader_from(captured_mfa) when is_function(captured_mfa) do
+    fun_name = captured_mfa |> captured_mfa_name()
+
+    dataloader_resolver(captured_mfa, fun_name)
   end
 
   # Private functions
@@ -86,6 +155,22 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
     end
   end
 
+  # The actual work for the dataloader cache resolver is done here.
+  #
+  # The most important part is how the cache is actually populated. The resolver
+  # returns a `{:middleware, Absinthe.Middleware.Dataloader, callback}` tuple.
+  # This is NOT the final result so it cannot be used. Instead, `callback` is replaced
+  # by a function that does the caching.
+  # That works because `callback` is always a function with one argument `loader`, which
+  # when executed (after `Dataloader.run` is called from the middleware) returns the
+  # actual result.
+  #
+  # The modified callback internally calls the original callback, passing it the argument,
+  # stores the value in the cache and returns the value. From the outside it works the same
+  # as the original callback
+  #
+  # Because Elixir's lambdas are correctly implemented, we can fetch what's needed
+  # from the context to use it for `cache_key`
   defp dataloader_resolver(resolver_fn, name) do
     # Works only for top-level resolvers and fields with root object Project
     fn
@@ -98,14 +183,15 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
 
             caching_callback = fn loader ->
               value = callback.(loader)
-              ConCache.put(@cache_name, cache_key, value)
+              ConCache.put(@cache_name, cache_key, {:ok, value})
 
               value
             end
 
             {:middleware, midl, {loader, caching_callback}}
 
-          value ->
+          # Wrap in a tuple to distinguish value = nil from not having a record
+          {:ok, value} ->
             value
         end
 
@@ -118,29 +204,33 @@ defmodule SanbaseWeb.Graphql.Helpers.Cache do
 
             caching_callback = fn loader_arg ->
               value = callback.(loader_arg)
-              ConCache.put(@cache_name, cache_key, value)
+              ConCache.put(@cache_name, cache_key, {:ok, value})
 
               value
             end
 
             {:middleware, midl, {loader, caching_callback}}
 
-          value ->
+          # Wrap in a tuple to distinguish value = nil from not having a record
+          {:ok, value} ->
             value
         end
     end
   end
 
+  # Calculate the cache key from a given name and arguments.
   defp cache_key(name, args) do
     args_hash =
       args
-      |> convert_values
+      |> convert_values()
       |> Poison.encode!()
-      |> sha256
+      |> sha256()
 
     {name, args_hash}
   end
 
+  # Convert the values for using in the cache. A special treatement is done for
+  # `%DateTime{}` so all datetimes in a @ttl sized window are treated the same
   defp convert_values(args) do
     args
     |> Enum.map(fn

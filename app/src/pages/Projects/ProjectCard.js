f92edad650a61f7e1d7c3b96004dc9eae43bd719
Lyudmil Lesinski <lyudmilles@gmail.com>
2018-04-27 12:30:50 +0300

    Refactor ProjectCard component
    Changes the ProjectCard component to show a small card in mobile view
    initially and after the user clicks on the card it expands to show more
    data.
    
    Updates the ProjectsMobile component so the size of each card is
    calculated automatically. This allows for having cards with different
    sizes and also when the size of the card changes (when the user clicks
    on the card) everything updates properly.

--- app/src/pages/Projects/ProjectCard.js
+++ app/src/pages/Projects/ProjectCard.js
@@ -44,9 +44,13 @@ const StatisticElement = ({name, value, up = undefined, disabled = false}) => (
 )
 
 const ProjectCard = ({
+  type = 'erc20',
+  history,
+  toggleExpandCard,
+  isExpanded,
   name,
   rank,
-  ticker,
+  coinmarketcapId,
   description,
   marketSegment,
   priceUsd,
@@ -56,133 +60,146 @@ const ProjectCard = ({
   averageDevActivity,
   twitterData,
   ethSpent,
-  btcBalance = 0,
   ethBalance = 0,
   marketcapUsd,
-  teamTokenWallet,
-  signals,
-  onClick,
-  type = 'erc20'
+  signals
 }) => {
   const warning = signals && signals.length > 0
+  const handleMoreClick = () => history.push(`/projects/${coinmarketcapId}`)
+
   return (
-    <Card fluid >
-      <Card.Content>
-        <Card.Header>
-          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-              {name}&nbsp;<ProjectIcon name={name} />
+    <div className='project-card'>
+      <Card fluid >
+        <Card.Content>
+          <Card.Header>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+              <div style={{display: 'flex', alignItems: 'center'}}>
+                {name}&nbsp;<ProjectIcon name={name} />
+              </div>
+              <Popup
+                position='left center'
+                hideOnScroll
+                wide
+                inverted
+                trigger={
+                  <div className='project-card-rank-label'>
+                    <div>rank by market cap</div>
+                    <div>&nbsp;{rank}</div>
+                  </div>
+                } on='click'>
+                Market capitalization in a cryptocurrency world is a multiple
+                of amount of tokens in the circulation * token price
+              </Popup>
             </div>
-            <Popup
-              position='left center'
-              hideOnScroll
-              wide
-              inverted
-              trigger={
-                <div className='project-card-rank-label'>
-                  <div>rank by market cap</div>
-                  <div>&nbsp;{rank}</div>
-                </div>
-              } on='click'>
-              Market capitalization in a cryptocurrency world is a multiple
-              of amount of tokens in the circulation * token price
-            </Popup>
-          </div>
-        </Card.Header>
-        <Card.Meta>
-          <Label
-            size='mini'
-            color={MARKET_SEGMENT_COLORS[marketSegment]} tag>
-            {marketSegment || 'unknown yet'}
-          </Label>
-        </Card.Meta>
-        <Card.Description>
-          {!description
-            ? "We don't have any description about this project yet."
-            : description}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra style={{position: 'relative'}}>
-        {warning &&
-        <Popup basic
-          position='right center'
-          hideOnScroll
-          wide
-          inverted
-          trigger={
+          </Card.Header>
+          <Card.Meta>
             <Label
-              style={{
-                position: 'absolute',
-                top: '10px',
-                left: '-14px'
-              }}
-              color='orange' ribbon>
-              <Icon name='warning sign' />
-            </Label>} on='click'>
-          {signals[0] && signals[0].description}
-        </Popup>}
-        <Statistic.Group size='mini' widths='two' style={{paddingBottom: '1em'}}>
-          <StatisticElement
-            name='Price'
-            value={priceUsd ? formatNumber(priceUsd, { currency: 'USD' }) : '---'}
-            disabled={!priceUsd} />
-          <StatisticElement
-            name='Volume'
-            value={volumeUsd
-              ? `$${millify(volumeUsd)}`
-              : '---'}
-            disabled={!volumeUsd} />
-          <StatisticElement
-            name='24h Price'
-            up={percentChange24h > 0}
-            value={percentChange24h || '---'}
-            disabled={!percentChange24h} />
-          <StatisticElement
-            name='24h Volume'
-            up={volumeChange24h > 0}
-            value={volumeChange24h || '---'}
-            disabled={!volumeChange24h} />
-          <StatisticElement
-            name='MarketCap'
-            value={marketcapUsd
-              ? `$${millify(marketcapUsd)}`
-              : '---'}
-            disabled={!marketcapUsd} />
-          {type === 'erc20' &&
-          <StatisticElement
-            name='Crypto Balance'
-            value={ethBalance
-              ? `ETH ${millify(parseFloat(ethBalance).toFixed(2))}`
-              : '---'}
-            disabled={!ethBalance} />}
-          {type === 'erc20' &&
-          <StatisticElement
-            name='ETH Spent 30d'
-            value={ethSpent
-              ? `ETH ${millify(parseFloat(ethSpent).toFixed(2))}`
-              : 0}
-            disabled={!ethSpent} />}
-          <StatisticElement
-            name='Dev Activity 30d'
-            value={averageDevActivity ? parseFloat(averageDevActivity).toFixed(2) : '---'}
-            disabled={!averageDevActivity} />
-          <HiddenElements>
+              size='mini'
+              color={MARKET_SEGMENT_COLORS[marketSegment]} tag>
+              {marketSegment || 'unknown yet'}
+            </Label>
+          </Card.Meta>
+          <Card.Description>
+            {!description
+              ? "We don't have any description about this project yet."
+              : description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content
+          extra
+          style={{position: 'relative'}}
+          onClick={toggleExpandCard}
+        >
+          {warning &&
+          <Popup basic
+            position='right center'
+            hideOnScroll
+            wide
+            inverted
+            trigger={
+              <Label
+                style={{
+                  position: 'absolute',
+                  top: '10px',
+                  left: '-14px'
+                }}
+                color='orange' ribbon>
+                <Icon name='warning sign' />
+              </Label>} on='click'>
+            {signals[0] && signals[0].description}
+          </Popup>}
+          <Statistic.Group size='mini' widths='two' style={{paddingBottom: '1em'}}>
             <StatisticElement
-              name='Twitter 30d'
-              value={twitterData || '---'}
-              disabled={!twitterData} />
-          </HiddenElements>
-        </Statistic.Group>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <HiddenElements>
-            <Button basic size='large' icon='star' />
-          </HiddenElements>
-          <Button basic size='large' onClick={onClick}>more...</Button>
-        </div>
-      </Card.Content>
-    </Card>
+              name='Price'
+              value={priceUsd ? formatNumber(priceUsd, { currency: 'USD' }) : '---'}
+              disabled={!priceUsd} />
+            <StatisticElement
+              name='Volume'
+              value={volumeUsd
+                ? `$${millify(volumeUsd)}`
+                : '---'}
+              disabled={!volumeUsd} />
+            {isExpanded &&
+              <React.Fragment>
+                <StatisticElement
+                  name='24h Price'
+                  up={percentChange24h > 0}
+                  value={percentChange24h || '---'}
+                  disabled={!percentChange24h}
+                />
+                <StatisticElement
+                  name='24h Volume'
+                  up={volumeChange24h > 0}
+                  value={volumeChange24h || '---'}
+                  disabled={!volumeChange24h}
+                />
+                <StatisticElement
+                  name='MarketCap'
+                  value={marketcapUsd
+                    ? `$${millify(marketcapUsd)}`
+                    : '---'}
+                  disabled={!marketcapUsd}
+                />
+                {type === 'erc20' &&
+                  <StatisticElement
+                    name='Crypto Balance'
+                    value={ethBalance
+                      ? `ETH ${millify(parseFloat(ethBalance).toFixed(2))}`
+                      : '---'}
+                    disabled={!ethBalance}
+                    />
+                }
+              </React.Fragment>
+            }
+            {type === 'erc20' &&
+            <StatisticElement
+              name='ETH Spent 30d'
+              value={ethSpent
+                ? `ETH ${millify(parseFloat(ethSpent).toFixed(2))}`
+                : 0}
+              disabled={!ethSpent} />}
+            <StatisticElement
+              name='Dev Activity 30d'
+              value={averageDevActivity ? parseFloat(averageDevActivity).toFixed(2) : '---'}
+              disabled={!averageDevActivity} />
+            <HiddenElements>
+              <StatisticElement
+                name='Twitter 30d'
+                value={twitterData || '---'}
+                disabled={!twitterData} />
+            </HiddenElements>
+          </Statistic.Group>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <HiddenElements>
+              <Button basic size='large' icon='star' />
+            </HiddenElements>
+            <Button basic size='large' onClick={handleMoreClick}>more...</Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
   )
 }
 

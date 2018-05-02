5e583adb5ffbd7f0b9c936095a2239fcdc65fd19
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-05-02 14:16:44 +0300

    Fetch the contract info by slug instead of ticker to avoid duplicates

--- lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
@@ -10,11 +10,11 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
   require Logger
 
   @doc ~S"""
-    Return the token burn rate for the given ticker and time period.
+    Return the token burn rate for the given slug and time period.
     Uses the influxdb cached values instead of issuing a GET request to etherbi
   """
-  def burn_rate(_root, %{ticker: ticker, from: from, to: to, interval: interval}, _resolution) do
-    with {:ok, contract_address, token_decimals} <- ticker_to_contract_info(ticker),
+  def burn_rate(_root, %{slug: slug, from: from, to: to, interval: interval}, _resolution) do
+    with {:ok, contract_address, token_decimals} <- slug_to_contract_info(slug),
          {:ok, burn_rates} <- BurnRate.Store.burn_rate(contract_address, from, to, interval) do
       result =
         burn_rates
@@ -28,22 +28,22 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
       {:ok, result}
     else
       error ->
-        error_msg = "Can't fetch burn rate for #{ticker}"
+        error_msg = "Can't fetch burn rate for #{slug}"
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
   end
 
   @doc ~S"""
-    Return the transaction volume for the given ticker and time period.
+    Return the transaction volume for the given slug and time period.
     Uses the influxdb cached values instead of issuing a GET request to etherbi
   """
   def transaction_volume(
         _root,
-        %{ticker: ticker, from: from, to: to, interval: interval},
+        %{slug: slug, from: from, to: to, interval: interval},
         _resolution
       ) do
-    with {:ok, contract_address, token_decimals} <- ticker_to_contract_info(ticker),
+    with {:ok, contract_address, token_decimals} <- slug_to_contract_info(slug),
          {:ok, trx_volumes} <-
            TransactionVolume.Store.transaction_volume(contract_address, from, to, interval) do
       result =
@@ -58,21 +58,21 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
       {:ok, result}
     else
       error ->
-        error_msg = "Can't fetch transaction for #{ticker}"
+        error_msg = "Can't fetch transaction for #{slug}"
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
   end
 
   @doc ~S"""
-    Return the number of daily active addresses for a given ticker
+    Return the number of daily active addresses for a given slug
   """
   def daily_active_addresses(
         _root,
-        %{ticker: ticker, from: from, to: to, interval: interval},
+        %{slug: slug, from: from, to: to, interval: interval},
         _resolution
       ) do
-    with {:ok, contract_address, _token_decimals} <- ticker_to_contract_info(ticker),
+    with {:ok, contract_address, _token_decimals} <- slug_to_contract_info(slug),
          {:ok, daily_active_addresses} <-
            DailyActiveAddresses.Store.daily_active_addresses(contract_address, from, to, interval) do
       result =
@@ -87,28 +87,28 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
       {:ok, result}
     else
       error ->
-        error_msg = "Can't fetch daily active addresses for #{ticker}"
+        error_msg = "Can't fetch daily active addresses for #{slug}"
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
   end
 
   @doc ~S"""
-    Return the transactions that happend in or out of an exchange wallet for a given ticker
+    Return the transactions that happend in or out of an exchange wallet for a given slug
     and time period.
     Uses the influxdb cached values instead of issuing a GET request to etherbi
   """
   def exchange_funds_flow(
         _root,
         %{
-          ticker: ticker,
+          slug: slug,
           from: from,
           to: to,
           interval: interval
         },
         _resolution
       ) do
-    with {:ok, contract_address, _token_decimals} <- ticker_to_contract_info(ticker),
+    with {:ok, contract_address, _token_decimals} <- slug_to_contract_info(slug),
          {:ok, funds_flow_list} <-
            Transactions.Store.transactions_in_out_difference(
              contract_address,
@@ -128,7 +128,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
       {:ok, result}
     else
       error ->
-        error_msg = "Can't fetch the exchange fund flow for #{ticker}."
+        error_msg = "Can't fetch the exchange fund flow for #{slug}."
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
@@ -139,7 +139,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
   end
 
   @doc ~S"""
-    Return the average number of daily active addresses for a given ticker and period of time
+    Return the average number of daily active addresses for a given slug and period of time
   """
   def average_daily_active_addresses(
         %Project{id: id} = project,
@@ -199,18 +199,18 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
     {:error, "Can't find contract address of #{project.coinmarketcap_id}"}
   end
 
-  defp ticker_to_contract_info(ticker) do
-    with project when not is_nil(project) <- get_project_by_ticker(ticker),
+  defp slug_to_contract_info(slug) do
+    with project when not is_nil(project) <- get_project_by_slug(slug),
          {:ok, contract_address, token_decimals} <- project_to_contract_info(project) do
       {:ok, contract_address, token_decimals}
     else
-      _ -> {:error, "Can't find ticker contract address"}
+      _ -> {:error, "Can't find contract address for #{slug}"}
     end
   end
 
-  defp get_project_by_ticker(ticker) do
+  defp get_project_by_slug(slug) do
     Project
-    |> where([p], p.ticker == ^ticker and not is_nil(p.coinmarketcap_id))
+    |> where([p], p.coinmarketcap_id == ^slug)
     |> Repo.one()
   end
 end

c59fbbd443704579b0aaea52569f3604d55872b9
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-05-09 14:32:54 +0300

    extract config vars

--- lib/sanbase/oauth2/hydra.ex
+++ lib/sanbase/oauth2/hydra.ex
@@ -1,20 +1,14 @@
 defmodule Sanbase.Oauth2.Hydra do
+  import Sanbase.Utils.Config, only: [parse_config_value: 1]
+
   alias Sanbase.Auth.User
-
-  @base_url "http://localhost:4444"
-  @token_url @base_url <> "/oauth2/token"
-  @consent_url @base_url <> "/oauth2/consent/requests"
-  @client_id "grafana"
-  @client_secret "grafana-secret"
-
-  @basic_auth [hackney: [basic_auth: {@client_id, @client_secret}]]
 
   def get_access_token() do
     HTTPoison.post!(
-      @token_url,
+      token_url(),
       "grant_type=client_credentials&scope=hydra.consent",
       [{"Accept", "application/json"}, {"content-type", "application/x-www-form-urlencoded"}],
-      @basic_auth
+      basic_auth()
     )
     |> case do
       %HTTPoison.Response{body: body, status_code: 200} ->
@@ -26,7 +20,7 @@ defmodule Sanbase.Oauth2.Hydra do
   end
 
   def get_consent_data(consent, access_token) do
-    HTTPoison.get!(@consent_url <> "/#{consent}", [
+    HTTPoison.get!(consent_url() <> "/#{consent}", [
       {"Authorization", "Bearer #{access_token}"},
       {"Accept", "application/json"}
     ])
@@ -51,7 +45,7 @@ defmodule Sanbase.Oauth2.Hydra do
       "subject" => "user:#{id}:#{username}"
     }
 
-    HTTPoison.patch!(@consent_url <> "/#{consent}/accept", Poison.encode!(data), [
+    HTTPoison.patch!(consent_url() <> "/#{consent}/accept", Poison.encode!(data), [
       {"Authorization", "Bearer #{access_token}"},
       {"Content-type", "application/json"},
       {"Accept", "application/json"}
@@ -64,4 +58,17 @@ defmodule Sanbase.Oauth2.Hydra do
         :error
     end
   end
+
+  defp get_config(key) do
+    Application.fetch_env!(:sanbase, Sanbase.Hydra)
+    |> IO.inspect()
+    |> Keyword.fetch!(key)
+    |> parse_config_value()
+  end
+
+  defp token_url(), do: get_config(:base_url) <> get_config(:token_uri)
+  defp consent_url(), do: get_config(:base_url) <> get_config(:consent_uri)
+
+  defp basic_auth(),
+    do: [hackney: [basic_auth: {get_config(:client_id), get_config(:client_secret)}]]
 end

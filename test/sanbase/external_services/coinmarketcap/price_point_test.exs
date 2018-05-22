07c9e5e456f4bef12f6ed768df70d7d56e34f70b
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-05-22 12:29:54 +0300

    Make tests for GraphData and PricePoint use the new modules

--- test/sanbase/external_services/coinmarketcap/price_point_test.exs
+++ test/sanbase/external_services/coinmarketcap/price_point_test.exs
@@ -1,82 +1,89 @@
 defmodule Sanbase.ExternalServices.Coinmarketcap.PricePointTest do
   use Sanbase.DataCase, async: false
 
-  alias Sanbase.ExternalServices.Coinmarketcap.PricePoint
+  alias Sanbase.ExternalServices.Coinmarketcap.PricePoint2, as: PricePoint
   alias Sanbase.Influxdb.Measurement
   alias Sanbase.Model.Project
 
+  @total_market_measurement "TOTAL_MARKET_total-market"
   setup do
-    dt = 1_367_174_820_000_000_000
+    ts =
+      DateTime.from_naive!(~N[2018-05-13 21:45:00], "Etc/UTC")
+      |> DateTime.to_unix(:nanoseconds)
 
     price_point = %PricePoint{
-      marketcap: 400,
+      price_btc: nil,
+      price_usd: nil,
+      marketcap_usd: 400,
       volume_usd: 500,
-      datetime: DateTime.from_unix!(dt, :nanosecond)
-    }
-
-    expectation = %Measurement{
-      timestamp: dt,
-      fields: %{
-        price: nil,
-        volume: price_point.volume_usd,
-        marketcap: price_point.marketcap
-      },
-      tags: []
+      datetime: DateTime.from_unix!(ts, :nanosecond)
     }
 
     project = %Project{
-      ticker: "SAN"
+      ticker: "SAN",
+      coinmarketcap_id: "santiment"
     }
 
-    {:ok, price_point: price_point, expectation: expectation, project: project}
+    expectation = %Measurement{
+      timestamp: ts,
+      fields: %{
+        price_usd: nil,
+        price_btc: nil,
+        volume_usd: price_point.volume_usd,
+        marketcap_usd: price_point.marketcap_usd
+      },
+      tags: [],
+      name: Measurement.name_from(project)
+    }
+
+    %{
+      price_point: price_point,
+      expectation: expectation,
+      project: project
+    }
   end
 
-  test "converting price point to measurement for BTC", %{
+  test "converting price point to measurement with BTC price", %{
     price_point: price_point,
-    expectation: expectation
+    expectation: expectation,
+    project: project
   } do
     price_point = Map.replace!(price_point, :price_btc, 100)
-    fields = Map.replace!(expectation.fields, :price, price_point.price_btc)
+    fields = Map.replace!(expectation.fields, :price_btc, price_point.price_btc)
 
     expectation =
       expectation
-      |> Map.replace!(:name, "SAN_BTC")
       |> Map.replace!(:fields, fields)
 
-    assert PricePoint.convert_to_measurement(price_point, "BTC", "SAN") == expectation
+    assert PricePoint.convert_to_measurement(price_point, Measurement.name_from(project)) ==
+             expectation
   end
 
-  test "converting price point to measurement for USD", %{
+  test "converting price point to measurement with USD price", %{
     price_point: price_point,
-    expectation: expectation
+    expectation: expectation,
+    project: project
   } do
     price_point = Map.replace!(price_point, :price_usd, 100)
-    fields = Map.replace!(expectation.fields, :price, price_point.price_usd)
+    fields = Map.replace!(expectation.fields, :price_usd, price_point.price_usd)
 
     expectation =
       expectation
-      |> Map.replace!(:name, "SAN_USD")
       |> Map.replace!(:fields, fields)
 
-    assert PricePoint.convert_to_measurement(price_point, "USD", "SAN") == expectation
-  end
-
-  test "converting price point to measurement for USD when price_usd is missing", %{
-    price_point: price_point,
-    expectation: expectation
-  } do
-    expectation = Map.replace!(expectation, :name, "SAN_USD")
-
-    assert PricePoint.convert_to_measurement(price_point, "USD", "SAN") == expectation
+    assert PricePoint.convert_to_measurement(price_point, Measurement.name_from(project)) ==
+             expectation
   end
 
   test "price_points_to_measurements called with one price point", %{
     price_point: price_point,
     expectation: expectation
   } do
-    expectation = Map.replace!(expectation, :name, "TOTAL_MARKET_USD")
+    expectation = Map.replace!(expectation, :name, @total_market_measurement)
 
-    assert PricePoint.price_points_to_measurements(price_point) == [expectation]
+    assert PricePoint.price_points_to_measurements(price_point, @total_market_measurement) == [
+             expectation
+           ]
   end
 
   test "price_points_to_measurements called with array of price point", %{
@@ -90,30 +97,19 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.PricePointTest do
         DateTime.from_unix!(1_367_174_821_000_000_000, :nanosecond)
       )
 
-    expectation = Map.replace!(expectation, :name, "TOTAL_MARKET_USD")
+    expectation = Map.replace!(expectation, :name, @total_market_measurement)
 
     expectation_new =
       expectation
       |> Map.replace!(:timestamp, 1_367_174_821_000_000_000)
-      |> Map.replace!(:name, "TOTAL_MARKET_USD")
+      |> Map.replace!(:name, @total_market_measurement)
 
-    assert PricePoint.price_points_to_measurements([price_point, price_point_new]) == [
+    assert PricePoint.price_points_to_measurements(
+             [price_point, price_point_new],
+             @total_market_measurement
+           ) == [
              expectation,
              expectation_new
-           ]
-  end
-
-  test "price_points_to_measurements called with one price point and project", %{
-    price_point: price_point,
-    expectation: expectation,
-    project: project
-  } do
-    expectation_usd = Map.replace!(expectation, :name, "SAN_USD")
-    expectation_btc = Map.replace!(expectation, :name, "SAN_BTC")
-
-    assert PricePoint.price_points_to_measurements(price_point, project) == [
-             expectation_usd,
-             expectation_btc
            ]
   end
 
@@ -129,20 +125,11 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.PricePointTest do
         DateTime.from_unix!(1_367_174_821_000_000_000, :nanosecond)
       )
 
-    expectation_usd = Map.replace!(expectation, :name, "SAN_USD")
-    expectation_btc = Map.replace!(expectation, :name, "SAN_BTC")
-
-    expectation_usd_new =
+    expectation_new =
       expectation
       |> Map.replace!(:timestamp, 1_367_174_821_000_000_000)
-      |> Map.replace!(:name, "SAN_USD")
-
-    expectation_btc_new =
-      expectation
-      |> Map.replace!(:timestamp, 1_367_174_821_000_000_000)
-      |> Map.replace!(:name, "SAN_BTC")
 
     assert PricePoint.price_points_to_measurements([price_point, price_point_new], project) ==
-             [expectation_usd, expectation_btc, expectation_usd_new, expectation_btc_new]
+             [expectation, expectation_new]
   end
 end

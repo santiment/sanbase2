ec6a46d2ee7fd993dc206e379730c34d67a79be6
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-05-29 14:25:04 +0500

    Reorganize the feedback form from popup to modal

--- app/src/reducers/rootReducers.js
+++ app/src/reducers/rootReducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import user, { initialState as userState } from './user'
 import projects, { initialState as projectsState } from './projects'
+import rootUi, { initialState as rootUiState } from './root-ui'
 import detailedPageUi, { initialState as detailedPageUiState } from './detailed-page-ui'
 import insightsPageUi, { initialState as insightsPageUiState } from './insights-page-ui'
 import notification, { initialState as initialNotificationState } from './notification'
@@ -10,12 +11,14 @@ export const intitialState = {
   projects: projectsState,
   detailedPageUi: detailedPageUiState,
   insightsPageUi: insightsPageUiState,
-  initialNotificationState
+  rootUi: rootUiState,
+  notification: initialNotificationState
 }
 
 export default combineReducers({
   user,
   projects,
+  rootUi,
   detailedPageUi,
   insightsPageUi,
   notification

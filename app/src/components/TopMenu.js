b3731d0235f6ee7e9a73d00bdae52fef11145028
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-05-29 13:12:54 +0500

    Refactored header menu

--- app/src/components/TopMenu.js
+++ app/src/components/TopMenu.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { withRouter, NavLink as Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import 'font-awesome/css/font-awesome.css'
 import logo from '../assets/logo_sanbase.png'
-import AuthControl from './AuthControl'
+import HeaderDropdownMenu from './HeaderDropdownMenu.js'
 import Search from './SearchContainer'
 import './AppMenu.css'
 import './TopMenu.css'
@@ -21,6 +21,7 @@ export const TopMenu = ({
       <div className='left'>
         <Link
           to={'/'}
+          exact
           className='brand'>
           <img
             src={logo}
@@ -48,7 +49,7 @@ export const TopMenu = ({
             Roadmap
           </Link>
         </ul>
-        <AuthControl
+        <HeaderDropdownMenu
           handleNavigation={nextRoute => {
             history.push(`/${nextRoute}`)
           }}

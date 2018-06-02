be86d4c722357989b21d984d8e1ec545ca7f433f
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-06-02 12:27:12 +0500

    Make app launching behavior, as separate module

--- app/src/epics/rootEpics.js
+++ app/src/epics/rootEpics.js
@@ -2,8 +2,10 @@ import 'rxjs'
 import { combineEpics } from 'redux-observable'
 import handleFollowProject from './handleFollowProject'
 import handleOffline from './handleOffline'
+import handleLauched from './handleLaunch'
 
 export default combineEpics(
   handleFollowProject,
-  handleOffline
+  handleOffline,
+  handleLauched
 )

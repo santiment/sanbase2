de5d408351109adf4e3a688a9f703534e9cacabe
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-06-04 12:36:22 +0500

    Changed polyfills

--- app/src/index.js
+++ app/src/index.js
@@ -22,40 +22,11 @@ import { changeNetworkStatus, launchApp } from './actions/rootActions'
 import uploadLink from './apollo/upload-link'
 import errorLink from './apollo/error-link'
 import authLink from './apollo/auth-link'
-// Look at 42 line. ;)
+// Look at 73 line. ;)
 // import * as serviceWorker from './serviceWorker'
 
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
-
-const run = (client, store, App) => {
-  ReactDOM.render(
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <Router>
-          <Route path='/' component={App} />
-        </Router>
-      </Provider>
-    </ApolloProvider>,
-    document.getElementById('root'))
-
-  // TODO: 2018-04-25 Yura Z.: Need to change deploy logic for frontend
-  // Until we don't use s3 for static, we have problem with webworkers,
-  // after each updates.
-  /* serviceWorker.register({
-    onUpdate: registration => {
-      console.log('App updated... Refresh your browser, please.')
-    },
-    onSuccess: registration => {
-      console.log('Your browser makes cached SANbase version')
-    }
-  }) */
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister()
-    })
-  }
-}
 
 const handleLoad = () => {
   const httpLink = createHttpLink({ uri: `${getOrigin()}/graphql` })
@@ -89,17 +60,31 @@ const handleLoad = () => {
     store.dispatch(changeNetworkStatus(online))
   })
 
-  if (!window.Intl) {
-    require.ensure([
-      'intl',
-      'intl/locale-data/jsonp/en.js'
-    ], () => {
-      require('intl')
-      require('intl/locale-data/jsonp/en.js')
-      run(client, store, App)
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Router>
+          <Route path='/' component={App} />
+        </Router>
+      </Provider>
+    </ApolloProvider>,
+    document.getElementById('root'))
+
+  // TODO: 2018-04-25 Yura Z.: Need to change deploy logic for frontend
+  // Until we don't use s3 for static, we have problem with webworkers,
+  // after each updates.
+  /* serviceWorker.register({
+    onUpdate: registration => {
+      console.log('App updated... Refresh your browser, please.')
+    },
+    onSuccess: registration => {
+      console.log('Your browser makes cached SANbase version')
+    }
+  }) */
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister()
     })
-  } else {
-    run(client, store, App)
   }
 }
 

403cfa51883c5feb3e54ba04df7ae257e44db0cc
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-06-08 15:27:52 +0300

    Use strings for config values

--- test/sanbase_web/graphql/plug_attack_test.exs
+++ test/sanbase_web/graphql/plug_attack_test.exs
@@ -10,14 +10,22 @@ defmodule PlugAttackTest do
   test "get graphql updates the remaining ratelimit headers", %{conn: conn} do
     conn = %{conn | remote_ip: {192, 168, 0, 1}}
     response = conn |> get("/graphql")
-    ratelimit_remaining = response |> get_resp_header("x-ratelimit-remaining") |> List.first()
+
+    ratelimit_remaining =
+      response
+      |> get_resp_header("x-ratelimit-remaining")
+      |> List.first()
+
     assert ratelimit_remaining == Integer.to_string(get_max_requests() - 1)
   end
 
   test "after max requests the api returns 403 Forbidden", %{conn: conn} do
     conn = %{conn | remote_ip: {192, 168, 0, 1}}
+
     for _n <- 1..get_max_requests(), do: conn |> get("/graphql")
+
     response = conn |> get("/graphql")
+
     assert response.status == 403
   end
 
@@ -25,5 +33,6 @@ defmodule PlugAttackTest do
     Application.fetch_env!(:sanbase, SanbaseWeb.Graphql.PlugAttack)
     |> Keyword.get(:rate_limit_max_requests)
     |> Config.parse_config_value()
+    |> String.to_integer()
   end
 end

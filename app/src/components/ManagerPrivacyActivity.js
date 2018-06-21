7a4e19b57272b2d547a7712b546a9011f797b9b3
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-06-22 00:23:32 +0500

    Added privacy policy settings (wip)

---
+++ app/src/components/ManagerPrivacyActivity.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Checkbox, Message } from 'semantic-ui-react'
+import * as actions from './../actions/types'
+import './ManagerPrivacyActivity.css'
+
+const ManagerPrivacyActivity = ({
+  privacyPolicyAccepted,
+  marketingAccepted,
+  togglePrivacyPolicy
+}) => {
+  return (
+    <div className='panel'>
+      <h2>Data & Privacy</h2>
+      <Message color='teal'>Manage your activity</Message>
+      <div className='gdpr-settings-privacy'>
+        <div className='gdpr-settings-privacy__card'>
+          <div className='gdpr-settings-privacy__card-header'>
+            Your Sanbase experience
+          </div>
+          <div className='gdpr-settings-privacy__card-content'>
+            <p>
+              Process the personal data provided by me for the
+              registration on this website to support
+              and administer my user account.
+            </p>
+            <Checkbox
+              toggle
+              onClick={togglePrivacyPolicy}
+              checked={privacyPolicyAccepted}
+            />
+          </div>
+        </div>
+        <div className='gdpr-settings-privacy__card'>
+          <div className='gdpr-settings-privacy__card-header'>
+            Marketing materials
+          </div>
+          <div className='gdpr-settings-privacy__card-content'>
+            <p>
+              Contact me to send me marketing materials related
+              to the Company’s services and operations.
+            </p>
+            <Checkbox
+              toggle
+              checked={marketingAccepted}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    privacyPolicyAccepted: state.user.privacyPolicyAccepted,
+    marketingAccepted: state.user.marketingAccepted
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    togglePrivacyPolicy: () => {
+      dispatch({
+        type: actions.USER_TOGGLE_PRIVACY_POLICY
+      })
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManagerPrivacyActivity)

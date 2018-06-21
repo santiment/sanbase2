01a20601cb5e45447e3ed000bb4d5e4322f73a31
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-06-21 15:02:12 +0300

    fix linter

--- app/src/components/ApiExamples.js
+++ app/src/components/ApiExamples.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 export const ApiExamples = (props) => {
   return (
     <div>
-      <iframe src="/apidocs" width="100%" height="1000"></iframe>
+      <iframe src='/apidocs' width='100%' height='1000' />
     </div>
   )
 }

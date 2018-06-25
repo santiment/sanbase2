152e2f5b23b4b23d44785ca5a631a5f209cb2197
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-06-25 11:59:55 +0300

    Update Erlang/OTP to 21 and Elixir to 1.6.6
    Erlang/OTP 21 ships by default without tuple calls. `Mockery.of` uses
    tuple calls, so update the version of `Mockery` and start using the new
    `Mockery.Macro.mockable`. It cannot be used via module attribute.

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule Sanbase.Mixfile do
       {:hammer, "~> 2.0.0"},
       {:ex_admin, github: "smpallen99/ex_admin", branch: "phx-1.3"},
       {:basic_auth, "~> 2.2"},
-      {:mockery, "~> 2.0"},
+      {:mockery, "~> 2.2"},
       {:distillery, "~> 1.5", runtime: false},
       {:timex, "~> 3.0"},
       {:timex_ecto, "~> 3.0"},

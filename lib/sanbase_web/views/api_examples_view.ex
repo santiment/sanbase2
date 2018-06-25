676ff8d9427f35eb7e3df9ba374766432b7a96ca
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-06-25 17:10:50 +0300

    Update the SAN stake to 1000. Add message explaining the API delay.

--- lib/sanbase_web/views/api_examples_view.ex
+++ lib/sanbase_web/views/api_examples_view.ex
@@ -1,11 +1,17 @@
 defmodule SanbaseWeb.ApiExamplesView do
   use SanbaseWeb, :view
+
   require Logger
+  require Sanbase.Utils.Config
+
+  alias Sanbase.Utils.Config
+  alias SanbaseWeb.Graphql.Middlewares.ApiDelay
 
   def render("apiexample_view.html", _assigns) do
     Phoenix.View.render_to_string(SanbaseWeb.ApiExamplesView, "examples.html", %{
       api_url: SanbaseWeb.Endpoint.api_url(),
       explorer_url: explorer_url(),
+      required_san_stake_realtime_api: required_san_stake_realtime_api(),
       daa: %{
         query: daa(),
         variables: "{}",
@@ -106,5 +112,9 @@ defmodule SanbaseWeb.ApiExamplesView do
     Absinthe.Schema.lookup_type(SanbaseWeb.Graphql.Schema, :query).fields
     |> Map.get(field)
     |> Map.get(:description)
+  end
+
+  defp required_san_stake_realtime_api() do
+    Config.module_get(ApiDelay, :required_san_stake_realtime_api) |> String.to_integer()
   end
 end

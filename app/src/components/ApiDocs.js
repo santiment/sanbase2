6dcfa2b3ed3e48c7717bc47d5d4da3dfca0c2651
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-06-28 18:53:37 +0300

    Fix responsivness and scrolling for mobile safari

--- app/src/components/ApiDocs.js
+++ app/src/components/ApiDocs.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './ApiDocs.css'
+import {isMobileSafari} from 'react-device-detect';
 
 export const ApiDocs = (props) => {
   return (
     <div className='apidocs-container'>
-      <iframe
-        src='/apiexamples'
-        title='API documentation'
-        className='apidocs-iframe'
-      />
+      {
+        isMobileSafari ? (
+          <iframe
+            scrolling='no'
+            src='/apiexamples'
+            title='API documentation'
+            className='apidocs-iframe-iphone'
+          />
+        ) : (
+          <iframe
+            src='/apiexamples'
+            title='API documentation'
+            className='apidocs-iframe'
+          />
+        )
+      }
     </div>
   )
 }

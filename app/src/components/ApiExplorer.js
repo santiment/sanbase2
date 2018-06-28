aca59b8800155893d6b4f13fc8d9de095459a9da
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-06-28 19:33:23 +0300

    fix also for mobile ios chrome and others

--- app/src/components/ApiExplorer.js
+++ app/src/components/ApiExplorer.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import './ApiExplorer.css'
-import {isMobileSafari} from 'react-device-detect'
 
 export const ApiExplorer = (props) => {
   return (
     <div className='apiexplorer-container'>
-      {
-        isMobileSafari ? (
-          <iframe
-            scrolling='no'
-            src={`/graphiql${props.location.search}`}
-            title='API explorer'
-            className='apiexplorer-iframe-iphone'
-          />
-        ) : (
-          <iframe
-            src={`/graphiql${props.location.search}`}
-            title='API explorer'
-            className='apiexplorer-iframe'
-          />
-        )
-      }
+      <iframe
+        src={`/graphiql${props.location.search}`}
+        title='API explorer'
+        className='apiexplorer-iframe'
+      />
     </div>
   )
 }

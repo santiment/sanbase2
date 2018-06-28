b18982457e184bda3e5392206c673e8e509b0582
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-06-28 17:31:36 +0300

    Remove deprecated fetching of etherbi data by ticker, use slug instead

--- lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/etherbi_resolver.ex
@@ -10,42 +10,6 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
   require Logger
 
   @doc ~S"""
-    Return the token burn rate for the given ticker and time period.
-    Uses the influxdb cached values instead of issuing a GET request to etherbi
-  """
-  @deprecated "Use burn_rate by slug"
-  def burn_rate(_root, %{ticker: ticker, from: from, to: to, interval: interval}, _resolution) do
-    with {:ok, contract_address, token_decimals} <- ticker_to_contract_info(ticker),
-         {:ok, from, to, interval} <-
-           Utils.calibrate_interval(
-             BurnRate.Store,
-             contract_address,
-             from,
-             to,
-             interval,
-             60 * 60,
-             50
-           ),
-         {:ok, burn_rates} <- BurnRate.Store.burn_rate(contract_address, from, to, interval) do
-      result =
-        burn_rates
-        |> Enum.map(fn {datetime, burn_rate} ->
-          %{
-            datetime: datetime,
-            burn_rate: burn_rate / :math.pow(10, token_decimals)
-          }
-        end)
-
-      {:ok, result}
-    else
-      error ->
-        error_msg = "Can't fetch burn rate for #{ticker}"
-        Logger.warn(error_msg <> "Reason: #{inspect(error)}")
-        {:error, error_msg}
-    end
-  end
-
-  @doc ~S"""
     Return the token burn rate for the given slug and time period.
     Uses the influxdb cached values instead of issuing a GET request to etherbi
   """
@@ -75,47 +39,6 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
     else
       error ->
         error_msg = "Can't fetch burn rate for #{slug}"
-        Logger.warn(error_msg <> "Reason: #{inspect(error)}")
-        {:error, error_msg}
-    end
-  end
-
-  @doc ~S"""
-    Return the transaction volume for the given ticker and time period.
-    Uses the influxdb cached values instead of issuing a GET request to etherbi
-  """
-  @deprecated "Use transaction_volume by slug"
-  def transaction_volume(
-        _root,
-        %{ticker: ticker, from: from, to: to, interval: interval},
-        _resolution
-      ) do
-    with {:ok, contract_address, token_decimals} <- ticker_to_contract_info(ticker),
-         {:ok, from, to, interval} <-
-           Utils.calibrate_interval(
-             TransactionVolume.Store,
-             contract_address,
-             from,
-             to,
-             interval,
-             60 * 60,
-             50
-           ),
-         {:ok, trx_volumes} <-
-           TransactionVolume.Store.transaction_volume(contract_address, from, to, interval) do
-      result =
-        trx_volumes
-        |> Enum.map(fn {datetime, trx_volume} ->
-          %{
-            datetime: datetime,
-            transaction_volume: trx_volume / :math.pow(10, token_decimals)
-          }
-        end)
-
-      {:ok, result}
-    else
-      error ->
-        error_msg = "Can't fetch transaction for #{ticker}"
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
@@ -156,46 +79,6 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
     else
       error ->
         error_msg = "Can't fetch transaction for #{slug}"
-        Logger.warn(error_msg <> "Reason: #{inspect(error)}")
-        {:error, error_msg}
-    end
-  end
-
-  @doc ~S"""
-    Return the number of daily active addresses for a given ticker
-  """
-  @deprecated "Use DAA by slug"
-  def daily_active_addresses(
-        _root,
-        %{ticker: ticker, from: from, to: to, interval: interval},
-        _resolution
-      ) do
-    with {:ok, contract_address, _token_decimals} <- ticker_to_contract_info(ticker),
-         {:ok, from, to, interval} <-
-           Utils.calibrate_interval(
-             DailyActiveAddresses.Store,
-             contract_address,
-             from,
-             to,
-             interval,
-             24 * 60 * 60,
-             50
-           ),
-         {:ok, daily_active_addresses} <-
-           DailyActiveAddresses.Store.daily_active_addresses(contract_address, from, to, interval) do
-      result =
-        daily_active_addresses
-        |> Enum.map(fn [datetime, active_addresses] ->
-          %{
-            datetime: datetime,
-            active_addresses: active_addresses |> round() |> trunc()
-          }
-        end)
-
-      {:ok, result}
-    else
-      error ->
-        error_msg = "Can't fetch daily active addresses for #{ticker}"
         Logger.warn(error_msg <> "Reason: #{inspect(error)}")
         {:error, error_msg}
     end
@@ -290,57 +173,6 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
     end
   end
 
-  @doc ~S"""
-    Return the transactions that happend in or out of an exchange wallet for a given ticker
-    and time period.
-    Uses the influxdb cached values instead of issuing a GET request to etherbi
-  """
-  @deprecated "Use exchange funds flow by slug"
-  def exchange_funds_flow(
-        _root,
-        %{
-          ticker: ticker,
-          from: from,
-          to: to,
-          interval: interval
-        },
-        _resolution
-      ) do
-    with {:ok, contract_address, _token_decimals} <- ticker_to_contract_info(ticker),
-         {:ok, from, to, interval} <-
-           Utils.calibrate_interval(
-             Transactions.Store,
-             contract_address,
-             from,
-             to,
-             interval,
-             60 * 60
-           ),
-         {:ok, funds_flow_list} <-
-           Transactions.Store.transactions_in_out_difference(
-             contract_address,
-             from,
-             to,
-             interval
-           ) do
-      result =
-        funds_flow_list
-        |> Enum.map(fn {datetime, funds_flow} ->
-          %{
-            datetime: datetime,
-            funds_flow: funds_flow
-          }
-        end)
-
-      {:ok, result}
-    else
-      error ->
-        error_msg = "Can't fetch the exchange fund flow for #{ticker}."
-        Logger.warn(error_msg <> "Reason: #{inspect(error)}")
-        {:error, error_msg}
-    end
-  end
-
   def exchange_wallets(_root, _args, _resolution) do
     {:ok, ExchangeEthAddress |> Repo.all()}
   end
@@ -418,23 +250,6 @@ defmodule SanbaseWeb.Graphql.Resolvers.EtherbiResolver do
   defp get_project_by_slug(slug) do
     Project
     |> where([p], p.coinmarketcap_id == ^slug)
-    |> Repo.one()
-  end
-
-  @deprecated "use slug_to_contract_info"
-  defp ticker_to_contract_info(ticker) do
-    with project when not is_nil(project) <- get_project_by_ticker(ticker),
-         {:ok, contract_address, token_decimals} <- project_to_contract_info(project) do
-      {:ok, contract_address, token_decimals}
-    else
-      _ -> {:error, "Can't find contract address for #{ticker}"}
-    end
-  end
-
-  @deprecated "use get_project_by_slug"
-  defp get_project_by_ticker(ticker) do
-    Project
-    |> where([p], not is_nil(p.coinmarketcap_id) and p.ticker == ^ticker)
     |> Repo.one()
   end
 end

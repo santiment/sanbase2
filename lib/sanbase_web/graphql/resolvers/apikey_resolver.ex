450583c224b90e8283626153c722fcbc29550a2a
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-07-02 17:22:28 +0300

    Refactor apikey resolver and add apikey resolver tests

--- lib/sanbase_web/graphql/resolvers/apikey_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/apikey_resolver.ex
@@ -7,16 +7,22 @@ defmodule SanbaseWeb.Graphql.Resolvers.ApikeyResolver do
   def generate_apikey(_root, _args, %{
         context: %{auth: %{auth_method: :user_token, current_user: user}}
       }) do
-    {:ok, _apikey} = Apikey.generate_apikey(user)
-    {:ok, user}
+    with {:ok, _apikey} <- Apikey.generate_apikey(user) do
+      {:ok, user}
+    else
+      error -> {:error, "Failed to generate apikey. Inspecting error: #{inspect(error)}"}
+    end
   end
 
   def revoke_apikey(_root, %{apikey: apikey}, %{
         context: %{auth: %{auth_method: :user_token, current_user: user}}
       }) do
-    :ok = Apikey.revoke_apikey(user, apikey)
-
-    {:ok, user}
+    with :ok <- Apikey.revoke_apikey(user, apikey) do
+      {:ok, user}
+    else
+      _error ->
+        {:error, "Failed to revoke apikey. Provided apikey is malformed or not valid."}
+    end
   end
 
   def apikeys_list(%User{} = user, _args, _resolution) do

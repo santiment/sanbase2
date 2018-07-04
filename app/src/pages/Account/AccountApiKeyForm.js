5330adcc1b1bba7a2ab59a2455e6b0b2fd1d44ae
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-04 13:29:39 +0300

    ApiKey and ApiKeyList component

--- app/src/pages/Account/AccountApiKeyForm.js
+++ app/src/pages/Account/AccountApiKeyForm.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Divider } from 'semantic-ui-react'
 import ApiKeyGenerateButton from '../../components/ApiKey/ApiKeyGenerateButton'
+import ApiKeyList from '../../components/ApiKey/ApiKeyList'
 
 const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate }) => {
   return (
@@ -10,9 +11,7 @@ const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate }) => {
       <div className='api-key'>
         <p>Here will be your API key</p>
         <ApiKeyGenerateButton dispatchApikeysGenerate={dispatchApikeysGenerate} />
-        {apikeys.length === 0
-          ? 'At this moment you don\'t have any api keys'
-          : apikeys.map(apiKey => `Your api key is: ${apiKey}`)}
+        <ApiKeyList apikeys={apikeys} />
       </div>
     </Fragment>
   )

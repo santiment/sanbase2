7ab4d3974351270d3ea424cdced8b478149377de
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-04 13:29:47 +0300

    ApiKey and ApiKeyList component

--- app/src/components/ApiKey/ApiKeyList.js
+++ app/src/components/ApiKey/ApiKeyList.js
@@ -5,6 +5,10 @@ export class ApiKeyList extends Component {
   render () {
     const {apikeys} = this.props
 
+    if (!apikeys.length) {
+      return 'You don\'t have any api keys for now'
+    }
+
     return (
       <ol>
         {apikeys.map(apikey => <ApiKey key={apikey} apikey={apikey} />)}

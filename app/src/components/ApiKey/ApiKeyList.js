e117b71713a5bb6ab89c98ca64a090f78a73741c
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-04 14:33:59 +0300

    Reveal/hide button

--- app/src/components/ApiKey/ApiKeyList.js
+++ app/src/components/ApiKey/ApiKeyList.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 import ApiKey from './ApiKey'
 import ApiKeyRevokeButton from './ApiKeyRevokeButton'
+import { Map } from 'core-js'
 
 export class ApiKeyList extends Component {
+  // eslint-disable-next-line
+  state = {
+    isHidden: new Map(this.props.apikeys.map(apikey => [apikey, true]))
+  }
+  // eslint-disable-next-line
+  onVisibilityButtonClick = (apikey) => {
+    this.setState(prevState => {
+      const {isHidden} = prevState
+      return ({
+        isHidden: new Map(isHidden).set(apikey, !isHidden.get(apikey))
+      })
+    })
+  }
+
   render () {
+    const {isHidden} = this.state
     const {apikeys, dispatchApikeyRevoke} = this.props
 
     if (!apikeys.length) {
@@ -13,7 +29,7 @@ export class ApiKeyList extends Component {
     return (
       <ol>
         {apikeys.map(apikey => <li key={apikey}>
-          <ApiKey apikey={apikey} />
+          <ApiKey apikey={apikey} isHidden={isHidden.get(apikey)} onVisibilityButtonClick={this.onVisibilityButtonClick} />
           {dispatchApikeyRevoke && <ApiKeyRevokeButton apikey={apikey} dispatchApikeyRevoke={dispatchApikeyRevoke} />}
         </li>)}
       </ol>

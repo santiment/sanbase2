c8c66e45a7d1d236807b66e63237a3842b4c39f0
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-04 13:52:49 +0300

    Revoke button functionality

--- app/src/components/ApiKey/ApiKeyList.js
+++ app/src/components/ApiKey/ApiKeyList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import ApiKey from './ApiKey'
+import ApiKeyRevokeButton from './ApiKeyRevokeButton'
 
 export class ApiKeyList extends Component {
   render () {
-    const {apikeys} = this.props
+    const {apikeys, dispatchApikeyRevoke} = this.props
 
     if (!apikeys.length) {
       return 'You don\'t have any api keys for now'
@@ -11,7 +12,10 @@ export class ApiKeyList extends Component {
 
     return (
       <ol>
-        {apikeys.map(apikey => <ApiKey key={apikey} apikey={apikey} />)}
+        {apikeys.map(apikey => <li key={apikey}>
+          <ApiKey apikey={apikey} />
+          {dispatchApikeyRevoke && <ApiKeyRevokeButton apikey={apikey} dispatchApikeyRevoke={dispatchApikeyRevoke} />}
+        </li>)}
       </ol>
     )
   }
--- app/src/pages/Account/AccountApiKeyForm.js
+++ app/src/pages/Account/AccountApiKeyForm.js
@@ -3,7 +3,7 @@ import { Divider } from 'semantic-ui-react'
 import ApiKeyGenerateButton from '../../components/ApiKey/ApiKeyGenerateButton'
 import ApiKeyList from '../../components/ApiKey/ApiKeyList'
 
-const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate }) => {
+const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate, dispatchApikeyRevoke }) => {
   return (
     <Fragment>
       <h3>API Key</h3>
@@ -11,7 +11,7 @@ const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate }) => {
       <div className='api-key'>
         <p>Here will be your API key</p>
         <ApiKeyGenerateButton dispatchApikeysGenerate={dispatchApikeysGenerate} />
-        <ApiKeyList apikeys={apikeys} />
+        <ApiKeyList apikeys={apikeys} dispatchApikeyRevoke={dispatchApikeyRevoke} />
       </div>
     </Fragment>
   )

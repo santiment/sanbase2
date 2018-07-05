702b705f6a6ad2c06083489a9a70dd7b15ad6a32
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-05 12:07:28 +0300

    Code prettify

--- app/src/components/ApiKey/ApiKey.js
+++ app/src/components/ApiKey/ApiKey.js
@@ -1,9 +1,9 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import { Input } from 'semantic-ui-react'
 import copy from 'copy-to-clipboard'
 import './ApiKey.css'
 
-const ApiKey = ({apikey, isHidden, onVisibilityButtonClick}) => {
+const ApiKey = ({ apikey, isHidden, onVisibilityButtonClick }) => {
   return (
     <Fragment>
       <form className='ApiKey'>
@@ -13,8 +13,20 @@ const ApiKey = ({apikey, isHidden, onVisibilityButtonClick}) => {
           value={isHidden ? '•'.repeat(apikey.length) : apikey}
           className='ApiKey__input'
         />
-        <button type='button' onClick={() => onVisibilityButtonClick(apikey)} className='ApiKey__btn' ><i className={`icon eye ${isHidden ? '' : 'slash'}`} /></button>
-        <button type='button' onClick={() => copy(apikey)} className='ApiKey__btn ApiKey__btn_copy'><i className='icon copy' /></button>
+        <button
+          type='button'
+          onClick={() => onVisibilityButtonClick(apikey)}
+          className='ApiKey__btn'
+        >
+          <i className={`icon eye ${isHidden ? '' : 'slash'}`} />
+        </button>
+        <button
+          type='button'
+          onClick={() => copy(apikey)}
+          className='ApiKey__btn ApiKey__btn_copy'
+        >
+          <i className='icon copy' />
+        </button>
       </form>
     </Fragment>
   )
--- app/src/components/ApiKey/ApiKeyGenerateButton.js
+++ app/src/components/ApiKey/ApiKeyGenerateButton.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { graphql } from 'react-apollo'
 import { Button } from 'semantic-ui-react'
-import {generateApikeyGQL} from './apikeyGQL'
+import { generateApikeyGQL } from './apikeyGQL'
 
-const ApiKeyGenerateButton = ({generateApikey, dispatchApikeysGenerate}) => {
+const ApiKeyGenerateButton = ({ generateApikey, dispatchApikeysGenerate }) => {
   return (
-    <Button positive onClick={() => generateApikey().then(({data: {generateApikey}}) => dispatchApikeysGenerate(generateApikey.apikeys)).catch(console.log)} >Generate new API Key</Button>
+    <Button
+      positive
+      onClick={() =>
+        generateApikey()
+          .then(({ data: { generateApikey } }) =>
+            dispatchApikeysGenerate(generateApikey.apikeys)
+          )
+          .catch(console.log)}
+    >
+      Generate new API Key
+    </Button>
   )
 }
 
-export default graphql(generateApikeyGQL, { name: 'generateApikey' })(ApiKeyGenerateButton)
+export default graphql(generateApikeyGQL, { name: 'generateApikey' })(
+  ApiKeyGenerateButton
+)

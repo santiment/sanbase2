54c0fafe697d2f7142f041d6f161c6927470c3f5
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-05 12:07:28 +0300

    Code prettify

--- app/src/components/ApiKey/ApiKeyList.js
+++ app/src/components/ApiKey/ApiKeyList.js
@@ -8,37 +8,52 @@ export class ApiKeyList extends Component {
   state = {
     isHidden: new Map(this.props.apikeys.map(apikey => [apikey, true]))
   }
-  componentWillReceiveProps ({apikeys}) {
-    const {isHidden} = this.state
+  componentWillReceiveProps ({ apikeys }) {
+    const { isHidden } = this.state
     if (apikeys.length !== isHidden.size) {
       this.setState({
-        isHidden: new Map(apikeys.map(apikey => {
-          const isPreviouslyHidden = isHidden.get(apikey)
-          return [apikey, isPreviouslyHidden !== undefined ? isPreviouslyHidden : true]
-        }))
+        isHidden: new Map(
+          apikeys.map(apikey => {
+            const isPreviouslyHidden = isHidden.get(apikey)
+            return [
+              apikey,
+              isPreviouslyHidden !== undefined ? isPreviouslyHidden : true
+            ]
+          })
+        )
       })
     }
   }
   // eslint-disable-next-line
-  onVisibilityButtonClick = (apikey) => {
+  onVisibilityButtonClick = apikey => {
     this.setState(prevState => {
-      const {isHidden} = prevState
-      return ({
+      const { isHidden } = prevState
+      return {
         isHidden: new Map(isHidden).set(apikey, !isHidden.get(apikey))
-      })
+      }
     })
   }
 
   render () {
-    const {isHidden} = this.state
-    const {apikeys, dispatchApikeyRevoke} = this.props
+    const { isHidden } = this.state
+    const { apikeys, dispatchApikeyRevoke } = this.props
 
     return (
-      <ol className='ApiKeyList' >
-        {apikeys.map(apikey => <li className='ApiKeyList__item' key={apikey}>
-          <ApiKey apikey={apikey} isHidden={isHidden.get(apikey)} onVisibilityButtonClick={this.onVisibilityButtonClick} />
-          {dispatchApikeyRevoke && <ApiKeyRevokeButton apikey={apikey} dispatchApikeyRevoke={dispatchApikeyRevoke} />}
-        </li>)}
+      <ol className='ApiKeyList'>
+        {apikeys.map(apikey => (
+          <li className='ApiKeyList__item' key={apikey}>
+            <ApiKey
+              apikey={apikey}
+              isHidden={isHidden.get(apikey)}
+              onVisibilityButtonClick={this.onVisibilityButtonClick}
+            />
+            {dispatchApikeyRevoke &&
+              <ApiKeyRevokeButton
+                apikey={apikey}
+                dispatchApikeyRevoke={dispatchApikeyRevoke}
+              />}
+          </li>
+        ))}
       </ol>
     )
   }

23f5582b5c9aad04ed1bdf12fbf3873eddb3b8b7
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-05 12:07:28 +0300

    Code prettify

--- app/src/components/ApiKey/ApiKeyRevokeButton.js
+++ app/src/components/ApiKey/ApiKeyRevokeButton.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { graphql } from 'react-apollo'
 import { Button } from 'semantic-ui-react'
-import {revokeApikeyGQL} from './apikeyGQL'
+import { revokeApikeyGQL } from './apikeyGQL'
 
-const ApiKeyRevokeButton = ({apikey, revokeApikey, dispatchApikeyRevoke}) => {
+const ApiKeyRevokeButton = ({ apikey, revokeApikey, dispatchApikeyRevoke }) => {
   return (
-    <Button negative onClick={() =>
-      revokeApikey({variables: {apikey}})
-    .then(({data: {revokeApikey}}) => dispatchApikeyRevoke(revokeApikey.apikeys)).catch(console.log)} >Revoke</Button>
+    <Button
+      negative
+      onClick={() =>
+        revokeApikey({ variables: { apikey } })
+          .then(({ data: { revokeApikey } }) =>
+            dispatchApikeyRevoke(revokeApikey.apikeys)
+          )
+          .catch(console.log)}
+    >
+      Revoke
+    </Button>
   )
 }
 
-export default graphql(revokeApikeyGQL, { name: 'revokeApikey' })(ApiKeyRevokeButton)
+export default graphql(revokeApikeyGQL, { name: 'revokeApikey' })(
+  ApiKeyRevokeButton
+)
--- app/src/pages/Account/AccountApiKeyForm.js
+++ app/src/pages/Account/AccountApiKeyForm.js
@@ -3,14 +3,23 @@ import { Divider } from 'semantic-ui-react'
 import ApiKeyGenerateButton from '../../components/ApiKey/ApiKeyGenerateButton'
 import ApiKeyList from '../../components/ApiKey/ApiKeyList'
 
-const AccountApiKeyForm = ({ apikeys, dispatchApikeysGenerate, dispatchApikeyRevoke }) => {
+const AccountApiKeyForm = ({
+  apikeys,
+  dispatchApikeysGenerate,
+  dispatchApikeyRevoke
+}) => {
   return (
     <Fragment>
       <h3>API Keys</h3>
       <Divider />
       <div className='api-key'>
-        <ApiKeyGenerateButton dispatchApikeysGenerate={dispatchApikeysGenerate} />
-        <ApiKeyList apikeys={apikeys} dispatchApikeyRevoke={dispatchApikeyRevoke} />
+        <ApiKeyGenerateButton
+          dispatchApikeysGenerate={dispatchApikeysGenerate}
+        />
+        <ApiKeyList
+          apikeys={apikeys}
+          dispatchApikeyRevoke={dispatchApikeyRevoke}
+        />
       </div>
     </Fragment>
   )

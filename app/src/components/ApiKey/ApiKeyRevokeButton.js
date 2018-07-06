e6ebf20c9a40c6509c5a7eaa3b4e545b3571be43
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-07 00:42:45 +0300

    API key revoke epic

--- app/src/components/ApiKey/ApiKeyRevokeButton.js
+++ app/src/components/ApiKey/ApiKeyRevokeButton.js
@@ -1,25 +1,18 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
 import { Button } from 'semantic-ui-react'
-import { revokeApikeyGQL } from './apikeyGQL'
 
-const ApiKeyRevokeButton = ({ apikey, revokeApikey, dispatchApikeyRevoke, onRevokeButtonClick }) => {
+const ApiKeyRevokeButton = ({ apikey, dispatchApikeyRevoke, onRevokeButtonClick }) => {
   return (
     <Button
       negative
-      onClick={() =>
-        revokeApikey({ variables: { apikey } })
-          .then(({ data: { revokeApikey } }) => {
-            dispatchApikeyRevoke(revokeApikey.apikeys)
-            onRevokeButtonClick(apikey)
-          })
-          .catch(console.log)}
+      onClick={() => {
+        dispatchApikeyRevoke(apikey)
+        onRevokeButtonClick(apikey)
+      }}
     >
       Revoke
     </Button>
   )
 }
 
-export default graphql(revokeApikeyGQL, { name: 'revokeApikey' })(
-  ApiKeyRevokeButton
-)
+export default ApiKeyRevokeButton

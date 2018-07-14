fe23d53b845add64e22a8f9bb0982f705f8ac046
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-14 11:56:06 +0300

    Account Settings appearance form

--- app/src/pages/Account/AccountSessions.js
+++ app/src/pages/Account/AccountSessions.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react'
-import { Divider, Button, Checkbox } from 'semantic-ui-react'
+import { Divider, Button } from 'semantic-ui-react'
 
-const AccountSessions = ({ onLogoutBtnClick, onColorModeToggleChange, isNightModeEnabled }) => {
+const AccountSessions = ({ onLogoutBtnClick }) => {
   return (
     <Fragment>
       <h3>Sessions</h3>
       <Divider />
       <div className='account-control'>
-        <p>Night Mode</p>
-        <Checkbox toggle onChange={onColorModeToggleChange} defaultChecked={isNightModeEnabled} />
         <p>Your current session</p>
         <Button basic color='red' onClick={onLogoutBtnClick}>Log out</Button>
       </div>

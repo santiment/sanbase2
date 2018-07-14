b18b3f0d622296f6e6945acad52e4ec0411c9732
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-14 11:56:06 +0300

    Account Settings appearance form

---
+++ app/src/pages/Account/AccountAppearance.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import { Divider, Checkbox } from 'semantic-ui-react'
+
+const AccountAppearance = ({ onColorModeToggleChange, isNightModeEnabled }) => {
+  return (
+    <Fragment>
+      <h3>Appearance</h3>
+      <Divider />
+      <div className='account-control'>
+        <p>Night Mode</p>
+        <Checkbox toggle onChange={onColorModeToggleChange} defaultChecked={isNightModeEnabled} />
+      </div>
+    </Fragment>
+  )
+}
+
+export default AccountAppearance

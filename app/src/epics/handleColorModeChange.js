4c5100e4a15ec09f2283e0bafa0a5e196adfd152
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-17 14:40:58 +0300

    Epic prettify

--- app/src/epics/handleColorModeChange.js
+++ app/src/epics/handleColorModeChange.js
@@ -4,12 +4,18 @@ import {
   APP_USER_COLOR_MODE_SAVE
 } from './../actions/types'
 
-const handleColorModeChange = (action$, store, { client }) =>
-  action$.ofType(USER_TOGGLE_COLOR_MODE).switchMap(() => Observable.of(document.body.classList.toggle('night-mode')))
-  .debounceTime(1000)
-  .mergeMap(isNightModeEnabled => Observable.of({
-    type: APP_USER_COLOR_MODE_SAVE,
-    payload: isNightModeEnabled
-  }))
+const handleColorModeChange = action$ =>
+  action$
+    .ofType(USER_TOGGLE_COLOR_MODE)
+    .switchMap(() =>
+      Observable.of(document.body.classList.toggle('night-mode'))
+    )
+    .debounceTime(1000)
+    .mergeMap(isNightModeEnabled =>
+      Observable.of({
+        type: APP_USER_COLOR_MODE_SAVE,
+        payload: isNightModeEnabled
+      })
+    )
 
 export default handleColorModeChange

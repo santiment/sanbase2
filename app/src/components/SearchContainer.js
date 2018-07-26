237c8748e5e3f577488f376579cb59630693832f
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-07-26 18:04:48 +0300

    Add keyboard shortcut for search

--- app/src/components/SearchContainer.js
+++ app/src/components/SearchContainer.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { graphql } from 'react-apollo'
 import { compose } from 'recompose'
 import { allProjectsForSearchGQL } from './../pages/Projects/allProjectsGQL'
 import { getAll } from './../pages/Projects/projectSelectors'
+import * as actions from './../actions/types'
 import Search from './Search'
 
-const SearchContainer = ({ history, projects = [] }) => {
+const SearchContainer = ({ history, projects = [], isFocused, resetFocus }) => {
   if (projects.length === 0) {
     return <Search loading onSelectProject={() => {}} projects={[]} />
   }
   return (
     <Search
       onSelectProject={cmcId => history.push(`/projects/${cmcId}`)}
+      focus={isFocused}
+      resetFocus={resetFocus}
       projects={projects}
     />
   )
@@ -22,7 +26,16 @@ const mapDataToProps = ({ allProjects }) => ({
   projects: getAll(allProjects.allProjects)
 })
 
+const mapStateToProps = ({ rootUi }) => ({
+  isFocused: rootUi.isSearchInputFocused
+})
+
+const mapDispatchToProps = dispatch => ({
+  resetFocus: () => dispatch({ type: actions.APP_TOGGLE_SEARCH_FOCUS })
+})
+
 const enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
   withRouter,
   graphql(allProjectsForSearchGQL, {
     name: 'allProjects',

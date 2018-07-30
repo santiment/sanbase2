42e6a0432ec7647cbee831ab7094e04e8b4f1346
Lyudmil Lesinski <lyudmilles@gmail.com>
2018-07-30 15:47:18 +0300

    Add tests remove duplicating code from resolver

--- lib/sanbase_web/graphql/resolvers/market_segment_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/market_segment_resolver.ex
@@ -8,57 +8,48 @@ defmodule SanbaseWeb.Graphql.Resolvers.MarketSegmentResolver do
   alias Sanbase.Repo
 
   def all_market_segments(_parent, _args, _resolution) do
-    market_segments =
-      Repo.all(MarketSegment)
-      |> Repo.preload(:projects)
-      |> Enum.map(fn %{name: name, projects: projects} ->
-        %{
-          name: name,
-          count: Enum.count(projects)
-        }
-      end)
+    filter = fn _ -> true end
+    market_segments = market_segments(filter)
 
     {:ok, market_segments}
   end
 
   def erc20_market_segments(_parent, _args, _resolution) do
-    market_segments =
-      Repo.all(MarketSegment)
-      |> Repo.preload(projects: :infrastructure)
-      |> Enum.filter(fn %{projects: projects} ->
-        Enum.any?(projects, fn project ->
-          not is_nil(project.coinmarketcap_id) and not is_nil(project.main_contract_address) and
-            not is_nil(project.infrastructure) and project.infrastructure.code === "ETH"
-        end)
+    filter = fn %{projects: projects} ->
+      Enum.any?(projects, fn project ->
+        not is_nil(project.coinmarketcap_id) and not is_nil(project.main_contract_address) and
+          not is_nil(project.infrastructure) and project.infrastructure.code === "ETH"
       end)
-      |> Enum.map(fn %{name: name, projects: projects} ->
-        %{
-          name: name,
-          count: Enum.count(projects)
-        }
-      end)
+    end
+
+    market_segments = market_segments(filter)
 
     {:ok, market_segments}
   end
 
   def currencies_market_segments(_parent, _args, _resolution) do
-    market_segments =
-      Repo.all(MarketSegment)
-      |> Repo.preload(projects: :infrastructure)
-      |> Enum.filter(fn %{projects: projects} ->
-        Enum.any?(projects, fn project ->
-          not is_nil(project.coinmarketcap_id) and
-            (is_nil(project.main_contract_address) or
-               (not is_nil(project.infrastructure) and project.infrastructure.code !== "ETH"))
-        end)
+    filter = fn %{projects: projects} ->
+      Enum.any?(projects, fn project ->
+        not is_nil(project.coinmarketcap_id) and
+          (is_nil(project.main_contract_address) or
+             (not is_nil(project.infrastructure) and project.infrastructure.code !== "ETH"))
       end)
-      |> Enum.map(fn %{name: name, projects: projects} ->
-        %{
-          name: name,
-          count: Enum.count(projects)
-        }
-      end)
+    end
+
+    market_segments = market_segments(filter)
 
     {:ok, market_segments}
+  end
+
+  defp market_segments(filter) do
+    Repo.all(MarketSegment)
+    |> Repo.preload(projects: :infrastructure)
+    |> Enum.filter(filter)
+    |> Enum.map(fn %{name: name, projects: projects} ->
+      %{
+        name: name,
+        count: Enum.count(projects)
+      }
+    end)
   end
 end

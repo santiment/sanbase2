fdec38fb98751e1708aae60f926bdd24964c66f4
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-08-02 00:53:55 +0300

    Add navigation for AssetsPage (fix)

---
+++ app/src/pages/assets/AssetsPageNavigation.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { NavLink as Link } from 'react-router-dom'
+import { Button, Checkbox, Popup } from 'semantic-ui-react'
+import './AssetsPageNavigation.css'
+
+const AssetsPageNavigation = ({ isLoggedIn = false }) => {
+  return (
+    <div className='projects-navigation'>
+      <div className='projects-navigation-list'>
+        <Link
+          activeClassName='projects-navigation-list__page-link--active'
+          className='projects-navigation-list__page-link'
+          to={'/assets/erc20'}
+        >
+          ERC20 Projects
+        </Link>
+        <Link
+          activeClassName='projects-navigation-list__page-link--active'
+          className='projects-navigation-list__page-link'
+          to={'/assets/currencies'}
+        >
+          Currencies
+        </Link>
+        {isLoggedIn && (
+          <Link
+            activeClassName='projects-navigation-list__page-link--active'
+            className='projects-navigation-list__page-link'
+            to={'/favorites'}
+          >
+            Favorites
+          </Link>
+        )}
+        <Link
+          activeClassName='projects-navigation-list__page-link--active'
+          className='projects-navigation-list__page-link'
+          to={'/projects/ethereum'}
+        >
+          More data about Ethereum
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default AssetsPageNavigation

e705c360b904281ab3dda42da21b7eab432651db
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-08-01 11:02:36 +0300

    Add TimescaleRepo test config

--- lib/sanbase/blockchain/timescaledb.ex
+++ lib/sanbase/blockchain/timescaledb.ex
@@ -1,20 +1,35 @@
 defmodule Sanbase.Timescaledb do
   defmacro time_bucket(interval) do
     quote do
-      # , unquote(interval))
-      fragment("time_bucket('35 days', timestamp) as san_internal_time_bucket")
+      fragment(
+        "time_bucket(?::interval, timestamp) as dt",
+        unquote(interval)
+      )
     end
   end
 
   defmacro time_bucket() do
     quote do
-      fragment("san_internal_time_bucket")
+      fragment("time_bucket")
     end
   end
 
   defmacro coalesce(left, right) do
     quote do
       fragment("coalesce(?, ?)", unquote(left), unquote(right))
+    end
+  end
+
+  defmacro generate_series(from, to, interval) do
+    quote do
+      fragment(
+        """
+        select generate_series(?, ?, ?::interval)::timestamp AS d
+        """,
+        unquote(from),
+        unquote(to),
+        unquote(interval)
+      )
     end
   end
 end

09ca6540245c6a2965b500283c5e60494a253262
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-08-03 17:47:23 +0300

    Do not run migrations on prod. Rework mix tasks and introduce new ones with _all suffix

--- priv/timescale_repo/migrations/20180803131502_add_exchange_funds_flow.exs
+++ priv/timescale_repo/migrations/20180803131502_add_exchange_funds_flow.exs
@@ -1,5 +1,6 @@
 defmodule Sanbase.TimescaleRepo.Migrations.AddExchangeFundsFlow do
   use Ecto.Migration
+
   def up do
     create table(:eth_exchange_funds_flow, primary_key: false) do
       add(:timestamp, :naive_datetime, primary_key: true)

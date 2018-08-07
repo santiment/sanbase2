09f1a551eb034cb607107084e76e7ac414695de3
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-08-07 13:32:41 +0300

    Polish styles

--- app/src/components/AssetsListPopup/AddNewAssetsListBtn.js
+++ app/src/components/AssetsListPopup/AddNewAssetsListBtn.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Input } from 'semantic-ui-react'
+import './AddNewWatchlistBtn.css'
 
 const initialState = {
   newAssetsListTitle: ''
@@ -26,30 +27,34 @@ class AddNewAssetsList extends React.Component {
   handleAddNewAssetsList = () => {
     const name = this.state.newAssetsListTitle
     if (name && name.length > 0) {
-      this.props.addNewAssetList({ name: this.state.newAssetsListTitle })
+      this.props.addNewAssetList({
+        name: this.state.newAssetsListTitle.toLowerCase()
+      })
     }
   }
 
   render () {
     return (
-      <Input
-        disabled={this.props.assetsListUI.assetsListNewItemPending}
-        value={this.state.newAssetsListTitle}
-        action={{
-          color: 'google plus',
-          labelPosition: 'left',
-          icon: 'plus',
-          content: 'create',
-          onClick: this.handleAddNewAssetsList
-        }}
-        onChange={this.handleOnChange}
-        onKeyPress={(e, data) => {
-          if (e.key === 'Enter') {
-            this.handleAddNewAssetsList()
-          }
-        }}
-        actionPosition='left'
-      />
+      <div className='create-new-watchlist-btn'>
+        <Input
+          disabled={this.props.assetsListUI.assetsListNewItemPending}
+          value={this.state.newAssetsListTitle}
+          action={{
+            color: 'google plus',
+            labelPosition: 'left',
+            icon: 'plus',
+            content: 'create',
+            onClick: this.handleAddNewAssetsList
+          }}
+          onChange={this.handleOnChange}
+          onKeyPress={(e, data) => {
+            if (e.key === 'Enter') {
+              this.handleAddNewAssetsList()
+            }
+          }}
+          actionPosition='left'
+        />
+      </div>
     )
   }
 }

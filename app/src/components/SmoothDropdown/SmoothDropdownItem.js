41d493075333e51f7ca0635523e7ac000d34a7bf
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-15 23:35:50 +0300

    Ref as dropdown item

--- app/src/components/SmoothDropdown/SmoothDropdownItem.js
+++ app/src/components/SmoothDropdown/SmoothDropdownItem.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
-import { SmoothDropdownContext } from './SmoothDropdown'
+import { SmoothDropdownContext, createDrop } from './SmoothDropdown'
 
 // const SmoothDropdownItem = ({ trigger, children, id }) => {
 //   const child = (
@@ -20,25 +20,54 @@ import { SmoothDropdownContext } from './SmoothDropdown'
 //     </Fragment>
 //   )
 // }
-const SmoothDropdownItem = ({ trigger, children, id }) => {
-  const child = (
-    <li className='dropdown smooth-dropdown__item' data-id={id}>
-      {children}
-    </li>
-  )
-  return (
-    <SmoothDropdownContext.Consumer>
-      {({ portal, changeDrop }) => (
-        <Fragment>
-          {console.log(portal)}
-          <div onMouseEnter={() => changeDrop(id)} data-id={id}>
-            {trigger}
-          </div>
-          {portal && ReactDOM.createPortal(child, portal)}
-        </Fragment>
-      )}
-    </SmoothDropdownContext.Consumer>
-  )
+
+export class SmoothDropdownItem extends Component {
+  myRef = React.createRef()
+
+  render () {
+    const { trigger, children } = this.props
+    const child = (
+      <li className='dropdown smooth-dropdown__item' ref={this.myRef}>
+        {children}
+      </li>
+    )
+    console.log(this.myRef)
+    createDrop(trigger, this.myRef.current)
+    return (
+      <SmoothDropdownContext.Consumer>
+        {({ portal, changeDrop }) => (
+          <Fragment>
+            {/* {console.log(portal)} */}
+            <div onMouseEnter={() => changeDrop(trigger)}>{trigger}</div>
+            {portal && ReactDOM.createPortal(child, portal)}
+          </Fragment>
+        )}
+      </SmoothDropdownContext.Consumer>
+    )
+  }
 }
+
+// const SmoothDropdownItem = ({ trigger, children, id }) => {
+//   const child = (
+//     <li className='dropdown smooth-dropdown__item' data-id={id}>
+//       {children}
+//     </li>
+//   )
+//   console.log(child)
+//   createDrop(trigger, child)
+//   return (
+//     <SmoothDropdownContext.Consumer>
+//       {({ portal, changeDrop }) => (
+//         <Fragment>
+//           {/* {console.log(portal)} */}
+//           <div onMouseEnter={() => changeDrop(trigger)} data-id={id}>
+//             {trigger}
+//           </div>
+//           {portal && ReactDOM.createPortal(child, portal)}
+//         </Fragment>
+//       )}
+//     </SmoothDropdownContext.Consumer>
+//   )
+// }
 
 export default SmoothDropdownItem

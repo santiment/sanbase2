71161f3f61e0e2f48713dbb623b6acf3875d655f
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-15 22:01:54 +0300

    currentDrop change success

--- app/src/components/SmoothDropdown/SmoothDropdownItem.js
+++ app/src/components/SmoothDropdown/SmoothDropdownItem.js
@@ -2,15 +2,42 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { SmoothDropdownContext } from './SmoothDropdown'
 
-const SmoothDropdownItem = ({ trigger, children }) => {
-  const child = <li className='dropdown smooth-dropdown__item'>{children}</li>
+// const SmoothDropdownItem = ({ trigger, children, id }) => {
+//   const child = (
+//     <li className='dropdown smooth-dropdown__item' data-id={id}>{children}</li>
+//   )
+//   return (
+//     <Fragment>
+//       <div
+//         onMouseEnter={evt => console.log(evt.currentTarget.dataset.id)}
+//         data-id={id}
+//       >
+//         {trigger}
+//       </div>
+//       <SmoothDropdownContext.Consumer>
+//         {portal => ReactDOM.createPortal(child, portal)}
+//       </SmoothDropdownContext.Consumer>
+//     </Fragment>
+//   )
+// }
+const SmoothDropdownItem = ({ trigger, children, id }) => {
+  const child = (
+    <li className='dropdown smooth-dropdown__item' data-id={id}>
+      {children}
+    </li>
+  )
   return (
-    <Fragment>
-      <div onMouseEnter={evt => console.log(evt)}>{trigger}</div>
-      <SmoothDropdownContext.Consumer>
-        {portal => ReactDOM.createPortal(child, portal)}
-      </SmoothDropdownContext.Consumer>
-    </Fragment>
+    <SmoothDropdownContext.Consumer>
+      {({ portal, changeDrop }) => (
+        <Fragment>
+          {console.log(portal)}
+          <div onMouseEnter={() => changeDrop(id)} data-id={id}>
+            {trigger}
+          </div>
+          {portal && ReactDOM.createPortal(child, portal)}
+        </Fragment>
+      )}
+    </SmoothDropdownContext.Consumer>
   )
 }
 

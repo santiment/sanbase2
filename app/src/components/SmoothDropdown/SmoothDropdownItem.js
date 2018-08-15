3824e64743675cd015d1d0e7be65cfb1321d3a77
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-15 13:45:38 +0300

    Trigger element render. onMouseEnter wrapper

--- app/src/components/SmoothDropdown/SmoothDropdownItem.js
+++ app/src/components/SmoothDropdown/SmoothDropdownItem.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { SmoothDropdownContext } from './SmoothDropdown'
 
-const SmoothDropdownItem = ({ children }) => (
-  <SmoothDropdownContext.Consumer>
-    {portal => ReactDOM.createPortal(children, portal)}
-  </SmoothDropdownContext.Consumer>
+const SmoothDropdownItem = ({ trigger, children }) => (
+  <Fragment>
+    <div onMouseEnter={evt => console.log(evt)}>{trigger}</div>
+    <SmoothDropdownContext.Consumer>
+      {portal => ReactDOM.createPortal(children, portal)}
+    </SmoothDropdownContext.Consumer>
+  </Fragment>
 )
 
 export default SmoothDropdownItem

a1ddd1c2e7c5e72d0f02ffcea2b9cb3bf661bf2a
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-15 18:25:36 +0300

    Markup and classes

--- app/src/components/SmoothDropdown/SmoothDropdownItem.js
+++ app/src/components/SmoothDropdown/SmoothDropdownItem.js
@@ -2,13 +2,16 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { SmoothDropdownContext } from './SmoothDropdown'
 
-const SmoothDropdownItem = ({ trigger, children }) => (
-  <Fragment>
-    <div onMouseEnter={evt => console.log(evt)}>{trigger}</div>
-    <SmoothDropdownContext.Consumer>
-      {portal => ReactDOM.createPortal(children, portal)}
-    </SmoothDropdownContext.Consumer>
-  </Fragment>
-)
+const SmoothDropdownItem = ({ trigger, children }) => {
+  const child = <li className='dropdown smooth-dropdown__item'>{children}</li>
+  return (
+    <Fragment>
+      <div onMouseEnter={evt => console.log(evt)}>{trigger}</div>
+      <SmoothDropdownContext.Consumer>
+        {portal => ReactDOM.createPortal(child, portal)}
+      </SmoothDropdownContext.Consumer>
+    </Fragment>
+  )
+}
 
 export default SmoothDropdownItem

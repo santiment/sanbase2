bb76a137495652799c97f9706ff32c17f6906774
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-16 15:33:05 +0300

    New arch

--- app/src/components/SmoothDropdown/SmoothDropdown.js
+++ app/src/components/SmoothDropdown/SmoothDropdown.js
@@ -3,14 +3,15 @@ import './SmoothDropdown.css'
 
 export const SmoothDropdownContext = React.createContext({
   portal: document.createElement('ul'),
-  currentDrop: null,
-  changeDrop: id => {}
+  currentTrigger: null,
+  changeDrop: id => {},
+  hideDrop: () => {}
 })
 
 const dropdowns = new WeakMap()
 
 export const createDrop = (trigger, dropdown) =>
-  dropdowns.set(trigger, dropdown)
+  trigger && dropdowns.set(trigger, dropdown)
 
 // const SmoothDropdown = ({ id, children }) => {
 //   const portal = document.createElement('ul')
@@ -32,34 +33,80 @@ class SmoothDropdown extends Component {
   myRef = React.createRef()
 
   state = {
-    currentDrop: null
+    currentTrigger: null
   }
 
   // changeDrop = id => {
   //   console.log(id)
   //   this.setState(prevState => ({
   //     ...prevState,
-  //     currentDrop: id
+  //     currentTrigger: id
   //   }))
   // }
   changeDrop = trigger => {
     // console.log(trigger)
-    console.log(dropdowns.has(trigger))
-    this.setState(() => ({
-      currentDrop: trigger
-    }))
+    this.setState(() => {
+      this.calculateGeometry()
+      return {
+        currentTrigger: trigger
+      }
+    })
+  }
+
+  hideDrop = () => {
+    const { currentTrigger } = this.state
+    const selectedDropdown = dropdowns.get(currentTrigger)
+    if (!selectedDropdown) return
+    selectedDropdown.classList.remove('active')
+    selectedDropdown.classList.remove('is-dropdown-visible')
+  }
+
+  calculateGeometry () {
+    const { currentTrigger } = this.state
+    const selectedDropdown = dropdowns.get(currentTrigger)
+    if (!selectedDropdown) return
+    const selectedDropdownHeight = selectedDropdown.offsetHeight
+    const selectedDropdownWidth = selectedDropdown.firstElementChild.clientWidth
+    // .innerWidth()
+    const selectedDropdownLeft =
+      currentTrigger.getBoundingClientRect().left +
+      currentTrigger.offsetWidth / 2 -
+      selectedDropdownWidth / 2
+
+    // console.log(
+    //   selectedDropdownHeight,
+    //   selectedDropdownWidth,
+    //   selectedDropdownLeft
+    // )
+    this.updateDropdown(
+      selectedDropdown,
+      selectedDropdownHeight,
+      selectedDropdownWidth,
+      selectedDropdownLeft
+    )
+
+    selectedDropdown.classList.add('is-dropdown-visible')
+    selectedDropdown.classList.add('active')
+  }
+
+  updateDropdown (dropdown, height, width, left) {
+    const dropdownList = document.querySelector('.dropdown-list')
+    dropdownList.style.transform = 'translateX(' + left + 'px)'
+    dropdownList.style.width = width + 'px'
+    dropdownList.style.height = height + 'px'
   }
 
   render () {
     const { children, id } = this.props
-    const { currentDrop } = this.state
+    const { currentTrigger } = this.state
     // portal.classList.add('smooth-dropdown__list')
     return (
       <SmoothDropdownContext.Provider
         value={{
           ...this.state,
           portal: this.myRef.current,
-          changeDrop: this.changeDrop
+          changeDrop: this.changeDrop,
+          hideDrop: this.hideDrop
         }}
       >
         {children('tester')}

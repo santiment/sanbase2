703af3c4d93e6e5e384e58b3e222a6353dd489f5
Lyudmil Lesinski <lyudmilles@gmail.com>
2018-08-22 13:00:19 +0300

    Change auth criteria
    Changes the authentication criteria for topic_search, social_volume and
    erc20_exchange_funds_flow. User needs to be logged in and have some SAN
    tokens in order to see data from the last 24 hours or older than 3
    months.

--- lib/sanbase_web/graphql/schema.ex
+++ lib/sanbase_web/graphql/schema.ex
@@ -363,6 +363,8 @@ defmodule SanbaseWeb.Graphql.Schema do
       arg(:from, non_null(:datetime))
       arg(:to, non_null(:datetime))
 
+      middleware(ApiTimeframeRestriction)
+
       cache_resolve(&TechIndicatorsResolver.erc20_exchange_funds_flow/3)
     end
 
@@ -471,10 +473,7 @@ defmodule SanbaseWeb.Graphql.Schema do
       arg(:interval, non_null(:string), default_value: "1d")
       arg(:social_volume_type, non_null(:social_volume_type))
 
-      middleware(MultipleAuth, [
-        {JWTAuth, san_tokens: 1000},
-        {ApikeyAuth, san_tokens: 1000}
-      ])
+      middleware(ApiTimeframeRestriction)
 
       complexity(&TechIndicatorsComplexity.social_volume/3)
       resolve(&TechIndicatorsResolver.social_volume/3)
@@ -507,10 +506,7 @@ defmodule SanbaseWeb.Graphql.Schema do
       arg(:to, :datetime, default_value: DateTime.utc_now())
       arg(:interval, non_null(:string), default_value: "1d")
 
-      middleware(MultipleAuth, [
-        {JWTAuth, san_tokens: 1000},
-        {ApikeyAuth, san_tokens: 1000}
-      ])
+      middleware(ApiTimeframeRestriction)
 
       complexity(&TechIndicatorsComplexity.topic_search/3)
       resolve(&TechIndicatorsResolver.topic_search/3)

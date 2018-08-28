ff2a792f68ea7fc2e0e5e2e523a280fcd45f3c9e
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-28 12:53:24 +0300

    Total market cap widget

--- app/src/App.js
+++ app/src/App.js
@@ -31,6 +31,7 @@ import GDPRModal from './components/GDPRModal.js'
 import ApiDocs from './components/ApiDocs'
 import ApiExplorer from './components/ApiExplorer'
 import AssetsPage from './pages/assets/AssetsPage'
+import TotalMarketcapWidget from './components/TotalMarketcapWidget/TotalMarketcapWidget'
 import './App.css'
 
 const LoadableDetailedPage = Loadable({
@@ -75,6 +76,7 @@ export const App = ({
   hasUsername
 }) => (
   <div className='App'>
+    <TotalMarketcapWidget />
     {isOffline && (
       <FadeInDown
         className='offline-status-message'

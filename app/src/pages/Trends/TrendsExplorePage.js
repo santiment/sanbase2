2e9a8996b9294070551a9035fb34d9be236634a9
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-28 20:09:14 +0300

    GraphQL bug fix. Merging sources to one Map

--- app/src/pages/Trends/TrendsExplorePage.js
+++ app/src/pages/Trends/TrendsExplorePage.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import { trendsExploreGQL } from '../../components/Trends/trendsExploreGQL'
+import TrendsExploreChart from '../../components/Trends/TrendsExploreChart'
 
-const TrendsExplorePage = props => {
-  return <div>{console.log(props)}</div>
+const TrendsExplorePage = ({ data: { topicSearch = {} } }) => {
+  return (
+    <div>
+      {/* {console.log(topicSearch)} */}
+      <TrendsExploreChart sources={topicSearch.chartsData} />
+    </div>
+  )
 }
 
-export default TrendsExplorePage
+export default graphql(trendsExploreGQL, {
+  options: ({ match }) => {
+    return {
+      variables: {
+        searchText: match.params.topic
+      }
+    }
+  }
+})(TrendsExplorePage)

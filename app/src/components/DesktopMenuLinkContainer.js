2eef0c53212daa6ee55ec718c0ea9191382433d1
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-08-28 12:56:11 +0300

    Update new look of top menu subitems

--- app/src/components/DesktopMenuLinkContainer.js
+++ app/src/components/DesktopMenuLinkContainer.js
@@ -4,18 +4,22 @@ import { Link } from 'react-router-dom'
 import linkIcon from './../assets/top_menu_insights.svg'
 import './DesktopMenuLinkContainer.css'
 
-const DesktopMenuLinkContainer = ({ title, description, linkIcon, to }) => (
-  <Link to={to} className='link-container'>
-    <img
-      className='link-icon'
-      src={require(`../assets/top_menu_${linkIcon}.svg`)}
-    />
-    <div className='link-text'>
-      <h3 className='link-title'>{title}</h3>
-      <p className='link-description'>{description}</p>
-    </div>
-  </Link>
-)
+const DesktopMenuLinkContainer = ({ title, description, linkIcon, to }) => {
+  const Component = /^https?:\/\//.test(to) ? 'a' : Link
+
+  return (
+    <Component href={to} to={to} className='link-container'>
+      <img
+        className='link-icon'
+        src={require(`../assets/top_menu_${linkIcon}.svg`)}
+      />
+      <div className='link-text'>
+        <h3 className='link-title'>{title}</h3>
+        <p className='link-description'>{description}</p>
+      </div>
+    </Component>
+  )
+}
 
 DesktopMenuLinkContainer.propTypes = {
   title: PropTypes.string.isRequired,

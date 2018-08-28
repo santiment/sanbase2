4306f493c288527c1514382ab35634136e04a9d2
DmitriVanGuard <dimavanguard@gmail.com>
2018-08-28 19:37:19 +0300

    Trends and Explore page init. TrendsForm and TrendsExplore graphql query

---
+++ app/src/components/Trends/trendsExploreGQL.js
@@ -0,0 +1,28 @@
+import gql from 'graphql-tag'
+
+// TODO: from and to parameters
+export const trendsExploreGQL = gql`
+query topicSearch($searchText: String!){
+  topicSearch(
+    sources: [TELEGRAM, PROFESSIONAL_TRADERS_CHAT, REDDIT],
+    searchText: $searchText,
+    from: "2018-08-01T12:00:00Z",
+    to: "2018-08-15T12:00:00Z",
+    interval: "6h"
+  ) {
+    chartsData {
+      telegram {
+        mentionsCount,
+        datetime
+      }
+      professionalTradersChat {
+        mentionsCount,
+        datetime
+      }
+      reddit {
+        mentionsCount,
+        datetime
+      }
+    }
+  }
+`

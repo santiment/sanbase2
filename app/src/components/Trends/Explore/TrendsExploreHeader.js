9cba7db8592cb907c97929ae9d3d4df8f526b3ce
DmitriVanGuard <dimavanguard@gmail.com>
2018-09-03 12:57:27 +0300

    Explore route redirect if no params. Explore source filters. Explore page styles

--- app/src/components/Trends/Explore/TrendsExploreHeader.js
+++ app/src/components/Trends/Explore/TrendsExploreHeader.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './TrendsExploreHeader.css'
 import TrendsExamplesItemQuery from '../Examples/TrendsExamplesItemQuery'
-
+import TrendsExploreHeaderCompareNew from './TrendsExploreHeaderCompareNew'
+/*
+borderImage: `linear-gradient(to right, red 33%, green 33%, green 66%, blue 66%)`;
+borderImageSlice: `0% 0% 3%`;
+borderStyle: `solid`;
+ */
 const TrendsExploreHeader = ({ topic }) => {
   return (
     <div className='TrendsExploreHeader'>
@@ -9,9 +14,8 @@ const TrendsExploreHeader = ({ topic }) => {
         <div className='TrendsExploreHeaderTitles__item'>
           <TrendsExamplesItemQuery fontSize='2em' query={topic} />
         </div>
-        <div className='TrendsExploreHeaderCompareNew'>+ New comparison</div>
+        {/* <TrendsExploreHeaderCompareNew /> */}
       </div>
-      {/* <TrendsExploreHeaderCompareNew /> */}
     </div>
   )
 }

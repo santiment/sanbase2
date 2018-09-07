4ae63df56645653c4c6695491c9db04ad3b27035
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-09-07 14:16:48 +0300

    Move ethSpent table to Ethereum page
      Fix Financial block in projec without wallets

--- app/src/pages/Detailed/FinancialsBlock.js
+++ app/src/pages/Detailed/FinancialsBlock.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import { Label } from 'semantic-ui-react'
@@ -62,30 +62,61 @@ const FinancialsBlock = ({
           </div>
         </div>
       )}
-    {ethBalance && (
-      <div
-        className={cx({
-          'row-info wallets': true,
-          'info-disabled': !ethBalance || ethAddresses.length === 0
-        })}
-      >
-        <div>Wallet Balances</div>
-      </div>
-    )}
     {ethAddresses &&
       ethAddresses.length > 0 && (
-        <div className='row-info wallets-balance'>
-          {ethAddresses.map((wallet, index) => (
-            <div key={index}>
-              <div className='wallets-addresses'>
-                <a href={`https://etherscan.io/address/${wallet.address}`}>
-                  {wallet.address}
-                </a>
-                <span>ETH {millify(wallet.balance, 2)}</span>
+        <Fragment>
+          {ethBalance && (
+            <div
+              className={cx({
+                'row-info wallets': true,
+                'info-disabled': !ethBalance || ethAddresses.length === 0
+              })}
+            >
+              <div>Wallet Balances</div>
+            </div>
+          )}
+          {ethAddresses &&
+            ethAddresses.length > 0 && (
+              <div className='row-info wallets-balance'>
+                {ethAddresses.map((wallet, index) => (
+                  <div key={index}>
+                    <div className='wallets-addresses'>
+                      <a
+                        href={`https://etherscan.io/address/${wallet.address}`}
+                      >
+                        {wallet.address}
+                      </a>
+                      <span>ETH {millify(wallet.balance, 2)}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          {ethBalance && (
+            <div
+              className={cx({
+                'row-info': true,
+                'info-disabled': ethAddresses.length === 0 && +ethBalance === 0
+              })}
+            >
+              <div>Total Balance</div>
+              {ethBalance ? `ETH ${millify(ethBalance, 2)}` : 0}
+            </div>
+          )}
+          {ethSpent && (
+            <div
+              className={cx({
+                'row-info': true,
+                'info-disabled': !ethBalance
+              })}
+            >
+              <div>ETH Spent 30d</div>
+              <div style={{ textAlign: 'right' }}>
+                {ethSpent ? `ETH ${millify(ethSpent, 2)}` : 0}
               </div>
             </div>
-          ))}
-        </div>
+          )}
+        </Fragment>
       )}
     {ethBalance && (
       <div

14f1363e4366a52e064e4812ea17c803369378ca
DmitriVanGuard <dimavanguard@gmail.com>
2018-09-07 16:55:58 +0300

    Source filter border bottom

--- app/src/components/Trends/Examples/TrendsExamplesItemTopic.js
+++ app/src/components/Trends/Examples/TrendsExamplesItemTopic.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import './TrendsExamplesItemTopic.css'
 
-import { composeBorderBottomGradient } from '../trendsUtils'
-
-const TrendsExamplesItemTopic = ({
-  topic,
-  selectedSources,
-  fontSize = '2em'
-}) => {
-  console.log(composeBorderBottomGradient(selectedSources))
+const TrendsExamplesItemTopic = ({ topic, fontSize = '2em' }) => {
   return (
     <div className='TrendsExamplesItemTopic' style={{ fontSize }}>
-      <span>
-        {topic}
-        <div
-          className='TrendsExamplesItemTopic__underline'
-          style={{ background: composeBorderBottomGradient(selectedSources) }}
-        />
-      </span>
+      <span>{topic}</span>
     </div>
   )
 }

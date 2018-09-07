ef875c46876a82dbacff42cad8346ecfeff04f8e
DmitriVanGuard <dimavanguard@gmail.com>
2018-09-07 17:38:20 +0300

    Examples now contain different source mentions

--- app/src/components/Trends/Examples/TrendsExamples.js
+++ app/src/components/Trends/Examples/TrendsExamples.js
@@ -4,19 +4,44 @@ import TrendsExamplesItem from './TrendsExamplesItem'
 import { gotoExplore } from '../trendsUtils'
 import './TrendsExamples.css'
 
-const examples = [{ topic: 'crypto' }, { topic: 'btc' }, { topic: 'eth' }]
+const examples = [
+  {
+    topic: 'crypto',
+    settings: {
+      interval: '1w',
+      sources: ['merged']
+    }
+  },
+  {
+    topic: 'btc',
+    settings: {
+      interval: '6m',
+      sources: ['telegram', 'reddit']
+    }
+  },
+  {
+    topic: 'eth',
+    settings: {
+      interval: '3m',
+      sources: ['professionalTradersChat']
+    }
+  }
+]
 
 export class TrendsExamples extends Component {
   handleExampleClick = ({ currentTarget }) => {
-    this.props.gotoExplore(currentTarget.dataset.topic)
+    this.props.gotoExplore(
+      currentTarget.dataset.topic + '?' + currentTarget.dataset.settings
+    )
   }
   render () {
     return (
       <ul className='TrendsExamples'>
-        {examples.map(({ topic }) => (
+        {examples.map(({ topic, settings }) => (
           <TrendsExamplesItem
             key={topic}
             topic={topic}
+            settings={settings}
             onClick={this.handleExampleClick}
           />
         ))}

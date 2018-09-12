08916a47f763d253a9a505a9c213b39acb8ee28f
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-09-12 16:26:22 +0300

    Edit insight - change moderation_comment and state.

--- lib/sanbase_web/admin/voting/post.ex
+++ lib/sanbase_web/admin/voting/post.ex
@@ -4,6 +4,10 @@ defmodule Sanbase.ExAdmin.Voting.Post do
   alias Sanbase.Voting.Post
 
   register_resource Sanbase.Voting.Post do
+    create_changeset(:create_changeset)
+    update_changeset(:update_changeset)
+    action_items(only: [:show, :edit, :delete])
+
     form post do
       inputs do
         input(post, :title)

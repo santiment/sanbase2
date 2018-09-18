5fa6e98747ebb4635048063b93117b343896b925
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2018-09-18 15:59:41 +0300

    Add open,high,low,close price values for interval

--- lib/sanbase_web/graphql/schema/price_types.ex
+++ lib/sanbase_web/graphql/schema/price_types.ex
@@ -9,4 +9,16 @@ defmodule SanbaseWeb.Graphql.PriceTypes do
     field(:volume, :float)
     field(:ticker, :string)
   end
+
+  object :ohlcv do
+    field(:price_points, list_of(:price_point))
+    field(:open_price_usd, :float)
+    field(:high_price_usd, :float)
+    field(:low_price_usd, :float)
+    field(:close_price_usd, :float)
+    field(:open_price_btc, :float)
+    field(:high_price_btc, :float)
+    field(:low_price_btc, :float)
+    field(:close_price_btc, :float)
+  end
 end

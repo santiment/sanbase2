c9074f9a756b2e1d00c471dceb14728eb2dd0c62
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-09-24 09:54:57 +0300

    Added help message for Trends

--- app/src/components/Trends/TrendsReChart.js
+++ app/src/components/Trends/TrendsReChart.js
@@ -10,8 +10,6 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Pie,
-  PieChart,
   Cell
 } from 'recharts'
 import { formatNumber } from './../../utils/formatting'
@@ -39,7 +37,7 @@ const displayLoadingState = branch(
   renderComponent(Loading)
 )
 
-const TrendsReChart = ({ chartsMeta = {}, pieData = [], merged }) => (
+const TrendsReChart = ({ chartsMeta = {}, merged }) => (
   <div className='TrendsExploreChart'>
     {Object.keys(chartsMeta).map(key => (
       <ResponsiveContainer key={key} width='100%' height={300}>
@@ -94,22 +92,6 @@ const TrendsReChart = ({ chartsMeta = {}, pieData = [], merged }) => (
         </ComposedChart>
       </ResponsiveContainer>
     ))}
-    <br />
-    <ResponsiveContainer width='100%' height={220}>
-      <PieChart>
-        <Pie
-          dataKey='value'
-          label={({ name, value }) => `${name}: ${value}`}
-          data={pieData}
-          outerRadius={80}
-          fill='#8884d8'
-        >
-          {pieData.map((entry, index) => (
-            <Cell key={index} fill={chartsMeta[entry.name].color} />
-          ))}
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
   </div>
 )
 
@@ -145,48 +127,7 @@ export default compose(
       key: 'datetime'
     })
 
-    const pieData = merged
-      .reduce(
-        (acc, val) => {
-          if (val.telegram) {
-            acc[0] = {
-              ...acc[0],
-              value: acc[0].value + val.telegram
-            }
-          }
-          if (val.reddit) {
-            acc[1] = {
-              ...acc[1],
-              value: acc[1].value + val.reddit
-            }
-          }
-          if (val.professional_traders_chat) {
-            acc[2] = {
-              ...acc[2],
-              value: acc[2].value + val.professional_traders_chat
-            }
-          }
-          return acc
-        },
-        [
-          {
-            name: 'telegram',
-            value: 0
-          },
-          {
-            name: 'reddit',
-            value: 0
-          },
-          {
-            name: 'professional_traders_chat',
-            value: 0
-          }
-        ]
-      )
-      .filter(source => source.value > 0)
-
     return {
-      pieData,
       isLoading: false,
       merged,
       chartsMeta

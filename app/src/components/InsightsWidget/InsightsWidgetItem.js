3eb5c51e3eab90d79850dd605e83f6dbedb93ab3
DmitriVanGuard <dimavanguard@gmail.com>
2018-09-24 20:55:25 +0300

    Working on Insight widget styles

--- app/src/components/InsightsWidget/InsightsWidgetItem.js
+++ app/src/components/InsightsWidget/InsightsWidgetItem.js
@@ -4,19 +4,29 @@ import { Link } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
 import './InsightsWidgetItem.css'
 
+const getTagsStrippedText = text => {
+  let tempHTMLElement = document.createElement('p')
+  tempHTMLElement.innerHTML = text
+  const insightText = tempHTMLElement.textContent
+  tempHTMLElement = null
+  return insightText
+}
+
 const InsightsWidgetItem = ({
   id,
   createdAt,
   title,
   text,
-  user: { username, id: userId }
+  user: { username, id: userId },
+  images
 }) => {
+  const insightText = getTagsStrippedText(text)
   return (
     <div className='InsightsWidgetItem'>
       <h2 className='InsightsWidgetItem__title'>
         <Link to={`/insights/${id}`}>{title}</Link>
       </h2>
-      <p className='InsightsWidgetItem__text'>{text}</p>
+      <p className='InsightsWidgetItem__text'>{insightText}</p>
       <div className='InsightsWidgetItem__bottom'>
         <h4 className='InsightsWidgetItem__info InsightsWidgetItem__info_author'>
           by <Link to={`/insights/users/${userId}`}>{username}</Link>

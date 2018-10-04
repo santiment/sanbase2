93df498bda46e9fab3278f325906f67f2f4770f5
DmitriVanGuard <dimavanguard@gmail.com>
2018-10-04 12:08:10 +0300

    Widget list on assets page

---
+++ app/src/components/WidgetList/WidgetList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TotalMarketcapWidget from '../TotalMarketcapWidget/TotalMarketcapWidget'
+import './WidgetList.scss'
+
+const WidgetList = () => {
+  return (
+    <div className='WidgetList'>
+      <TotalMarketcapWidget />
+    </div>
+  )
+}
+
+export default WidgetList

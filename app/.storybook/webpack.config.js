1249ce78ec1d7981cc425a96e08eaaa173176044
DmitriVanGuard <dimavanguard@gmail.com>
2018-10-08 18:57:20 +0300

    Junk removed

--- app/.storybook/webpack.config.js
+++ app/.storybook/webpack.config.js
@@ -1,30 +1,10 @@
-// const path = require('path')
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         loaders: ['style-loader', 'css-loader'],
-//         include: path.resolve(__dirname, '../src/')
-//       },
-
-//     ]
-//   }
-// }
-
 const path = require('path')
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  // Extend defaultConfig as you need.
-
-  // For example, add typescript loader:
   defaultConfig.module.rules.push({
     test: /\.scss$/,
     loaders: ['style-loader', 'css-loader', 'sass-loader'],
     include: path.resolve(__dirname, '../src/')
   })
-  // defaultConfig.resolve.extensions.push(".ts", ".tsx");
-
   return defaultConfig
 }

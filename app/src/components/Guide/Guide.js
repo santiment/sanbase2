94349b78788ae5e3aafc16d6a2ef6141b67def2d
DmitriVanGuard <dimavanguard@gmail.com>
2018-10-09 13:51:40 +0300

    Commit for rebase

---
+++ app/src/components/Guide/Guide.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Guide.scss'
+
+const Guide = () => {
+  return (
+    <div className='Guide'>
+      <div className='Guide__left'>
+        <div className='Guide__description'>asdf sadf asdfas dfasfdsdf</div>
+      </div>
+      <div className='Guide__right'>
+        <ol className='Guide__topics'>
+          <li className='Guide__topic'>teset</li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Guide

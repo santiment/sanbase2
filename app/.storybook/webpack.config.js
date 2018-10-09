81b8f8bab19fd509a7938cb1596a9ce600832d9a
Yura Zatcepin <yura.zatsepin@gmail.com>
2018-10-09 11:23:51 +0000

    Support css modules in stories (Storybook)

--- app/.storybook/webpack.config.js
+++ app/.storybook/webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path')
+require('imports-loader')
+
+// Extend default storybook config is based on
+// https://github.com/m-allanson/gatsby-storybook-css-modules/blob/master/storybook/webpack.config.js
+//
+// It supports scss and css modules (*.module.css)
 
 module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.module.rules.push({
@@ -6,5 +12,33 @@ module.exports = (baseConfig, env, defaultConfig) => {
     loaders: ['style-loader', 'css-loader', 'sass-loader'],
     include: path.resolve(__dirname, '../src/')
   })
+
+  // Find Storybook's default CSS processing rule
+  const cssLoaderIndex = defaultConfig.module.rules.findIndex(
+    rule => rule.test.source === `\\.css$`
+  );
+
+  if (!Number.isInteger(cssLoaderIndex))
+    throw new Error("Could not find Storybook's CSS loader")
+
+  // Exclude CSS Modules from Storybook's standard CSS processing
+  defaultConfig.module.rules[cssLoaderIndex].exclude = /\.module\.css$/
+
+  defaultConfig.module.rules.push({
+    test: /\.module\.css$/,
+    use: [
+      { loader: `style-loader` },
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[path]-[local]-[hash:base64:5]'
+        }
+      }
+    ],
+    include: path.resolve(__dirname, '../src')
+  });
+
   return defaultConfig
 }

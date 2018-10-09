4af2e9e79fc0477d040d306b5ae426909ea98b27
DmitriVanGuard <dimavanguard@gmail.com>
2018-10-09 14:24:03 +0300

    Topic filter

--- app/src/components/Guide/Guide.js
+++ app/src/components/Guide/Guide.js
@@ -1,19 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Icon, Input } from 'semantic-ui-react'
+
 import './Guide.scss'
 
-const Guide = () => {
-  return (
-    <div className='Guide'>
-      <div className='Guide__left'>
-        <div className='Guide__description'>asdf sadf asdfas dfasfdsdf</div>
+const topics = [
+  {
+    title: 'Token aging'
+  },
+  {
+    title: 'Transaction volume'
+  },
+  {
+    title: 'Development activity'
+  }
+]
+
+class Guide extends Component {
+  state = {
+    searchTerm: ''
+  }
+
+  handleSearchTermChange = ({ currentTarget }) => {
+    this.setState({
+      searchTerm: currentTarget.value
+    })
+  }
+
+  render () {
+    const { searchTerm } = this.state
+
+    return (
+      <div className='Guide'>
+        <div className='Guide__left'>
+          <div className='Guide__description'>asdf sadf asdfas dfasfdsdf</div>
+        </div>
+        <div className='Guide__right'>
+          <Input
+            className='Guide__search'
+            icon='search'
+            iconPosition='left'
+            onChange={this.handleSearchTermChange}
+          />
+          <ol className='Guide__topics'>
+            {topics
+              .filter(({ title }) =>
+                title.toUpperCase().includes(searchTerm.toUpperCase())
+              )
+              .map(topic => (
+                <li className='Guide__topic'>{topic.title}</li>
+              ))}
+          </ol>
+        </div>
       </div>
-      <div className='Guide__right'>
-        <ol className='Guide__topics'>
-          <li className='Guide__topic'>teset</li>
-        </ol>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Guide

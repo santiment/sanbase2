30812118898ede1316592d078f39566f59ee6652
DmitriVanGuard <dimavanguard@gmail.com>
2018-10-22 16:46:01 +0300

    Publicity toggle. Copy link btn

---
+++ app/src/components/WatchlistShare/WatchlistShare.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { graphql } from 'react-apollo'
+import { Button, Popup } from 'semantic-ui-react'
+import cx from 'classnames'
+import * as qs from 'query-string'
+import { compose } from 'recompose'
+import copy from 'copy-to-clipboard'
+import { updateUserListGQL, fetchUserListsGQL } from './watchlistShareGQL'
+import styles from './WatchlistShare.module.css'
+
+const copyUrl = () => {
+  copy(window.location.href)
+}
+
+const WatchlistShare = ({
+  isPublic,
+  toggleWatchlistPublicity,
+  watchlistId
+}) => (
+  <div
+    className={cx({
+      [styles.wrapper]: true,
+      [styles.public]: isPublic
+    })}
+  >
+    <Button
+      toggle
+      active={isPublic}
+      style={{ boxShadow: 'none !important' }}
+      onClick={() => {
+        toggleWatchlistPublicity({
+          variables: { id: parseInt(watchlistId, 10), isPublic: !isPublic }
+        }).catch(error => {
+          alert('Error in publicity query: ', error)
+        })
+      }}
+      className={styles.publicityBtn}
+    >
+      {isPublic ? 'Public' : 'Private'}
+    </Button>
+
+    <Popup
+      trigger={
+        <Button
+          style={{ boxShadow: 'none !important' }}
+          icon='linkify'
+          className={styles.linkCopy}
+          onClick={copyUrl}
+        />
+      }
+      content='Click to copy the watchlist link'
+      inverted
+    />
+  </div>
+)
+
+const enhance = compose(
+  withRouter,
+  graphql(updateUserListGQL, {
+    name: 'toggleWatchlistPublicity'
+  }),
+  graphql(fetchUserListsGQL, {
+    name: 'fetchUserLists',
+    props: ({ fetchUserLists, ownProps }) => {
+      const { fetchUserLists: watchlists } = fetchUserLists
+      const {
+        location: { search }
+      } = ownProps
+      const [, watchlistId] = qs.parse(search).name.split('@')
+
+      return {
+        isPublic:
+          watchlists &&
+          watchlists.find(watchlist => watchlist.id === watchlistId).isPublic,
+        watchlistId
+      }
+    }
+  })
+)
+
+export default enhance(WatchlistShare)
--- app/src/pages/assets/AssetsPage.js
+++ app/src/pages/assets/AssetsPage.js
@@ -5,6 +5,7 @@ import Assets from './Assets'
 import AssetsTable from './AssetsTable'
 import HelpPopupAssets from './HelpPopupAssets'
 import AssetsPageNavigation from './AssetsPageNavigation'
+import WatchlistShare from '../../components/WatchlistShare/WatchlistShare'
 import './Assets.css'
 
 const AssetsPage = props => (
@@ -17,6 +18,7 @@ const AssetsPage = props => (
       <div className='page-head-projects__left'>
         <h1>Assets</h1>
         <HelpPopupAssets />
+        {props.type === 'list' ? <WatchlistShare /> : null}
       </div>
       <AssetsPageNavigation isLoggedIn={props.isLoggedIn} />
     </div>

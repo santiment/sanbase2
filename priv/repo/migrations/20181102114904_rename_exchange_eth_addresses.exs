c1bba7819cf494b3bdff904e4e679ec91a561330
Lyudmil Lesinski <lyudmilles@gmail.com>
2018-11-07 15:42:53 +0200

    Create a copy of exchange_eth_addresses table
    Creates a copy of the exchange_eth_addresses table and copies all the
    constraints and the data.
    Creates a new sequence for the new table's id and copies the position of
    the current sequence.

--- priv/repo/migrations/20181102114904_rename_exchange_eth_addresses.exs
+++
@@ -1,9 +0,0 @@
-defmodule Sanbase.Repo.Migrations.RenameExchangeEthAddresses do
-  use Ecto.Migration
-
-  def up do
-    rename(table(:exchange_eth_addresses), to: table(:exchange_addresses))
-  end
-
-  def down, do: :ok
-end

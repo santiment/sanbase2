f9b1656fbf0f4ac4a0a0e1c787046b22166b8809
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-11-08 12:27:28 +0200

    Rework Coinmarketcap/Ticker to use api v2

--- lib/sanbase/external_services/coinmarketcap/ticker.ex
+++ lib/sanbase/external_services/coinmarketcap/ticker.ex
@@ -1,5 +1,17 @@
 defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
-  # A module which fetches the ticker data from coinmarketcap
+  @projects_number 10_000
+  @moduledoc ~s"""
+  NOTE: Old module that will be deprecated when all places where the data from it is used is removed.
+
+  Fetches the ticker data from coinmarketcap API `https://api.coinmarketcap.com/v2/ticker`
+
+  A single request fetchest all top #{@projects_number} tickers information. The coinmarketcap API
+  has somewhat misleading name for this api - `ticker` is _NOT_ unique - there
+  duplicated tickers. The `id` field (called coinmarketcap_id everywhere in sanbase)
+  is unique. Sanbase uses names in the format `TICKER_coinmarketcap_id` to construct
+  informative and unique names.
+  """
+
   defstruct [
     :id,
     :name,
@@ -19,18 +31,19 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
 
   use Tesla
 
-  alias Sanbase.ExternalServices.RateLimiting
+  import Sanbase.Utils.Math, only: [to_integer: 1, to_float: 1]
+
   alias Sanbase.ExternalServices.Coinmarketcap.PricePoint
 
-  plug(RateLimiting.Middleware, name: :api_coinmarketcap_rate_limiter)
+  plug(Sanbase.ExternalServices.RateLimiting.Middleware, name: :api_coinmarketcap_rate_limiter)
   plug(Tesla.Middleware.BaseUrl, "https://api.coinmarketcap.com/v2/ticker")
   plug(Tesla.Middleware.Compression)
   plug(Tesla.Middleware.Logger)
 
-  alias Sanbase.ExternalServices.Coinmarketcap.Ticker
+  alias __MODULE__
 
   def fetch_data() do
-    "/?limit=10000"
+    "/?limit=#{@projects_number}"
     |> get()
     |> case do
       {:ok, %Tesla.Env{status: 200, body: body}} ->
@@ -42,10 +55,59 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
   end
 
   def parse_json(json) do
-    json
-    |> Poison.decode!(as: [%Ticker{}])
-    |> Stream.filter(fn ticker -> ticker.last_updated end)
-    |> Enum.map(&make_timestamp_integer/1)
+    %{"data" => data} =
+      json
+      |> Poison.decode!()
+
+    data
+    |> Enum.map(fn {_, project_data} ->
+      %{
+        "name" => name,
+        "symbol" => symbol,
+        "website_slug" => website_slug,
+        "rank" => rank,
+        "circulating_supply" => circulating_supply,
+        "total_supply" => total_supply,
+        "max_supply" => _max_supply,
+        "last_updated" => last_updated,
+        "quotes" => %{
+          "USD" => %{
+            "price" => price_usd,
+            "volume_24h" => volume_24h_usd,
+            "market_cap" => mcap_usd,
+            "percent_change_1h" => percent_change_1h_usd,
+            "percent_change_24h" => percent_change_24h_usd,
+            "percent_change_7d" => _percent_change_7d_usd
+          },
+          "BTC" => %{
+            "price" => price_btc,
+            "volume_24h" => _volume_btc,
+            "market_cap" => _mcap_btc,
+            "percent_change_1h" => _percent_change_1h_btc,
+            "percent_change_24h" => _percent_change_24h_btc,
+            "percent_change_7d" => _percent_change_7d_btc
+          }
+        }
+      } = project_data
+
+      %Ticker{
+        id: website_slug,
+        name: name,
+        symbol: symbol,
+        price_usd: price_usd,
+        price_btc: price_btc,
+        rank: rank,
+        "24h_volume_usd": volume_24h_usd,
+        market_cap_usd: mcap_usd,
+        last_updated: last_updated,
+        available_supply: circulating_supply,
+        total_supply: total_supply,
+        percent_change_1h: percent_change_1h_usd,
+        percent_change_24h: percent_change_24h_usd,
+        percent_change_7d: percent_change_24h_usd
+      }
+    end)
+    |> Enum.filter(fn %Ticker{last_updated: last_updated} -> last_updated end)
   end
 
   def convert_for_importing(%Ticker{
@@ -68,28 +130,5 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.Ticker do
       PricePoint.convert_to_measurement(price_point, "USD", ticker),
       PricePoint.convert_to_measurement(price_point, "BTC", ticker)
     ]
-  end
-
-  # Helper functions
-
-  defp make_timestamp_integer(ticker) do
-    {ts, ""} = Integer.parse(ticker.last_updated)
-    %{ticker | last_updated: ts}
-  end
-
-  defp to_float(nil), do: nil
-  defp to_float(fl) when is_float(fl), do: fl
-
-  defp to_float(str) when is_binary(str) do
-    {num, _} = str |> Float.parse()
-    num
-  end
-
-  defp to_integer(nil), do: nil
-  defp to_integer(int) when is_integer(int), do: int
-
-  defp to_integer(str) when is_binary(str) do
-    {num, _} = str |> Integer.parse()
-    num
   end
 end

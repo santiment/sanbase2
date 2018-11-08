1eb25d3f1b9f5f11c528332654698d47397c3847
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-11-08 12:49:26 +0200

    Update tests to use cmc api v2

--- test/sanbase/external_services/coinmarketcap/ticker_fetcher_test.exs
+++ test/sanbase/external_services/coinmarketcap/ticker_fetcher_test.exs
@@ -16,7 +16,7 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.TickerFetcherTest do
     Store.create_db()
 
     Tesla.Mock.mock(fn %{method: :get} ->
-      %Tesla.Env{status: 200, body: File.read!(Path.join(__DIR__, "v1_ticker_5.json"))}
+      %Tesla.Env{status: 200, body: File.read!(Path.join(__DIR__, "v2_ticker_5.json"))}
     end)
 
     Store.drop_measurement(@btc_measurement)
@@ -27,61 +27,61 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.TickerFetcherTest do
 
     TickerFetcher.work()
 
-    from = DateTime.from_naive!(~N[2018-05-20 22:15:00], "Etc/UTC")
-    to = DateTime.from_naive!(~N[2018-05-22 22:15:00], "Etc/UTC")
+    from = DateTime.from_naive!(~N[2018-11-08 10:35:00], "Etc/UTC")
+    to = DateTime.from_naive!(~N[2018-11-08 10:40:00], "Etc/UTC")
 
     # Test bitcoin is in influx
     assert Store.fetch_price_points!(@btc_measurement, from, to) == [
              [
-               DateTime.from_naive!(~N[2018-05-21 13:34:30], "Etc/UTC"),
-               8492.42,
+               DateTime.from_naive!(~N[2018-11-08 10:38:15], "Etc/UTC"),
+               6481.63850144,
                1,
-               144_762_215_941,
-               5_442_600_000
+               112_558_027_683,
+               4_545_288_572
              ]
            ]
 
     # Test Ethereum is in influx
     assert Store.fetch_price_points!(@eth_measurement, from, to) == [
              [
-               DateTime.from_naive!(~N[2018-05-21 13:34:18], "Etc/UTC"),
-               708.686,
-               0.0833915,
-               70_566_984_832,
-               2_160_260_000
+               DateTime.from_naive!(~N[2018-11-08 10:38:44], "Etc/UTC"),
+               214.428922185,
+               0.033082518,
+               22_101_582_138,
+               1_642_522_945
              ]
            ]
 
     # Test Ripple is in influx
     assert Store.fetch_price_points!(@xrp_measurement, from, to) == [
              [
-               DateTime.from_naive!(~N[2018-05-21 13:34:03], "Etc/UTC"),
-               0.689748,
-               8.116e-5,
-               27_031_202_213,
-               281_028_000
+               DateTime.from_naive!(~N[2018-11-08 10:38:06], "Etc/UTC"),
+               0.5086152806,
+               7.84702e-5,
+               20_449_136_107,
+               643_178_477
              ]
            ]
 
     # Test Bitcoin Cash is in influx
     assert Store.fetch_price_points!(@bch_measurement, from, to) == [
              [
-               DateTime.from_naive!(~N[2018-05-21 13:34:13], "Etc/UTC"),
-               1251.63,
-               0.14728,
-               21_452_562_711,
-               726_915_000
+               DateTime.from_naive!(~N[2018-11-08 10:38:39], "Etc/UTC"),
+               596.873154491,
+               0.0920867701,
+               10_413_526_552,
+               924_146_674
              ]
            ]
 
     # Test EOS is in influx
     assert Store.fetch_price_points!(@eos_measurement, from, to) == [
              [
-               DateTime.from_naive!(~N[2018-05-21 13:34:12], "Etc/UTC"),
-               13.5937,
-               0.00159958,
-               11_833_771_308,
-               1_197_690_000
+               DateTime.from_naive!(~N[2018-11-08 10:38:32], "Etc/UTC"),
+               5.554970117,
+               8.570318e-4,
+               5_034_164_547,
+               698_336_214
              ]
            ]
   end

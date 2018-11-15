d42d2a09d119437aea4b6af49194bba28995beec
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-11-15 16:58:23 +0200

    Start the histogram graphql bucket from 10ms instead of 250ms

--- lib/sanbase_web/graphql/prometheus/instrumenter.ex
+++
@@ -1,6 +0,0 @@
-defmodule SanbaseWeb.Graphql.Prometheus.Instrumenter do
-  use AbsintheMetrics,
-    adapter: AbsintheMetrics.Backend.PrometheusHistogram,
-    # See prometheus.ex for more examples
-    arguments: [buckets: {:exponential, 250, 1.5, 7}]
-end

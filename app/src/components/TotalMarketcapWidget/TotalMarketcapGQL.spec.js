fec96ca0a46f677f77b72cf5d021001b5b258758
DmitriVanGuard <dimavanguard@gmail.com>
2018-11-22 18:01:09 +0300

    Junk removed

--- app/src/components/TotalMarketcapWidget/TotalMarketcapGQL.spec.js
+++ app/src/components/TotalMarketcapWidget/TotalMarketcapGQL.spec.js
@@ -2,15 +2,9 @@ import { constructTotalMarketcapGQL } from './TotalMarketcapGQL'
 import { getEscapedGQLFieldAlias } from '../../utils/utils'
 
 const SLUGS = ['bitcoin', 'bitcoin-cash', 'ab-chain-rtb', '0x']
-const ESCAPED_SLUGS = ['_bitcoin', '_bitcoincash', '_abchainrtb', '_0x']
 const FROM_DATETIME = '2018-08-22T10:53:13Z'
 
 describe('Total market cap widget queries', () => {
-  it('should correctly escape slugs', () => {
-    const escapedSlugs = SLUGS.map(getEscapedGQLFieldAlias)
-    expect(escapedSlugs).toEqual(ESCAPED_SLUGS)
-  })
-
   it('should correctly construct alias query', () => {
     const escapedSlugsMap = SLUGS.map(slug => [
       slug,

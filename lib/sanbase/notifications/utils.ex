2a601b6ea3a5e5bbd9aceaf8a7a6a77764229f1f
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-12-01 16:50:51 +0200

    Fix negative hour in exchange inflow signal

---
+++ lib/sanbase/notifications/utils.ex
@@ -0,0 +1,14 @@
+defmodule Sanbase.Notifications.Utils do
+  def time_difference(dt1, dt2) do
+    case Timex.diff(dt1, dt2, :seconds) |> abs() do
+      x when x < 3600 ->
+        "#{div(x, 60)} minutes"
+
+      x when x <= 7200 ->
+        "1 hour"
+
+      x when x < 86400 ->
+        "#{div(x, 3600)} hours"
+    end
+  end
+end

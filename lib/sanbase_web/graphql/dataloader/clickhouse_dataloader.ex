99b344f656f4d9917a5502033ee2cb826b7752a0
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-12-28 15:23:55 +0200

    Chunk the projects by 200 before doing the dataloader batch query

--- lib/sanbase_web/graphql/dataloader/clickhouse_dataloader.ex
+++ lib/sanbase_web/graphql/dataloader/clickhouse_dataloader.ex
@@ -12,19 +12,20 @@ defmodule SanbaseWeb.Graphql.ClickhouseDataloader do
     args = Enum.to_list(args)
     [%{from: from, to: to, days: days} | _] = args
 
-    organizations =
-      Enum.map(args, fn %{project: project} ->
-        case Project.github_organization(project) do
-          {:ok, organization} -> organization
-          _ -> nil
-        end
-      end)
-      |> Enum.reject(&is_nil/1)
+    Enum.map(args, fn %{project: project} ->
+      case Project.github_organization(project) do
+        {:ok, organization} -> organization
+        _ -> nil
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.chunk_every(200)
+    |> Enum.flat_map(fn organizations ->
+      {:ok, dev_activity} = Github.total_dev_activity(organizations, from, to)
 
-    {:ok, dev_activity} = Github.total_dev_activity(organizations, from, to)
-
-    dev_activity
-    |> Enum.map(fn {organization, dev_activity} -> {organization, dev_activity / days} end)
+      dev_activity
+      |> Enum.map(fn {organization, dev_activity} -> {organization, dev_activity / days} end)
+    end)
     |> Map.new()
   end
 end

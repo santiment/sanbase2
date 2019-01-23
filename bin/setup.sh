86ce31174a548e3723cd919dd8461e096eb95b92
Valentin Mihov <valentin.mihov@gmail.com>
2019-01-23 21:35:30 +0200

    Updated the README and the docker-compose file. Added a test script
    The test script allows to easily run the tests in docker using the
    provided docker compose file.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose run sanbase sh -c "mix deps.get && mix ecto.setup_all && mix run priv/repo/seeds.exs && cd assets/; yarn install"
+docker-compose build && \
+  docker-compose run sanbase sh -c "mix deps.get && mix ecto.setup_all && mix run priv/repo/seeds.exs && cd assets/; yarn install"

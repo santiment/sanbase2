fc60dd44a0a3488ffedbd897c00ba1e354c79807
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-11 09:40:25 +0200

    Allow negative percent

--- lib/sanbase/signals/validation.ex
+++ lib/sanbase/signals/validation.ex
@@ -3,7 +3,7 @@ defmodule Sanbase.Signals.Validation do
 
   def valid_notification_channels(), do: @notification_channels
 
-  def valid_percent?(percent) when is_float(percent), do: percent > 0
+  def valid_percent?(percent) when is_float(percent), do: true
   def valid_percent?(_), do: false
 
   def valid_time_window?(time_window) when is_binary(time_window) do

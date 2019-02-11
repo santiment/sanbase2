0ac8f7fd9e21e848dc372b02ec406648b73c2441
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-11 14:47:36 +0200

    add seconds in the regex

--- lib/sanbase/signals/validation.ex
+++ lib/sanbase/signals/validation.ex
@@ -7,7 +7,7 @@ defmodule Sanbase.Signals.Validation do
   def valid_percent?(_), do: false
 
   def valid_time_window?(time_window) when is_binary(time_window) do
-    Regex.match?(~r/^\d+[mhdw]$/, time_window)
+    Regex.match?(~r/^\d+[smhdw]$/, time_window)
   end
 
   def valid_time_window?(_), do: false

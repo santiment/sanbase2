66a58a11deadeb7a0c6c52c77e696256c2c873fe
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-12 16:59:03 +0200

    refactor historical trigger points

--- lib/sanbase/signals/trigger/settings/daily_active_addresses_settings.ex
+++ lib/sanbase/signals/trigger/settings/daily_active_addresses_settings.ex
@@ -14,7 +14,19 @@ defmodule Sanbase.Signals.Trigger.DailyActiveAddressesSettings do
   alias Sanbase.Clickhouse.{Erc20DailyActiveAddresses, EthDailyActiveAddresses}
   alias Sanbase.Signals.Evaluator.Cache
 
-  @derive Jason.Encoder
+  use Vex.Struct
+
+  import Sanbase.Signals.Utils
+  import Sanbase.Signals.Validation
+
+  alias __MODULE__
+  alias Sanbase.Model.Project
+  alias Sanbase.Clickhouse.{Erc20DailyActiveAddresses, EthDailyActiveAddresses}
+  alias Sanbase.Signals.Evaluator.Cache
+  alias Sanbase.DateTimeUtils
+  alias Sanbase.Signals.Type
+
+  @derive [Jason.Encoder]
   @trigger_type "daily_active_addresses"
   @enforce_keys [:type, :target, :channel, :time_window, :percent_threshold]
   defstruct type: @trigger_type,
@@ -42,6 +54,11 @@ defmodule Sanbase.Signals.Trigger.DailyActiveAddressesSettings do
           repeating: boolean(),
           triggered?: boolean(),
           payload: Type.payload()
+        }
+  @type historical_trigger_points_type :: %{
+          datetime: %DateTime{},
+          active_addresses: non_neg_integer(),
+          triggered: boolean()
         }
 
   @spec type() :: Type.trigger_type()
@@ -83,6 +100,47 @@ defmodule Sanbase.Signals.Trigger.DailyActiveAddressesSettings do
     end)
   end
 
+  @spec historical_trigger_points(%__MODULE__{}) ::
+          {:ok, list(historical_trigger_points_type)} | {:error, String.t()}
+  def historical_trigger_points(%__MODULE{target: target} = settings) when is_binary(target) do
+    settings
+    |> get_daily_active_addresses()
+    |> case do
+      {:ok, daa_result} when is_list(daa_result) and daa_result != [] ->
+        time_window_in_days = time_window_in_days(settings.time_window)
+
+        first_possible_trigger_date =
+          first_possible_trigger_date(daa_result, time_window_in_days + 1)
+
+        result =
+          for %{datetime: datetime, active_addresses: active_addresses} <- daa_result do
+            triggered =
+              point_triggered?(
+                settings,
+                {datetime, active_addresses},
+                daa_result,
+                first_possible_trigger_date,
+                time_window_in_days
+              )
+
+            %{
+              datetime: datetime,
+              active_addresses: active_addresses,
+              triggered: triggered
+            }
+          end
+
+        {:ok, result}
+
+      _ ->
+        {:error, "No data available to calculate historical trigger points"}
+    end
+  end
+
+  def historical_trigger_points(_), do: {:error, "Not implemented"}
+
+  # private functions
+
   defp average_daily_active_addresses("ethereum", from, to) do
     {:ok, result} = EthDailyActiveAddresses.average_active_addresses(from, to)
     result
@@ -94,6 +152,80 @@ defmodule Sanbase.Signals.Trigger.DailyActiveAddressesSettings do
     case result do
       [{_, value}] -> value
       _ -> 0
+    end
+  end
+
+  defp point_triggered?(
+         settings,
+         {datetime, active_addresses},
+         daa_result,
+         first_possible_trigger_date,
+         time_window_in_days
+       ) do
+    case DateTime.compare(datetime, first_possible_trigger_date) do
+      :lt ->
+        false
+
+      cmp when cmp in [:gt, :eq] ->
+        avg_time_window =
+          calc_average_daa_for_interval(
+            daa_result,
+            Timex.shift(datetime, days: -time_window_in_days),
+            Timex.shift(datetime, days: -1)
+          )
+
+        percent_change(avg_time_window, active_addresses) >= settings.percent_threshold
+    end
+  end
+
+  defp get_daily_active_addresses(settings) do
+    {:ok, contract, _token_decimals} = Project.contract_info_by_slug(settings.target)
+
+    from = Timex.shift(Timex.now(), days: -@historical_days_from)
+    to = Timex.shift(Timex.now(), days: -@historical_days_to)
+    interval = @historical_days_interval
+
+    if contract == "ETH" do
+      EthDailyActiveAddresses.average_active_addresses(from, to, interval)
+    else
+      Erc20DailyActiveAddresses.average_active_addresses(contract, from, to, interval)
+    end
+  end
+
+  defp first_possible_trigger_date(daa_result, days_shift) do
+    daa_result
+    |> List.first()
+    |> Map.get(:datetime)
+    |> Timex.shift(days: days_shift)
+  end
+
+  def time_window_in_days(time_window) do
+    time_window_in_seconds = Sanbase.DateTimeUtils.compound_duration_to_seconds(time_window)
+    one_day_in_seconds = 3600 * 24
+
+    if time_window_in_seconds <= @minimal_time_window_in_days * one_day_in_seconds do
+      @minimal_time_window_in_days
+    else
+      Sanbase.Utils.Math.to_integer(time_window_in_seconds / one_day_in_seconds)
+    end
+  end
+
+  defp calc_average_daa_for_interval(points, from, to) do
+    filtered_points =
+      points
+      |> Enum.filter(fn %{datetime: dt} ->
+        DateTime.to_unix(dt) >= DateTime.to_unix(from) &&
+          DateTime.to_unix(dt) <= DateTime.to_unix(from)
+      end)
+
+    sum =
+      filtered_points
+      |> Enum.reduce(0, fn %{active_addresses: daa}, acc -> acc + daa end)
+
+    if length(filtered_points) > 0 do
+      Sanbase.Utils.Math.to_integer(sum / length(filtered_points))
+    else
+      0
     end
   end
 

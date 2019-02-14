a6f29d8b279ff12614b737a7bd4b90b4914fabf0
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-14 17:54:14 +0200

    Add price points

--- lib/sanbase/signals/history/daily_active_addresses_history.ex
+++ lib/sanbase/signals/history/daily_active_addresses_history.ex
@@ -4,6 +4,9 @@ defmodule Sanbase.Signals.History.DailyActiveAddressesHistory do
   alias Sanbase.Signals.Trigger.DailyActiveAddressesSettings
   alias Sanbase.Model.Project
   alias Sanbase.Clickhouse.{Erc20DailyActiveAddresses, EthDailyActiveAddresses}
+  alias Sanbase.Influxdb.Measurement
+  alias Sanbase.Prices.Store, as: PricesStore
+  require Logger
 
   defimpl Sanbase.Signals.History, for: DailyActiveAddressesSettings do
     @historical_days_from 90
@@ -20,40 +23,53 @@ defmodule Sanbase.Signals.History.DailyActiveAddressesHistory do
     @spec historical_trigger_points(%DailyActiveAddressesSettings{}) ::
             {:ok, list(historical_trigger_points_type)} | {:error, String.t()}
     def historical_trigger_points(%DailyActiveAddressesSettings{target: target} = settings) do
-      settings
-      |> get_daily_active_addresses()
-      |> case do
-        {:ok, daa_result} when is_list(daa_result) and daa_result != [] ->
-          daa_result = Enum.map(daa_result, fn point -> Map.put(point, :triggered?, false) end)
-          time_window_in_days = time_window_in_days(settings.time_window)
-          {:ok, sma} = sma(daa_result, time_window_in_days + 1)
+      with {:ok, contract, _token_decimals} = Project.contract_info_by_slug(settings.target),
+           measurement when not is_nil(measurement) <-
+             Measurement.name_from_slug(settings.target),
+           {from, to, interval} <- get_timeseries_params(),
+           {:ok, daa_result} when is_list(daa_result) and daa_result != [] <-
+             get_daily_active_addresses(contract, from, to, interval),
+           {:ok, prices} when is_list(prices) and prices != [] <-
+             PricesStore.fetch_prices_with_resolution(measurement, from, to, interval) do
+        daa_result = Enum.map(daa_result, fn point -> Map.put(point, :triggered?, false) end)
+        prices = Enum.map(prices, fn [dt, price | _rest] -> %{datetime: dt, price: price} end)
 
-          sma =
-            sma
-            |> Enum.map(fn point ->
-              triggered? =
-                percent_change(point.average_daa, point.active_addresses) >=
-                  settings.percent_threshold
+        daa_result =
+          Enum.zip(daa_result, prices)
+          |> Enum.map(fn {daa_item, price_item} -> Map.merge(daa_item, price_item) end)
 
-              Map.put(point, :triggered?, triggered?)
-            end)
+        time_window_in_days = time_window_in_days(settings.time_window)
+        {:ok, sma} = sma(daa_result, time_window_in_days + 1)
 
-          {:ok, merge_daa_chunks_by_datetime(daa_result, sma)}
+        sma =
+          sma
+          |> Enum.map(fn point ->
+            triggered? =
+              percent_change(point.average_daa, point.active_addresses) >=
+                settings.percent_threshold
 
-        _ ->
+            Map.put(point, :triggered?, triggered?)
+          end)
+
+        {:ok, merge_daa_chunks_by_datetime(daa_result, sma)}
+      else
+        error ->
+          Logger.error("Can't calculate historical trigger points: #{inspect(error)}")
           {:error, "No data available to calculate historical trigger points"}
       end
     end
 
     def historical_trigger_points(_), do: {:error, "Not implemented"}
 
-    defp get_daily_active_addresses(settings) do
-      {:ok, contract, _token_decimals} = Project.contract_info_by_slug(settings.target)
-
+    defp get_timeseries_params() do
       from = Timex.shift(Timex.now(), days: -@historical_days_from)
       to = Timex.shift(Timex.now(), days: -@historical_days_to)
       interval = @historical_days_interval
 
+      {from, to, interval}
+    end
+
+    defp get_daily_active_addresses(contract, from, to, interval) do
       if contract == "ETH" do
         EthDailyActiveAddresses.average_active_addresses(from, to, interval)
       else

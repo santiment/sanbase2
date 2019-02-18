da9dff81931606514ec46ae0248d81f7a8fb65cf
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-18 15:54:01 +0200

    fix website_url and creation transaction fields parsing etherscan

--- lib/sanbase/external_services/etherscan/scraper.ex
+++ lib/sanbase/external_services/etherscan/scraper.ex
@@ -65,7 +65,7 @@ defmodule Sanbase.ExternalServices.Etherscan.Scraper do
         total_supply: total_supply(html) || project_info.total_supply,
         main_contract_address: project_info.main_contract_address || main_contract_address(html),
         token_decimals: project_info.token_decimals || token_decimals(html),
-        website_link: project_info.website_link || official_link(html, "Website"),
+        website_link: project_info.website_link || website_link(html),
         email: project_info.email || official_link(html, "Email") |> email(),
         reddit_link: project_info.reddit_link || official_link(html, "Reddit"),
         twitter_link: project_info.twitter_link || official_link(html, "Twitter"),
@@ -77,6 +77,12 @@ defmodule Sanbase.ExternalServices.Etherscan.Scraper do
         facebook_link: project_info.facebook_link || official_link(html, "Facebook"),
         whitepaper_link: project_info.whitepaper_link || official_link(html, "Whitepaper")
     }
+  end
+
+  defp website_link(html) do
+    Floki.find(html, ~s/#ContentPlaceHolder1_tr_officialsite_1 > div > div.col-md-8 > a/)
+    |> Floki.attribute("href")
+    |> List.first()
   end
 
   defp official_link(html, media) do
@@ -92,7 +98,7 @@ defmodule Sanbase.ExternalServices.Etherscan.Scraper do
   end
 
   defp creation_transaction(html) do
-    Floki.find(html, ~s/a[title="Creator Transaction Hash"]/)
+    Floki.find(html, ~s/a[title="Creator TxHash"]/)
     |> List.first()
     |> case do
       nil -> nil

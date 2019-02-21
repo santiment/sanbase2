0cdc51734ad686d85614348a7e334f2e68578a5f
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-02-21 11:54:22 +0200

    add private func at bottom

--- test/sanbase/signals/history/utils_test.exs
+++ test/sanbase/signals/history/utils_test.exs
@@ -22,12 +22,6 @@ defmodule Sanbase.Signals.TriggerHistoryTest do
     assert filter_with_bigger_threshold(percent_change_calculations) |> length() == 5
   end
 
-  defp filter_with_bigger_threshold(percent_change_calculations) do
-    Enum.filter(percent_change_calculations, fn {_percent_change, is_bigger_than_threshold?} ->
-      is_bigger_than_threshold?
-    end)
-  end
-
   test "#average" do
     assert Utils.average([4, 6, 8, 2]) == 5.0
     assert Utils.average([]) == 0
@@ -50,5 +44,11 @@ defmodule Sanbase.Signals.TriggerHistoryTest do
       max = Enum.max(list)
       assert average >= min - 1 and average <= max + 1
     end
+  end
+
+  defp filter_with_bigger_threshold(percent_change_calculations) do
+    Enum.filter(percent_change_calculations, fn {_percent_change, is_bigger_than_threshold?} ->
+      is_bigger_than_threshold?
+    end)
   end
 end

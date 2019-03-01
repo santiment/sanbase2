6ae041e30ada52e967edcbb490de71171f07a3d0
Vassil Kalkov <vassilkalkov@gmail.com>
2019-03-01 16:09:41 +0200

    Fix development docker setup
    It was not compiling assets.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker-compose build && \
-  docker-compose run sanbase sh -c "mix deps.get && mix ecto.setup_all && mix run priv/repo/seeds.exs && cd assets/; yarn install"
+  docker-compose run sanbase sh -c "mix deps.get &&
+                                    mix ecto.setup_all &&
+                                    mix run priv/repo/seeds.exs &&
+                                    cd assets &&
+                                    npm install &&
+                                    npx brunch build"

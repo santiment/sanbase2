c76f43caf308623c31e5f5800de12af768c005d0
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-03-08 16:26:38 +0200

    Fix tags migrations and commit structure.sql

--- priv/repo/migrations/20190227153610_drop_tags_with_null_name.exs
+++ priv/repo/migrations/20190227153610_drop_tags_with_null_name.exs
@@ -3,8 +3,17 @@ defmodule Sanbase.Repo.Migrations.DropTagsWithNullName do
   import Ecto.Query
 
   def up() do
-    from(t in Sanbase.Tag, where: is_nil(t.name))
-    |> Sanbase.Repo.delete_all()
+    # NOTE: This is a workaround for how the ecto prometheus exporter works
+    # `delete_all` tries to write data in an ETS table that is started during
+    # application start. `delete_all` does not raise exceptions on some failure
+    # so the try/rescue block is concerning the exception raised by the prometheus
+    # exporter
+    try do
+      from(t in Sanbase.Tag, where: is_nil(t.name))
+      |> Sanbase.Repo.delete_all()
+    rescue
+      _ -> :ok
+    end
   end
 
   def down(), do: :ok
--- priv/repo/migrations/20190227153724_add_tag_name_null_constrain.exs
+++ priv/repo/migrations/20190227153724_add_tag_name_null_constrain.exs
@@ -1,9 +1,15 @@
 defmodule Sanbase.Repo.Migrations.AddTagNameNullConstrain do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:tags) do
       modify(:name, :string, null: false)
+    end
+  end
+
+  def down do
+    alter table(:tags) do
+      modify(:name, :string, null: true)
     end
   end
 end

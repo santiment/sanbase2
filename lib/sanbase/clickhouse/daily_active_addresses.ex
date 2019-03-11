c1a70857b5095d76991121035b28b0a9d289f516
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-03-11 14:02:49 +0200

    Fix handling of empty results in the DAA signals
    To simplify things the following is done:
    1. Rewrite the cache to support :error tuples that are not persisted
    2. Add a Clickhouse DAA dispatch module for easier access

---
+++ lib/sanbase/clickhouse/daily_active_addresses.ex
@@ -0,0 +1,35 @@
+defmodule Sanbase.Clickhouse.DailyActiveAddresses do
+  @moduledoc ~s"""
+  Dispatch the calculations of the daily active addresses to the correct module
+  """
+  alias Sanbase.Model.Project
+
+  alias Sanbase.Clickhouse.Erc20DailyActiveAddresses, as: Erc20
+  alias Sanbase.Clickhouse.EthDailyActiveAddresses, as: Eth
+
+  @ethereum ["ethereum", "ETH"]
+
+  def realtime_active_addresses(eth) when eth in @ethereum do
+    Eth.realtime_active_addresses()
+  end
+
+  def realtime_active_addresses(contract) do
+    Erc20.realtime_active_addresses(contract)
+  end
+
+  def average_active_addresses(eth, from, to) when eth in @ethereum do
+    Eth.average_active_addresses(from, to)
+  end
+
+  def average_active_addresses(contract, from, to) do
+    Erc20.average_active_addresses(contract, from, to)
+  end
+
+  def average_active_addresses(eth, from, to, interval) when eth in @ethereum do
+    Eth.average_active_addresses(from, to, interval)
+  end
+
+  def average_active_addresses(contract, from, to, interval) do
+    Erc20.average_active_addresses(contract, from, to, interval)
+  end
+end

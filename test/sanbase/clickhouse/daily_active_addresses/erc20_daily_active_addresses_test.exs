cdd79777a0d6d51efc7629d405a3adc70d2b90c6
Vassil Kalkov <vassilkalkov@gmail.com>
2019-04-06 16:03:01 +0300

    Add more tests for Daily Active Addresses

---
+++ test/sanbase/clickhouse/daily_active_addresses/erc20_daily_active_addresses_test.exs
@@ -0,0 +1,135 @@
+defmodule Sanbase.Clickhouse.DailyActiveAddresses.Erc20DailyActiveAddressesTest do
+  use Sanbase.DataCase
+  require Sanbase.ClickhouseRepo
+  import Mock
+  import Sanbase.Factory
+  import Sanbase.DateTimeUtils, only: [from_iso8601_to_unix!: 1, from_iso8601!: 1]
+  alias Sanbase.Clickhouse.Erc20DailyActiveAddresses
+
+  setup do
+    project1 =
+      insert(:project, %{
+        ticker: "PRJ1",
+        coinmarketcap_id: "project1",
+        main_contract_address: "0x123"
+      })
+
+    project2 =
+      insert(:project, %{
+        ticker: "PRJ2",
+        coinmarketcap_id: "project2",
+        main_contract_address: "0x456"
+      })
+
+    [
+      project1: project1,
+      contract1: project1.main_contract_address,
+      project2: project2,
+      contract2: project2.main_contract_address,
+      from: from_iso8601!("2019-01-01T00:00:00Z"),
+      to: from_iso8601!("2019-01-03T00:00:00Z")
+    ]
+  end
+
+  test "returns a single current value for active addresses per contract", context do
+    with_mock Sanbase.ClickhouseRepo,
+      query: fn _, _ ->
+        {:ok, %{rows: [["0x123", 100_000], ["0x456", 200_000]]}}
+      end do
+      result =
+        Erc20DailyActiveAddresses.realtime_active_addresses([context.contract1, context.contract2])
+
+      assert result == {:ok, [{"0x123", 100_000}, {"0x456", 200_000}]}
+    end
+  end
+
+  test "returns average active addresses for given period per contract", context do
+    with_mock Sanbase.ClickhouseRepo,
+      query: fn _, _ ->
+        {:ok, %{rows: [[context.contract1, 5], [context.contract2, 8]]}}
+      end do
+      result =
+        Erc20DailyActiveAddresses.average_active_addresses(
+          [context.contract1, context.contract2],
+          context.from,
+          context.to
+        )
+
+      assert result ==
+               {:ok, [{context.contract1, 5}, {context.contract2, 8}]}
+    end
+  end
+
+  test "returns active addresses for given period and contract in chunks", context do
+    with_mock Sanbase.ClickhouseRepo,
+      query: fn _, _ ->
+        {:ok,
+         %{
+           rows: [
+             [from_iso8601_to_unix!("2019-01-08T00:00:00Z"), 200_000],
+             [from_iso8601_to_unix!("2019-01-09T00:00:00Z"), 100_000]
+           ]
+         }}
+      end do
+      result =
+        Erc20DailyActiveAddresses.average_active_addresses(
+          context.contract1,
+          context.from,
+          context.to,
+          "1d"
+        )
+
+      assert result ==
+               {:ok,
+                [
+                  %{
+                    datetime: from_iso8601!("2019-01-08T00:00:00Z"),
+                    active_addresses: 200_000
+                  },
+                  %{
+                    datetime: from_iso8601!("2019-01-09T00:00:00Z"),
+                    active_addresses: 100_000
+                  }
+                ]}
+    end
+  end
+
+  test "returns active addresses with deposits share for given period and contract in chunks",
+       context do
+    with_mock Sanbase.ClickhouseRepo,
+      query: fn _, _ ->
+        {:ok,
+         %{
+           rows: [
+             [from_iso8601_to_unix!("2019-01-08T00:00:00Z"), 200_000, 20_000, 10.0],
+             [from_iso8601_to_unix!("2019-01-09T00:00:00Z"), 100_000, 10_000, 10.0]
+           ]
+         }}
+      end do
+      result =
+        Erc20DailyActiveAddresses.average_active_addresses_with_deposits(
+          context.contract1,
+          context.from,
+          context.to,
+          "1d"
+        )
+
+      assert result ==
+               {:ok,
+                [
+                  %{
+                    datetime: from_iso8601!("2019-01-08T00:00:00Z"),
+                    active_addresses: 200_000,
+                    active_deposits: 20_000,
+                    share_of_deposits: 10.0
+                  },
+                  %{
+                    datetime: from_iso8601!("2019-01-09T00:00:00Z"),
+                    active_addresses: 100_000,
+                    active_deposits: 10_000,
+                    share_of_deposits: 10.0
+                  }
+                ]}
+    end
+  end
+end
--- test/sanbase_web/graphql/clickhouse/daily_active_addresses_test.exs
+++ test/sanbase_web/graphql/clickhouse/daily_active_addresses_test.exs
@@ -10,72 +10,49 @@ defmodule SanbaseWeb.Graphql.Clickhouse.DailyActiveAddressesTest do
   alias Sanbase.Clickhouse.DailyActiveAddresses
 
   setup do
-    project = insert(:project, %{main_contract_address: "0x123"})
+    project1 =
+      insert(:project, %{
+        ticker: "PRJ1",
+        coinmarketcap_id: "project1",
+        main_contract_address: "0x123"
+      })
+
+    project2 =
+      insert(:project, %{
+        ticker: "PRJ2",
+        coinmarketcap_id: "project2",
+        main_contract_address: "0x456"
+      })
+
+    project_ethereum =
+      insert(:project, %{
+        coinmarketcap_id: "ethereum",
+        ticker: "ETH",
+        main_contract_address: "0x789"
+      })
 
     [
-      contract: project.main_contract_address,
-      slug: project.coinmarketcap_id,
+      project: project1,
+      contract: project1.main_contract_address,
+      slug: project1.coinmarketcap_id,
+      project2: project2,
+      contract2: project2.main_contract_address,
+      project_ethereum: project_ethereum,
       from: from_iso8601!("2019-01-01T00:00:00Z"),
       to: from_iso8601!("2019-01-03T00:00:00Z"),
       interval: "1d"
     ]
   end
 
-  test "returns data from daily active addresses calculation", context do
-    with_mock DailyActiveAddresses,
-      average_active_addresses: fn _, _, _, _ ->
-        {:ok,
-         [
-           %{active_addresses: 100, datetime: from_iso8601!("2019-01-01T00:00:00Z")},
-           %{active_addresses: 200, datetime: from_iso8601!("2019-01-02T00:00:00Z")}
-         ]}
-      end do
-      response = execute_query(context)
-      addresses = parse_response(response)
-
-      assert_called(
-        DailyActiveAddresses.average_active_addresses(
-          context.contract,
-          context.from,
-          context.to,
-          context.interval
-        )
-      )
-
-      assert addresses == [
-               %{"activeAddresses" => 100, "datetime" => "2019-01-01T00:00:00Z"},
-               %{"activeAddresses" => 200, "datetime" => "2019-01-02T00:00:00Z"}
-             ]
-    end
-  end
-
-  test "returns empty array when there is no data", context do
-    with_mock DailyActiveAddresses, average_active_addresses: fn _, _, _, _ -> {:ok, []} end do
-      response = execute_query(context)
-      addresses = parse_response(response)
-
-      assert_called(
-        DailyActiveAddresses.average_active_addresses(
-          context.contract,
-          context.from,
-          context.to,
-          context.interval
-        )
-      )
-
-      assert addresses == []
-    end
-  end
-
   test "logs warning when calculation errors", context do
     with_mock DailyActiveAddresses,
       average_active_addresses: fn _, _, _, _ -> {:error, "Some error description here"} end do
       assert capture_log(fn ->
-               response = execute_query(context)
+               response = execute_active_addresses_query(context, context.slug)
                addresses = parse_response(response)
                assert addresses == nil
              end) =~
-               ~s/[warn] Can't calculate daily active addresses for project with coinmarketcap_id: santiment. Reason: "Some error description here"/
+               ~s/[warn] Can't calculate daily active addresses for project with coinmarketcap_id: project1. Reason: "Some error description here"/
     end
   end
 
@@ -108,10 +85,173 @@ defmodule SanbaseWeb.Graphql.Clickhouse.DailyActiveAddressesTest do
     end
   end
 
-  test "fetches average daily active addresses", context do
+  describe "for tokens" do
+    test "returns daily active addresses", context do
+      with_mock DailyActiveAddresses,
+        average_active_addresses: fn _, _, _, _ ->
+          {:ok,
+           [
+             %{active_addresses: 100, datetime: from_iso8601!("2019-01-01T00:00:00Z")},
+             %{active_addresses: 200, datetime: from_iso8601!("2019-01-02T00:00:00Z")}
+           ]}
+        end do
+        response = execute_active_addresses_query(context, context.slug)
+        addresses = parse_response(response)
+
+        assert_called(
+          DailyActiveAddresses.average_active_addresses(
+            context.contract,
+            context.from,
+            context.to,
+            context.interval
+          )
+        )
+
+        assert addresses == [
+                 %{"activeAddresses" => 100, "datetime" => "2019-01-01T00:00:00Z"},
+                 %{"activeAddresses" => 200, "datetime" => "2019-01-02T00:00:00Z"}
+               ]
+      end
+    end
+
+    test "returns daily active addresses with deposits", context do
+      with_mock DailyActiveAddresses,
+        average_active_addresses_with_deposits: fn _, _, _, _ ->
+          {:ok,
+           [
+             %{
+               active_addresses: 100,
+               active_deposits: 10,
+               share_of_deposits: 10.0,
+               datetime: from_iso8601!("2019-01-01T00:00:00Z")
+             },
+             %{
+               active_addresses: 200,
+               active_deposits: 10,
+               share_of_deposits: 5.0,
+               datetime: from_iso8601!("2019-01-02T00:00:00Z")
+             }
+           ]}
+        end do
+        response = execute_active_addresses_with_deposits_query(context, context.slug)
+        addresses = parse_response(response)
+
+        assert_called(
+          DailyActiveAddresses.average_active_addresses_with_deposits(
+            context.contract,
+            context.from,
+            context.to,
+            context.interval
+          )
+        )
+
+        assert addresses == [
+                 %{
+                   "activeAddresses" => 100,
+                   "activeDeposits" => 10,
+                   "shareOfDeposits" => 10.0,
+                   "datetime" => "2019-01-01T00:00:00Z"
+                 },
+                 %{
+                   "activeAddresses" => 200,
+                   "activeDeposits" => 10,
+                   "shareOfDeposits" => 5.0,
+                   "datetime" => "2019-01-02T00:00:00Z"
+                 }
+               ]
+      end
+    end
+  end
+
+  describe "for ethereum" do
+    test "returns daily active addresses", context do
+      with_mock DailyActiveAddresses,
+        average_active_addresses: fn _, _, _, _ ->
+          {:ok,
+           [
+             %{active_addresses: 100, datetime: from_iso8601!("2019-01-01T00:00:00Z")},
+             %{active_addresses: 200, datetime: from_iso8601!("2019-01-02T00:00:00Z")}
+           ]}
+        end do
+        response =
+          execute_active_addresses_query(context, context.project_ethereum.coinmarketcap_id)
+
+        addresses = parse_response(response)
+
+        assert_called(
+          DailyActiveAddresses.average_active_addresses(
+            context.project_ethereum.ticker,
+            context.from,
+            context.to,
+            context.interval
+          )
+        )
+
+        assert addresses == [
+                 %{"activeAddresses" => 100, "datetime" => "2019-01-01T00:00:00Z"},
+                 %{"activeAddresses" => 200, "datetime" => "2019-01-02T00:00:00Z"}
+               ]
+      end
+    end
+
+    test "returns daily active addresses with deposits", context do
+      with_mock DailyActiveAddresses,
+        average_active_addresses_with_deposits: fn _, _, _, _ ->
+          {:ok,
+           [
+             %{
+               active_addresses: 100,
+               active_deposits: 10,
+               share_of_deposits: 10.0,
+               datetime: from_iso8601!("2019-01-01T00:00:00Z")
+             },
+             %{
+               active_addresses: 200,
+               active_deposits: 10,
+               share_of_deposits: 5.0,
+               datetime: from_iso8601!("2019-01-02T00:00:00Z")
+             }
+           ]}
+        end do
+        response =
+          execute_active_addresses_with_deposits_query(
+            context,
+            context.project_ethereum.coinmarketcap_id
+          )
+
+        addresses = parse_response(response)
+
+        assert_called(
+          DailyActiveAddresses.average_active_addresses_with_deposits(
+            context.project_ethereum.ticker,
+            context.from,
+            context.to,
+            context.interval
+          )
+        )
+
+        assert addresses == [
+                 %{
+                   "activeAddresses" => 100,
+                   "activeDeposits" => 10,
+                   "shareOfDeposits" => 10.0,
+                   "datetime" => "2019-01-01T00:00:00Z"
+                 },
+                 %{
+                   "activeAddresses" => 200,
+                   "activeDeposits" => 10,
+                   "shareOfDeposits" => 5.0,
+                   "datetime" => "2019-01-02T00:00:00Z"
+                 }
+               ]
+      end
+    end
+  end
+
+  test "returns the average single value for active addresses", context do
     with_mock DailyActiveAddresses,
       average_active_addresses: fn _, _, _ ->
-        {:ok, [{"0x123", 150}]}
+        {:ok, [{context.contract, 150}]}
       end do
       query = """
       {
@@ -138,8 +278,15 @@ defmodule SanbaseWeb.Graphql.Clickhouse.DailyActiveAddressesTest do
     json_response(response, 200)["data"]["dailyActiveAddresses"]
   end
 
-  defp execute_query(context) do
-    query = active_addresses_query(context.slug, context.from, context.to, context.interval)
+  defp execute_active_addresses_query(context, slug) do
+    query = active_addresses_query(slug, context.from, context.to, context.interval)
+
+    context.conn
+    |> post("/graphql", query_skeleton(query, "dailyActiveAddresses"))
+  end
+
+  defp execute_active_addresses_with_deposits_query(context, slug) do
+    query = active_addresses_with_deposits_query(slug, context.from, context.to, context.interval)
 
     context.conn
     |> post("/graphql", query_skeleton(query, "dailyActiveAddresses"))
@@ -157,6 +304,25 @@ defmodule SanbaseWeb.Graphql.Clickhouse.DailyActiveAddressesTest do
       {
         datetime,
         activeAddresses
+      }
+    }
+    """
+  end
+
+  defp active_addresses_with_deposits_query(slug, from, to, interval) do
+    """
+    {
+      dailyActiveAddresses(
+        slug: "#{slug}",
+        from: "#{from}",
+        to: "#{to}",
+        interval: "#{interval}"
+      )
+      {
+        datetime,
+        activeAddresses
+        activeDeposits
+        shareOfDeposits
       }
     }
     """

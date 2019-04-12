405971ee17e5169e505805ab2ae6836d485829ea
Valentin Mihov <valentin.mihov@gmail.com>
2019-04-12 10:38:44 +0300

    Do not quote the sources in the docs

--- lib/sanbase_web/graphql/schema/social_data_queries.ex
+++ lib/sanbase_web/graphql/schema/social_data_queries.ex
@@ -17,11 +17,11 @@ defmodule SanbaseWeb.Graphql.Schema.SocialDataQueries do
       * from - a string representation of datetime value according to the iso8601 standard, e.g. "2018-04-16T10:02:19Z"
       * to - a string representation of datetime value according to the iso8601 standard, e.g. "2018-04-16T10:02:19Z"
       * source - the source of mention counts, one of the following:
-        1. "PROFESSIONAL_TRADERS_CHAT" - shows the relative social dominance of this project on the web chats where trades talk
-        2. "TELEGRAM" - shows the relative social dominance of this project in the telegram crypto channels
-        3. "DISCORD" - shows the relative social dominance of this project on discord crypto communities
-        4. "REDDIT" - shows the relative social dominance of this project on crypto subreddits
-        5. "ALL" - shows the average value of the social dominance across all sources
+        1. PROFESSIONAL_TRADERS_CHAT - shows the relative social dominance of this project on the web chats where trades talk
+        2. TELEGRAM - shows the relative social dominance of this project in the telegram crypto channels
+        3. DISCORD - shows the relative social dominance of this project on discord crypto communities
+        4. REDDIT - shows the relative social dominance of this project on crypto subreddits
+        5. ALL - shows the average value of the social dominance across all sources
     """
     field :social_dominance, list_of(:social_dominance) do
       arg(:slug, non_null(:string))

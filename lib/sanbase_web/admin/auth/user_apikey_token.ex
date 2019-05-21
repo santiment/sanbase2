65fccdac1bcbaa9289bc104d1561779964d627f5
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-05-21 15:40:03 +0300

    Include username and email to the apikey token exporter

--- lib/sanbase_web/admin/auth/user_apikey_token.ex
+++ lib/sanbase_web/admin/auth/user_apikey_token.ex
@@ -6,5 +6,15 @@ defmodule Sanbase.ExAdmin.Auth.UserApikeyToken do
     # out of it. To generate the whole apikey a secret key, known only by the
     # server is needed.
     action_items(only: [:show])
+
+    csv([
+      {"Id", & &1.id},
+      {"Token", & &1.token},
+      {"User Id", & &1.user.id},
+      {"User Email", & &1.user.email},
+      {"Username", & &1.user.username},
+      {"Inserted at", & &1.inserted_at},
+      {"Updated at", & &1.updated_at}
+    ])
   end
 end

8ccf09b29f3a1903540f53f73caacdbf26f90418
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-06-03 15:11:35 +0300

    Better error messages
    Co-Authored-By: ivan <IvanIvanoff@users.noreply.github.com>

--- lib/sanbase/notifications/insight.ex
+++ lib/sanbase/notifications/insight.ex
@@ -15,11 +15,11 @@ defmodule Sanbase.Notifications.Insight do
 
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
         Logger.error("Can't publish insight [#{post.id}] in discord: code[#{status_code}]")
-        {:error, "Can't publish insight"}
+        {:error, "Can't publish insight creation notification to Discord"}
 
       {:error, error} ->
         Logger.error("Cannot publish insight [#{post.id}] in discord: " <> inspect(error))
-        {:error, "Can't publish insight"}
+        {:error, "Can't publish insight creation notification to Discord"}
     end
   end
 

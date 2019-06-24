07a1a886fdd7ebbba9871fa368d7afe7d48b2707
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-06-24 12:51:02 +0300

    Run migrateion to create product and plans in stripe only if stripe api key is present

--- priv/repo/migrations/20190624081130_create_plans_and_products_in_stripe.exs
+++ priv/repo/migrations/20190624081130_create_plans_and_products_in_stripe.exs
@@ -1,6 +1,9 @@
 defmodule Sanbase.Repo.Migrations.CreatePlansAndProductsInStripe do
   use Ecto.Migration
 
+  require Sanbase.Utils.Config
+
+  alias Sanbase.Utils.Config
   alias Sanbase.Pricing.{Product, Plan}
   alias Sanbase.Repo
 
@@ -9,12 +12,23 @@ defmodule Sanbase.Repo.Migrations.CreatePlansAndProductsInStripe do
     Application.ensure_all_started(:prometheus_ecto)
     Sanbase.Prometheus.EctoInstrumenter.setup()
 
-    Product |> Repo.all() |> Enum.map(&Product.maybe_create_product_in_stripe/1)
+    stripe_api_key = stripe_api_key()
 
-    Plan |> Repo.all() |> Repo.preload(:product) |> Enum.map(&Plan.maybe_create_plan_in_stripe/1)
+    if stripe_api_key != nil and stripe_api_key != "" do
+      Product |> Repo.all() |> Enum.map(&Product.maybe_create_product_in_stripe/1)
+
+      Plan
+      |> Repo.all()
+      |> Repo.preload(:product)
+      |> Enum.map(&Plan.maybe_create_plan_in_stripe/1)
+    end
   end
 
   def down do
     :ok
+  end
+
+  defp stripe_api_key() do
+    Config.module_get(Sanbase.StripeConfig, :api_key)
   end
 end

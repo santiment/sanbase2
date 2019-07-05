4d99f70e6465179af0444ab440db300aac576ffe
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-07-05 18:05:45 +0300

    Gracefully handle errors in user settings update resolver

--- lib/sanbase_web/graphql/resolvers/user_settings_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/user_settings_resolver.ex
@@ -1,6 +1,8 @@
 defmodule SanbaseWeb.Graphql.Resolvers.UserSettingsResolver do
   require Logger
 
+  import SanbaseWeb.Graphql.Helpers.Utils, only: [error_details: 1]
+
   alias Sanbase.Auth.{User, UserSettings}
   alias SanbaseWeb.Graphql.Helpers.Utils
 
@@ -11,9 +13,13 @@ defmodule SanbaseWeb.Graphql.Resolvers.UserSettingsResolver do
   def update_user_settings(_root, %{settings: settings}, %{
         context: %{auth: %{current_user: current_user}}
       }) do
-    {:ok, %{settings: settings}} = UserSettings.update_settings(current_user, settings)
+    case UserSettings.update_settings(current_user, settings) do
+      {:ok, %{settings: settings}} ->
+        {:ok, settings}
 
-    {:ok, settings}
+      {:error, changeset} ->
+        {:error, "Cannot update user settings. Reason: #{error_details(changeset)}"}
+    end
   end
 
   def settings_toggle_channel(_root, args, %{

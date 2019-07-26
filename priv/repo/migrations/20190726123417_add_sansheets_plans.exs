ffdbce2f32c1993753cf7fb0a76768dec584ca62
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-07-26 15:41:11 +0300

    Add sansheets plans

---
+++ priv/repo/migrations/20190726123417_add_sansheets_plans.exs
@@ -0,0 +1,20 @@
+defmodule Sanbase.Repo.Migrations.AddSansheetsPlans do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    INSERT INTO plans (id, name, product_id, amount, currency, interval) VALUES
+      (21, 'FREE', 3, 0, 'USD', 'month'),
+      (22, 'BASIC', 3, 8900, 'USD', 'month'),
+      (23, 'PRO', 3, 18900, 'USD', 'month'),
+      (24, 'ENTERPRISE', 3, 0, 'USD', 'month'),
+      (25, 'BASIC', 3, 8000, 'USD', 'year'),
+      (26, 'PRO', 3, 17000, 'USD', 'year'),
+      (27, 'ENTERPRISE', 3, 0, 'USD', 'year')
+    """)
+  end
+
+  def down do
+    execute("DELETE FROM plans where id IN (21, 22, 23, 24, 25, 26, 27)")
+  end
+end

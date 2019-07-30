b19ada1f96b72bfc9fbb4aca2060e6bbe6886217
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-07-30 15:17:09 +0300

    Fix wrongly applied logo migration
    An old version of a migration was applied and subsequent changes were
    not deployed because the migration version was already part of
    schem_migrations. Manually execute the remove if exists because the DSL
    for it is available only in Ecto 3.0

---
+++ priv/repo/migrations/20190730121330_rename_logo_columns.exs
@@ -0,0 +1,25 @@
+defmodule Sanbase.Repo.Migrations.RenameLogoColumns do
+  use Ecto.Migration
+
+  def up do
+    # Drop all possible logo columns. The frist 2 are present on stage
+    # The second 2 will be present on prod because the migration that will execute
+    # is the new one. They must be dropped so the `add` succeeds
+    execute("ALTER TABLE project DROP COLUMN IF EXISTS logo_url_32;")
+    execute("ALTER TABLE project DROP COLUMN IF EXISTS logo_url_64;")
+    execute("ALTER TABLE project DROP COLUMN IF EXISTS logo_32_url;")
+    execute("ALTER TABLE project DROP COLUMN IF EXISTS logo_64_url;")
+
+    alter table("project") do
+      add(:logo_32_url, :string)
+      add(:logo_64_url, :string)
+    end
+  end
+
+  def down do
+    alter table("project") do
+      remove(:logo_32_url)
+      remove(:logo_64_url)
+    end
+  end
+end

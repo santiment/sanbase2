bf0b2b8fdfad020110bacc3941a5177d602fabf7
Vassil Kalkov <vassilkalkov@gmail.com>
2019-08-05 14:35:44 +0300

    Add migration for renaming logo columns

---
+++ priv/repo/migrations/20190805111217_rename_logo_columns_again.exs
@@ -0,0 +1,13 @@
+defmodule Sanbase.Repo.Migrations.RenameLogoFields do
+  use Ecto.Migration
+
+  def up do
+    rename(table("project"), :logo_32_url, to: :logo32_url)
+    rename(table("project"), :logo_64_url, to: :logo64_url)
+  end
+
+  def down do
+    rename(table("project"), :logo32_url, to: :logo_32_url)
+    rename(table("project"), :logo64_url, to: :logo_64_url)
+  end
+end

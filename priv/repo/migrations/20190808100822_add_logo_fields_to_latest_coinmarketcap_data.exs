e0cbc960fc1a8bb02daefd3589b88026a144c6d9
Vassil Kalkov <vassilkalkov@gmail.com>
2019-08-08 13:14:41 +0300

    Add logo_hash to latest_coinmarketcap_data

---
+++ priv/repo/migrations/20190808100822_add_logo_fields_to_latest_coinmarketcap_data.exs
@@ -0,0 +1,10 @@
+defmodule Sanbase.Repo.Migrations.AddLogoFieldsToLatestCoinmarketcapData do
+  use Ecto.Migration
+
+  def change do
+    alter table(:latest_coinmarketcap_data) do
+      add(:logo_hash, :string)
+      add(:logo_updated_at, :naive_datetime)
+    end
+  end
+end

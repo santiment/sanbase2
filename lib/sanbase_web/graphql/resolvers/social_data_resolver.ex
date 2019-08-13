c96a7e569c1574556f0c749bac44df3793019912
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-08-13 11:59:30 +0300

    Reformat the social data resolver in a concise way

--- lib/sanbase_web/graphql/resolvers/social_data_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/social_data_resolver.ex
@@ -32,20 +32,10 @@ defmodule SanbaseWeb.Graphql.Resolvers.SocialDataResolver do
 
   def emojis_sentiment(
         _root,
-        %{
-          from: from,
-          to: to,
-          interval: interval,
-          result_size_tail: result_size_tail
-        },
+        %{from: from, to: to, interval: interval, result_size_tail: result_size_tail},
         _resolution
       ) do
-    TechIndicators.emojis_sentiment(
-      from,
-      to,
-      interval,
-      result_size_tail
-    )
+    TechIndicators.emojis_sentiment(from, to, interval, result_size_tail)
   end
 
   def social_volume(
@@ -68,74 +58,39 @@ defmodule SanbaseWeb.Graphql.Resolvers.SocialDataResolver do
     )
   end
 
-  def social_volume_projects(
-        _root,
-        %{},
-        _resolution
-      ) do
+  def social_volume_projects(_root, %{}, _resolution) do
     TechIndicators.social_volume_projects()
   end
 
   def topic_search(
         _root,
-        %{
-          source: source,
-          search_text: search_text,
-          from: from,
-          to: to,
-          interval: interval
-        },
+        %{source: source, search_text: search_text, from: from, to: to, interval: interval},
         _resolution
       ) do
-    TechIndicators.topic_search(
-      source,
-      search_text,
-      from,
-      to,
-      interval
-    )
+    TechIndicators.topic_search(source, search_text, from, to, interval)
   end
 
   def trending_words(
         _root,
-        %{
-          source: source,
-          size: size,
-          hour: hour,
-          from: from,
-          to: to
-        },
+        %{source: source, size: size, hour: hour, from: from, to: to},
         _resolution
       ) do
-    size = Enum.min([size, 100])
+    size = Enum.min([size, 30])
     SocialData.trending_words(source, size, hour, from, to)
   end
 
   def word_context(
         _root,
-        %{
-          word: word,
-          source: source,
-          size: size,
-          from: from,
-          to: to
-        },
+        %{word: word, source: source, size: size, from: from, to: to},
         _resolution
       ) do
-    size = Enum.min([size, 100])
+    size = Enum.min([size, 30])
     SocialData.word_context(word, source, size, from, to)
   end
 
-  def word_context(
-        %{word: word},
-        _args,
-        resolution
-      ) do
-    %{
-      source: source,
-      from: from,
-      to: to
-    } = Utils.extract_root_query_args(resolution, "trending_words")
+  def word_context(%{word: word}, _args, resolution) do
+    %{source: source, from: from, to: to} =
+      Utils.extract_root_query_args(resolution, "trending_words")
 
     async(fn ->
       SocialData.word_context(word, source, @context_words_default_size, from, to)
@@ -144,12 +99,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.SocialDataResolver do
 
   def word_trend_score(
         _root,
-        %{
-          word: word,
-          source: source,
-          from: from,
-          to: to
-        },
+        %{word: word, source: source, from: from, to: to},
         _resolution
       ) do
     SocialData.word_trend_score(word, source, from, to)
@@ -165,39 +115,13 @@ defmodule SanbaseWeb.Graphql.Resolvers.SocialDataResolver do
 
   def social_dominance(
         _root,
-        %{
-          slug: slug,
-          from: from,
-          to: to,
-          interval: interval,
-          source: source
-        },
+        %{slug: slug, from: from, to: to, interval: interval, source: source},
         _resolution
       ) do
-    SocialData.social_dominance(
-      slug,
-      from,
-      to,
-      interval,
-      source
-    )
+    SocialData.social_dominance(slug, from, to, interval, source)
   end
 
-  def news(
-        _root,
-        %{
-          tag: tag,
-          from: from,
-          to: to,
-          size: size
-        },
-        _resolution
-      ) do
-    SocialData.google_news(
-      tag,
-      from,
-      to,
-      size
-    )
+  def news(_root, %{tag: tag, from: from, to: to, size: size}, _resolution) do
+    SocialData.google_news(tag, from, to, size)
   end
 end

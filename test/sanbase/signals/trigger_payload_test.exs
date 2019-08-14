818fb5779e523a7247699671e1da570653b0b41d
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-08-14 17:51:46 +0300

    Fix test for checking the payload extension

--- test/sanbase/signals/trigger_payload_test.exs
+++ test/sanbase/signals/trigger_payload_test.exs
@@ -4,8 +4,7 @@ defmodule Sanbase.Signal.TriggerPayloadTest do
   import Mock
   import Sanbase.Factory
   import Sanbase.TestHelpers
-  alias Sanbase.Signal.UserTrigger
-  alias Sanbase.Signal.Evaluator
+  alias Sanbase.Signal.{UserTrigger, Scheduler}
   alias Sanbase.Signal.Trigger.DailyActiveAddressesSettings
 
   setup_with_mocks([
@@ -16,83 +15,53 @@ defmodule Sanbase.Signal.TriggerPayloadTest do
      ]}
   ]) do
     Sanbase.Signal.Evaluator.Cache.clear()
-
     user = insert(:user)
     Sanbase.Auth.UserSettings.set_telegram_chat_id(user.id, 123_123_123_123)
 
-    Sanbase.Factory.insert(:project, %{
-      name: "Santiment",
-      ticker: "SAN",
-      coinmarketcap_id: "santiment",
-      main_contract_address: "0x7c5a0ce9267ed19b22f8cae653f198e3e8daf098"
-    })
+    datetimes = generate_datetimes(~U[2019-01-01 00:00:00Z], "1d", 7)
+    project = insert(:random_project)
 
-    trigger_settings1 = %{
+    [user: user, project: project, datetimes: datetimes]
+  end
+
+  test "payload is extended", context do
+    %{user: user, project: project, datetimes: datetimes} = context
+
+    daily_active_addresses =
+      Enum.zip(datetimes, [100, 120, 100, 80, 20, 10, 5])
+      |> Enum.map(&%{datetime: elem(&1, 0), active_addresses: elem(&1, 1)})
+
+    trigger_settings = %{
       type: "daily_active_addresses",
-      target: %{slug: "santiment"},
+      target: %{slug: project.coinmarketcap_id},
       channel: "telegram",
-      operation: %{above: 5},
+      time_window: "1d",
+      operation: %{above: 5}
     }
 
-    {:ok, trigger1} =
+    {:ok, trigger} =
       UserTrigger.create_user_trigger(user, %{
         title: "Generic title",
         is_public: true,
         cooldown: "12h",
-        settings: trigger_settings1
+        settings: trigger_settings
       })
 
-    datetimes = generate_datetimes(~U[2019-01-01 00:00:00Z], "1d", 7)
+    self_pid = self()
 
-    [
-      user: user,
-      trigger1: trigger1,
-      datetimes: datetimes
-    ]
-  end
+    with_mocks([
+      {DailyActiveAddressesSettings, [:passthrough],
+       get_data: fn _ -> [{project.coinmarketcap_id, daily_active_addresses}] end},
+      {Sanbase.Telegram, [:passthrough],
+       send_message: fn _user, text ->
+         send(self_pid, {:telegram_to_self, text})
+         :ok
+       end}
+    ]) do
+      Scheduler.run_signal(DailyActiveAddressesSettings)
 
-  test "payload is extended", context do
-      payload = "Some payload"
-
-      target_list =
-        Enum.zip(context.datetimes, [100, 120, 100, 80, 20, 10, 5])
-        |> IO.inspect()
-        |> Enum.map(&%{datetime: elem(&1, 0), active_addresses: elem(&1, 1)})
-        |> IO.inspect()
-
-      with_mock DailyActiveAddressesSettings, [:passthrough],
-        get_data: fn _ ->
-          [{"santiment", target_list}]
-        end do
-        trigger_settings1 = %{
-          type: "daily_active_addresses",
-          target: %{slug: "santiment"},
-          channel: "telegram",
-          time_window: "1d",
-          operation: %{percent_up: 2}
-        }
-
-        {:ok, trigger1} =
-          UserTrigger.create_user_trigger(context.user, %{
-            title: "Generic title",
-            is_public: true,
-            cooldown: "12h",
-            settings: trigger_settings1
-          })
-
-        UserTrigger.update_user_trigger(context.user, %{
-          id: trigger1.id,
-          last_triggered: %{}
-        })
-
-        type = DailyActiveAddressesSettings.type()
-
-        triggered =
-          DailyActiveAddressesSettings.type()
-          |> UserTrigger.get_active_triggers_by_type()
-          |> Sanbase.Signal.Evaluator.run(type)
-
-        assert triggered =~ payload
-      end
+      assert_receive({:telegram_to_self, message}, 1000)
+      assert message =~ SanbaseWeb.Endpoint.show_signal_url(trigger.id)
+    end
   end
 end

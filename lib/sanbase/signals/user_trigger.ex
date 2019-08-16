82694c1e61f152ebd4366597b27a3bd806770102
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-08-16 10:14:13 +0300

    simplify further using count(*)

--- lib/sanbase/signals/user_trigger.ex
+++ lib/sanbase/signals/user_trigger.ex
@@ -81,9 +81,8 @@ defmodule Sanbase.Signal.UserTrigger do
 
   @spec triggers_count_for(%User{}) :: integer()
   def triggers_count_for(user) do
-    from(ut in UserTrigger, where: ut.user_id == ^user.id)
-    |> Repo.all()
-    |> Enum.count()
+    from(ut in UserTrigger, where: ut.user_id == ^user.id, select: fragment("count(*)"))
+    |> Repo.one()
   end
 
   @doc ~s"""

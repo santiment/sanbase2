e1f6a2225379b06287ca7d01f1c5a636d0b90986
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-08-19 14:35:41 +0300

    Rework the timeline events API to return more user-friendly response

--- lib/sanbase_web/graphql/resolvers/timeline/timeline_event_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/timeline/timeline_event_resolver.ex
@@ -1,11 +1,31 @@
 defmodule SanbaseWeb.Graphql.Resolvers.TimelineEventResolver do
   require Logger
 
+  import SanbaseWeb.Graphql.Helpers.Utils, only: [transform_user_trigger: 1]
   alias Sanbase.Timeline.TimelineEvent
 
   def timeline_events(_root, args, %{
         context: %{auth: %{current_user: current_user}}
       }) do
-    TimelineEvent.events(current_user, args)
+    case TimelineEvent.events(current_user, args) do
+      {:ok, %{events: events} = result} ->
+        events =
+          events
+          |> Enum.map(fn
+            %{user_trigger: ut} = elem when not is_nil(ut) ->
+              elem
+              |> Map.from_struct()
+              |> Map.delete(:user_trigger)
+              |> Map.put(:trigger, Map.get(transform_user_trigger(ut), :trigger))
+
+            elem ->
+              elem
+          end)
+
+        {:ok, %{result | events: events}}
+
+      {:error, error} ->
+        {:error, error}
+    end
   end
 end

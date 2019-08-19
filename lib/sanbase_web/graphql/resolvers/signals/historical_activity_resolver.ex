7be778527d3e2b5947dc22eb6bd795344cb1f069
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-08-19 14:56:42 +0300

    Abstract the transformation from user_trigger to just trigger in API

--- lib/sanbase_web/graphql/resolvers/signals/historical_activity_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/signals/historical_activity_resolver.ex
@@ -1,5 +1,5 @@
 defmodule SanbaseWeb.Graphql.Resolvers.SignalsHistoricalActivityResolver do
-  import SanbaseWeb.Graphql.Helpers.Utils, only: [transform_user_trigger: 1]
+  import SanbaseWeb.Graphql.Helpers.Utils, only: [replace_user_trigger_with_trigger: 1]
 
   alias Sanbase.Signal.HistoricalActivity
 
@@ -10,16 +10,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.SignalsHistoricalActivityResolver do
       }) do
     case HistoricalActivity.fetch_historical_activity_for(current_user, args) do
       {:ok, %{activity: activity} = result} ->
-        activity =
-          activity
-          |> Enum.map(fn %{user_trigger: ut} = elem ->
-            elem
-            |> Map.from_struct()
-            |> Map.delete(:user_trigger)
-            |> Map.put(:trigger, Map.get(transform_user_trigger(ut), :trigger))
-          end)
-
-        {:ok, %{result | activity: activity}}
+        {:ok, %{result | activity: replace_user_trigger_with_trigger(activity)}}
 
       {:error, error} ->
         {:error, error}
--- lib/sanbase_web/graphql/resolvers/timeline/timeline_event_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/timeline/timeline_event_resolver.ex
@@ -1,7 +1,7 @@
 defmodule SanbaseWeb.Graphql.Resolvers.TimelineEventResolver do
   require Logger
 
-  import SanbaseWeb.Graphql.Helpers.Utils, only: [transform_user_trigger: 1]
+  import SanbaseWeb.Graphql.Helpers.Utils, only: [replace_user_trigger_with_trigger: 1]
   alias Sanbase.Timeline.TimelineEvent
 
   def timeline_events(_root, args, %{
@@ -9,20 +9,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.TimelineEventResolver do
       }) do
     case TimelineEvent.events(current_user, args) do
       {:ok, %{events: events} = result} ->
-        events =
-          events
-          |> Enum.map(fn
-            %{user_trigger: ut} = elem when not is_nil(ut) ->
-              elem
-              |> Map.from_struct()
-              |> Map.delete(:user_trigger)
-              |> Map.put(:trigger, Map.get(transform_user_trigger(ut), :trigger))
-
-            elem ->
-              elem
-          end)
-
-        {:ok, %{result | events: events}}
+        {:ok, %{result | events: replace_user_trigger_with_trigger(events)}}
 
       {:error, error} ->
         {:error, error}

e3bd25b22653600f0d8d36cd42922fb6c7347e8e
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-08-19 15:57:15 +0300

    Refactoring of promo subscription implementation

--- lib/sanbase/billing/subscription.ex
+++ lib/sanbase/billing/subscription.ex
@@ -17,8 +17,13 @@ defmodule Sanbase.Billing.Subscription do
 
   require Logger
 
-  @promo_plans [5, 14, 24]
   @promo_trial_period_days 14
+  @doc """
+  Enterprise plans from API, Sanbase and Sheets products are used for #{@promo_trial_period_days} days
+  promotional free trial of our services.
+  """
+  @promo_plans [5, 14, 24]
+  @promo_metadata %{"promo_event" => "devcon2019"}
 
   @percent_discount_1000_san 20
   @percent_discount_200_san 4
@@ -278,40 +283,25 @@ defmodule Sanbase.Billing.Subscription do
 
   def plan_name(subscription), do: subscription.plan.name
 
-  def promo_subscription(user) do
-    unless user.stripe_customer_id do
-      create_or_update_stripe_customer(user)
-    end
-
+  def promo_subscription(%User{stripe_customer_id: stripe_customer_id} = user)
+      when is_binary(stripe_customer_id) do
     promo_subscribe(user)
   end
 
-  defp promo_subscribe(user) do
-    from(p in Plan, where: p.id in ^@promo_plans)
-    |> Repo.all()
-    |> Enum.map(&promo_subscribe(user, &1))
-    |> Enum.all?(&match?({:ok, _}, &1))
+  def promo_subscription(%User{} = user) do
+    create_or_update_stripe_customer(user)
     |> case do
-      true ->
-        user_subscriptions(user)
+      {:ok, user} ->
+        promo_subscribe(user)
 
-      false ->
+      {:error, error} ->
+        Logger.error(
+          "Error creating promotional subscription for user: #{inspect(user)}, reason: #{
+            inspect(error)
+          }"
+        )
+
         {:error, @generic_error_message}
-    end
-  end
-
-  defp promo_subscribe(user, plan) do
-    data = %{
-      customer: user.stripe_customer_id,
-      items: [%{plan: plan.stripe_id}],
-      trial_period_days: @promo_trial_period_days,
-      metadata: %{"devcon2019" => true},
-      cancel_at: Timex.shift(Timex.now(), days: @promo_trial_period_days) |> DateTime.to_unix()
-    }
-
-    with {:ok, stripe_subscription} <- StripeApi.create_subscription(data),
-         {:ok, subscription} <- create_subscription_db(stripe_subscription, user, plan) do
-      {:ok, subscription}
     end
   end
 
@@ -403,5 +393,43 @@ defmodule Sanbase.Billing.Subscription do
       {:ok, %Decimal{} = balance} -> balance |> Decimal.to_float()
       _ -> 0
     end
+  end
+
+  defp promo_subscribe(user) do
+    from(p in Plan, where: p.id in ^@promo_plans)
+    |> Repo.all()
+    |> Enum.map(&promo_subscribe(user, &1))
+    |> Enum.filter(&match?({:error, _}, &1))
+    |> case do
+      [] ->
+        {:ok, user_subscriptions(user)}
+
+      errors ->
+        Logger.error(
+          "Error creating promotional subscription for user: #{inspect(user)}, reason: #{
+            inspect(errors)
+          }"
+        )
+
+        {:error, @generic_error_message}
+    end
+  end
+
+  defp promo_subscribe(user, plan) do
+    with {:ok, stripe_subscription} <-
+           promotional_subsciption_data(user, plan) |> StripeApi.create_subscription(),
+         {:ok, subscription} <- create_subscription_db(stripe_subscription, user, plan) do
+      {:ok, subscription}
+    end
+  end
+
+  defp promotional_subsciption_data(user, plan) do
+    %{
+      customer: user.stripe_customer_id,
+      items: [%{plan: plan.stripe_id}],
+      trial_period_days: @promo_trial_period_days,
+      metadata: @promo_metadata,
+      cancel_at: Timex.shift(Timex.now(), days: @promo_trial_period_days) |> DateTime.to_unix()
+    }
   end
 end

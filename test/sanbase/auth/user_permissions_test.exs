6159ab634d74f4bf273a397c126af57b14100998
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-09-02 16:58:08 +0300

    Optimize subscription db queries

--- test/sanbase/auth/user_permissions_test.exs
+++ test/sanbase/auth/user_permissions_test.exs
@@ -1,20 +1,20 @@
 defmodule Sanbase.Auth.UserPermissionsTest do
   use Sanbase.DataCase, async: false
 
-  import Mockery
   import Sanbase.Factory
 
-  alias Sanbase.Auth.{User, EthAccount}
+  alias Sanbase.Auth.User
 
   setup do
+    Sanbase.Billing.TestSeed.seed_products_and_plans()
     %{user: insert(:user)}
   end
 
-  test "user with subscriptions to some products", context do
-    insert(:subscription_pro, user: context.user)
-    insert(:subscription_pro_sheets, user: context.user)
+  test "user with subscriptions to some products", %{user: user} do
+    insert(:subscription_pro, user: user)
+    insert(:subscription_pro_sheets, user: user)
 
-    assert User.permissions(context.user) == %{
+    assert User.Permissions.permissions(user) == %{
              api: true,
              sanbase: false,
              spreadsheet: true,

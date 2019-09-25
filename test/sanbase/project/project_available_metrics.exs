5e9ed7a46e4b30454d90f18b6761c33813da2d42
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-09-25 15:03:16 +0300

    Add tests for availableMetrics

---
+++ test/sanbase/project/project_available_metrics.exs
@@ -0,0 +1,35 @@
+defmodule Sanbase.Project.AvailableMetricsTest do
+  use SanbaseWeb.ConnCase, async: false
+
+  import Mock
+  import Sanbase.Factory
+  import SanbaseWeb.Graphql.TestHelpers
+
+  test "get project's available metrics" do
+    project = insert(:random_erc20_project)
+
+    with_mock(Sanbase.Clickhouse.Metric, [:passthrough],
+      available_slugs: fn -> {:ok, [project.slug]} end
+    ) do
+      result = get_available_metrics(project)
+      %{"data" => %{"projectBySlug" => %{"availableMetrics" => available_metrics}}} = result
+
+      expected_metrics = Sanbase.Clickhouse.Metric.available_metrics!()
+      assert available_metrics == expected_metrics
+    end
+  end
+
+  defp get_available_metrics(project) do
+    query = """
+    {
+      projectBySlug(slug: "#{project.slug}"){
+        availableMetrics
+      }
+    }
+    """
+
+    build_conn()
+    |> post("/graphql", query_skeleton(query))
+    |> json_response(200)
+  end
+end

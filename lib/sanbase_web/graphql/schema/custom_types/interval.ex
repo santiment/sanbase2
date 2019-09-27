6d9315e0c997587cfc5cf9f07bc9ceff0f6139d1
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-09-27 18:25:03 +0300

    Extend the interval type documentation

--- lib/sanbase_web/graphql/schema/custom_types/interval.ex
+++ lib/sanbase_web/graphql/schema/custom_types/interval.ex
@@ -6,8 +6,24 @@ defmodule SanbaseWeb.Graphql.CustomTypes.Interval do
 
   scalar :interval, name: "interval" do
     description("""
-    The `interval` scalar type represents arbitrary interval, represented as UTF-8
-    character sequences.
+    The `interval` scalar type represents arbitrary time range interval,
+    represented as UTF-8 character sequences (string). The interval starts
+    with a number and ends with one of the following suffixes:
+    - s - represents second
+    - m - represents minute
+    - h - represents hour
+    - d - represents day
+    - w - represents week
+
+    There is no predefined suffix for a month due to not fixed number of days
+    a month can have.
+
+    Examples for valid intervals:
+    - 5m - 5 minutes
+    - 12h - 12 hours
+    - 1d - 1 day
+    - 30d - 30 days
+    - 2w - 2 weeks
     """)
 
     serialize(&encode/1)

1a6b3aee4ff74afc15bd07225f7a6f7e3843748f
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-11-05 15:54:38 +0200

    Make the current metric tests use the timeseries metrics only

--- test/sanbase/metric/metric_test.exs
+++ test/sanbase/metric/metric_test.exs
@@ -26,62 +26,64 @@ defmodule Sanbase.MetricTest do
     []
   end
 
-  test "can fetch all available metrics" do
-    metrics = Metric.available_metrics()
+  describe "timeseries data" do
+    test "can fetch all available metrics" do
+      metrics = Metric.available_timeseries_metrics()
 
-    results =
-      for metric <- metrics do
-        Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
-      end
+      results =
+        for metric <- metrics do
+          Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
+        end
 
-    assert Enum.all?(results, &match?({:ok, _}, &1))
-  end
+      assert Enum.all?(results, &match?({:ok, _}, &1))
+    end
 
-  test "cannot fetch available metrics that are not in the available list" do
-    metrics = Metric.available_metrics()
-    rand_metrics = Enum.map(1..100, fn _ -> rand_str() end)
-    rand_metrics = rand_metrics -- metrics
+    test "cannot fetch available metrics that are not in the available list" do
+      metrics = Metric.available_timeseries_metrics()
+      rand_metrics = Enum.map(1..100, fn _ -> rand_str() end)
+      rand_metrics = rand_metrics -- metrics
 
-    results =
-      for metric <- rand_metrics do
-        Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
-      end
+      results =
+        for metric <- rand_metrics do
+          Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
+        end
 
-    assert Enum.all?(results, &match?({:error, _}, &1))
-  end
+      assert Enum.all?(results, &match?({:error, _}, &1))
+    end
 
-  test "can use all available aggregations" do
-    [metric | _] = Metric.available_metrics()
-    aggregations = Metric.available_aggregations()
+    test "can use all available aggregations" do
+      [metric | _] = Metric.available_timeseries_metrics()
+      aggregations = Metric.available_aggregations()
 
-    results =
-      for aggregation <- aggregations do
-        Metric.timeseries_data(metric, "santiment", @from, @to, "1d", aggregation)
-      end
+      results =
+        for aggregation <- aggregations do
+          Metric.timeseries_data(metric, "santiment", @from, @to, "1d", aggregation)
+        end
 
-    assert Enum.all?(results, &match?({:ok, _}, &1))
-  end
+      assert Enum.all?(results, &match?({:ok, _}, &1))
+    end
 
-  test "cannot use aggregation that is not available" do
-    # Fetch some available metric
-    [metric | _] = Metric.available_metrics()
-    aggregations = Metric.available_aggregations()
-    rand_aggregations = Enum.map(1..10, fn _ -> rand_str() |> String.to_atom() end)
-    rand_aggregations = rand_aggregations -- aggregations
+    test "cannot use aggregation that is not available" do
+      # Fetch some available metric
+      [metric | _] = Metric.available_timeseries_metrics()
+      aggregations = Metric.available_aggregations()
+      rand_aggregations = Enum.map(1..10, fn _ -> rand_str() |> String.to_atom() end)
+      rand_aggregations = rand_aggregations -- aggregations
 
-    results =
-      for aggregation <- rand_aggregations do
-        Metric.timeseries_data(metric, "santiment", @from, @to, "1d", aggregation)
-      end
+      results =
+        for aggregation <- rand_aggregations do
+          Metric.timeseries_data(metric, "santiment", @from, @to, "1d", aggregation)
+        end
 
-    assert Enum.all?(results, &match?({:error, _}, &1))
-  end
+      assert Enum.all?(results, &match?({:error, _}, &1))
+    end
 
-  test "fetch a single metric" do
-    [metric | _] = Metric.available_metrics()
+    test "fetch a single metric" do
+      [metric | _] = Metric.available_timeseries_metrics()
 
-    result = Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
+      result = Metric.timeseries_data(metric, "santiment", @from, @to, "1d", :avg)
 
-    assert result == {:ok, @resp}
+      assert result == {:ok, @resp}
+    end
   end
 end

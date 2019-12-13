d8a0e966671ae6a3da5326c09f9ac55d89ac4ff0
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2019-12-13 15:18:43 +0200

    Add access to queries

--- lib/sanbase_web/graphql/schema/queries/exchange_queries.ex
+++ lib/sanbase_web/graphql/schema/queries/exchange_queries.ex
@@ -10,6 +10,8 @@ defmodule SanbaseWeb.Graphql.Schema.ExchangeQueries do
     Returns last market depth calculations for given exchange and ticker pair
     """
     field :last_exchange_market_depth, list_of(:exchange_market_depth) do
+      meta(access: :free)
+
       arg(:exchange, non_null(:string))
       arg(:ticker_pair, non_null(:string))
       arg(:limit, non_null(:integer), default_value: 100)
@@ -21,6 +23,8 @@ defmodule SanbaseWeb.Graphql.Schema.ExchangeQueries do
     Returns last trades for given exchange and ticker pair
     """
     field :last_exchange_trades, list_of(:exchange_trade) do
+      meta(access: :free)
+
       arg(:exchange, non_null(:string))
       arg(:ticker_pair, non_null(:string))
       arg(:limit, non_null(:integer), default_value: 100)
@@ -33,6 +37,8 @@ defmodule SanbaseWeb.Graphql.Schema.ExchangeQueries do
     Optionally the data can be aggregated and put into interval length buckets if `interval` arg is used.
     """
     field :exchange_trades, list_of(:exchange_trade) do
+      meta(access: :free)
+
       arg(:exchange, non_null(:string))
       arg(:ticker_pair, non_null(:string))
       arg(:from_slug, non_null(:datetime))
@@ -46,6 +52,8 @@ defmodule SanbaseWeb.Graphql.Schema.ExchangeQueries do
     Returns the mapping between exchange market pair and asset slugs.
     """
     field :exchange_market_pair_slug_mapping, :slug_pair do
+      meta(access: :free)
+
       arg(:exchange, non_null(:string))
       arg(:ticker_pair, non_null(:string))
 

a12d8357e42b366cef4d08b86c54b9ebfa3ae44c
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-01-14 15:30:59 +0200

    Fix payload render in timeline events in admin panel

--- lib/sanbase_web/admin/timeline_event.ex
+++ lib/sanbase_web/admin/timeline_event.ex
@@ -4,4 +4,10 @@ defmodule Sanbase.ExAdmin.TimelineEvent do
   register_resource Sanbase.Timeline.TimelineEvent do
     action_items(only: [:show, :edit, :delete])
   end
+
+  defimpl ExAdmin.Render, for: Tuple do
+    def to_string({_type, field}) do
+      Atom.to_string(field)
+    end
+  end
 end

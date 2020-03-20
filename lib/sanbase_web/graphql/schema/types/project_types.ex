0e819270a6b4b319513c867d82548f05d90c6366
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2020-03-20 17:58:45 +0200

    Remove cache_resolve for available_metrics

--- lib/sanbase_web/graphql/schema/types/project_types.ex
+++ lib/sanbase_web/graphql/schema/types/project_types.ex
@@ -109,7 +109,7 @@ defmodule SanbaseWeb.Graphql.ProjectTypes do
     ```
     """
     field :available_metrics, list_of(:string) do
-      cache_resolve(&ProjectMetricsResolver.available_metrics/3, ttl: 1200)
+      resolve(&ProjectMetricsResolver.available_metrics/3)
     end
 
     @desc ~s"""

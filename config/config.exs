b512fa2d717a65279beb820d4dd4f3e0eb87e398
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2020-06-09 19:10:50 +0300

    Replace pool_timeout with the new queue config options

--- config/config.exs
+++ config/config.exs
@@ -43,13 +43,18 @@ config :kaffe,
 config :sanbase, Sanbase.ExternalServices.RateLimiting.Server,
   implementation_module: Sanbase.ExternalServices.RateLimiting.WaitServer
 
-config :sanbase, Sanbase.ClickhouseRepo, adapter: Ecto.Adapters.Postgres
+config :sanbase, Sanbase.ClickhouseRepo,
+  adapter: Ecto.Adapters.Postgres,
+  queue_target: 1000,
+  queue_interval: 1000
 
 config :sanbase, Sanbase.Repo,
   loggers: [Ecto.LogEntry, Sanbase.Prometheus.EctoInstrumenter],
   adapter: Ecto.Adapters.Postgres,
   pool_size: {:system, "SANBASE_POOL_SIZE", "20"},
   max_overflow: 5,
+  queue_target: 500,
+  queue_interval: 1000,
   # because of pgbouncer
   prepare: :unnamed,
   migration_timestamps: [type: :naive_datetime_usec]

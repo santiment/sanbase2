421c03772a91f8b9a5fe6dcd4a4ee1c363153e6e
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-06-23 15:25:15 +0300

    Pass boolean expression instead of function

--- lib/sanbase/timeline/post_process.ex
+++ lib/sanbase/timeline/post_process.ex
@@ -3,7 +3,10 @@ defmodule Sanbase.Timeline.PostProcess do
   Module with post processing fuctions for the timeline events.
   """
 
-  # All tags: [:by_me, :by_sanfam, :by_followed, :insight, :pulse, :alert]
+  @doc """
+  Tag timeline events by owner and by type.
+  All used tags: [:by_me, :by_sanfam, :by_followed, :insight, :pulse, :alert]
+  """
 
   def tag(events, current_user_id \\ nil) do
     sanfamily_ids = Sanbase.Auth.Role.san_family_ids()
@@ -16,24 +19,25 @@ defmodule Sanbase.Timeline.PostProcess do
         []
       end
 
-    add_tag = fn tags, tag, pred ->
-      if pred.(), do: tags ++ [tag], else: tags
+    add_tag = fn
+      tags, _tag, false -> tags
+      tags, tag, true -> [tag | tags]
     end
 
     events
     |> Enum.map(fn event ->
       []
-      |> add_tag.(:by_me, fn -> current_user_id == event.user.id end)
-      |> add_tag.(:by_sanfam, fn -> event.user.id in sanfamily_ids end)
-      |> add_tag.(:by_followed, fn -> event.user.id in followed_users_ids end)
-      |> add_tag.(:insight, fn -> event.post != nil end)
-      |> add_tag.(:pulse, fn -> event.post != nil and event.post.is_pulse end)
-      |> add_tag.(:alert, fn -> event.user_trigger != nil end)
+      |> add_tag.(:by_me, current_user_id == event.user.id)
+      |> add_tag.(:by_sanfam, event.user.id in sanfamily_ids)
+      |> add_tag.(:by_followed, event.user.id in followed_users_ids)
+      |> add_tag.(:insight, event.post != nil)
+      |> add_tag.(:pulse, event.post != nil and event.post.is_pulse)
+      |> add_tag.(:alert, event.user_trigger != nil)
       |> add_tags(event)
     end)
   end
 
   defp add_tags(tags, event) do
-    %{event | tags: tags}
+    %{event | tags: Enum.reverse(tags)}
   end
 end

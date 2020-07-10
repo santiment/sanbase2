b8b01bb18d5ef82fb5b86ea67e0200b563da6571
LyudmilD <47607374+spiderjako@users.noreply.github.com>
2020-07-10 12:21:56 +0300

    Revert "Rework sources social metrics"

--- lib/sanbase/social_data/sentiment.ex
+++ lib/sanbase/social_data/sentiment.ex
@@ -13,9 +13,25 @@ defmodule Sanbase.SocialData.Sentiment do
 
   def sentiment(selector, from, to, interval, source, type)
       when source in [:all, "all", :total, "total"] do
-    sources_string = SocialHelper.sources() |> Enum.join(",")
+    result_tuples =
+      SocialHelper.sources()
+      |> Sanbase.Parallel.map(
+        fn source -> sentiment(selector, from, to, interval, source, type) end,
+        max_concurrency: 4
+      )
 
-    sentiment(selector, from, to, interval, sources_string, type)
+    case Enum.find(result_tuples, &match?({:error, _}, &1)) do
+      error when not is_nil(error) ->
+        error
+
+      nil ->
+        result =
+          result_tuples
+          |> Enum.flat_map(fn {:ok, data} -> data end)
+          |> Sanbase.Utils.Transform.sum_by_datetime(:value)
+
+        {:ok, result}
+    end
   end
 
   def sentiment(selector, from, to, interval, source, type) do

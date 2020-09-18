33a66d44df5c0d776b272d05c45f94cea243f4a5
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-09-18 15:22:32 +0300

    Update uniswap dustribution query

--- lib/sanbase/clickhouse/research/uniswap.ex
+++ lib/sanbase/clickhouse/research/uniswap.ex
@@ -29,28 +29,34 @@ defmodule Sanbase.Clickhouse.Research.Uniswap do
     sum(value)/1e18 as token_value
     FROM erc20_transfers
     PREWHERE (contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
-      AND (value = 400000000000000000000.)
       AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e')
     UNION ALL
     SELECT multiIf(hasAny(labels, ['decentralized_exchange']), 'decentralized_exchange',
                 hasAny(labels, ['centralized_exchange', 'deposit', 'withdrawal']), 'centralized_exchange',
                 hasAny(labels, ['dex_trader']), 'dex_trader', 'other transfers' ) as exchange_status,
-        sum(value_) as token_value
+        sum(value_transfered_after_claiming) as token_value
     FROM (
     SELECT
-        from as address,
-        splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(to), toUInt64(0)))) as labels,
-        if(value/1e18>400, 400, value/1e18)  as value_
-    FROM erc20_transfers
+        address,
+        splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(address), toUInt64(0)))) as labels,
+        if(value_transfered>value_claimed, value_claimed, value_transfered) as value_transfered_after_claiming
+    FROM(
+        SELECT
+            from as address,
+            sum(value)/1e18  as value_transfered
+        FROM erc20_transfers
+        PREWHERE contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+              AND dt >= toDateTime('2020-09-16 21:32:52')
+        GROUP BY address)
     GLOBAL ALL INNER JOIN (
-        SELECT distinct to as address
+        SELECT
+            to as address,
+            sum(value)/1e18 as value_claimed
         FROM erc20_transfers
         PREWHERE (contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
-              AND (value = 400000000000000000000.)
-              AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e'))
+              AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e')
+        GROUP BY address)
     USING address
-    PREWHERE contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
-          AND dt >= toDateTime('2020-09-17 00:06:53')
     )
     GROUP BY exchange_status
     """

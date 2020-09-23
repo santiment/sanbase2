d449cd1a878133f90b35548e520e1c5930d60079
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2020-09-23 15:27:56 +0300

    Update uniswap queries with cex_trader and cex_dex_trader

--- lib/sanbase/clickhouse/research/uniswap.ex
+++ lib/sanbase/clickhouse/research/uniswap.ex
@@ -4,17 +4,10 @@ defmodule Sanbase.Clickhouse.Research.Uniswap do
   def who_claimed() do
     {query, args} = who_claimed_query()
 
-    ClickhouseRepo.query_transform(query, args, fn [_, value] ->
-      value
-    end)
+    ClickhouseRepo.query_transform(query, args, fn [k, v] -> {k, v} end)
     |> case do
-      {:ok, [cex, other, dex]} ->
-        {:ok,
-         %{
-           centralized_exchanges: cex,
-           decentralized_exchanges: dex,
-           other_addresses: other
-         }}
+      {:ok, pairs_list} ->
+        {:ok, Map.new(pairs_list, fn {k, v} -> {String.to_atom(k), v} end)}
 
       {:error, reason} ->
         {:error, reason}
@@ -24,59 +17,61 @@ defmodule Sanbase.Clickhouse.Research.Uniswap do
   def value_distribution() do
     {query, args} = value_distribution_query()
 
-    ClickhouseRepo.query_transform(query, args, fn [_, value] ->
-      value
-    end)
+    ClickhouseRepo.query_transform(query, args, fn [k, v] -> {k, v} end)
     |> case do
-      {:ok, [total_minted, cex, dex, other, dex_trader]} ->
-        {:ok,
-         %{
-           total_minted: total_minted,
-           centralized_exchanges: cex,
-           decentralized_exchanges: dex,
-           other_transfers: other,
-           dex_trader: dex_trader
-         }}
+      {:ok, pairs_list} ->
+        {:ok, Map.new(pairs_list, fn {k, v} -> {String.to_atom(k), v} end)}
 
       {:error, reason} ->
         {:error, reason}
     end
   end
 
+  # Private Functions
+
   defp value_distribution_query() do
     query = """
-    SELECT 'total minted' as exchange_status,
-    sum(value)/1e18 as token_value
+    SELECT
+      'total_minted' AS exchange_status,
+      sum(value)/1e18 AS token_value
     FROM erc20_transfers
     PREWHERE (contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
       AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e')
+
     UNION ALL
-    SELECT multiIf(hasAny(labels, ['decentralized_exchange']), 'decentralized_exchange',
-                hasAny(labels, ['centralized_exchange', 'deposit', 'withdrawal']), 'centralized_exchange',
-                hasAny(labels, ['dex_trader']), 'dex_trader', 'other transfers' ) as exchange_status,
-        sum(value_transfered_after_claiming) as token_value
+
+    SELECT multiIf(
+                  hasAny(labels, ['decentralized_exchange']), 'decentralized_exchange',
+                  hasAny(labels, ['centralized_exchange', 'deposit']), 'centralized_exchange',
+                  hasAll(labels, ['withdrawal', 'dex_trader']), 'cex_dex_trader',
+                  hasAny(labels, ['withdrawal']), 'cex_trader',
+                  hasAny(labels, ['dex_trader']), 'dex_trader',
+                  'other_transfers') AS exchange_status,
+                  sum(value_transfered_after_claiming) AS token_value
     FROM (
-    SELECT
+      SELECT
         address,
-        splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(address), toUInt64(0)))) as labels,
-        if(value_transfered>value_claimed, value_claimed, value_transfered) as value_transfered_after_claiming
-    FROM(
-        SELECT
-            from as address,
-            sum(value)/1e18  as value_transfered
-        FROM erc20_transfers
-        PREWHERE contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
-              AND dt >= toDateTime('2020-09-16 21:32:52')
-        GROUP BY address)
-    GLOBAL ALL INNER JOIN (
-        SELECT
-            to as address,
-            sum(value)/1e18 as value_claimed
-        FROM erc20_transfers
-        PREWHERE (contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
-              AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e')
-        GROUP BY address)
-    USING address
+        splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(address), toUInt64(0)))) AS labels,
+        if(value_transfered>value_claimed, value_claimed, value_transfered) AS value_transfered_after_claiming
+
+      FROM(
+          SELECT
+            from AS address,
+            sum(value)/1e18 AS value_transfered
+          FROM erc20_transfers
+          PREWHERE contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+                AND dt >= toDateTime('2020-09-16 21:32:52')
+          GROUP BY address)
+      GLOBAL ALL INNER JOIN (
+          SELECT
+            to AS address,
+            sum(value)/1e18 AS value_claimed
+          FROM erc20_transfers
+          PREWHERE
+            contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' AND
+            from = '0x090d4613473dee047c3f2706764f49e0821d256e'
+          GROUP BY address)
+      USING address
     )
     GROUP BY exchange_status
     """
@@ -86,18 +81,21 @@ defmodule Sanbase.Clickhouse.Research.Uniswap do
 
   defp who_claimed_query() do
     query = """
-    SELECT exchange_status, sum(value_) as value__
+    SELECT exchange_status, sum(value2) AS value3
     FROM (
-        SELECT splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(to), toUInt64(0)))) as labels,
-              multiIf(hasAny(labels, ['decentralized_exchange']), 'decentralized_exchange',
-                      hasAny(labels, ['centralized_exchange', 'deposit', 'withdrawal']), 'centralized_exchange',
-                      'other addresses' ) as exchange_status,
-              value/1e18 as value_
-        FROM erc20_transfers
-        PREWHERE (contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
-            AND (from = '0x090d4613473dee047c3f2706764f49e0821d256e'))
+      SELECT
+        splitByChar(',', dictGetString('default.eth_label_dict', 'labels', tuple(cityHash64(to), toUInt64(0)))) AS labels,
+        multiIf(hasAny(labels, ['decentralized_exchange']), 'decentralized_exchange',
+                hasAny(labels, ['centralized_exchange', 'deposit']), 'centralized_exchange',
+                'other_transfers') AS exchange_status,
+                value/1e18 AS value2
+      FROM erc20_transfers
+      PREWHERE
+        contract = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' AND
+        from = '0x090d4613473dee047c3f2706764f49e0821d256e'
+    )
     GROUP BY exchange_status
-    ORDER BY value__ DESC
+    ORDER BY value3 DESC
     """
 
     {query, []}

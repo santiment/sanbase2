4b080eeb069daf1dd4449117b553bd0eb752862d
AnatoliyPozdeyev <anatoliy.pozdeyev@gmail.com>
2020-09-24 20:24:54 +0300

    Move CH change metrics to a separate json file

--- lib/sanbase/clickhouse/metric/file_handler.ex
+++ lib/sanbase/clickhouse/metric/file_handler.ex
@@ -40,6 +40,7 @@ defmodule Sanbase.Clickhouse.Metric.FileHandler do
   @external_resource Path.join(__DIR__, "metric_files/defi_metrics.json")
   @external_resource Path.join(__DIR__, "metric_files/balance_and_flow_labeled_metrics.json")
   @external_resource Path.join(__DIR__, "metric_files/uniswap_metrics.json")
+  @external_resource Path.join(__DIR__, "metric_files/change_metrics.json")
 
   @metrics_json Enum.reduce(@external_resource, [], fn file, acc ->
                   (File.read!(file) |> Jason.decode!()) ++ acc

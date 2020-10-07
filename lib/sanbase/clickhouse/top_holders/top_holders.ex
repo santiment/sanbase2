fdff6938187dc9ff02f80b6ea23d5c3478d4dd1f
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2020-10-07 17:55:56 +0300

    Add eth and erc20 top holders api

--- lib/sanbase/clickhouse/top_holders/top_holders.ex
+++ lib/sanbase/clickhouse/top_holders/top_holders.ex
@@ -6,6 +6,7 @@ defmodule Sanbase.Clickhouse.TopHolders do
   alias Sanbase.DateTimeUtils
 
   alias Sanbase.ClickhouseRepo
+  alias Sanbase.Clickhouse.Label
 
   @type percent_of_total_supply :: %{
           datetime: DateTime.t(),
@@ -13,6 +14,41 @@ defmodule Sanbase.Clickhouse.TopHolders do
           outside_exchanges: number(),
           in_top_holders_total: number()
         }
+
+  def top_holders(slug, contract, token_decimals, from, to, number_of_holders) do
+    {query, args} =
+      top_holders_query(
+        slug,
+        contract,
+        token_decimals,
+        from,
+        to,
+        number_of_holders
+      )
+
+    transform_func = fn [dt, address, value, value_usd, part_of_total] ->
+      %{
+        datetime: DateTime.from_unix!(dt),
+        address: address,
+        value: value,
+        value_usd: value_usd,
+        part_of_total: part_of_total
+      }
+    end
+
+    with {:ok, top_holders} <- ClickhouseRepo.query_transform(query, args, transform_func),
+         addresses <- Enum.map(top_holders, & &1.address),
+         {:ok, address_labels_map} <- Label.get_address_labels("santiment", addresses) do
+      labelled_top_holders =
+        top_holders
+        |> Enum.map(fn top_holder ->
+          labels = Map.get(address_labels_map, top_holder.address, [])
+          Map.put(top_holder, :labels, labels)
+        end)
+
+      {:ok, labelled_top_holders}
+    end
+  end
 
   @spec percent_of_total_supply(
           contract :: String.t(),
@@ -45,6 +81,89 @@ defmodule Sanbase.Clickhouse.TopHolders do
         }
       end
     )
+  end
+
+  # helpers
+
+  defp top_holders_query(
+         slug,
+         contract,
+         token_decimals,
+         from,
+         to,
+         number_of_holders
+       ) do
+    from_datetime_unix = DateTime.to_unix(from)
+    to_datetime_unix = DateTime.to_unix(to)
+
+    query = """
+    SELECT
+      toUnixTimestamp(dt),
+      address,
+      val as value,
+      val * price as value_usd,
+      partOfTotal
+    FROM(
+      SELECT
+        max(dt) as dt,
+        address,
+        anyLast(value) as val,
+        any(partOfTotal) as partOfTotal
+      FROM(
+        SELECT
+          dt,
+          contract,
+          address,
+          rank,
+          value / pow(10, ?3) AS value,
+          multiIf(valueTotal > 0,
+          value / (valueTotal / pow(10, ?3)),
+          0) AS partOfTotal
+        FROM(
+          SELECT *
+          FROM
+            eth_top_holders PREWHERE (contract = ?2)
+            AND (address NOT IN ('TOTAL',
+            'freeze'))
+            AND ((dt >= toStartOfDay(toDateTime(?4)))
+            AND (dt <= toStartOfDay(toDateTime(?5)))) ) GLOBAL ANY
+        LEFT JOIN (
+          SELECT
+            dt,
+            sum(value) AS valueTotal
+          FROM
+            eth_top_holders PREWHERE (contract = ?2)
+            AND (address IN ('TOTAL','freeze'))
+            AND dt >= toStartOfDay(toDateTime(?4))
+            AND dt <= toStartOfDay(toDateTime(?5))
+          GROUP BY dt
+        ) USING (dt) )
+      GROUP BY address
+      ORDER BY val DESC
+      LIMIT ?6
+    )
+    ALL LEFT JOIN (
+      SELECT
+        toStartOfDay(dt) as dt,
+        avg(value) AS price
+      FROM
+        intraday_metrics FINAL
+        PREWHERE asset_id = (SELECT asset_id FROM asset_metadata FINAL PREWHERE name = ?1 LIMIT 1)
+        AND metric_id = (SELECT metric_id FROM metric_metadata FINAL PREWHERE name = 'price_usd' LIMIT 1)
+      GROUP BY dt
+    ) USING (dt)
+    """
+
+    args = [
+      slug,
+      contract,
+      token_decimals,
+      from_datetime_unix,
+      to_datetime_unix,
+      number_of_holders
+    ]
+
+    {query, args}
   end
 
   defp percent_of_total_supply_query(

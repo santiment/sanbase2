390bce2dd0607b9ee940f25f75b8587be4aab404
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2020-10-16 13:54:58 +0300

    Migrate old price signals to metric signal

---
+++ priv/repo/migrations/20201016091443_migrate_price_percent_change_to_metric_signals.exs
@@ -0,0 +1,47 @@
+defmodule Sanbase.Repo.Migrations.MigratePricePercentChangeToMetricSignals do
+  use Ecto.Migration
+
+  alias Sanbase.Signal.Trigger.{
+    PricePercentChangeSettings,
+    PriceAbsoluteChangeSettings,
+    MetricTriggerSettings
+  }
+
+  alias Sanbase.Signal.UserTrigger
+
+  def up do
+    setup()
+    migrate_price_percent_change_signals()
+  end
+
+  def down do
+    :ok
+  end
+
+  defp migrate_price_percent_change_signals() do
+    PricePercentChangeSettings.type()
+    |> UserTrigger.get_all_triggers_by_type()
+    |> Enum.each(fn user_trigger ->
+      %{trigger: %{settings: settings}} = user_trigger
+
+      {:ok, _} =
+        UserTrigger.update_user_trigger(user_trigger.user, %{
+          id: user_trigger.id,
+          settings: %{
+            type: MetricTriggerSettings.type(),
+            metric: "price_usd",
+            target: settings.target,
+            channel: settings.channel,
+            time_window: settings.time_window,
+            operation: settings.operation
+          }
+        })
+    end)
+  end
+
+  defp setup() do
+    Application.ensure_all_started(:tzdata)
+    Application.ensure_all_started(:prometheus_ecto)
+    Sanbase.Prometheus.EctoInstrumenter.setup()
+  end
+end

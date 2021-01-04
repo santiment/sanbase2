a0aa7111183dd4291e41eedede540940b0049995
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-01-04 13:07:08 +0200

    Make api call limits' has_limits field updatable in admin panel

--- lib/sanbase_web/admin/auth/api_call_limit.ex
+++ lib/sanbase_web/admin/auth/api_call_limit.ex
@@ -2,5 +2,23 @@ defmodule SanbaseWeb.ExAdmin.ApiCallLimit do
   use ExAdmin.Register
 
   register_resource Sanbase.ApiCallLimit do
+    index do
+      column(:id)
+      column(:user, link: true)
+      column(:remote_ip)
+      column(:has_limits)
+      column(:api_calls_limit_plan)
+      column(:api_calls)
+    end
+
+    show acl do
+      attributes_table(all: true)
+    end
+
+    form acl do
+      inputs do
+        input(acl, :has_limits)
+      end
+    end
   end
 end

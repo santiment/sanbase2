4c751548e724b00f7cbb67a55cecde3b19e36bdf
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-02-24 14:48:03 +0200

    Fix sending telegram_channel alerts

--- lib/sanbase/alerts/alert.ex
+++ lib/sanbase/alerts/alert.ex
@@ -183,7 +183,11 @@ defimpl Sanbase.Alert, for: Any do
     end)
   end
 
-  defp send_telegram_channel(trigger, channel, max_alerts_to_send) do
+  defp send_telegram_channel(
+         trigger,
+         channel,
+         %{"telegram_channel" => max_alerts_to_send}
+       ) do
     fun = fn _identifier, payload ->
       # Do not extend the payload with the trigger id and link. This channel
       # would be used when serving the same alert to many users and not only

e4dc59447349065d9a495a62e85ffe7ba80c741b
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-02-26 17:38:28 +0200

    Migrate chart_configurations metrics type

---
+++ priv/repo/migrations/20210226144252_change_metrics_array_type.exs
@@ -0,0 +1,16 @@
+defmodule Sanbase.Repo.Migrations.ChangeMetricsArrayType do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER TABLE chart_configurations ALTER COLUMN metrics TYPE text[];")
+    execute("ALTER TABLE chart_configurations ALTER COLUMN metrics SET DEFAULT array[]::text[];")
+  end
+
+  def down do
+    execute("ALTER TABLE chart_configurations ALTER COLUMN metrics TYPE varchar(255)[];")
+
+    execute(
+      "ALTER TABLE chart_configurations ALTER COLUMN metrics SET DEFAULT array[]::varchar[];"
+    )
+  end
+end

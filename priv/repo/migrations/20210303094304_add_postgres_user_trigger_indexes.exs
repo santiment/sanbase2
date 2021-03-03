dbe8788794430883451339c1f29a0cd6114b6bea
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-03-03 12:16:14 +0200

    Add missing migration

---
+++ priv/repo/migrations/20210303094304_add_postgres_user_trigger_indexes.exs
@@ -0,0 +1,8 @@
+defmodule Sanbase.Repo.Migrations.AddPostgresUserTriggerIndexes do
+  use Ecto.Migration
+
+  def change do
+    create(index(:timeline_events, [:user_trigger_id]))
+    create(index(:signals_historical_activity, [:user_trigger_id]))
+  end
+end

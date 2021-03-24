dda04feab63d48ff1cdd239cdf0bd6ab25426eee
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2021-03-24 15:22:06 +0200

    Fix test

--- test/sanbase/billing/query_access_level_test.exs
+++ test/sanbase/billing/query_access_level_test.exs
@@ -15,6 +15,7 @@ defmodule Sanbase.Billing.QueryAccessLevelTest do
       expected_free_queries =
         [
           :active_widgets,
+          :all_wallet_hunter_proposals,
           :address_historical_balance_change,
           :alerts_historical_activity,
           :all_currency_projects,

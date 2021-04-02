0cb453fd889389f35a6913387a1bfddaf6db4755
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-04-02 13:01:17 +0300

    Do not raise on invalid event bus events in prod

--- lib/sanbase/event_bus/event_bus.ex
+++ lib/sanbase/event_bus/event_bus.ex
@@ -32,7 +32,14 @@ defmodule Sanbase.EventBus do
     defexception [:message]
   end
 
-  @topics [:user_events, :watchlist_events, :alert_events, :insight_events, :payment_events]
+  @topics [
+    :user_events,
+    :watchlist_events,
+    :alert_events,
+    :insight_events,
+    :payment_events,
+    :invalid_events
+  ]
   @subscribers [
     __MODULE__.KafkaExporterSubscriber,
     __MODULE__.UserEventsSubscriber,
@@ -53,19 +60,38 @@ defmodule Sanbase.EventBus do
         error_topic: Map.fetch!(params, :topic)
       })
 
-    EventSource.notify params do
-      data = Map.fetch!(params, :data)
-
-      case Sanbase.EventBus.EventValidation.valid?(data) do
-        true ->
-          data
-
-        false ->
-          raise(
-            InvalidEventError,
-            message: "Invalid event submitted: #{inspect(params)}"
-          )
+    # In case the event is not valid, in prod this will rewrite the params so
+    # the event is emitted in a special invalid_events topic. In dev/test the
+    # behavior is to raise so errors are catched straight away. Invalid events
+    # should not be emitted at all but they can slip in without good testing
+    # and in this case prod should not break
+    params =
+      case Sanbase.EventBus.EventValidation.valid?(params.data) do
+        true -> params
+        false -> handle_invalid_event(params)
       end
+
+    EventSource.notify params do
+      Map.fetch!(params, :data)
     end
+  end
+
+  case Application.compile_env(:sanbase, :env) do
+    :prod ->
+      defp handle_invalid_event(params) do
+        # Replace the topic with the invalid events topic so the other topics
+        # always contain valid events. Also put the original topic in the data
+        params
+        |> put_in([:data, :original_topic], params.topic)
+        |> Map.put(:topic, :invalid_events)
+      end
+
+    _ ->
+      defp handle_invalid_event(params) do
+        raise(
+          InvalidEventError,
+          message: "Invalid event submitted: #{inspect(params)}"
+        )
+      end
   end
 end

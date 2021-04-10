dc6e24ba4fe6666093d3a0f588f230840e8d63c3
ivan <IvanIvanoff@users.noreply.github.com>
2021-04-10 19:04:49 +0300

    Revert "Precompute the autogenerated social volume query."

--- priv/repo/migrations/20210409081625_add_timestamps_to_social_query.exs
+++
@@ -1,9 +0,0 @@
-defmodule Sanbase.Repo.Migrations.AddTimestampsToSocialQuery do
-  use Ecto.Migration
-
-  def change do
-    alter table(:project_social_volume_query) do
-      timestamps()
-    end
-  end
-end

999379985a5f467710367534582b020fc66f21ed
Atanas Ormanov <atanasormanow@gmail.com>
2021-05-31 13:54:44 +0300

    Extend signal metadata (#2735)
    * Extend signal metadata and getAccessRestrictions

---
+++ test/sanbase/signal/signal_metadata_test.exs
@@ -0,0 +1,33 @@
+defmodule Sanbase.SignalMetadataTest do
+  use Sanbase.DataCase, async: true
+
+  import Sanbase.Factory, only: [rand_str: 0]
+
+  alias Sanbase.Signal
+
+  test "can fetch metadata for all available signals" do
+    signal = Signal.available_signals()
+    results = for signal <- signal, do: Signal.metadata(signal)
+    assert Enum.all?(results, &match?({:ok, _}, &1))
+  end
+
+  test "cannot fetch metadata for not available signals" do
+    rand_signals = Enum.map(1..100, fn _ -> rand_str() end)
+    rand_signals = rand_signals -- Signal.available_signals()
+
+    results = for signal <- rand_signals, do: Signal.metadata(signal)
+
+    assert Enum.all?(results, &match?({:error, _}, &1))
+  end
+
+  test "metadata properties" do
+    signals = Signal.available_signals()
+    aggregations = Signal.available_aggregations()
+
+    for signal <- signals do
+      {:ok, metadata} = Signal.metadata(signal)
+      assert metadata.default_aggregation in aggregations
+      assert metadata.min_interval == "1m"
+    end
+  end
+end

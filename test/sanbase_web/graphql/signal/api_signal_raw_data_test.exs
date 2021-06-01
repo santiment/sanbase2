28f7c7a6a18d986caccd5df13b4ebb6a568e7ed3
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-06-01 14:14:12 +0300

    Add tests for get_raw_signals selector filtering

--- test/sanbase_web/graphql/signal/api_signal_raw_data_test.exs
+++ test/sanbase_web/graphql/signal/api_signal_raw_data_test.exs
@@ -15,7 +15,7 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     ]
   end
 
-  test "returns data for an available signal without signals filtering", context do
+  test "signal without signals filtering", context do
     %{conn: conn, from: from, to: to} = context
 
     # TODO: Update with different signals when they are added to the JSON file
@@ -39,7 +39,7 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     Sanbase.Mock.prepare_mock2(&Sanbase.ClickhouseRepo.query/2, {:ok, %{rows: rows}})
     |> Sanbase.Mock.run_with_mocks(fn ->
       result =
-        get_raw_signals(conn, :all, from, to)
+        get_raw_signals(conn, :all, :all, from, to)
         |> get_in(["data", "getRawSignals"])
 
       assert result == [
@@ -69,7 +69,7 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     end)
   end
 
-  test "returns data for an available signal with signals filtering", context do
+  test "signal with signals filtering", context do
     %{conn: conn, from: from, to: to} = context
 
     rows = [
@@ -92,7 +92,7 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     Sanbase.Mock.prepare_mock2(&Sanbase.ClickhouseRepo.query/2, {:ok, %{rows: rows}})
     |> Sanbase.Mock.run_with_mocks(fn ->
       result =
-        get_raw_signals(conn, ["dai_mint", "large_transactions"], from, to)
+        get_raw_signals(conn, ["dai_mint", "large_transactions"], :all, from, to)
         |> get_in(["data", "getRawSignals"])
 
       assert result == [
@@ -122,17 +122,63 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     end)
   end
 
+  test "signal with selector filtering", context do
+    %{conn: conn, from: from, to: to} = context
+
+    # When the slugs in the selector are validated they must exist
+    insert(:random_erc20_project, slug: "multi-collateral-dai")
+    insert(:random_erc20_project, slug: "not-dai")
+
+    rows = [
+      [
+        ~U[2019-01-01 00:00:00Z] |> DateTime.to_unix(),
+        "dai_mint",
+        "multi-collateral-dai",
+        21029,
+        ~s|{"txHash": "0xecdeb8435aff6e18e08177bb94d52b2da6dd15b95aee7f442021911a7c9861e6", "address": "0x183c9077fb7b74f02d3badda6c85a19c92b1f648"}|
+      ],
+      [
+        ~U[2019-01-02 00:00:00Z] |> DateTime.to_unix(),
+        "dai_mint",
+        "not-dai",
+        12_308_120,
+        ~s|{"txHash": "0x0bb27622fa4fcdf39344251e9b0776467eaa5d9dbf0f025d254f55093848f2bd", "address": "0x61c808d82a3ac53231750dadc13c777b59310bd9"}|
+      ]
+    ]
+
+    Sanbase.Mock.prepare_mock2(&Sanbase.ClickhouseRepo.query/2, {:ok, %{rows: rows}})
+    |> Sanbase.Mock.run_with_mocks(fn ->
+      result =
+        get_raw_signals(conn, :all, ["multi-collateral-dai"], from, to)
+        |> get_in(["data", "getRawSignals"])
+
+      assert result == [
+               %{
+                 "datetime" => "2019-01-01T00:00:00Z",
+                 "metadata" => %{
+                   "address" => "0x183c9077fb7b74f02d3badda6c85a19c92b1f648",
+                   "txHash" =>
+                     "0xecdeb8435aff6e18e08177bb94d52b2da6dd15b95aee7f442021911a7c9861e6"
+                 },
+                 "value" => 21029.0,
+                 "signal" => "dai_mint",
+                 "slug" => "multi-collateral-dai"
+               }
+             ]
+    end)
+  end
+
   # Private functions
 
-  defp get_raw_signals(conn, signals, from, to) do
-    query = get_raw_signals_query(signals, from, to)
+  defp get_raw_signals(conn, signals, slugs, from, to) do
+    query = get_raw_signals_query(signals, slugs, from, to)
 
     conn
     |> post("/graphql", query_skeleton(query, "getSignal"))
     |> json_response(200)
   end
 
-  defp get_raw_signals_query(:all, from, to) do
+  defp get_raw_signals_query(:all, :all, from, to) do
     """
       {
         getRawSignals(from: "#{from}", to: "#{to}"){
@@ -146,10 +192,26 @@ defmodule SanbaseWeb.Graphql.Clickhouse.ApiSignalRawDataTest do
     """
   end
 
-  defp get_raw_signals_query([_ | _] = signals, from, to) do
+  defp get_raw_signals_query([_ | _] = signals, :all, from, to) do
     """
       {
         getRawSignals(signals: #{string_list_to_string(signals)}, from: "#{from}", to: "#{to}"){
+          datetime
+          signal
+          slug
+          value
+          metadata
+        }
+      }
+    """
+  end
+
+  defp get_raw_signals_query(:all, slugs, from, to) do
+    slugs_str = Enum.map(slugs, &~s/"#{&1}"/) |> Enum.join(",")
+
+    """
+      {
+        getRawSignals(from: "#{from}", to: "#{to}", selector: {slugs: [#{slugs_str}]}){
           datetime
           signal
           slug

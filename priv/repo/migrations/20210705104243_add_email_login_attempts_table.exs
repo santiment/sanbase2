ab63498e218e723f94d1963e4edcca35f8dd0438
Atanas <atanasormanow@gmail.com>
2021-07-14 16:14:05 +0300

    Add indexes to login attempts on user_id and ip

--- priv/repo/migrations/20210705104243_add_email_login_attempts_table.exs
+++ priv/repo/migrations/20210705104243_add_email_login_attempts_table.exs
@@ -6,6 +6,9 @@ defmodule Sanbase.Repo.Migrations.AddEmailLoginAttemptsTable do
       add(:user_id, references(:users), on_delete: :delete_all, null: false)
       add(:ip_address, :string, size: 15)
 
+      index(:email_login_attempts, [:user_id])
+      index(:email_login_attempts, [:ip_address])
+
       timestamps()
     end
   end

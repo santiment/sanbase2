bce58e5eeaaac2ec350ca4bc0da6fc3589b0bfc4
Atanas <atanasormanow@gmail.com>
2021-07-14 12:21:30 +0300

    Restrict login if user OR ip had too many attempts

--- lib/sanbase/accounts/email_login_attempt.ex
+++ lib/sanbase/accounts/email_login_attempt.ex
@@ -16,9 +16,11 @@ defmodule Sanbase.Accounts.EmailLoginAttempt do
   end
 
   def has_allowed_login_attempts(user, remote_ip) do
-    login_attempts = login_attempts_count(user, remote_ip)
+    email_login_attempts = login_attempts_count(user)
+    ip_login_attempts = login_attempts_count(remote_ip)
 
-    if login_attempts <= @allowed_login_attempts do
+    if email_login_attempts <= @allowed_login_attempts ||
+         ip_login_attempts <= @allowed_login_attempts do
       :ok
     else
       {:error, :too_many_login_attempts}
@@ -31,14 +33,20 @@ defmodule Sanbase.Accounts.EmailLoginAttempt do
   end
 
   # Private
-  defp login_attempts_count(%{id: user_id}, remote_ip) do
+  defp login_attempts_count(remote_ip) when is_binary(remote_ip) do
     interval_limit = Timex.shift(Timex.now(), minutes: -@interval_in_minutes)
 
     from(attempt in __MODULE__,
-      where:
-        attempt.user_id == ^user_id and
-          attempt.ip_address == ^remote_ip and
-          attempt.inserted_at > ^interval_limit
+      where: attempt.ip_address == ^remote_ip and attempt.inserted_at > ^interval_limit
+    )
+    |> Repo.aggregate(:count, :id)
+  end
+
+  defp login_attempts_count(%{id: user_id}) do
+    interval_limit = Timex.shift(Timex.now(), minutes: -@interval_in_minutes)
+
+    from(attempt in __MODULE__,
+      where: attempt.user_id == ^user_id and attempt.inserted_at > ^interval_limit
     )
     |> Repo.aggregate(:count, :id)
   end

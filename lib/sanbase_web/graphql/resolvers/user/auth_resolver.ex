dfbc4a0bd609ed6bef17dbd8fa7b124f7bc20cd2
Atanas <atanasormanow@gmail.com>
2021-07-19 10:40:21 +0300

    backup

--- lib/sanbase_web/graphql/resolvers/user/auth_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/user/auth_resolver.ex
@@ -87,7 +87,7 @@ defmodule SanbaseWeb.Graphql.Resolvers.AuthResolver do
          {:ok, user} <- User.update_email_token(user, args[:consent]),
          {:ok, _user} <- User.send_login_email(user, origin_host_parts, args),
          {:ok, %EmailLoginAttempt{}} <- EmailLoginAttempt.create(user, remote_ip) do
-      k({:ok, %{success: true, first_login: user.first_login}})
+      {:ok, %{success: true, first_login: user.first_login}}
     else
       {:error, :too_many_login_attempts} ->
         {:error, message: "Too many login attempts, try again after a few minutes"}

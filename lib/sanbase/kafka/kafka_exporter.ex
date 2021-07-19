ef9442a24642b0de7d488044fb7fe292bafaae1d
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-07-19 18:06:57 +0300

    Increase the GenServer call timeout for persist_sync

--- lib/sanbase/kafka/kafka_exporter.ex
+++ lib/sanbase/kafka/kafka_exporter.ex
@@ -77,8 +77,8 @@ defmodule Sanbase.KafkaExporter do
   end
 
   @spec persist_sync(data | [data], pid() | atom()) :: result
-  def persist_sync(data, exporter \\ __MODULE__) do
-    GenServer.call(exporter, {:persist, data})
+  def persist_sync(data, exporter, timeout \\ 60_000) do
+    GenServer.call(exporter, {:persist, data}, timeout)
   end
 
   def flush(exporter \\ __MODULE__) do

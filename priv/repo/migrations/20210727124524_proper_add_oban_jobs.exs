3b336a384ea020e348e7227ee732c2b8886ab6a0
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-07-27 15:50:56 +0300

    Run the Oban migrations with the proper prefix

---
+++ priv/repo/migrations/20210727124524_proper_add_oban_jobs.exs
@@ -0,0 +1,21 @@
+defmodule Sanbase.Repo.Migrations.ProperAddObanJobs do
+  use Ecto.Migration
+
+  require Sanbase.Utils.Config, as: Config
+
+  def up do
+    Oban.Migrations.up(prefix: get_prefix())
+  end
+
+  def down do
+    Oban.Migrations.down(version: 1, prefix: get_prefix())
+  end
+
+  defp get_prefix() do
+    case Config.module_get(Sanbase, :deployment_env) do
+      "stage" -> "sanbase"
+      "prod" -> "sanbase2"
+      _ -> "public"
+    end
+  end
+end

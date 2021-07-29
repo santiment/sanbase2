dfba75e3f488553986351cad1815e13e389266b4
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2021-07-29 16:14:04 +0200

    add analytics records to kafka

--- lib/sanbase/intercom/user_attributes.ex
+++ lib/sanbase/intercom/user_attributes.ex
@@ -5,6 +5,8 @@ defmodule Sanbase.Intercom.UserAttributes do
 
   alias Sanbase.Repo
 
+  @topic "sanbase_user_intercom_attributes"
+
   schema "user_intercom_attributes" do
     field(:properties, :map)
 
@@ -23,6 +25,7 @@ defmodule Sanbase.Intercom.UserAttributes do
     %__MODULE__{}
     |> changeset(params)
     |> Repo.insert()
+    |> persist_sync()
   end
 
   def get_attributes_for_users(user_ids, from, to) do
@@ -33,5 +36,30 @@ defmodule Sanbase.Intercom.UserAttributes do
           ua.inserted_at <= ^to
     )
     |> Repo.all()
+  end
+
+  # helpers
+
+  defp persist_sync({:ok, user_attributes} = result) do
+    SanExporterEx.Producer.send_data(@topic, format_for_kafka([user_attributes]))
+    result
+  end
+
+  defp persist_sync(result), do: result
+
+  defp format_for_kafka(user_attributes) do
+    user_attributes
+    |> Enum.map(fn %{user_id: user_id, properties: attributes, inserted_at: timestamp} ->
+      timestamp = DateTime.to_unix(timestamp)
+      key = "#{user_id}_#{timestamp}"
+
+      data = %{
+        user_id: user_id,
+        attributes: attributes |> Map.delete("email") |> Map.delete("name") |> Jason.encode!(),
+        timestamp: timestamp
+      }
+
+      {key, Jason.encode!(data)}
+    end)
   end
 end

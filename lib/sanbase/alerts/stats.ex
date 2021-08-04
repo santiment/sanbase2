b5516c4d58bb5597e475d18fbe1c892951a18a41
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2021-08-04 10:28:26 +0200

    Add stats as a table

--- lib/sanbase/alerts/stats.ex
+++ lib/sanbase/alerts/stats.ex
@@ -21,20 +21,57 @@ defmodule Sanbase.Alerts.Stats do
     total_fired =
       Enum.reduce(stats_day, 0, fn {_slug, alerts}, count -> count + length(alerts.alerts) end)
 
+    total_fired_weekly_avg =
+      Enum.reduce(stats_week, 0, fn {_slug, alerts}, count -> count + length(alerts.alerts) end) /
+        7
+
+    {total_fired_percent_change, direction_text} =
+      percent_change(total_fired_weekly_avg, total_fired)
+
+    total_fired_text =
+      "#{Float.round(total_fired_percent_change, 2)}% #{direction_text} than weekly average"
+
+    slugs = Enum.map(stats_day, fn {slug, _fired_data} -> slug end)
+    slug_ticker_map = slug_ticker_map(slugs)
+
     fired_alerts =
       stats_day
       |> Enum.map(fn {slug, fired_data} ->
         avg_week = stats_week[slug].count / 7
         {percent, direction_text} = percent_change(avg_week, fired_data.count)
-        percent_change = "#{Float.round(percent, 2)}% #{direction_text} than weekly average"
+        percent_change = "#{Float.round(percent, 2)}%"
         alerts_types = fired_data.alerts |> Enum.map(&alert_type(&1))
-        [slug, fired_data.count, percent_change, alerts_types]
+
+        [
+          slug_ticker_map[slug] || slug,
+          fired_data.count,
+          percent_change,
+          alerts_types
+        ]
       end)
 
     %{
       total_fired: total_fired,
       fired_alerts: fired_alerts
     }
+
+    alerts_text =
+      Enum.reduce(fired_alerts, "", fn [ticker, count, percent_change_text, metrics], acc_text ->
+        acc_text <>
+          "|#{String.pad_trailing(ticker, 10)}|#{String.pad_leading(Integer.to_string(count), 10)}|#{String.pad_leading(percent_change_text, 30)}|#{Enum.join(metrics, ", ")}\n"
+      end)
+
+    """
+    *Stats for last 24 hours:*
+
+    *Total alerts:* #{total_fired} _(#{total_fired_text})_
+
+    ```
+    |#{String.pad_trailing("Asset", 10)}|#{String.pad_leading("Count", 10)}|#{String.pad_leading("% change vs weekly avg", 30)}|Metrics
+    |#{String.pad_trailing("", 10, "_")}|#{String.pad_trailing("", 10, "_")}|#{String.pad_trailing("", 30, "_")}|
+    #{alerts_text}
+    ```
+    """
   end
 
   defp alert_type(alert) do
@@ -44,6 +81,14 @@ defmodule Sanbase.Alerts.Stats do
       "signal_data" -> alert.signal
       _ -> alert.type
     end
+    |> String.replace("_", " ")
+    |> String.capitalize()
+  end
+
+  defp slug_ticker_map(slugs) do
+    slugs
+    |> Sanbase.Model.Project.tickers_by_slug_list()
+    |> Enum.into(%{}, fn {t, s} -> {s, t} end)
   end
 
   def compute_stats(query) do

bc63659a48c02ce51d10d3bdac417e93a101ee5c
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-08-17 16:31:54 +0300

    Add tests for removing unsupported base assets jobs

--- lib/sanbase/cryptocompare/jobs.ex
+++ lib/sanbase/cryptocompare/jobs.ex
@@ -33,8 +33,18 @@ defmodule Sanbase.Cryptocompare.Jobs do
     unsupported_base_assets = oban_jobs_base_assets -- supported_base_assets
 
     Enum.map(unsupported_base_assets, fn base_asset ->
-      {:ok, _} = delete_base_asset_jobs(@queue, base_asset)
+      {:ok, _} = delete_not_completed_base_asset_jobs(@queue, base_asset)
     end)
+  end
+
+  def get_oban_jobs_base_assets(queue) do
+    query = """
+    SELECT distinct(args->>'base_asset') FROM oban_jobs
+    WHERE queue = $1 AND completed_at IS NULL
+    """
+
+    {:ok, %{rows: rows}} = Ecto.Adapters.SQL.query(Sanbase.Repo, query, [queue], timeout: 150_000)
+    {:ok, List.flatten(rows)}
   end
 
   # Private functions
@@ -55,17 +65,7 @@ defmodule Sanbase.Cryptocompare.Jobs do
     {:ok, affected_rows_count}
   end
 
-  defp get_oban_jobs_base_assets(queue) do
-    query = """
-    SELECT distinct(args->>'base_asset') FROM oban_jobs
-    WHERE queue = $1 AND completed_at IS NULL
-    """
-
-    {:ok, %{rows: rows}} = Ecto.Adapters.SQL.query(Sanbase.Repo, query, [queue], timeout: 150_000)
-    {:ok, List.flatten(rows)}
-  end
-
-  defp delete_base_asset_jobs(queue, base_asset) do
+  defp delete_not_completed_base_asset_jobs(queue, base_asset) do
     query = """
     DELETE FROM oban_jobs
     WHERE queue = $1 AND args->>'base_asset' = $2 AND completed_at IS NULL;
---
+++ test/sanbase/cryptocompare/remove_unsupported_base_assets_test.exs
@@ -0,0 +1,54 @@
+defmodule Sanbase.Cryptocompare.RemoveUnsupportedBaseAssetsTest do
+  use Sanbase.DataCase
+  use Oban.Testing, repo: Sanbase.Repo
+
+  import Sanbase.Factory
+
+  alias Sanbase.Model.Project
+
+  setup do
+    [p1, p2, p3, p4] = projects = for _ <- 1..4, do: insert(:random_erc20_project)
+
+    for p <- projects do
+      insert(:source_slug_mapping, source: "cryptocompare", slug: p.ticker, project_id: p.id)
+    end
+
+    %{p1: p1, p2: p2, p3: p3, p4: p4}
+  end
+
+  test "remove jobs for unsupported assets", context do
+    %{p1: p1, p2: p2, p3: p3, p4: p4} = context
+    from = ~D[2021-01-01]
+    to = ~D[2021-01-10]
+
+    for p <- [p1, p2, p3, p4] do
+      Sanbase.Cryptocompare.HistoricalScheduler.add_jobs(p.ticker, "USD", from, to)
+    end
+
+    # Check that all 40 jobs and 4 different base assets are present
+    assert {:ok, %{rows: [[40]]}} =
+             Ecto.Adapters.SQL.query(Sanbase.Repo, "SELECT count(*) FROM oban_jobs;", [])
+
+    {:ok, base_assets} =
+      Sanbase.Cryptocompare.Jobs.get_oban_jobs_base_assets("cryptocompare_historical_jobs_queue")
+
+    assert [p1.ticker, p2.ticker, p3.ticker, p4.ticker] |> Enum.sort() ==
+             base_assets |> Enum.sort()
+
+    # Remove two of the mappings making those 2 no longer supported
+    assert {1, nil} = Project.SourceSlugMapping.remove(p1.id, "cryptocompare")
+    assert {1, nil} = Project.SourceSlugMapping.remove(p4.id, "cryptocompare")
+
+    # Remove the unsupported base assets and test that the jobs are removed
+    Sanbase.Cryptocompare.Jobs.remove_oban_jobs_unsupported_assets()
+
+    assert {:ok, %{rows: [[20]]}} =
+             Ecto.Adapters.SQL.query(Sanbase.Repo, "SELECT count(*) FROM oban_jobs;", [])
+
+    {:ok, base_assets} =
+      Sanbase.Cryptocompare.Jobs.get_oban_jobs_base_assets("cryptocompare_historical_jobs_queue")
+
+    assert [p2.ticker, p3.ticker] |> Enum.sort() ==
+             base_assets |> Enum.sort()
+  end
+end

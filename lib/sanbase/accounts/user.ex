ea7fbd4113c34b99ca2018cff2f060eef085aa68
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2021-09-17 10:04:30 +0200

    Improve cascade deletes

--- lib/sanbase/accounts/user.ex
+++ lib/sanbase/accounts/user.ex
@@ -89,6 +89,8 @@ defmodule Sanbase.Accounts.User do
     has_many(:chart_configurations, Sanbase.Chart.Configuration, on_delete: :delete_all)
     has_many(:user_attributes, Sanbase.Intercom.UserAttributes, on_delete: :delete_all)
     has_many(:user_events, Sanbase.Intercom.UserEvent, on_delete: :delete_all)
+    has_many(:email_login_attempts, Sanbase.Accounts.EmailLoginAttempt, on_delete: :delete_all)
+    has_many(:short_urls, Sanbase.ShortUrl, on_delete: :delete_all)
 
     has_one(:user_settings, UserSettings, on_delete: :delete_all)
 

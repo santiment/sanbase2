2b00c038fc93a60a39ad38eee7d13361da3553f7
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-11-04 11:08:33 +0200

    Fix warnings

--- lib/sanbase/kaffy.ex
+++ lib/sanbase/kaffy.ex
@@ -1,4 +1,12 @@
 defmodule Sanbase.Kaffy do
-  # The kaffy admin uses String.to_existing_atom and fails if there is no atom. Add a bunch of atoms that are needed here.
+  @moduledoc false
+  # The kaffy admin uses String.to_existing_atom and fails
+  # if there is no atom. Add a bunch of atoms that are needed here.
   @atoms [:accounts]
+
+  @doc false
+  def atoms() do
+    # Do this only to not have an unused module attribute warning.
+    @atoms
+  end
 end

50d1816832ff2b2487e37d3eca527b51c9f9144e
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-01-21 14:28:20 +0200

    Add test for updating frozen triggers

--- test/sanbase_web/graphql/alerts/update_trigger_api_test.exs
+++ test/sanbase_web/graphql/alerts/update_trigger_api_test.exs
@@ -6,20 +6,38 @@ defmodule SanbaseWeb.Graphql.UpdateTriggerApiTest do
 
   setup do
     user = insert(:user)
+    user2 = insert(:user)
     conn = setup_jwt_auth(build_conn(), user)
 
-    %{conn: conn, user: user}
+    %{conn: conn, user: user, user2: user2}
   end
 
-  test "cannot update others' featured trigger", context do
+  test "cannot update other users' triggers", context do
+    %{conn: conn, user2: user2} = context
     # create a trigger for a random new user
-    user_trigger = insert(:user_trigger)
-    Sanbase.FeaturedItem.update_item(user_trigger, true)
+    user_trigger = insert(:user_trigger, user: user2)
 
-    result = update_trigger(context.conn, trigger_id: user_trigger.id, is_active: true)
+    result = update_trigger(conn, trigger_id: user_trigger.id, is_active: false)
 
     assert result["errors"] |> List.first() |> Map.get("message") ==
              "The trigger with id #{user_trigger.id} does not exists or does not belong to the current user"
+  end
+
+  test "cannot update frozen triggers", context do
+    %{conn: conn, user: user} = context
+    # create a trigger for a random new user
+    user_trigger = insert(:user_trigger, user: user)
+
+    {:ok, _} =
+      Sanbase.Alert.UserTrigger.update_user_trigger(user.id, %{
+        id: user_trigger.id,
+        is_frozen: true
+      })
+
+    result = update_trigger(conn, trigger_id: user_trigger.id, is_active: false)
+
+    assert result["errors"] |> List.first() |> Map.get("message") ==
+             "The trigger with id #{user_trigger.id} is frozen"
   end
 
   defp update_trigger(conn, opts) do

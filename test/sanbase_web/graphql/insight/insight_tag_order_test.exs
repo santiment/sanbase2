22de18e25d3cab41dff5135d83e69239b29ff219
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2022-03-16 11:45:19 +0100

    Fix test

--- test/sanbase_web/graphql/insight/insight_tag_order_test.exs
+++ test/sanbase_web/graphql/insight/insight_tag_order_test.exs
@@ -19,7 +19,7 @@ defmodule SanbaseWeb.Graphql.InsightTagOrderTest do
     mutation = create_insight_mutation("Title", "Text", tags)
     result = execute_mutation(conn, mutation)
 
-    assert result["data"]["createInsight"]["tags"] |> Enum.map(& &1["name"]) == tags
+    assert result["tags"] |> Enum.map(& &1["name"]) == tags
   end
 
   test "tags preserved order in update", %{conn: conn} do
@@ -27,15 +27,15 @@ defmodule SanbaseWeb.Graphql.InsightTagOrderTest do
     mutation = create_insight_mutation("Title", "Text", tags)
     result = execute_mutation(conn, mutation)
 
-    id = result["data"]["createInsight"]["id"]
+    id = result["id"]
 
-    assert result["data"]["createInsight"]["tags"] |> Enum.map(& &1["name"]) == tags
+    assert result["tags"] |> Enum.map(& &1["name"]) == tags
 
     tags2 = ["BTC" | tags]
     mutation2 = update_insight_tags_mutation(id, tags2)
     result2 = execute_mutation(conn, mutation2)
 
-    assert result2["data"]["updateInsight"]["tags"] |> Enum.map(& &1["name"]) == tags2
+    assert result2["tags"] |> Enum.map(& &1["name"]) == tags2
   end
 
   test "tags preserved order after two updates", %{conn: conn} do
@@ -43,21 +43,19 @@ defmodule SanbaseWeb.Graphql.InsightTagOrderTest do
     mutation = create_insight_mutation("Title", "Text", tags)
     result = execute_mutation(conn, mutation)
 
-    id = result["data"]["createInsight"]["id"]
+    id = result["id"]
 
-    assert result["data"]["createInsight"]["tags"] |> Enum.map(& &1["name"]) == tags
+    assert result["tags"] |> Enum.map(& &1["name"]) == tags
 
     tags2 = ["BTC" | tags]
     mutation2 = update_insight_tags_mutation(id, tags2)
     result2 = execute_mutation(conn, mutation2)
-
-    assert result2["data"]["updateInsight"]["tags"] |> Enum.map(& &1["name"]) == tags2
+    assert result2["tags"] |> Enum.map(& &1["name"]) == tags2
 
     tags3 = ["SAN", "PESH" | tags]
     mutation3 = update_insight_tags_mutation(id, tags3)
     result3 = execute_mutation(conn, mutation3)
-
-    assert result3["data"]["updateInsight"]["tags"] |> Enum.map(& &1["name"]) == tags3
+    assert result3["tags"] |> Enum.map(& &1["name"]) == tags3
   end
 
   test "create tags the first", %{conn: conn} do
@@ -65,20 +63,17 @@ defmodule SanbaseWeb.Graphql.InsightTagOrderTest do
 
     mutation = create_insight_mutation("Title", "Text", tags)
     result = execute_mutation(conn, mutation)
-    id = result["data"]["createInsight"]["id"]
-
+    id = result["id"]
     assert get_insight_tags(conn, id) == tags
 
     tags2 = ["third", "first", "BTC", "second"]
     mutation2 = update_insight_tags_mutation(id, tags2)
     execute_mutation(conn, mutation2)
-
     assert get_insight_tags(conn, id) == tags2
 
     tags3 = ["SAN", "PESH" | tags2 |> Enum.reverse()] ++ ["ASD"]
     mutation3 = update_insight_tags_mutation(id, tags3)
     execute_mutation(conn, mutation3)
-
     assert get_insight_tags(conn, id) == tags3
   end
 
@@ -117,12 +112,6 @@ defmodule SanbaseWeb.Graphql.InsightTagOrderTest do
     execute_query(conn, query)
     |> get_in(["data", "insight", "tags"])
     |> Enum.map(& &1["name"])
-  end
-
-  defp execute_mutation(conn, mutation) do
-    conn
-    |> post("/graphql", mutation_skeleton(mutation))
-    |> json_response(200)
   end
 
   defp execute_query(conn, query) do

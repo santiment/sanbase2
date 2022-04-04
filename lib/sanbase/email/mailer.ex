bfb52f470091a3a7695d549bbcd71a119a43e586
ivan <IvanIvanoff@users.noreply.github.com>
2022-04-04 15:38:57 +0300

    Check if the user has email before sending it with Sanbase.Mailer (#3183)

--- lib/sanbase/email/mailer.ex
+++ lib/sanbase/email/mailer.ex
@@ -9,6 +9,17 @@ defmodule Sanbase.Mailer do
     vars = args["vars"] || %{}
     opts = args["opts"] || %{}
 
-    Sanbase.MandrillApi.send(template, user.email, vars, opts)
+    case user.email do
+      nil ->
+        # If the user does not have an email just mark the job as finished.
+        # In case of missing email it is better to just mark every job as
+        # finished instead of not scheduling the emails at all. This is because
+        # the user might enter their email at some point and we want to send
+        # the rest of the emails in this case.
+        :ok
+
+      _ ->
+        Sanbase.MandrillApi.send(template, user.email, vars, opts)
+    end
   end
 end

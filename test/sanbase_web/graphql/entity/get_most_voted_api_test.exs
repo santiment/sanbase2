5c550861cd62523b1622c302fb4733df307bea5a
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-04-07 18:24:29 +0300

    Add most voted entities test for multiple entities

--- test/sanbase_web/graphql/entity/get_most_voted_api_test.exs
+++ test/sanbase_web/graphql/entity/get_most_voted_api_test.exs
@@ -28,15 +28,15 @@ defmodule SanbaseWeb.Graphql.GetMostVotedApitest do
 
   test "get most voted screener", %{conn: conn} do
     # The non-screener should not be in the result
-    watchlist0_1 = insert(:watchlist, is_public: true, is_screener: false)
-    watchlist0_2 = insert(:watchlist, is_public: true, is_screener: true, inserted_at: days_ago())
+    watchlist0 = insert(:watchlist, is_public: true)
+    screener0 = insert(:screener, is_public: true, inserted_at: days_ago())
 
     watchlist1 = insert(:screener, is_public: true)
     watchlist2 = insert(:screener, is_public: true)
     _watchlist_without_votes = insert(:screener, is_public: true)
 
-    for _ <- 1..20, do: vote(conn, "watchlistId", watchlist0_1.id)
-    for _ <- 1..20, do: vote(conn, "watchlistId", watchlist0_2.id)
+    for _ <- 1..20, do: vote(conn, "watchlistId", watchlist0.id)
+    for _ <- 1..20, do: vote(conn, "watchlistId", screener0.id)
     for _ <- 1..10, do: vote(conn, "watchlistId", watchlist1.id)
     for _ <- 1..5, do: vote(conn, "watchlistId", watchlist2.id)
 
@@ -89,6 +89,32 @@ defmodule SanbaseWeb.Graphql.GetMostVotedApitest do
              chart_configuration2.id
   end
 
+  test "get most voted combined", %{conn: conn} do
+    insight1 = insert(:published_post)
+    insight2 = insert(:published_post)
+    conf1 = insert(:chart_configuration, is_public: true)
+    conf2 = insert(:chart_configuration, is_public: true)
+    screener1 = insert(:screener, is_public: true)
+    watchlist1 = insert(:watchlist, is_public: true)
+
+    for _ <- 1..10, do: vote(conn, "insightId", insight1.id)
+    for _ <- 1..9, do: vote(conn, "insightId", insight2.id)
+    for _ <- 1..8, do: vote(conn, "chartConfigurationId", conf2.id)
+    for _ <- 1..7, do: vote(conn, "chartConfigurationId", conf1.id)
+    for _ <- 1..6, do: vote(conn, "watchlistId", watchlist1.id)
+    for _ <- 1..5, do: vote(conn, "watchlistId", screener1.id)
+
+    result = get_most_voted(conn, [:insight, :watchlist, :screener, :chart_configuration])
+
+    assert length(result) == 6
+    assert Enum.at(result, 0)["insight"]["id"] == insight1.id
+    assert Enum.at(result, 1)["insight"]["id"] == insight2.id
+    assert Enum.at(result, 2)["chartConfiguration"]["id"] == conf2.id
+    assert Enum.at(result, 3)["chartConfiguration"]["id"] == conf1.id
+    assert Enum.at(result, 4)["watchlist"]["id"] |> String.to_integer() == watchlist1.id
+    assert Enum.at(result, 5)["screener"]["id"] |> String.to_integer() == screener1.id
+  end
+
   defp vote(conn, entity_key, entity_id) do
     mutation = """
     mutation {
@@ -105,6 +131,31 @@ defmodule SanbaseWeb.Graphql.GetMostVotedApitest do
       |> post("/graphql", mutation_skeleton(mutation))
       |> json_response(200)
       |> get_in(["data", "vote"])
+  end
+
+  defp get_most_voted(conn, entities) when is_list(entities) do
+    types = Enum.map(entities, &(&1 |> Atom.to_string() |> String.upcase())) |> Enum.join(", ")
+
+    query = """
+    {
+      getMostVoted(
+        types: [#{types}]
+        page: 1
+        pageSize: 10
+        cursor: { type: AFTER, datetime: "utc_now-7d" }
+      ){
+          insight{ id }
+          watchlist{ id }
+          screener{ id }
+          chartConfiguration{ id }
+      }
+    }
+    """
+
+    conn
+    |> post("/graphql", query_skeleton(query))
+    |> json_response(200)
+    |> get_in(["data", "getMostVoted"])
   end
 
   defp get_most_voted(conn, entity) do

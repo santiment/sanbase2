c7a04264200da5f4a9fb5d1b1d2315471fcf778c
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-04-08 16:22:13 +0300

    Enable the freeze/unfreeze Quantum jobs

--- config/scheduler_config.exs
+++ config/scheduler_config.exs
@@ -12,11 +12,14 @@ config :sanbase, Sanbase.Alerts.Scheduler,
   timeout: 30_000,
   overlap: false,
   jobs: [
-    # Enable the freezing cron job only after the frontend handles the frozen alerts
-    # freeze_user_alerts: [
-    #   schedule: "0 5 * * *",
-    #   task: {Sanbase.Alert.Job, :freeze_alerts, []}
-    # ],
+    freeze_user_alerts: [
+      schedule: "0 5 * * *",
+      task: {Sanbase.Alert.Job, :freeze_alerts, []}
+    ],
+    unfreeze_user_alerts: [
+      schedule: "0 10 * * *",
+      task: {Sanbase.Alert.Job, :unfreeze_alerts, []}
+    ],
     price_volume_difference_sonar_alert: [
       schedule: "1-59/5 * * * *",
       task: {Sanbase.Alert.Scheduler, :run_alert, [Trigger.PriceVolumeDifferenceTriggerSettings]}

50088034f774144a6cecde4001763a022fffd121
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-04-14 11:13:17 +0300

    Introduce entity API stats. Rework response structure

--- lib/sanbase_web/graphql/resolvers/entity_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/entity_resolver.ex
@@ -2,34 +2,82 @@ defmodule SanbaseWeb.Graphql.Resolvers.EntityResolver do
   import Sanbase.Utils.Transform, only: [maybe_apply_function: 2]
   import SanbaseWeb.Graphql.Helpers.Utils, only: [transform_user_trigger: 1]
 
-  def get_most_voted(_root, args, resolution) do
-    types = Map.get(args, :types) || [Map.get(args, :type)]
+  def get_most_voted(_root, args, _resolution) do
+    {:ok, %{query: :get_most_voted, args: args}}
+  end
 
-    opts =
-      [
-        page: Map.get(args, :page, 1),
-        page_size: Map.get(args, :page_size, 10),
-        cursor: Map.get(args, :cursor)
-      ]
-      |> maybe_add_current_user_data_only(args, resolution)
+  def get_most_recent(_root, args, _resolution) do
+    {:ok, %{query: :get_most_recent, args: args}}
+  end
+
+  def get_most_voted_data(_root, _args, resolution) do
+    %{source: %{args: args}} = resolution
+    types = get_types(args)
+    opts = get_opts(args, resolution)
 
     Sanbase.Entity.get_most_voted(types, opts)
     |> maybe_apply_function(&handle_result/1)
   end
 
-  def get_most_recent(_root, args, resolution) do
-    types = Map.get(args, :types) || [Map.get(args, :type)]
+  def get_most_voted_stats(_root, _args, resolution) do
+    %{source: %{args: args}} = resolution
+    types = get_types(args)
+    opts = get_opts(args, resolution)
+    {:ok, total_count} = Sanbase.Entity.get_most_voted_total_count(types, opts)
 
-    opts =
-      [
-        page: Map.get(args, :page, 1),
-        page_size: Map.get(args, :page_size, 10),
-        cursor: Map.get(args, :cursor)
-      ]
-      |> maybe_add_current_user_data_only(args, resolution)
+    stats = %{
+      current_page: opts[:page],
+      current_page_size: opts[:page_size],
+      total_count: total_count
+    }
+
+    {:ok, stats}
+  end
+
+  def get_most_recent_data(_root, _args, resolution) do
+    %{source: %{args: args}} = resolution
+    types = get_types(args)
+    opts = get_opts(args, resolution)
 
     Sanbase.Entity.get_most_recent(types, opts)
     |> maybe_apply_function(&handle_result/1)
+  end
+
+  def get_most_recent_stats(_root, _args, resolution) do
+    %{source: %{args: args}} = resolution
+    types = get_types(args)
+    opts = get_opts(args, resolution)
+    {:ok, total_count} = Sanbase.Entity.get_most_recent_total_count(types, opts)
+
+    stats = %{
+      current_page: opts[:page],
+      current_page_size: opts[:page_size],
+      total_count: total_count
+    }
+
+    {:ok, stats}
+  end
+
+  defp handle_result(list) do
+    Enum.map(list, fn map ->
+      case Map.to_list(map) do
+        [{:user_trigger, entity}] -> %{:user_trigger => transform_user_trigger(entity)}
+        _ -> map
+      end
+    end)
+  end
+
+  defp get_types(args) do
+    Map.get(args, :types) || [Map.get(args, :type)]
+  end
+
+  defp get_opts(args, resolution) do
+    [
+      page: Map.get(args, :page, 1),
+      page_size: Map.get(args, :page_size, 10),
+      cursor: Map.get(args, :cursor)
+    ]
+    |> maybe_add_current_user_data_only(args, resolution)
   end
 
   defp maybe_add_current_user_data_only(opts, args, resolution) do
@@ -40,14 +88,5 @@ defmodule SanbaseWeb.Graphql.Resolvers.EntityResolver do
     else
       _ -> opts
     end
-  end
-
-  defp handle_result(list) do
-    Enum.map(list, fn map ->
-      case Map.to_list(map) do
-        [{:user_trigger, entity}] -> %{:user_trigger => transform_user_trigger(entity)}
-        _ -> map
-      end
-    end)
   end
 end
--- lib/sanbase_web/graphql/schema/types/entity_types.ex
+++ lib/sanbase_web/graphql/schema/types/entity_types.ex
@@ -1,6 +1,8 @@
 defmodule SanbaseWeb.Graphql.EntityTypes do
   use Absinthe.Schema.Notation
 
+  alias SanbaseWeb.Graphql.Resolvers.EntityResolver
+
   enum :entity_type do
     value(:user_trigger)
     value(:insight)
@@ -10,12 +12,38 @@ defmodule SanbaseWeb.Graphql.EntityTypes do
     value(:chart_configuration)
   end
 
-  object :entity_result do
+  object :entity_stats do
+    field(:total_count, non_null(:integer))
+    field(:current_page, non_null(:integer))
+    field(:current_page_size, non_null(:integer))
+  end
+
+  object :single_entity_result do
     field(:user_trigger, :user_trigger)
     field(:insight, :post)
     field(:project_watchlist, :user_list)
     field(:address_watchlist, :user_list)
     field(:screener, :user_list)
     field(:chart_configuration, :chart_configuration)
+  end
+
+  object :most_voted_entity_result do
+    field :data, list_of(:single_entity_result) do
+      resolve(&EntityResolver.get_most_voted_data/3)
+    end
+
+    field :stats, :entity_stats do
+      resolve(&EntityResolver.get_most_voted_stats/3)
+    end
+  end
+
+  object :most_recent_entity_result do
+    field :data, list_of(:single_entity_result) do
+      resolve(&EntityResolver.get_most_recent_data/3)
+    end
+
+    field :stats, :entity_stats do
+      resolve(&EntityResolver.get_most_recent_stats/3)
+    end
   end
 end

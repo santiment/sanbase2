9897af200b4bb1cf76c4b734a772426132f27071
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-04-22 12:07:26 +0300

    Improve entities API caching

--- lib/sanbase/alerts/evaluator/scheduler.ex
+++ lib/sanbase/alerts/evaluator/scheduler.ex
@@ -187,6 +187,7 @@ defmodule Sanbase.Alert.Scheduler do
       alerts_count: alerts_count
     } = info_map
 
+    # TODO: Fix logging. It can print:  Batch 1/4, with size 63. Alerts 339-402 out of 221.
     Logger.info("""
     [#{run_uuid}] Run batch of alerts of type #{type}. Batch #{index}/#{batches_count}, \
     with size #{batch_map.batch_size}. Alerts #{batch_map.alerts_from}-#{batch_map.alerts_to} \

e9c3846c761a19cc12b3a72f9bf8437a8f7c098f
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2022-06-20 11:02:44 +0200

    Toggle subscribe unsubscribe email lists

--- lib/sanbase/event_bus/user_events_subscriber.ex
+++ lib/sanbase/event_bus/user_events_subscriber.ex
@@ -3,6 +3,7 @@ defmodule Sanbase.EventBus.UserEventsSubscriber do
   Export all the event bus events to a kafka topic for persistence
   """
   use GenServer
+  alias Sanbase.Email.MailchimpApi
 
   def topics(), do: ["user_events"]
 
@@ -41,37 +42,45 @@ defmodule Sanbase.EventBus.UserEventsSubscriber do
   end
 
   defp handle_event(
-         %{data: %{event_type: :subscribe_monthly_newsletter}},
+         %{data: %{event_type: :subscribe_monthly_newsletter, user_id: user_id}},
          event_shadow,
          state
        ) do
+    email = Sanbase.Accounts.get_user!(user_id).email
+    Sanbase.Email.MailchimpApi.subscribe(:monthly_newsletter, email)
     EventBus.mark_as_completed({__MODULE__, event_shadow})
     state
   end
 
   defp handle_event(
-         %{data: %{event_type: :unsubscribe_monthly_newsletter}},
+         %{data: %{event_type: :unsubscribe_monthly_newsletter, user_id: user_id}},
          event_shadow,
          state
        ) do
+    email = Sanbase.Accounts.get_user!(user_id).email
+    Sanbase.Email.MailchimpApi.unsubscribe(:monthly_newsletter, email)
     EventBus.mark_as_completed({__MODULE__, event_shadow})
     state
   end
 
   defp handle_event(
-         %{data: %{event_type: :subscribe_biweekly_pro}},
+         %{data: %{event_type: :subscribe_biweekly_pro, user_id: user_id}},
          event_shadow,
          state
        ) do
+    email = Sanbase.Accounts.get_user!(user_id).email
+    Sanbase.Email.MailchimpApi.subscribe(:bi_weekly, email)
     EventBus.mark_as_completed({__MODULE__, event_shadow})
     state
   end
 
   defp handle_event(
-         %{data: %{event_type: :unsubscribe_biweekly_pro}},
+         %{data: %{event_type: :unsubscribe_biweekly_pro, user_id: user_id}},
          event_shadow,
          state
        ) do
+    email = Sanbase.Accounts.get_user!(user_id).email
+    Sanbase.Email.MailchimpApi.unsubscribe(:bi_weekly, email)
     EventBus.mark_as_completed({__MODULE__, event_shadow})
     state
   end

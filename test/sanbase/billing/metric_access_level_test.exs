7b29a2d0d070a203a3a8cc742547fef4cab05edc
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2022-06-30 17:05:13 +0300

    Add NFT metrics to the incomplete metrics test

--- test/sanbase/billing/metric_access_level_test.exs
+++ test/sanbase/billing/metric_access_level_test.exs
@@ -321,7 +321,14 @@ defmodule Sanbase.Billing.MetricAccessLevelTest do
         "realized_value_usd_365d",
         "holders_distribution_combined_balance_over_100",
         "sentiment_volume_consumed_total",
-        "holders_labeled_negative_distribution_combined_balance_10k_to_100k"
+        "holders_labeled_negative_distribution_combined_balance_10k_to_100k",
+        "nft_market_volume",
+        "nft_market_count",
+        "nft_collection_holders_balance",
+        "nft_network_profit_loss",
+        "nft_network_profit_loss_usd",
+        "nft_collection_profit_loss",
+        "nft_collection_profit_loss_usd"
       ]
       |> Enum.sort()
 

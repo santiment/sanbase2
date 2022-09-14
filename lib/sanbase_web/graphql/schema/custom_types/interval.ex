9110bf009d01001f509b8e6e30c0f2a24db260a9
ivan <IvanIvanoff@users.noreply.github.com>
2022-09-14 11:50:11 +0300

    Fixes and improvements (#3443)
    * Improve GQL interval type docs
    
    * Fix get_metric SQL timestamp generation

--- lib/sanbase_web/graphql/schema/custom_types/interval.ex
+++ lib/sanbase_web/graphql/schema/custom_types/interval.ex
@@ -7,23 +7,28 @@ defmodule SanbaseWeb.Graphql.CustomTypes.Interval do
   scalar :interval, name: "interval" do
     description("""
     The `interval` scalar type represents arbitrary time range interval,
-    represented as UTF-8 character sequences (string). The interval starts
-    with a number and ends with one of the following suffixes:
+    represented as UTF-8 character sequences (string). The interval has two
+    representations: fixed range time and functions.
+
+    The fixed range interval starts with a number and ends with one of the following suffixes:
     - s - represents second
     - m - represents minute
     - h - represents hour
     - d - represents day
     - w - represents week
 
-    There is no predefined suffix for a month due to not fixed number of days
-    a month can have.
+    The function-defined interval helps when the interval need to be aligned at a given
+    date (start of week - Monday or Sunday) or at not fixed range intervals like months.
+    The supported functions are:
+    - toStartOfHour
+    - toStartOfDay
+    - toMonday
+    - toStartOfWeek (aligns dates on Sundays)
+    - toStartOfMonth
+    - toStartOfQuarter
+    - toStartOfYear
 
-    Examples for valid intervals:
-    - 5m - 5 minutes
-    - 12h - 12 hours
-    - 1d - 1 day
-    - 30d - 30 days
-    - 2w - 2 weeks
+    More details can be found here: https://academy.santiment.net/glossary/#interval
     """)
 
     serialize(&encode/1)

218d3e516780e97efe074e6ad3f4d6250c343016
ivan <IvanIvanoff@users.noreply.github.com>
2022-10-28 12:59:02 +0300

    Add UserAuth that accepts all types of user authentication (#3526)
    In some cases we want to allow only authenticated users, but we don't care
    if the authentication is API key or JWT

---
+++ lib/sanbase_web/graphql/middlewares/auth.ex
@@ -0,0 +1,31 @@
+defmodule SanbaseWeb.Graphql.Middlewares.UserAuth do
+  @moduledoc """
+  Authenticate that the request contains a valid user
+
+  """
+  @behaviour Absinthe.Middleware
+
+  alias Absinthe.Resolution
+  alias SanbaseWeb.Graphql.Middlewares.Helpers
+
+  @doc ~s"""
+  Decides whether the user has access or not.
+
+  The user must have accepted the privacy policy in order to access resources.
+  This allows both API key authentication and JWT authentication
+  """
+  def call(
+        %Resolution{
+          context: %{
+            auth: %{
+              current_user: current_user
+            }
+          }
+        } = resolution,
+        opts
+      ) do
+    Helpers.handle_user_access(current_user, opts, resolution)
+  end
+
+  def call(resolution, _), do: Resolution.put_result(resolution, {:error, :unauthorized})
+end

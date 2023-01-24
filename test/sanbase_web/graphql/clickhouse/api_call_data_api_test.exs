4852a28e80e01c22b1a44563bd189a48d9926876
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-01-24 13:23:40 +0200

    Add getMetric selector exported to Kafka test

--- test/sanbase_web/graphql/clickhouse/api_call_data_api_test.exs
+++ test/sanbase_web/graphql/clickhouse/api_call_data_api_test.exs
@@ -7,38 +7,73 @@ defmodule SanbaseWeb.Graphql.ApiCallDataApiTest do
   setup do
     user = insert(:user)
     project = insert(:random_project)
-    insert(:subscription_premium, user: user)
+    project2 = insert(:random_project)
+    insert(:subscription_pro_sanbase, user: user)
     conn = setup_jwt_auth(build_conn(), user)
-    %{conn: conn, project: project}
+    %{conn: conn, project: project, project2: project2}
   end
 
-  test "export get_metric api calls with the metric as argument", context do
-    %{conn: conn, project: project} = context
+  test "export get_metric api calls with the metric and slug as arguments", context do
+    %{conn: conn, project: %{slug: slug}, project2: %{slug: slug2}} = context
 
     Sanbase.Mock.prepare_mock2(&Sanbase.Clickhouse.MetricAdapter.timeseries_data/6, {:ok, []})
+    |> Sanbase.Mock.prepare_mock2(
+      &Sanbase.Clickhouse.MetricAdapter.timeseries_data_per_slug/6,
+      {:ok, []}
+    )
     |> Sanbase.Mock.run_with_mocks(fn ->
       from = ~U[2019-01-05 00:00:00Z]
       to = ~U[2019-01-06 00:00:00Z]
 
       Sanbase.InMemoryKafka.Producer.clear_state()
-      get_metric(conn, "mvrv_usd", project.slug, from, to, "1d")
-      get_metric(conn, "nvt", project.slug, from, to, "1d")
-      get_metric(conn, "daily_active_addresses", project.slug, from, to, "1d")
+      get_metric(conn, "mvrv_usd", slug, from, to, "1d")
+      get_metric(conn, "nvt", slug, from, to, "1d")
+      get_metric(conn, "daily_active_addresses", slug, from, to, "1d")
+
+      get_metric_timeseries_data_per_slug(conn, "nvt", [slug, slug2], from, to, "1d")
+      get_metric_timeseries_data_per_slug(conn, "mvrv_usd", [slug, slug2], from, to, "1d")
 
       # force the sending
       Sanbase.KafkaExporter.flush(:api_call_exporter)
 
       %{"sanbase_api_call_data" => api_calls} = Sanbase.InMemoryKafka.Producer.get_state()
 
-      api_calls_queries =
-        Enum.map(api_calls, fn {_, data} -> Jason.decode!(data) |> Map.get("query") end)
+      api_calls =
+        Enum.map(api_calls, fn {_, data} ->
+          data = Jason.decode!(data)
+          %{query: data["query"], selector: data["selector"]}
+        end)
 
       # There could be some test that exported api calls data and that happens async
       # so something could happend even after the `clear_state` is called
-      assert length(api_calls_queries) >= 3
-      assert "getMetric|mvrv_usd" in api_calls_queries
-      assert "getMetric|nvt" in api_calls_queries
-      assert "getMetric|daily_active_addresses" in api_calls_queries
+      assert length(api_calls) >= 5
+      slug_selector = [%{slug: slug}] |> Jason.encode!()
+      slugs_selector = [%{slugs: [slug, slug2]}] |> Jason.encode!()
+
+      assert %{
+               query: "getMetric|daily_active_addresses",
+               selector: slug_selector
+             } in api_calls
+
+      assert %{
+               query: "getMetric|nvt",
+               selector: slug_selector
+             } in api_calls
+
+      assert %{
+               query: "getMetric|mvrv_usd",
+               selector: slug_selector
+             } in api_calls
+
+      assert %{
+               query: "getMetric|nvt",
+               selector: slugs_selector
+             } in api_calls
+
+      assert %{
+               query: "getMetric|mvrv_usd",
+               selector: slugs_selector
+             } in api_calls
     end)
   end
 
@@ -49,6 +84,28 @@ defmodule SanbaseWeb.Graphql.ApiCallDataApiTest do
         timeseriesData(slug: "#{slug}", from: "#{from}", to: "#{to}", interval: "#{interval}"){
           datetime
           value
+        }
+      }
+    }
+    """
+
+    conn
+    |> post("/graphql", query_skeleton(query))
+    |> json_response(200)
+  end
+
+  defp get_metric_timeseries_data_per_slug(conn, metric, slugs, from, to, interval) do
+    slugs_str = Enum.map(slugs, &~s|"#{&1}"|) |> Enum.join(", ")
+
+    query = """
+    {
+      get_metric(metric: "#{metric}") {
+        timeseriesDataPerSlug(selector: {slugs: [#{slugs_str}]}, from: "#{from}", to: "#{to}", interval: "#{interval}"){
+          datetime
+          data{
+            slug
+            value
+          }
         }
       }
     }

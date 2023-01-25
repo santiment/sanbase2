2ae58e0d2777018d822dffe017316687435e62db
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-01-25 16:50:41 +0200

    Allow trackEvents to be executed by anon users

--- lib/sanbase_web/graphql/resolvers/intercom_resolver.ex
+++ lib/sanbase_web/graphql/resolvers/intercom_resolver.ex
@@ -1,5 +1,6 @@
 defmodule SanbaseWeb.Graphql.Resolvers.IntercomResolver do
   import Norm
+  import Sanbase.DateTimeUtils, only: [from_iso8601!: 1]
 
   alias Sanbase.Intercom.UserEvent
   alias Sanbase.Clickhouse.ApiCallData
@@ -26,19 +27,22 @@ defmodule SanbaseWeb.Graphql.Resolvers.IntercomResolver do
     {:ok, ApiCallData.api_metric_distribution_per_user()}
   end
 
-  def track_events(_, %{events: events}, %{context: %{auth: %{current_user: user}}}) do
+  def track_events(_, %{events: events}, resolution) do
     if valid_events?(events) do
       events =
         events
         |> Enum.map(fn %{"event_name" => event_name, "created_at" => created_at} = event ->
+          %{user_id: user_id, anonymous_user_id: anonymous_user_id} =
+            get_identification_fields(resolution, event)
+
           %{
             event_name: event_name,
-            created_at:
-              Sanbase.DateTimeUtils.from_iso8601!(created_at) |> DateTime.truncate(:second),
-            user_id: user.id,
+            created_at: from_iso8601!(created_at) |> DateTime.truncate(:second),
             metadata: event["metadata"],
-            inserted_at: Timex.now() |> DateTime.truncate(:second),
-            updated_at: Timex.now() |> DateTime.truncate(:second)
+            inserted_at: DateTime.utc_now() |> DateTime.truncate(:second),
+            updated_at: DateTime.utc_now() |> DateTime.truncate(:second),
+            user_id: user_id,
+            anonymous_user_id: anonymous_user_id
           }
         end)
 
@@ -50,11 +54,24 @@ defmodule SanbaseWeb.Graphql.Resolvers.IntercomResolver do
     end
   end
 
-  def valid_events?(events) do
+  # Private functions
+
+  # Get the user_id and anonymous_user_id. They cannot be NULL as primary key
+  # fields in Clickhouse cannot be NULL. use 0 and empty string when these fields
+  # need to not be filled
+  defp get_identification_fields(%{context: %{auth: %{current_user: user}}}, _events) do
+    %{user_id: user.id, anonymous_user_id: ""}
+  end
+
+  defp get_identification_fields(_resolution, events) do
+    %{user_id: 0, anonymous_user_id: events["anonymous_user_id"] || ""}
+  end
+
+  defp valid_events?(events) do
     Enum.all?(events, &valid_event?/1)
   end
 
-  def valid_event?(event) do
+  defp valid_event?(event) do
     event_schema =
       selection(
         schema(%{

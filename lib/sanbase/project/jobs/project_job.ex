09d210c11ab66c2bdf77d3fd629f62484f81dc95
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-01-26 15:10:08 +0200

    Add ecosystem to the RepoReader validation and update

--- lib/sanbase/project/jobs/project_job.ex
+++ lib/sanbase/project/jobs/project_job.ex
@@ -1,4 +1,17 @@
 defmodule Sanbase.Project.Job do
+  @doc ~s"""
+  Projects have two fields for ecosystem: `ecosystem` and `ecosystem_full_path`.
+  `ecosystem` holds just the ecosystem name, while `ecosystem_full_path` is the
+  path enumeration, going to the root. For example if X is built on top of Arbitrum,
+  and Arbitrum is built on top of Ethereum, then we'll have the following:
+    ecosystem of Arbitrum: Ethereum
+    ecosystem_full_path of Arbitrum: /ethereum/
+    ecosystem of X: Arbitrum
+    ecosystem_full_path of X: /ethereum/arbitrum
+
+  This function uses the ecosystem to compute the ecosystem_full_path (recurisvelly,
+  where needed).
+  """
   def compute_ecosystem_full_path() do
     projects = Sanbase.Project.List.projects(include_hidden: true)
     slug_to_project_map = Map.new(projects, &{&1.slug, &1})

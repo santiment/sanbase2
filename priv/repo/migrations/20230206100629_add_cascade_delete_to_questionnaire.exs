2140d499f1e58180cff0ad25ed974c070441237e
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-02-06 12:22:18 +0200

    Add cascade delete to questionnaire references

---
+++ priv/repo/migrations/20230206100629_add_cascade_delete_to_questionnaire.exs
@@ -0,0 +1,37 @@
+defmodule Sanbase.Repo.Migrations.AddCascadeDeleteToQuestionnaire do
+  use Ecto.Migration
+
+  def up do
+    drop(constraint(:questionnaire_questions, :questionnaire_questions_questionnaire_uuid_fkey))
+
+    alter table(:questionnaire_questions) do
+      modify(
+        :questionnaire_uuid,
+        references(:questionnaires,
+          column: :uuid,
+          type: :uuid,
+          null: false,
+          on_delete: :delete_all
+        )
+      )
+    end
+
+    drop(constraint(:questionnaire_answers, :questionnaire_answers_question_uuid_fkey))
+
+    alter table(:questionnaire_answers) do
+      modify(
+        :question_uuid,
+        references(:questionnaire_questions,
+          column: :uuid,
+          type: :uuid,
+          null: false,
+          on_delete: :delete_all
+        )
+      )
+    end
+  end
+
+  def down do
+    :ok
+  end
+end

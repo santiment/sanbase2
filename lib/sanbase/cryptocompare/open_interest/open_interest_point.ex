b8d91e72e548acaf2a03083d614e6376ff6acc48
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-03-22 09:55:27 +0200

    Rework open interest data exporter
    Instead of hourly OHLC data, export minute data

---
+++ lib/sanbase/cryptocompare/open_interest/open_interest_point.ex
@@ -0,0 +1,29 @@
+defmodule Sanbase.Cryptocompare.OpenInterestPoint do
+  @fields [
+    :market,
+    :instrument,
+    :mapped_instrument,
+    :quote_currency,
+    :settlement_currency,
+    :contract_currency,
+    :close_settlement,
+    :close_mark_price,
+    :close_quote,
+    :timestamp
+  ]
+  defstruct @fields
+
+  def new(map) when is_map(map) do
+    struct!(__MODULE__, Map.take(map, @fields))
+  end
+
+  def json_kv_tuple(%__MODULE__{} = point) do
+    point = point |> Map.from_struct()
+
+    key =
+      [point.market, point.mapped_instrument, point.timestamp]
+      |> Enum.join("_")
+
+    {key, Jason.encode!(point)}
+  end
+end

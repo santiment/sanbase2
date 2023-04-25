f0d27c5b3c54c0981506d009e7a0506f15545815
ivan <IvanIvanoff@users.noreply.github.com>
2023-04-25 09:59:04 +0300

    Migrate distillery to mix release (#3757)

--- lib/mix/release_tasks/release_tasks.ex
+++ lib/mix/release_tasks/release_tasks.ex
@@ -1,6 +1,17 @@
 defmodule Sanbase.ReleaseTasks do
   alias Sanbase.ReleaseTasks.Migrate
 
+  def migrate do
+    # Load the code for myapp, but don't start it
+    Application.load(:sanbase)
+
+    Migrate.run()
+
+    # Alert shutdown
+    IO.puts("Success!")
+    :init.stop()
+  end
+
   def run do
     # Load the code for myapp, but don't start it
     Application.load(:sanbase)

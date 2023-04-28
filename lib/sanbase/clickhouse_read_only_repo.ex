15183b79f79c698410913fc7e34204d5031da471
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-04-28 14:59:45 +0300

    Fix ReadOnly CH init

--- lib/sanbase/clickhouse_read_only_repo.ex
+++ lib/sanbase/clickhouse_read_only_repo.ex
@@ -31,8 +31,7 @@ defmodule Sanbase.ClickhouseRepo.ReadOnly do
     pool_size = Config.module_get(__MODULE__, :pool_size) |> Sanbase.Math.to_integer()
     url = System.get_env("CLICKHOUSE_READONLY_DATABASE_URL")
 
-    url_opts =
-      opts =
+    opts =
       opts
       |> Keyword.merge(Sanbase.ClickhouseRepo.url_to_opts(url))
       |> Keyword.put(:pool_size, pool_size)

d4bbb1503e1ca90e9c3b06feb8e1d68f7e6f38c4
ivan <IvanIvanoff@users.noreply.github.com>
2023-08-24 15:12:49 +0300

    Undo rate limiting comment (#3897)

--- lib/sanbase/external_services/rate_limiting/middleware.ex
+++ lib/sanbase/external_services/rate_limiting/middleware.ex
@@ -4,7 +4,7 @@ defmodule Sanbase.ExternalServices.RateLimiting.Middleware do
   alias Sanbase.ExternalServices.RateLimiting.Server
 
   def call(env, next, options) do
-    # Server.wait(Keyword.get(options, :name))
+    Server.wait(Keyword.get(options, :name))
 
     Tesla.run(env, next)
   end

bd0aac8dd25303e2ec166c538417ac76249e5028
ivan <IvanIvanoff@users.noreply.github.com>
2023-11-01 16:58:24 +0200

    Update esbuild config to include externals (#3960)

--- config/config.exs
+++ config/config.exs
@@ -2,9 +2,10 @@ import Config
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.17.11",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --external:/js/* --external:/css/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]

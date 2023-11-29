2a091cdb15832ac7ec2d05a708c04884917f87d2
volynvlad <volynvlad@gmail.com>
2023-11-29 17:22:33 +0100

    Rewrite query without FINAL

--- lib/sanbase/social_data/trending_words.ex
+++ lib/sanbase/social_data/trending_words.ex
@@ -270,23 +270,24 @@ defmodule Sanbase.SocialData.TrendingWords do
     (
       SELECT
         #{to_unix_timestamp(interval, "dt", argument_name: "interval")} AS t,
-        dt,
+        argMax(dt, computed_at) AS dt,
         max(dt) OVER (PARTITION BY t) AS last_dt_in_group,
-        word,
-        project,
-        score / {{score_equalizer}} AS score,
-        words_context AS context,
-        summary,
-        bullish_summary,
-        bearish_summary,
-        tuple(positive_ratio, neutral_ratio, negative_ratio) AS sentiment_ratios,
-        tuple(positive_bb_ratio, neutral_bb_ratio, negative_bb_ratio) AS bb_sentiment_ratios
-      FROM #{@table} FINAL
+        argMax(word, computed_at) AS word,
+        argMax(project, computed_at) AS project,
+        argMax(score, computed_at) / {{score_equalizer}} AS score,
+        argMax(words_context, computed_at) AS context,
+        argMax(summary, computed_at) AS summary,
+        argMax(bullish_summary, computed_at) AS bullish_summary,
+        argMax(bearish_summary, computed_at) AS bearish_summary,
+        tuple(argMax(positive_ratio, computed_at), argMax(neutral_ratio, computed_at), argMax(negative_ratio, computed_at)) AS sentiment_ratios,
+        tuple(argMax(positive_bb_ratio, computed_at), argMax(neutral_bb_ratio, computed_at), argMax(negative_bb_ratio, computed_at)) AS bb_sentiment_ratios
+      FROM #{@table}
       WHERE
         dt >= toDateTime({{from}}) AND
         dt < toDateTime({{to}}) AND
         source = {{source}}
         #{word_type_filter_str(word_type_filter)}
+      GROUP BY source, docs_id, t, last_dt_in_group
     )
     WHERE dt = last_dt_in_group
     ORDER BY t, score DESC

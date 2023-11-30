35916416fece7418dc884216c5bb41b48c5b43ad
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-11-30 17:48:53 +0200

    Implement pow named function call evaluation

--- lib/sanbase/san_lang/interpreter.ex
+++ lib/sanbase/san_lang/interpreter.ex
@@ -5,6 +5,7 @@ defmodule Sanbase.SanLang.Interpreter do
   def eval({:int, _, value}, _env), do: value
   def eval({:float, _, value}, _env), do: value
   def eval({:ascii_string, _, value}, _env), do: value
+  def eval({:env_var, _, "@" <> env_var}, env), do: Environment.get_env_binding(env, env_var)
 
   # Arithemtic
   def eval({:+, l, r}, env), do: eval(l, env) + eval(r, env)
@@ -13,14 +14,23 @@ defmodule Sanbase.SanLang.Interpreter do
   def eval({:/, l, r}, env), do: eval(l, env) / eval(r, env)
 
   # Access Operator
-
   def eval({:access_op, {:access_op, _, _} = inner_access_op, {:ascii_string, _, key}}, env) do
     env_var = eval(inner_access_op, env)
     Map.get(env_var, key)
   end
 
-  def eval({:access_op, {:env_var, _, "@" <> env_var}, {:ascii_string, _, key}}, env) do
-    env_var = Environment.get_env_binding(env, env_var)
+  def eval({:access_op, {:env_var, _, _} = env_var, {:ascii_string, _, key}}, env) do
+    env_var = eval(env_var, env)
     Map.get(env_var, key)
+  end
+
+  # Named Function Calls
+  def eval({:function_call, {:identifier, _, function_name}, args}, env) do
+    eval_function_call(function_name, args, env)
+  end
+
+  defp eval_function_call("pow", args, env) when length(args) == 2 do
+    args = Enum.map(args, &eval(&1, env))
+    apply(Sanbase.SanLang.Kernel, :pow, args)
   end
 end

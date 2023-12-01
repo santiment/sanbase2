e17f4180001e42abe4521017581e22c70d229229
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-12-01 12:46:45 +0200

    Implement map in SanLang

--- lib/sanbase/san_lang/interpreter.ex
+++ lib/sanbase/san_lang/interpreter.ex
@@ -1,11 +1,23 @@
 defmodule Sanbase.SanLang.Interpreter do
+  alias Sanbase.SanLang
   alias Sanbase.SanLang.Environment
+
+  defmodule UnboundError do
+    defexception [:message]
+  end
+
+  defmodule UndefinedFunctionError do
+    defexception [:message]
+  end
 
   # Terminal values
   def eval({:int, _, value}, _env), do: value
   def eval({:float, _, value}, _env), do: value
   def eval({:ascii_string, _, value}, _env), do: value
-  def eval({:env_var, _, "@" <> env_var}, env), do: Environment.get_env_binding(env, env_var)
+  def eval({:env_var, _, _} = env_var, env), do: eval_env_var(env_var, env)
+  def eval({:identifier, _, _} = identifier, env), do: eval_identifier(identifier, env)
+  # The true evaluation is called from withing the Kernel.map/filter/etc. functions
+  def eval({:lambda_fn, _args, _body} = lambda, _env), do: lambda
 
   # Arithemtic
   def eval({:+, l, r}, env), do: eval(l, env) + eval(r, env)
@@ -19,9 +31,9 @@ defmodule Sanbase.SanLang.Interpreter do
     Map.get(env_var, key)
   end
 
-  def eval({:access_op, {:env_var, _, _} = env_var, {:ascii_string, _, key}}, env) do
-    env_var = eval(env_var, env)
-    Map.get(env_var, key)
+  def eval({:access_op, env_var_or_identifier, {:ascii_string, _, key}}, env) do
+    map = eval(env_var_or_identifier, env)
+    Map.get(map, key)
   end
 
   # Named Function Calls
@@ -29,8 +41,36 @@ defmodule Sanbase.SanLang.Interpreter do
     eval_function_call(function_name, args, env)
   end
 
-  defp eval_function_call(function_name, args, env) do
-    args = Enum.map(args, &eval(&1, env))
-    apply(Sanbase.SanLang.Kernel, :pow, args)
+  def true_eval_lambda_fn({:lambda_fn, _args, _body} = lambda, env),
+    do: eval_lambda_fn(lambda, env)
+
+  @supported_functions SanLang.Kernel.__info__(:functions)
+                       |> Enum.map(fn {name, _arity} -> to_string(name) end)
+  defp eval_function_call(function_name, args, env)
+       when is_binary(function_name) and function_name in @supported_functions do
+    args = Enum.map(args, &eval(&1, env)) ++ [env]
+    apply(SanLang.Kernel, String.to_existing_atom(function_name), args)
+  end
+
+  defp eval_function_call(function_name, _args, _env) when is_binary(function_name) do
+    raise UndefinedFunctionError, message: "Function #{function_name} is not supported"
+  end
+
+  defp eval_env_var({:env_var, _, "@" <> key}, env) do
+    case Environment.get_env_binding(env, key) do
+      {:ok, value} -> value
+      {:error, error} -> raise UnboundError, message: error
+    end
+  end
+
+  defp eval_identifier({:identifier, _, key}, env) do
+    case Environment.get_local_binding(env, key) do
+      {:ok, value} -> value
+      {:error, error} -> raise UnboundError, message: error
+    end
+  end
+
+  defp eval_lambda_fn({:lambda_fn, _args, body}, env) do
+    eval(body, env)
   end
 end
--- lib/sanbase/san_lang/kernel.ex
+++ lib/sanbase/san_lang/kernel.ex
@@ -1,9 +1,24 @@
 defmodule Sanbase.SanLang.Kernel do
+  alias Sanbase.SanLang.Environment
+  alias Sanbase.SanLang.Interpreter
+
   def pow(base, pow) when is_number(base) and is_number(pow) do
     base ** pow
   end
 
-  def map(enum, fun) do
-    Enum.map(enum, fun)
+  def map(
+        enumerable,
+        {:lambda_fn, [{:identifier, _, local_binding}], _body} = fun,
+        %Environment{} = env
+      ) do
+    enumerable
+    |> Enum.reduce({[], env}, fn elem, {acc, env} ->
+      env = Environment.add_local_binding(env, local_binding, elem)
+
+      computed_item = Interpreter.true_eval_lambda_fn(fun, env)
+      {[computed_item | acc], env}
+    end)
+    |> elem(0)
+    |> Enum.reverse()
   end
 end

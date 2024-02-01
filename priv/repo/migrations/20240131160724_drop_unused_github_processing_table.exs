d4a76e1ca5ed2be2e3e8b17b5e295be381add2a6
ivan <IvanIvanoff@users.noreply.github.com>
2024-02-01 10:01:24 +0200

    Drop unused processed_github_archives table (#4084)
    The work is now done by github-exporter in a different postgres
    database. This table has no new data since 2019-01-09, but it takes up
    8GB of space (53 million records), which is around 13% of the total space used by RDS.

---
+++ priv/repo/migrations/20240131160724_drop_unused_github_processing_table.exs
@@ -0,0 +1,20 @@
+defmodule Sanbase.Repo.Migrations.DropUnusedGithubProcessingTable do
+  use Ecto.Migration
+
+  def up do
+    drop(table(:processed_github_archives))
+  end
+
+  def down do
+    # This table is unused for a long time. Its purpose and work has been moved
+    # to the github-exporter project
+    create table(:processed_github_archives) do
+      add(:project_id, references(:project, on_delete: :delete_all))
+      add(:archive, :string, null: false)
+
+      timestamps()
+    end
+
+    create(unique_index(:processed_github_archives, [:project_id, :archive]))
+  end
+end

7b3a524c71507c23a114f8cc68388882f22cf33b
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2024-02-20 16:44:23 +0100

    Replace svgs with icon components

--- lib/sanbase_web/components/table.ex
+++ lib/sanbase_web/components/table.ex
@@ -410,21 +410,7 @@ defmodule SanbaseWeb.TableComponent do
           >
             <div class="flex items-center">
               <span x-text="selectedField"></span>
-              <svg
-                class="w-2.5 h-2.5 ms-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
+              <.icon name="hero-chevron-down" class="w-2.5 h-2.5 ms-2.5" />
             </div>
           </button>
           <div
@@ -464,21 +450,7 @@ defmodule SanbaseWeb.TableComponent do
               type="submit"
               class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <svg
-                class="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+              <.icon name="hero-magnifying-glass" class="w-4 h-4" />
               <span class="sr-only">Search</span>
             </button>
           </div>

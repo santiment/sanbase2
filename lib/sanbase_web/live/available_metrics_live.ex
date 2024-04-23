419b78402441089af47b8d99292185cf2e71c178
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-04-23 16:59:28 +0300

    Update available metrics table

--- lib/sanbase_web/live/available_metrics_live.ex
+++ lib/sanbase_web/live/available_metrics_live.ex
@@ -3,21 +3,28 @@ defmodule SanbaseWeb.AvailableMetricsLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    metrics_map = get_metrics() |> Map.new(fn m -> {m.api_name, m} end)
+    visible_metrics = metrics_map |> Map.keys() |> Enum.sort(:asc)
+
     {:ok,
      socket
-     |> assign(:rows, get_metrics())}
+     |> assign(
+       visible_metrics: visible_metrics,
+       metrics_map: metrics_map
+     )}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      <div class="flex justify-evenly">
-        <.table id="available_metrics" rows={@rows}>
+    <div class="flex justify-center">
+      <div class="grid">
+        <.controls />
+        <.table id="available_metrics" rows={Map.take(@metrics_map, @visible_metrics) |> Map.values()}>
           <:col :let={row} label="API Name"><%= row.api_name %></:col>
           <:col :let={row} label="Internal Name"><%= row.internal_name %></:col>
           <:col :let={row} label="Docs">
-            <.docs_links links={row.docs} />
+            <.docs_links docs={row.docs} />
           </:col>
           <:col :let={row} label="Sanbase Access"><%= row.sanbase_access %></:col>
           <:col :let={row} label="SanAPI Access"><%= row.sanapi_access %></:col>
@@ -31,13 +38,37 @@ defmodule SanbaseWeb.AvailableMetricsLive do
     """
   end
 
-  defp available_assets(assigns) do
-    ~H"""
-    <span>
-      bitcoin, ethereum, xrp, ...
-    </span>
-    """
+  @impl true
+  def handle_event("apply_controls", params, socket) do
+    visible_metrics =
+      socket.assigns.metrics_map
+      |> Map.values()
+      |> maybe_apply_filter(:only_with_docs, params)
+      |> maybe_apply_filter(:only_intraday_metrics, params)
+      |> maybe_apply_filter(:match_metric_name, params)
+      |> Enum.map(& &1.api_name)
+
+    {:noreply,
+     socket
+     |> assign(:visible_metrics, visible_metrics)}
   end
+
+  defp maybe_apply_filter(metrics, :only_with_docs, %{"only_with_docs" => "on"}) do
+    metrics
+    |> Enum.filter(&(&1.docs != []))
+  end
+
+  defp maybe_apply_filter(metrics, :only_intraday_metrics, %{"only_intraday_metrics" => "on"}) do
+    metrics
+    |> Enum.filter(&(&1.frequency_seconds < 86400))
+  end
+
+  defp maybe_apply_filter(metrics, :match_metric_name, %{"match_metric_name" => str}) do
+    metrics
+    |> Enum.filter(&String.contains?(&1.api_name, str))
+  end
+
+  defp maybe_apply_filter(metrics, _, _), do: metrics
 
   defp get_metrics() do
     metrics = Sanbase.Metric.available_metrics()
@@ -49,21 +80,58 @@ defmodule SanbaseWeb.AvailableMetricsLive do
       %{
         api_name: m.metric,
         internal_name: m.metric,
-        docs: Map.get(m, :docs, []),
+        docs: Map.get(m, :docs) || [],
         available_assets: [],
         frequency: m.min_interval,
+        frequency_seconds: Sanbase.DateTimeUtils.str_to_sec(m.min_interval),
         sanbase_access: "free",
         sanapi_access: "free"
       }
     end)
+    |> Enum.sort_by(& &1.api_name, :asc)
+  end
+
+  defp controls(assigns) do
+    ~H"""
+    <div>
+      <form phx-change="apply_controls" class="flex space-x-8">
+        <div>
+          <input type="checkbox" name="only_with_docs" />
+          <label>Only metrics with docs</label>
+        </div>
+
+        <div>
+          <input type="checkbox" name="only_intraday_metrics" />
+          <label>Only intraday metrics</label>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="match_metric_name"
+            phx-debounce="200"
+            placeholder="Filter by metric name"
+          />
+        </div>
+      </form>
+    </div>
+    """
+  end
+
+  defp available_assets(assigns) do
+    ~H"""
+    <span>
+      bitcoin, ethereum, xrp, ...
+    </span>
+    """
   end
 
   defp docs_links(assigns) do
     ~H"""
     <div class="flex flex-row">
-      <.link href="https://academy.santiment.net/metrics/mvrv">
+      <a :for={doc <- assigns.docs} href={doc.link} target="_blank">
         Open Docs
-      </.link>
+      </a>
     </div>
     """
   end

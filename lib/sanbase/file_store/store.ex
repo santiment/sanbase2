bf6455db3b5e562bb40154952340fabd8aa07682
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-05-15 11:39:30 +0300

    Increase FileStore size from 5mb to 10mb

--- lib/sanbase/file_store/store.ex
+++ lib/sanbase/file_store/store.ex
@@ -4,7 +4,7 @@ defmodule Sanbase.FileStore do
   @versions [:original]
   @acl :public_read
   @extension_whitelist ~w(.jpg .jpeg .gif .png .pdf .csv)
-  @max_file_size 5 * 1024 * 1024
+  @max_file_size 10 * 1024 * 1024
   @cache_max_age 2_592_000
   @doc ~s"""
     Whitelist file extensions. Now allowing only images.

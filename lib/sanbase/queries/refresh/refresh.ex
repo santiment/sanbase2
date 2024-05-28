4be349146f196def57b76c0896d9410cd46a216d
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2024-05-28 09:10:21 +0200

    Refresh dashboards cache

--- lib/sanbase/queries/refresh/refresh.ex
+++ lib/sanbase/queries/refresh/refresh.ex
@@ -2,9 +2,15 @@ defmodule Sanbase.Queries.Refresh do
   alias Sanbase.Queries.Query
   alias Sanbase.Queries.QueryMetadata
   alias Sanbase.Queries.RefreshWorker
+  alias Sanbase.Queries.RefreshSchedulerWorker
   alias Sanbase.Accounts.User
 
   @oban_conf_name :oban_web
+
+  def schedule_refresh_all_user_queries(user_id) do
+    data = RefreshSchedulerWorker.new(%{user_id: user_id}, scheduled_at: tomorrow_1_am())
+    Oban.insert!(@oban_conf_name, data)
+  end
 
   def refresh_all_user_queries(user_id) do
     queries = all_user_queries(user_id)
@@ -34,7 +40,8 @@ defmodule Sanbase.Queries.Refresh do
     # Don't store the execution details in the database. Also don't deduct credits from users for refreshing queries
     with {:ok, result} <-
            Sanbase.Queries.run_query(query, user, query_metadata, store_execution_details: false),
-         {:ok, _} <- Sanbase.Queries.cache_query_execution(query.id, result, user.id) do
+         {:ok, _} <- Sanbase.Queries.cache_query_execution(query.id, result, user.id),
+         :ok <- refresh_query_dashboards_cache(result, user.id) do
       {:ok, result}
     end
   end
@@ -45,6 +52,22 @@ defmodule Sanbase.Queries.Refresh do
     refresh_query(query, user)
   end
 
+  def refresh_query_dashboards_cache(query_result, user_id) do
+    Sanbase.Dashboards.DashboardQueryMapping.dashboards_by_query_and_user(
+      query_result.query_id,
+      user_id
+    )
+    |> Sanbase.Repo.all()
+    |> Enum.each(fn mapping ->
+      Sanbase.Dashboards.cache_dashboard_query_execution(
+        mapping.dashboard_id,
+        mapping.id,
+        query_result,
+        user_id
+      )
+    end)
+  end
+
   def all_user_queries(user_id, page \\ 1, acc \\ []) do
     {:ok, queries} =
       Sanbase.Queries.get_user_queries(user_id, user_id, page: page, page_size: 100)
@@ -53,5 +76,12 @@ defmodule Sanbase.Queries.Refresh do
       [] -> acc
       _ -> all_user_queries(user_id, page + 1, acc ++ queries)
     end
+  end
+
+  defp tomorrow_1_am do
+    # tomorrow 1AM after midnight
+    Timex.now()
+    |> Timex.beginning_of_day()
+    |> Timex.shift(days: 1, hours: 1)
   end
 end
---
+++ lib/sanbase/queries/refresh/scheduler_worker.ex
@@ -0,0 +1,12 @@
+defmodule Sanbase.Queries.RefreshSchedulerWorker do
+  use Oban.Worker,
+    queue: :refresh_queries,
+    max_attempts: 3
+
+  alias Sanbase.Queries.Refresh
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"user_id" => user_id}}) do
+    Refresh.refresh_all_user_queries(user_id)
+  end
+end

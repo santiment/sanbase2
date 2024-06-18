a325773a83fd69ef99e991aeb4627db2588111fd
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-06-18 15:21:56 +0300

    Add availableLabelFqns(slug: <slug>) getMetric's metadata field

--- lib/sanbase_web/graphql/schema/types/metric_types.ex
+++ lib/sanbase_web/graphql/schema/types/metric_types.ex
@@ -368,6 +368,7 @@ defmodule SanbaseWeb.Graphql.MetricTypes do
     end
 
     field :available_label_fqns, list_of(:string) do
+      arg(:slug, :string)
       cache_resolve(&MetricResolver.get_available_label_fqns/3, ttl: 300)
     end
 

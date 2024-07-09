acaa8fb3e6546a5ebdee3746657c91ff3f987261
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-07-09 10:19:46 +0300

    Add popover to /available_metrics table th

--- lib/sanbase_web/live/available_metrics/metric_details_live.ex
+++ lib/sanbase_web/live/available_metrics/metric_details_live.ex
@@ -1,7 +1,10 @@
 defmodule SanbaseWeb.MetricDetailsLive do
   use SanbaseWeb, :live_view
 
-  import SanbaseWeb.AvailableMetricsComponents
+  import SanbaseWeb.CoreComponents
+  import SanbaseWeb.AvailableMetricsDescription
+
+  alias SanbaseWeb.AvailableMetricsComponents
 
   @impl true
   def mount(%{"metric" => metric}, _session, socket) do
@@ -23,7 +26,7 @@ defmodule SanbaseWeb.MetricDetailsLive do
         Showing details for <span class="text-blue-700"><%= @metric %></span>
       </h1>
       <div class="my-4">
-        <.available_metrics_button
+        <AvailableMetricsComponents.available_metrics_button
           text="Back to Available Metrics"
           href={~p"/available_metrics"}
           icon="hero-arrow-uturn-left"
@@ -31,25 +34,11 @@ defmodule SanbaseWeb.MetricDetailsLive do
       </div>
       <.table id="available_metrics" rows={@rows}>
         <:col :let={row} col_class="w-40">
-          <div class="relative">
-            <div
-              data-popover-target={Map.get(row, :popover_target)}
-              data-popover-style="light"
-              data-popover-placement="right"
-            >
-              <span class="border-b border-dotted border-gray-500 hover:cursor-pointer">
-                <%= row.key %>
-              </span>
-            </div>
-
-            <div
-              id={Map.get(row, :popover_target)}
-              role="tooltip"
-              class="absolute max-h-[580px] min-w-[860px] overflow-y-auto z-10 invisible inline-block px-8 py-6 text-sm font-medium text-gray-600 bg-white border border-gray-200 rounded-lg shadow-2xl sans"
-            >
-              <span class="[&>pre]:font-sans"><%= Map.get(row, :popover_target_text) %></span>
-            </div>
-          </div>
+          <AvailableMetricsComponents.popover
+            display_text={row.key}
+            popover_target={Map.get(row, :popover_target)}
+            popover_target_text={Map.get(row, :popover_target_text)}
+          />
         </:col>
 
         <:col :let={row}>
@@ -92,7 +81,7 @@ defmodule SanbaseWeb.MetricDetailsLive do
   defp formatted_value(%{key: "Docs"} = assigns) do
     ~H"""
     <div class="flex flex-row">
-      <.available_metrics_button
+      <AvailableMetricsComponents.available_metrics_button
         :for={doc <- assigns.value}
         href={doc.link}
         text="Docs"
@@ -222,281 +211,6 @@ defmodule SanbaseWeb.MetricDetailsLive do
         else: rows
 
     rows
-  end
-
-  defp get_popover_text(%{key: "Name"} = assigns) do
-    ~H"""
-    <pre>
-    The name of the metric that is used in the public API.
-    For example, if the metric is `price_usd` it is provided as the `metric` argument.
-
-    Example:
-
-      {
-        getMetric(<b>metric: "price_usd"</b>){
-          timeseriesData(asset: "ethereum" from: "utc_now-90d" to: "utc_now" interval: "1d"){
-            datetime
-            value
-          }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Internal Name"} = assigns) do
-    ~H"""
-    <pre>
-    The name of the metric that is used in the database tables.
-    The database tables are accessed through Santiment Queries when the
-    user interacts with the data via SQL.
-
-    How to use Santiment Queries, check <.link href={"https://academy.santiment.net/santiment-queries"} class="underline text-blue-600">this link</.link>
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Frequency"} = assigns) do
-    ~H"""
-    <pre>
-    The minimum interval at which the metric is updated.
-
-    For more details check <.link href="https://academy.santiment.net/metrics/details/frequency" class="underline text-blue-600">this link</.link>
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Docs"} = assigns) do
-    ~H"""
-    <pre>
-    The link to the documentation page for the metric.
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Has Incomplete Data"} = assigns) do
-    ~H"""
-    <pre>
-    A boolean that indicates whether the metric has incomplete data.
-    Only daily metrics (metrics with Frequency of 1d or bigger) can have incomplete data.
-
-    In some cases, if the day is not yet complete, the current value can be misleading.
-    For instance, fetching daily active addresses at 12pm UTC would
-    include only half a day's data, potentially making the metric value for that day appear too low.
-
-    By default the incomplete data is not returned by the API.
-    To obtain this last incomplete data point, provide the `includeIncompleteData` flag
-    Example:
-      {
-        getMetric(metric: "daily_active_addresses"){
-          timeseriesData(
-            slug: "bitcoin"
-            from: "utc_now-3d"
-            to: "utc_now"
-            <b>includeIncompleteData: true</b>){
-              datetime
-              value
-            }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Is Timebound"} = assigns) do
-    ~H"""
-    <pre>
-    A boolean that indicates whether the metric is timebound.
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Available Aggregations"} = assigns) do
-    ~H"""
-    <pre>
-    The available aggregations for the metric.
-
-    The aggregation controls how multiple data points are combined into one.
-
-    For example, if the metric is `price_usd`, the aggregation is `LAST`, and the
-    interval is `1d`, then each data point will be represented by the last price in the
-    given day.
-
-
-    All aggregations except `OHLC` are queried the same way:
-
-    Example:
-      {
-        getMetric(metric: "price_usd"){
-          timeseriesData(
-          slug: "bitcoin"
-          from: "utc_now-90d"
-          to: "utc_now"
-          <b>aggregation: MAX</b>){
-            datetime
-            value
-          }
-        }
-      }
-
-    When `OHLC` aggregation is used, the result is fetched in a different way -
-    use `valueOhlc` instead of `value`:
-
-    Example:
-      {
-        getMetric(metric: "price_usd"){
-          timeseriesData(
-          slug: "bitcoin"
-          from: "utc_now-90d"
-          to: "utc_now"
-          <b>aggregation: OHLC</b>){
-            datetime
-            <b>valueOhlc {
-              open high close low
-            }</b>
-          }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Default Aggregation"} = assigns) do
-    ~H"""
-    <pre>
-    The default aggregation for the metric.
-
-    The default aggregation is hand picked so it makes most sense for the given metric.
-
-    For example, the default aggregation for `price_usd` is `LAST`, as other aggregations like
-    `SUM` do not make sense for that metric.
-
-    To override the default aggregation, provide the `aggregation` parameter.
-
-    Example:
-      {
-        getMetric(metric: "price_usd"){
-          timeseriesData(
-          slug: "bitcoin"
-          from: "utc_now-90d"
-          to: "utc_now"
-          <b>aggregation: MAX</b>){
-            datetime
-            value
-          }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Available Selectors"} = assigns) do
-    ~H"""
-    <pre>
-    The available selectors for the metric.
-
-    The selectors control what entity the data is fetched for.
-    For example, if the metric is `price_usd`, the selector is `asset`, and the
-    value is `ethereum`, then the data will be fetched for.
-
-    To provide any selector other than `slug`, use the `selector` input parameter.
-
-    Example:
-      {
-        getMetric(metric: "active_withdrawals_per_exchange"){
-          timeseriesData(
-          <b>selector: { slug: "bitcoin" owner: "binance" }</b>
-          from: "utc_now-90d"
-          to: "utc_now"){
-            datetime
-            value
-          }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Required Selectors"} = assigns) do
-    ~H"""
-    <pre>
-    The required selectors for the metric.
-
-    This list includes the selectors that must be provided in order to get data.
-    Not providing the required selectors will lead to an error and no data will be returned.
-
-    Check the information for `Available Selectors` for an example.
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Data Type"} = assigns) do
-    ~H"""
-    <pre>
-    The data type of the metric.
-    The data type is used to determine how the data is stored and fetched.
-
-    All metrics with `timeseries` data type are fetched in a generic way using `timeseriesData` field.
-
-    Example:
-      {
-        getMetric(metric: "price_usd"){
-          <b>timeseriesData</b>(
-            slug: "bitcoin"
-            from: "utc_now-90d"
-            to: "utc_now"){
-              datetime
-              value
-            }
-        }
-      }
-
-    The metrics with `histogram` data type are fetched in different ways as their result format
-    could differ. Check the documentation of each such metric to see an example.
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Available Assets"} = assigns) do
-    ~H"""
-    <pre>
-    The assets for which the metric is available.
-    The metric can be fetched for any of the listed assets.
-
-    Each asset is uniquely identified by its `slug`:
-
-    Example:
-      {
-        getMetric(metric: "daily_active_addresses"){
-          timeseriesData(
-            <b>slug: "bitcoin"</b>
-            from: "utc_now-90d"
-            to: "utc_now"){
-              datetime
-              value
-            }
-        }
-      }
-    </pre>
-    """
-  end
-
-  defp get_popover_text(%{key: "Access"} = assigns) do
-    ~H"""
-    <pre>
-    <b>FREE</b> - The metrics labeled <b>FREE</b> have their entire historical data and realtime data
-    available without any restrictions. These metrics are available to all users, regardless of their
-    subscription level.
-
-    <b>RESTRICTED</b> - The metrics labeled <b>RESTRICTED</b> have their historical and realtime data
-    restricted based on the subscription plan of the user.
-
-    To see how much of the historical and realtime data is restricted, check the restrictions
-    at the <.link class="underline text-blue-600" href="https://app.santiment.net/pricing?plans=business">pricing page</.link>.
-    The documentation about the restriction is avaialble at <.link class="underline text-blue-600" href="https://academy.santiment.net/sanapi/historical-and-realtime-data-restrictions">this Academy page</.link>.
-
-    </pre>
-    """
   end
 
   defp simplify_access(%{"historical" => :free, "realtime" => :free}), do: "FREE"

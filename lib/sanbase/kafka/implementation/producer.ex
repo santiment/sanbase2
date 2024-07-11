82c33476ce47b442548494f5d9763774295e4cf0
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-07-11 17:04:28 +0300

    Manually start the InMemoryKafka in test env
    Previously it was started through the :supervisor config of
    KafkaExporter, which was passed to SanExporterEx, which then starts it.

--- lib/sanbase/kafka/implementation/producer.ex
+++ lib/sanbase/kafka/implementation/producer.ex
@@ -3,9 +3,6 @@ defmodule Sanbase.Kafka.Implementation.Producer do
 
   require Logger
 
-  def start_link(opts) do
-  end
-
   def produce_sync(topic, list) when is_list(list) do
     if Enum.any?(list, &(not match?({<<_::binary>>, _}, &1))) do
       raise ArgumentError, "Invalid list format. Each element should be a tuple {key, value}"
@@ -25,7 +22,7 @@ defmodule Sanbase.Kafka.Implementation.Producer do
         produce_list_to_topic(topic, messages)
 
       {:error, reason} ->
-        Logger.warn("Error while grouping by partition #{inspect(reason)}")
+        Logger.warning("Error while grouping by partition #{inspect(reason)}")
         {:error, reason}
     end
   end

e87440d5e62c220d3f90cbd0d384f3672e75bc37
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-07-23 12:05:06 +0300

    Fix credo warning about Enum.filter in sequence and IO.inspect

--- lib/mix/tasks/list_schemas.ex
+++ lib/mix/tasks/list_schemas.ex
@@ -13,8 +13,15 @@ defmodule Mix.Tasks.List.Schema.Modules.Without.Admin do
 
     modules =
       modules
-      |> Enum.filter(fn module -> to_string(module) =~ ~r/^Elixir\.Sanbase/ end)
-      |> Enum.filter(&({:__schema__, 1} in &1.__info__(:functions)))
+      |> Enum.filter(fn module ->
+        sanbase_module? = String.starts_with?(to_string(module), "Elixir.Sanbase")
+
+        schema_module? =
+          function_exported?(module, :__info__, 1) and
+            {:__schema__, 1} in module.__info__(:functions)
+
+        sanbase_module? and schema_module?
+      end)
 
     schema_modules_with_admin =
       SanbaseWeb.GenericAdmin.resource_module_map()
@@ -24,7 +31,6 @@ defmodule Mix.Tasks.List.Schema.Modules.Without.Admin do
 
     modules
     |> Enum.reject(fn module -> module in schema_modules_with_admin end)
-    # credo:disable-for-next-line
-    |> Enum.each(&IO.inspect(&1))
+    |> Enum.each(&IO.puts(&1))
   end
 end

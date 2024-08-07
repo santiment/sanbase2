e441d77a005bf63b36a131dea808a8d649bffceb
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-08-07 11:19:58 +0300

    Improve SanR Subscription tests' coverage
    Mock the HTTP request itself, so the test also covers the logic that
    transforms the HTTP response body to the proper internal format needed.

--- test/sanbase/billing/sanr_nft_subscription_test.exs
+++ test/sanbase/billing/sanr_nft_subscription_test.exs
@@ -5,20 +5,40 @@ defmodule Sanbase.Billing.SanrNFTSubscriptionTest do
   alias Sanbase.Accounts.EthAccount
   alias Sanbase.Billing.Subscription
 
-  test "create subscription" do
+  setup do
     user = insert(:user)
-    address = "0x23918E95d234eEc054566DDe0841d69311814495"
+
+    address =
+      "0x23918E95d234eEc054566DDe0841d69311814495"
+      |> Sanbase.BlockchainAddress.to_internal_format()
+
     {:ok, _} = EthAccount.create(user.id, address)
     start_date = DateTime.utc_now()
     end_date = DateTime.shift(start_date, month: 12) |> DateTime.truncate(:second)
 
-    Sanbase.Mock.prepare_mock2(
-      &Sanbase.SmartContracts.SanrNFT.get_all_nft_owners/0,
-      {:ok, %{address => %{token_id: 1}}}
-    )
-    |> Sanbase.Mock.prepare_mock2(
-      &Sanbase.SmartContracts.SanrNFT.get_all_nft_expiration_dates/0,
-      {:ok, %{1 => %{start_date: start_date, end_date: end_date}}}
+    %{
+      user: user,
+      address: address,
+      start_date: start_date,
+      end_date: end_date
+    }
+  end
+
+  test "create subscription", context do
+    %{user: user, address: address, start_date: start_date, end_date: end_date} = context
+
+    Sanbase.Mock.prepare_mock(
+      Req,
+      :get,
+      fn req, _params ->
+        case req.options.base_url do
+          "https://zksync-mainnet.g.alchemy.com" ->
+            get_owners_for_contract_mock(address)
+
+          "https://sanrnew-api.production.internal.santiment.net/api/v1/SanbaseSubscriptionNFTCollection/all" ->
+            sanr_nft_collections_mock(start_date, end_date)
+        end
+      end
     )
     |> Sanbase.Mock.run_with_mocks(fn ->
       assert Subscription.user_has_active_sanbase_subscriptions?(user.id) == false
@@ -34,11 +54,7 @@ defmodule Sanbase.Billing.SanrNFTSubscriptionTest do
     end)
   end
 
-  test "remove subscription" do
-    user = insert(:user)
-    address = "0x23918E95d234eEc054566DDe0841d69311814495"
-    {:ok, _} = EthAccount.create(user.id, address)
-
+  test "remove subscription", %{user: user} do
     end_date = DateTime.shift(DateTime.utc_now(), month: 12) |> DateTime.truncate(:second)
     Subscription.NFTSubscription.create_nft_subscription(user.id, :sanr_points_nft, end_date)
 
@@ -59,5 +75,42 @@ defmodule Sanbase.Billing.SanrNFTSubscriptionTest do
 
       assert Subscription.user_has_active_sanbase_subscriptions?(user.id) == false
     end)
+  end
+
+  defp get_owners_for_contract_mock(address) do
+    {:ok,
+     %Req.Response{
+       status: 200,
+       headers: %{},
+       body: %{
+         "owners" => [
+           %{
+             "ownerAddress" => address,
+             "tokenBalances" => [%{"balance" => "1", "tokenId" => "1"}]
+           }
+         ],
+         "pageKey" => nil
+       },
+       trailers: %{},
+       private: %{}
+     }}
+  end
+
+  defp sanr_nft_collections_mock(start_date, end_date) do
+    {:ok,
+     %Req.Response{
+       status: 200,
+       headers: %{},
+       body: [
+         %{
+           "id" => 1,
+           "sanr_points_amount" => 10000,
+           "subscription_end_date" => end_date,
+           "subscription_start_date" => start_date
+         }
+       ],
+       trailers: %{},
+       private: %{}
+     }}
   end
 end

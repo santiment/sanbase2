4508a615f13014c427d4631952612b83e3390bfc
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-08-26 15:31:48 +0300

    Metrics marked with is_hidden: true are not shown in availableMetrics queries

--- test/sanbase/billing/metric_access_level_test.exs
+++ test/sanbase/billing/metric_access_level_test.exs
@@ -944,10 +944,10 @@ defmodule Sanbase.Billing.MetricAccessLevelTest do
         "social_active_users",
         "trending_words_rank",
         # histogram metrics
+        "mentions_count_total",
         "mentions_percentage_twitter",
         "mentions_percentage_total",
         "mentions_count_twitter",
-        "social_volume_all_accounts",
         # social metrics for all accounts
         "age_distribution",
         "price_histogram",

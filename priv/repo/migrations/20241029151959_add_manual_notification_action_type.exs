cf6b3afa322019db7847fd6cf7239474ba9d2a1a
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-11-13 12:13:53 +0200

    Fix migration

--- priv/repo/migrations/20241029151959_add_manual_notification_action_type.exs
+++ priv/repo/migrations/20241029151959_add_manual_notification_action_type.exs
@@ -2,7 +2,7 @@ defmodule Sanbase.Repo.Migrations.AddManualNotificationActionType do
   use Ecto.Migration
 
   def up do
-    execute("ALTER TYPE notification_action_type ADD VALUE IF NOT EXISTS 'manual'")
+    execute("ALTER TYPE public.notification_action_type ADD VALUE IF NOT EXISTS 'manual'")
   end
 
   def down do

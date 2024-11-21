09844434344cb51ddb912aa5fa9e083bc47b9a15
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2024-11-21 10:23:32 +0100

    Fix first promoter change of api

--- test/sanbase_web/graphql/promoter/promoter_api_test.exs
+++ test/sanbase_web/graphql/promoter/promoter_api_test.exs
@@ -26,6 +26,9 @@ defmodule SanbaseWeb.Graphql.PromoterApiTest do
         promotion = promoter["promotions"] |> hd
 
         assert promoter["email"] == resp["email"]
+        assert promoter["earningsBalance"] == resp["earnings_balance"]["cash"]
+        assert promoter["currentBalance"] == resp["current_balance"]["cash"]
+        assert promoter["paidBalance"] == resp["paid_balance"]
         assert promotion["visitorsCount"] == resp["promotions"] |> hd |> Map.get("visitors_count")
       end)
     end
@@ -58,6 +61,9 @@ defmodule SanbaseWeb.Graphql.PromoterApiTest do
         promotion = promoter["promotions"] |> hd
 
         assert promoter["email"] == resp["email"]
+        assert promoter["earningsBalance"] == resp["earnings_balance"]["cash"]
+        assert promoter["currentBalance"] == resp["current_balance"]["cash"]
+        assert promoter["paidBalance"] == resp["paid_balance"]
 
         assert promoter["dashboardUrl"] ==
                  "https://santiment.firstpromoter.com/view_dashboard_as?at=#{resp["auth_token"]}"
@@ -139,54 +145,71 @@ defmodule SanbaseWeb.Graphql.PromoterApiTest do
   def promoter_response() do
     """
     {
-      "id": 2348,
-      "cust_id": "cus_sd4gh302fjlsd",
-      "email": "jon@doe.com",
-      "temp_password": null,
-      "default_promotion_id": 3341,
-      "default_ref_id": "jon56",
-      "earnings_balance": null,
-      "current_balance": null,
+      "id": 8798847,
+      "cust_id": "165319",
+      "email": "test@example.com",
+      "temp_password": "YXaRog",
+      "default_promotion_id": 10093828,
+      "default_ref_id": "test123",
+      "earnings_balance": {"cash": 30690},
+      "current_balance": {"cash": 30690},
       "paid_balance": null,
       "note": null,
-      "auth_token": "QvpsK_rzpzjYCBxfbATV8ubffmYDUf6u",
+      "created_at": "2024-09-06T08:25:35.353Z",
+      "status": "active",
+      "pref": "xxxxx",
+      "parent_promoter_id": null,
+      "w8_form_url": null,
+      "w9_form_url": null,
+      "auth_token": "test_auth_token",
       "profile": {
-        "id": 3390,
-        "first_name": "John",
-        "last_name": "Doe",
-        "website": "https://google.com",
+        "id": 8845623,
+        "first_name": null,
+        "last_name": null,
+        "website": null,
         "paypal_email": null,
         "avatar_url": null,
         "description": null,
+        "company_name": null,
+        "address": null,
+        "country": null,
+        "phone_number": null,
+        "vat_id": null,
         "social_accounts": {}
       },
       "promotions": [
         {
-          "id": 3341,
+          "id": 10093828,
           "status": "offer_inactive",
-          "ref_id": "jon56",
+          "ref_id": "test123",
           "promo_code": null,
           "target_reached_at": null,
-          "promoter_id": 2348,
-          "campaign_id": 1286,
-          "referral_link": "http://test.com#_r_jon56",
+          "promoter_id": 8798847,
+          "campaign_id": 21911,
+          "campaign_name": "Santiment referral program",
+          "referral_link": "https://app.santiment.net/?fpr=test123",
           "current_referral_reward": {
-            "id": 205,
-            "amount": 2000,
+            "id": 19088,
+            "amount": null,
             "type": "per_referral",
             "unit": "cash",
-            "name": "20% recurring commission",
-            "default_promo_code": ""
+            "name": "30% recurring commission",
+            "default_promo_code": null,
+            "per_of_sale": 30
           },
           "current_promotion_reward": null,
           "current_target_reward": null,
-          "visitors_count": 1,
-          "leads_count": 0,
-          "customers_count": 0,
+          "current_offer": null,
+          "customer_promo_code": null,
+          "hidden": false,
+          "visitors_count": 507,
+          "leads_count": 78,
+          "customers_count": 5,
+          "active_customers_count": 3,
           "refunds_count": 0,
-          "cancellations_count": 0,
-          "sales_count": 0,
-          "sales_total": 0,
+          "cancellations_count": 3,
+          "sales_count": 7,
+          "sales_total": 102300,
           "refunds_total": 0
         }
       ]

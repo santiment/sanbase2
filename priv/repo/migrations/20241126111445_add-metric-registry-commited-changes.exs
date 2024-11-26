7e018712c3c57f1c621fdee7c39132efe6319985
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2024-11-26 16:37:50 +0200

    Add migration for metric registry commited

---
+++ priv/repo/migrations/20241126111445_add-metric-registry-commited-changes.exs
@@ -0,0 +1,8 @@
+defmodule :"Elixir.Sanbase.Repo.Migrations.Add-metric-registry-commited-changes" do
+  use Ecto.Migration
+
+  def change do
+    create table(:metric_registry_commited) do
+    end
+  end
+end

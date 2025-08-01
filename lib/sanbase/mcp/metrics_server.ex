03b573ee055bd18d8a99c704e001fc6f036635ec
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-08-01 13:29:47 +0300

    Add MCP Auhorization test

--- lib/sanbase/mcp/metrics_server.ex
+++ lib/sanbase/mcp/metrics_server.ex
@@ -6,7 +6,7 @@ defmodule Sanbase.MCP.MetricsServer do
     version: "1.0.0",
     capabilities: [:tools]
 
-  def init(client_info, frame) do
+  def init(_client_info, frame) do
     user = Sanbase.MCP.Auth.headers_list_to_user(frame.transport.req_headers)
     frame = if user, do: assign(frame, :current_user, user), else: frame
     {:ok, frame |> assign(:is_authenticated, not is_nil(user))}
@@ -16,7 +16,8 @@ defmodule Sanbase.MCP.MetricsServer do
   component(Sanbase.MCP.DiscoveryTool)
   component(Sanbase.MCP.FetchMetricDataTool)
 
-  if Application.compile_env(:sanbase, :env) == :dev do
+  if Application.compile_env(:sanbase, :env) in [:test, :dev] do
+    IO.puts("Defining the extra MCP Server tools used in dev and test")
     # Some tools are enabled only in dev mode so we can test things during development
     component(Sanbase.MCP.CheckAuthentication)
   end

470c823af1da47a5cf45df5e54c6d31ff3bf03be
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-09-20 16:02:20 +0200

    Add function for re-embedding all posts

--- lib/sanbase/insight/post_embedding.ex
+++ lib/sanbase/insight/post_embedding.ex
@@ -1,11 +1,16 @@
 defmodule Sanbase.Insight.PostEmbedding do
   use Ecto.Schema
+
   import Ecto.Query
   import Ecto.Changeset
 
+  alias Sanbase.Insight.Post
+
+  require(Logger)
+
   schema "posts_embeddings" do
     field(:embedding, Pgvector.Ecto.Vector)
-    belongs_to(:post, Sanbase.Insight.Post)
+    belongs_to(:post, Post)
 
     timestamps()
   end
@@ -17,26 +22,45 @@ defmodule Sanbase.Insight.PostEmbedding do
     |> validate_required([:post_id, :embedding])
   end
 
-  def embed_post(%Sanbase.Insight.Post{id: post_id, title: title, text: text}) do
-    markdown = Htmd.convert!(text)
-    chunks = TextChunker.split(markdown, chunk_size: 2000, chunk_overlap: 200, format: :markdown)
+  def embed_all_posts() do
+    all_published_posts =
+      from(p in Post,
+        where: p.ready_state == ^Post.published(),
+        select: [:id, :title, :text]
+      )
+      |> Sanbase.Repo.all()
 
-    chunk_texts = [title] ++ Enum.map(chunks, &String.trim(&1.text))
+    all_published_posts
+    |> Enum.chunk_every(50)
+    |> Enum.each(fn posts ->
+      embed_posts_batch(posts)
+    end)
+  end
 
-    case Sanbase.OpenAI.Embedding.generate_embedding(chunk_texts, 1536) do
+  def embed_posts_batch(posts) when is_list(posts) do
+    chunks =
+      Enum.flat_map(posts, fn post ->
+        markdown = Htmd.convert!(post.text)
+
+        chunks =
+          TextChunker.split(markdown, chunk_size: 2000, chunk_overlap: 200, format: :markdown)
+
+        [{post.id, post.title}] ++ Enum.map(chunks, &{post.id, String.trim(&1.text)})
+      end)
+
+    chunk_texts = Enum.map(chunks, fn {_post_id, text} -> text end)
+
+    case Sanbase.AI.Embedding.generate_embeddings(chunk_texts, 1536) do
       {:ok, embeddings} ->
-        Sanbase.Repo.delete_all(from(pe in __MODULE__, where: pe.post_id == ^post_id))
+        post_ids = Enum.map(posts, & &1.id)
+        Sanbase.Repo.delete_all(from(pe in __MODULE__, where: pe.post_id in ^post_ids))
 
         data =
-          embeddings
-          |> Enum.map(fn embedding ->
+          Enum.zip_with(chunks, embeddings, fn {post_id, _text}, embedding ->
             %{embedding: embedding, post_id: post_id}
           end)
 
         Sanbase.Repo.insert_all(__MODULE__, data)
-
-      {:error, reason} ->
-        IO.inspect(reason, label: "Error generating embedding for post #{post_id}")
     end
   end
 end

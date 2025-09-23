46b2bc2c93fe94065e54ce99b30387258b145208
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-09-23 10:47:12 +0200

    backup

---
+++ lib/sanbase_web/live/admin/faq/history_live.ex
@@ -0,0 +1,13 @@
+defmodule SanbaseWeb.Admin.FaqLive.History do
+  use SanbaseWeb, :live_view
+
+  def mount(params, session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div></div>
+    """
+  end
+end

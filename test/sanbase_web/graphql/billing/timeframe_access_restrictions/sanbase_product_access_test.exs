25c97e4e1c8ed0d2b00548e02bce7f2e5b53c451
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-10-08 10:23:28 +0300

    Improve failing test

--- test/sanbase_web/graphql/billing/timeframe_access_restrictions/sanbase_product_access_test.exs
+++ test/sanbase_web/graphql/billing/timeframe_access_restrictions/sanbase_product_access_test.exs
@@ -28,7 +28,7 @@ defmodule Sanbase.Billing.SanbaseProductAccessTest do
   end
 
   setup do
-    user = insert(:user)
+    user = insert(:user, metric_access_level: "alpha")
     project = insert(:random_erc20_project)
 
     conn = setup_jwt_auth(build_conn(), user)

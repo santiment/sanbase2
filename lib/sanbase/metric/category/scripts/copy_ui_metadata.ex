1bd79ad4e5fe81c4610aafc6ee9f2271d4ead8cf
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-10-23 15:31:12 +0300

    Fix mistyped metrics in DisplayOrder when copying them

--- lib/sanbase/metric/category/scripts/copy_ui_metadata.ex
+++ lib/sanbase/metric/category/scripts/copy_ui_metadata.ex
@@ -3,19 +3,27 @@ defmodule Sanbase.Metric.Category.Scripts.CopyUIMetadata do
   alias Sanbase.Metric.Category
 
   def run() do
+    available_metrics = Sanbase.Metric.available_metrics()
+
     DisplayOrder.get_ordered_metrics()
     |> Map.fetch!(:metrics)
     |> Enum.each(fn m ->
-      import_ui_metric_metadata(m)
+      import_ui_metric_metadata(m, available_metrics)
     end)
   end
 
-  defp import_ui_metric_metadata(%{metric_registry_id: metric_registry_id} = map)
+  defp import_ui_metric_metadata(
+         %{metric_registry_id: metric_registry_id} = map,
+         available_metrics
+       )
        when is_integer(metric_registry_id) do
     with {:ok, mapping} <- Category.get_mapping_by_metric_registry_id(metric_registry_id) do
+      metric = Map.fetch!(map, :metric) |> maybe_fix_metric_name(available_metrics)
+
       params =
-        Map.take(map, [:metric, :args, :ui_key, :ui_human_readable_name, :unit, :chart_style])
+        Map.take(map, [:args, :ui_key, :ui_human_readable_name, :unit, :chart_style])
         |> Map.merge(%{
+          metric: metric,
           display_order_in_mapping: 1,
           metric_category_mapping_id: mapping.id
         })
@@ -33,7 +41,7 @@ defmodule Sanbase.Metric.Category.Scripts.CopyUIMetadata do
     end
   end
 
-  defp import_ui_metric_metadata(%{code_module: module, metric: metric} = map)
+  defp import_ui_metric_metadata(%{code_module: module, metric: metric} = map, _available_metrics)
        when is_binary(metric) do
     module = module || "__none__"
 
@@ -41,6 +49,7 @@ defmodule Sanbase.Metric.Category.Scripts.CopyUIMetadata do
       params =
         Map.take(map, [:metric, :args, :ui_key, :ui_human_readable_name, :unit, :chart_style])
         |> Map.merge(%{
+          metric: metric,
           display_order_in_mapping: 1,
           metric_category_mapping_id: mapping.id
         })
@@ -56,5 +65,18 @@ defmodule Sanbase.Metric.Category.Scripts.CopyUIMetadata do
           """)
       end
     end
+  end
+
+  defp maybe_fix_metric_name(metric, available_metrics) do
+    closest_metric = Enum.max_by(available_metrics, fn m -> String.jaro_distance(metric, m) end)
+
+    if metric != closest_metric do
+      IO.puts("""
+      Metric from DisplayOrder #{metric} is likely mistyped or no longer supported.
+      The closest metric to it is: #{closest_metric} with jaro distance: #{String.jaro_distance(metric, closest_metric)}
+      """)
+    end
+
+    closest_metric
   end
 end

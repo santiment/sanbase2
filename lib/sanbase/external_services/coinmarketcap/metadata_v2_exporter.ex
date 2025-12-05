cbed3b0b2464f296bc76f3261d946ed54e5ad41d
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2025-12-05 16:52:32 +0200

    backup

--- lib/sanbase/external_services/coinmarketcap/metadata_v2_exporter.ex
+++ lib/sanbase/external_services/coinmarketcap/metadata_v2_exporter.ex
@@ -26,30 +26,50 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.MetadataV2Exporter do
   @store_file "/tmp/cmc_metadata_v2.json"
 
   def save_cmc_metadata() do
-    with {:ok, data} <-
-           Sanbase.Project.List.projects(preload: [:latest_coinmarketcap_data])
-           |> Enum.filter(& &1.coinmarketcap_id)
-           |> Enum.map(& &1.coinmarketcap_id)
-           |> Enum.chunk_every(200)
-           |> Enum.map(&get/1)
-           |> Enum.reduce(%{}, &Map.merge(&1, &2)) do
-      IO.puts("Storing data to " <> @store_file)
-      File.write!(@store_file, Jason.encode!(data))
-    end
+    data =
+      Sanbase.Project.List.projects(preload: [:latest_coinmarketcap_data])
+      |> Enum.filter(& &1.coinmarketcap_id)
+      |> Enum.map(& &1.coinmarketcap_id)
+      |> Enum.chunk_every(500)
+      |> Enum.map(&get/1)
+      |> Enum.map(fn {:ok, body} -> body["data"] end)
+      |> Enum.reduce(%{}, &Map.merge(&1, &2))
+
+    IO.puts("Storing data to " <> @store_file)
+    IO.puts("Storing map size: #{map_size(data)}")
+    File.write!(@store_file, Jason.encode!(data))
   end
 
-  def get_cmc_metadata() do
-    File.read!(@store_file) |> Jason.decode!()
+  def read_cmc_metadata() do
+    File.read!(@store_file)
+    |> Jason.decode!()
+    |> Enum.map(fn {cmc_id, map} ->
+      map = %{
+        map
+        | "contract_address" =>
+            Enum.filter(map["contract_address"], fn ca_map ->
+              santiment_supported_platform?(ca_map)
+            end)
+      }
+
+      {cmc_id, map}
+    end)
+    |> Map.new()
+  end
+
+  def contracts_from_cmc(cmc_id_or_ids) do
+    Sanbase.ExternalServices.Coinmarketcap.MetadataV2Exporter.read_cmc_metadata()
+    |> Enum.filter(fn {_, map} -> map["slug"] in List.wrap(cmc_id_or_ids) end)
+    |> Enum.map(fn {_cmc_id, map} ->
+      map
+      |> Map.get("contract_address")
+      |> Enum.map(&{&1["contract_address"], &1["platform"]["name"]})
+    end)
   end
 
   def run() do
-    Sanbase.Project.List.projects(preload: [:latest_coinmarketcap_data])
-    |> Enum.filter(&(&1.coinmarketcap_id && &1.latest_coinmarketcap_data))
-    |> Enum.sort_by(& &1.latest_coinmarketcap_data.rank, :asc)
-    |> Enum.map(& &1.coinmarketcap_id)
-    |> Enum.chunk(100)
-    |> Enum.take(1)
-    |> Enum.each(&get/1)
+    read_cmc_metadata()
+    |> generate_report()
   end
 
   @spec get(String.t() | [String.t()]) :: {:ok, map()} | {:error, String.t()}
@@ -59,8 +79,6 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.MetadataV2Exporter do
     case Req.get(url(), headers: headers(), params: %{slug: slugs_param}) do
       {:ok, %{status: 200, body: body}} ->
         {:ok, body}
-
-      # {:ok, process_result(body, cmc_slugs)}
 
       {:ok, %{status: status, body: body}} ->
         error_msg = "CoinMarketCap API error: status #{status} - #{inspect(body)}"
@@ -77,72 +95,166 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.MetadataV2Exporter do
   defp normalize_slugs(slugs) when is_list(slugs), do: Enum.join(slugs, ",")
   defp normalize_slugs(slugs) when is_binary(slugs), do: slugs
 
-  @spec process_result(map(), list()) :: map()
-  def process_result(%{"data" => data}, cmc_slugs) when is_map(data) do
+  defp generate_report(map_data) do
+    IO.puts("Checking #{map_size(map_data)} CMC projects for contract address mismatches")
+
+    report_missing_contract_on_santiment(map_data)
+
+    report_contract_mismatch_by_coinmarketcap_id(map_data)
+
+    report_contract_existing_for_wrong_project_or_platform(map_data)
+
+    report_wrong_santiment_infrastructure()
+  end
+
+  defp report_wrong_santiment_infrastructure() do
+    san_contracts = contract_to_project_map()
+
+    cmc_contracts_maps = cmc_contract_to_cmc_id_map()
+
+    Enum.each(san_contracts, fn {address, project} ->
+      case Map.get(cmc_contracts_maps, address, []) do
+        [] ->
+          :ok
+
+        cmc_maps when cmc_maps != [] ->
+          if project.infrastructure do
+            infr = project.infrastructure.code
+
+            if list = Map.get(cmc_contracts_maps, address) do
+              if not Enum.any?(list, fn m -> m["infrastructure"] == infr end) do
+                IO.puts(
+                  "Infrastructure mismatch for #{address} for #{project.slug}. SAN infr: #{infr}, CMC infrs: #{Enum.map(list, fn m -> m["infrastructure"] end) |> Enum.uniq() |> Enum.join(", ")}"
+                )
+              end
+            end
+          end
+      end
+    end)
+  end
+
+  defp report_contract_mismatch_by_coinmarketcap_id(map_data) do
+    projects = Sanbase.Project.List.projects(preload: [:contract_addresses, :infrastructure])
+
+    san_contracts_by_cmc_id =
+      projects
+      |> Enum.filter(& &1.coinmarketcap_id)
+      |> Enum.reduce(%{}, fn project, acc ->
+        Enum.reduce(project.contract_addresses, acc, fn ca, acc_inner ->
+          Map.update(
+            acc_inner,
+            project.coinmarketcap_id,
+            MapSet.new([String.downcase(ca.address)]),
+            fn set -> MapSet.put(set, String.downcase(ca.address)) end
+          )
+        end)
+      end)
+
+    cmc_contracts_by_cmc_id =
+      Enum.reduce(map_data, %{}, fn {_integer_id, data}, acc ->
+        addresses =
+          Enum.map(data["contract_address"], fn %{"contract_address" => addr} ->
+            String.downcase(addr)
+          end)
+          |> MapSet.new()
+
+        Map.put(acc, data["slug"], addresses)
+      end)
+
+    Enum.each(san_contracts_by_cmc_id, fn {cmc_id, addresses_mapset} ->
+      Enum.each(addresses_mapset, fn address ->
+        if Map.get(cmc_contracts_by_cmc_id, cmc_id, MapSet.new()) |> MapSet.member?(address) do
+          :ok
+        else
+          IO.puts(
+            "Address #{address} is supported on Santiment for #{cmc_id}, but is missing from CMC"
+          )
+        end
+      end)
+    end)
+  end
+
+  defp report_missing_contract_on_santiment(map_data) do
+    map_data
+    |> Enum.flat_map(fn {_integer_id, map} ->
+      map["contract_address"] |> Enum.map(fn m -> Map.put(m, "slug", map["slug"]) end)
+    end)
+    |> Enum.each(fn %{
+                      "slug" => slug,
+                      "contract_address" => address,
+                      "platform" => %{"name" => platform_name}
+                    } ->
+      if santiment_supported_platform?(%{"platform" => %{"name" => platform_name}}) do
+        if not Map.has_key?(contract_to_project_map(), String.downcase(address)) do
+          IO.puts(
+            "CMC data: #{String.pad_trailing(platform_name, 25)} addr for #{String.pad_trailing(slug, 30)}: #{String.pad_trailing(address, 45)} but not on santiment"
+          )
+        end
+      end
+    end)
+  end
+
+  defp report_contract_existing_for_wrong_project_or_platform(map_data) do
     cmc_slug_to_project =
       Sanbase.Project.List.projects(preload: [:contract_addresses, :infrastructure])
       |> Map.new(fn p -> {p.coinmarketcap_id, p} end)
 
-    data
-    |> tap(fn data ->
-      Enum.map(data, fn {_integer_id, map} ->
-        process_project_data(cmc_slug_to_project[map["slug"]], map)
-      end)
+    map_data
+    |> Enum.each(fn {_integer_id, map} ->
+      if p = cmc_slug_to_project[map["slug"]],
+        do: do_report_contract_existing_for_wrong_project_or_platform(p, map)
     end)
-
-    # |> Enum.map(fn {_id, crypto_data} -> extract_contract_info(crypto_data) end)
-    # |> Enum.filter(&(not is_nil(&1)))
   end
 
-  def process_result(body) do
-    Logger.warning("[CMC MetadataV2] Unexpected response structure: #{inspect(body)}")
-    %{}
-  end
+  defp do_report_contract_existing_for_wrong_project_or_platform(project, map) do
+    contract_addresses =
+      map["contract_address"]
+      |> Enum.filter(&santiment_supported_platform?/1)
+      |> Enum.filter(fn %{"contract_address" => address} ->
+        Map.has_key?(contract_to_project_map(), String.downcase(address))
+      end)
 
-  defp process_project_data(nil, _data) do
-    :ok
-  end
+    if contract_addresses == [] do
+      :ok
+    else
+      Enum.map(
+        contract_addresses,
+        fn %{"contract_address" => address, "platform" => %{"name" => platform_name}} ->
+          existing_project = Map.get(contract_to_project_map(), String.downcase(address))
+          cmc_infr = @cmc_platform_name_to_infrastructure[platform_name]
 
-  defp process_project_data(project, map) do
-    map["contract_address"]
-    |> Enum.filter(&santiment_supported_platform?/1)
-    |> tap(fn contract_maps -> Enum.map(contract_maps, &generate_report(project, &1)) end)
-  end
+          cond do
+            is_nil(existing_project.infrastructure) ->
+              # IO.puts(
+              #   "Contract #{address} on platform #{cmc_infr} is associated with proejct #{existing_project.slug} but it has no infrastructure set"
+              # )
+              :ok
 
-  defp generate_report(project, %{
-         "contract_address" => address,
-         "platform" => %{"name" => platform_name}
-       }) do
-    report_missing_contract_on_santiment(project, address, platform_name)
-    report_contract_existing_for_wrong_project_or_platform(project, address, platform_name)
-  end
+            map["slug"] != existing_project.coinmarketcap_id and
+              not String.contains?(existing_project.slug, map["slug"]) and
+              not String.contains?(map["slug"], existing_project.slug) and
+                String.jaro_distance(project.slug, existing_project.slug) < 0.8 ->
+              IO.puts(
+                "Contract #{String.pad_trailing(address, 45)} for #{String.pad_trailing(map["slug"], 30)} and platform #{String.pad_trailing(cmc_infr, 12)} but on santiment it is associated with a different slug #{existing_project.slug} and infrastructure #{existing_project.infrastructure.code}"
+              )
 
-  defp report_missing_contract_on_santiment(project, address, platform_name) do
-    if not Map.has_key?(contract_to_project_map(), String.downcase(address)) do
-      IO.puts(
-        "CMC has a contract for #{project.slug} on platform #{project.infrastructure.code}: #{address}, but not on santiment"
-      )
-    end
-  end
+            existing_project.infrastructure.code != cmc_infr ->
+              slug_infr_maps = Map.get(cmc_contract_to_cmc_id_map(), String.downcase(address), [])
 
-  defp report_contract_existing_for_wrong_project_or_platform(project, address, platform_name) do
-    case Map.get(contract_to_project_map(), String.downcase(address)) do
-      nil ->
-        :ok
+              # If none of the infra
+              if not Enum.any?(slug_infr_maps, fn m ->
+                   m["infrastructure"] == existing_project.infrastructure.code
+                 end) do
+                IO.puts(
+                  "Contract #{String.pad_trailing(address, 45)} for #{String.pad_trailing(map["slug"], 30)} and platform #{String.pad_trailing(cmc_infr, 12)} but on santiment it is associated with #{existing_project.slug} and infrastructure #{existing_project.infrastructure.code}"
+                )
+              end
 
-      %Sanbase.Project{} = existing_project when existing_project.id != project.id ->
-        if existing_project.infrastructure &&
-             existing_project.infrastructure.code !=
-               @cmc_platform_name_to_infrastructure[platform_name] do
-          IO.puts(
-            "Contract #{address} for #{existing_project.slug} and platform #{existing_project.infrastructure && existing_project.infrastructure.code} but CMC associates it with #{project.slug} on platform #{platform_name}"
-          )
-        else
-          :ok
+            true ->
+              :ok
+          end
         end
-
-      _ ->
-        :ok
+      )
     end
   end
 
@@ -206,5 +318,60 @@ defmodule Sanbase.ExternalServices.Coinmarketcap.MetadataV2Exporter do
       project.contract_addresses |> Enum.map(fn ca -> {String.downcase(ca.address), project} end)
     end)
     |> Map.new()
+  end
+
+  def cmc_contract_to_cmc_id_map() do
+    case :persistent_term.get(:cmc_contract_to_cmc_id_map, nil) do
+      nil ->
+        contract_to_project = compute_cmc_contract_to_cmc_id_map()
+
+        :persistent_term.put(
+          :cmc_contract_to_cmc_id_map,
+          {contract_to_project, DateTime.utc_now()}
+        )
+
+        contract_to_project
+
+      {contract_to_project, added_at} ->
+        if DateTime.diff(DateTime.utc_now(), added_at, :minute) > 10 do
+          contract_to_project = compute_cmc_contract_to_cmc_id_map()
+
+          :persistent_term.put(
+            :cmc_contract_to_cmc_id_map,
+            {contract_to_project, DateTime.utc_now()}
+          )
+
+          contract_to_project
+        else
+          contract_to_project
+        end
+    end
+  end
+
+  defp compute_cmc_contract_to_cmc_id_map() do
+    json = read_cmc_metadata()
+
+    json
+    |> Enum.flat_map(fn {_integer_id, map} ->
+      map["contract_address"]
+      |> Enum.map(fn %{"contract_address" => address, "platform" => %{"name" => platform_name}} ->
+        {String.downcase(address),
+         %{
+           "platform_name" => platform_name,
+           "infrastructure" => @cmc_platform_name_to_infrastructure[map["platform"]["name"]],
+           "slug" => map["slug"]
+         }}
+      end)
+    end)
+    |> IO.inspect()
+    |> Enum.reject(fn {_addr, %{"infrastructure" => infr}} -> is_nil(infr) end)
+    |> Enum.reduce(%{}, fn {addr, m}, acc ->
+      Map.update(
+        acc,
+        addr,
+        [m],
+        &[m | &1]
+      )
+    end)
   end
 end

f5b33036f9bc107cf76b2a0b454486c2973a069d
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2026-02-17 15:13:04 +0200

    Add crypto_market slug

--- lib/sanbase/available_slugs/available_slugs.ex
+++ lib/sanbase/available_slugs/available_slugs.ex
@@ -10,7 +10,7 @@ defmodule Sanbase.AvailableSlugs do
 
   # There are 2 special cases that are not a project slug but refer to big groups
   # of projects and there is marketcap and volume data for them
-  @group_of_slugs ["TOTAL_MARKET", "TOTAL_ERC20"]
+  @group_of_slugs ["TOTAL_MARKET", "crypto_market", "TOTAL_ERC20"]
   @non_project_slugs ~w(s-and-p-500 gold silver crude-oil dxy gbtc ibit fbtc arkb btco bitb hodl m2-money)
 
   def non_project_slugs(), do: @non_project_slugs

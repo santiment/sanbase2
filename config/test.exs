b83abd53d6a8ab76365abd4563b4bc8265577665
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2026-02-18 16:36:42 +0200

    Disable MetricRegistrySubscriber in test and enable it where needed

--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,7 @@ config :sanbase, Sanbase.EventBus,
   ],
   disabled_subscribers: [
     Sanbase.EventBus.KafkaExporterSubscriber,
+    Sanbase.EventBus.MetricRegistrySubscriber,
     Sanbase.EventBus.AppNotificationsSubscriber,
     Sanbase.EventBus.UserEventsSubscriber
   ]

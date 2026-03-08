96593b8165739d0adc112a51c929629e7137e834
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2026-03-08 14:55:55 +0200

    Refactor GenericAdmin

--- lib/sanbase_web/generic_admin.ex
+++ lib/sanbase_web/generic_admin.ex
@@ -77,10 +77,69 @@ defmodule SanbaseWeb.GenericAdmin do
   end
 
   def call_module_function_or_default(module, function, data, default_value) do
-    try do
+    if function_exported?(module, function, length(data)) do
       apply(module, function, data)
-    rescue
-      UndefinedFunctionError -> default_value
+    else
+      default_value
     end
+  end
+
+  def schema_to_resource_name(schema_module) do
+    schema_module
+    |> to_string()
+    |> String.split(".")
+    |> List.last()
+    |> Inflex.underscore()
+    |> Inflex.pluralize()
+  end
+
+  # ---------------------------------------------------------------------------
+  # Shared helpers for resource modules
+  # ---------------------------------------------------------------------------
+
+  @doc """
+  Generates an HTML link to a GenericAdmin show page for a given resource.
+  Used by resource modules to create clickable links in value_modifier functions.
+  """
+  def resource_link(resource, id, label) do
+    relative_url =
+      SanbaseWeb.Router.Helpers.generic_admin_path(SanbaseWeb.Endpoint, :show, id,
+        resource: resource
+      )
+
+    PhoenixHTMLHelpers.Link.link(label,
+      to: relative_url,
+      class: "text-blue-600 hover:text-blue-800"
+    )
+  end
+
+  @doc """
+  Common belongs_to definition for Project associations.
+  Used by many resource modules that have a project_id foreign key.
+  """
+  def belongs_to_project do
+    import Ecto.Query, only: [from: 2], warn: false
+
+    %{
+      query: from(p in Sanbase.Project, order_by: p.id),
+      transform: fn rows -> Enum.map(rows, &{&1.name, &1.id}) end,
+      resource: "projects",
+      search_fields: [:name, :slug, :ticker]
+    }
+  end
+
+  @doc """
+  Common belongs_to definition for User associations.
+  Used by resource modules that have a user_id foreign key.
+  """
+  def belongs_to_user do
+    import Ecto.Query, only: [from: 2], warn: false
+
+    %{
+      query: from(u in Sanbase.Accounts.User, order_by: [desc: u.id]),
+      transform: fn rows -> Enum.map(rows, &{&1.email, &1.id}) end,
+      resource: "users",
+      search_fields: [:email, :username]
+    }
   end
 end

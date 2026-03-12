7c56e51d769c3ff13b03d2196f735e72acf65c6a
Tsvetozar Penov <tsvetozar.penov@gmail.com>
2026-03-12 16:28:33 +0100

    OAuth adjustments

--- lib/sanbase_web/controllers/oauth_dev_login_controller.ex
+++ lib/sanbase_web/controllers/oauth_dev_login_controller.ex
@@ -5,24 +5,24 @@ defmodule SanbaseWeb.OAuthDevLoginController do
   """
   use SanbaseWeb, :controller
 
-  def show(conn, params) do
-    return_to = params["return_to"] || "/oauth/authorize"
-
+  def show(conn, _params) do
     conn
     |> put_resp_content_type("text/html")
-    |> send_resp(200, dev_login_html(return_to))
+    |> send_resp(200, dev_login_html())
   end
 
-  def submit(conn, %{"email" => email, "return_to" => return_to}) do
+  def submit(conn, %{"email" => email}) do
     case Sanbase.Accounts.User.by_email(email) do
       {:ok, user} ->
         device_data = SanbaseWeb.Guardian.device_data(conn)
 
         case SanbaseWeb.Guardian.get_jwt_tokens(user, device_data) do
           {:ok, jwt_tokens_map} ->
+            return_to = get_session(conn, :user_return_to) || "/oauth/authorize"
+
             conn
             |> SanbaseWeb.Guardian.add_jwt_tokens_to_conn_session(jwt_tokens_map)
-            |> redirect(to: safe_return_to(return_to))
+            |> redirect(to: return_to)
 
           {:error, reason} ->
             require Logger
@@ -36,13 +36,11 @@ defmodule SanbaseWeb.OAuthDevLoginController do
       _ ->
         conn
         |> put_resp_content_type("text/html")
-        |> send_resp(400, dev_login_html(return_to, "User not found for email: #{email}"))
+        |> send_resp(400, dev_login_html("User not found for email: #{email}"))
     end
   end
 
-  defp dev_login_html(return_to, error \\ nil) do
-    escaped_return_to = Plug.HTML.html_escape(return_to)
-
+  defp dev_login_html(error \\ nil) do
     error_html =
       if error,
         do: ~s(<p style="color:red">#{Plug.HTML.html_escape(error)}</p>),
@@ -65,7 +63,6 @@ defmodule SanbaseWeb.OAuthDevLoginController do
       #{error_html}
       <form method="post" action="/oauth/dev_login">
         <input type="hidden" name="_csrf_token" value="#{Plug.CSRFProtection.get_csrf_token()}" />
-        <input type="hidden" name="return_to" value="#{escaped_return_to}" />
         <label>Email of existing user:</label>
         <input type="email" name="email" placeholder="user@example.com" required />
         <button type="submit">Sign In</button>
@@ -74,14 +71,4 @@ defmodule SanbaseWeb.OAuthDevLoginController do
     </html>
     """
   end
-
-  defp safe_return_to(url) when is_binary(url) do
-    if String.starts_with?(url, "/") and not String.starts_with?(url, "//") do
-      url
-    else
-      "/oauth/authorize"
-    end
-  end
-
-  defp safe_return_to(_), do: "/oauth/authorize"
 end

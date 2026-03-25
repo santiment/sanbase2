7a413ffc3377af5c87039338378379ea629e662a
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2026-03-25 19:57:01 +0200

    Extract shared unsupported_selector_error to Sanbase.Metric.Utils
    6 metric adapters had near-identical unsupported_selector_error/1 functions.
    Extracted the common logic into Sanbase.Metric.Utils.unsupported_selector_error/2
    with an optional required_fields_hint parameter. Added doctests and unit tests.

--- test/sanbase/metric/metric_utils_test.exs
+++ test/sanbase/metric/metric_utils_test.exs
@@ -1,5 +1,29 @@
 defmodule Sanbase.Metric.UtilsTest do
   use ExUnit.Case, async: true
 
+  alias Sanbase.Metric.Utils
+
   doctest Sanbase.Metric.Utils
+
+  describe "unsupported_selector_error/2" do
+    test "without required fields hint" do
+      result = Utils.unsupported_selector_error(%{text: "hello"})
+      assert result =~ "is not supported"
+      assert result =~ ":text"
+      refute result =~ "must have"
+    end
+
+    test "with required fields hint" do
+      result = Utils.unsupported_selector_error(%{text: "hello"}, "slug")
+      assert result =~ "is not supported"
+      assert result =~ "must have the following fields: slug"
+      assert result =~ ":text"
+    end
+
+    test "with multiple keys in selector" do
+      result = Utils.unsupported_selector_error(%{a: 1, b: 2})
+      assert result =~ ":a"
+      assert result =~ ":b"
+    end
+  end
 end
